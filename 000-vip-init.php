076e69d8785d9d587d40f91878bf26a11ed7aa06
Nick Daugherty <ndaugherty987@gmail.com>
2015-05-12 15:57:46 -0600

    Clean up the VIP bootstrap file

--- 000-vip-init.php
+++ 000-vip-init.php
@@ -6,24 +6,17 @@ define( 'WPCOM_IS_VIP_ENV', true );
 
 // Load our development and environment helpers
 require_once( __DIR__ . '/vip/vip-utils.php' );
+require_once( __DIR__ . '/vip/vip-caching.php' );
 require_once( __DIR__ . '/vip/vip-roles.php' );
 require_once( __DIR__ . '/vip/vip-permastructs.php' );
 require_once( __DIR__ . '/vip/vip-mods.php' );
 require_once( __DIR__ . '/vip/vip-media.php' );
+require_once( __DIR__ . '/vip/vip-stats.php' );
 require_once( __DIR__ . '/vip/vip-deprecated.php' );
 
 // Load WP_CLI helpers
 if ( defined( 'WP_CLI' ) && WP_CLI ) {
     require_once( __DIR__ . '/vip/vip-wp-cli.php' );
 }
-
-
-
-// These are helper functions specific to WP.com-related functionality
-wpcom_vip_load_helper_wpcom(); // vip-helper-wpcom.php
-wpcom_vip_load_helper_stats(); // vip-helper-stats-wpcom.php
-
-// Load the "works everywhere" helper file
-wpcom_vip_load_helper(); // vip-helper.php
 
 do_action( 'vip_loaded' );
