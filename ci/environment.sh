d962db3c38d08fb60bef5cee168aa475532bbd78
Simon Wheatley <simon@simonwheatley.co.uk>
2015-06-04 14:17:23 +0100

    Switch to a new key. Don't bother encrypting it.

--- ci/environment.sh
+++ ci/environment.sh
@@ -7,12 +7,7 @@
 set -ex
 
 mkdir -p ~/.ssh
-echo -e "Host github.com\n  IdentityFile ${TRAVIS_BUILD_DIR}/ci/ssh/insecure-key" > ~/.ssh/config
+echo -e "Host github.com\n  IdentityFile ${TRAVIS_BUILD_DIR}/ci/new-key" > ~/.ssh/config
 cat ~/.ssh/config
 cp ${TRAVIS_BUILD_DIR}/ci/ssh/known_hosts ~/.ssh/known_hosts
 cat ~/.ssh/known_hosts
-
-# Install unit tests
-# ==================
-
-bash bin/install-wp-tests.sh wordpress_test root '' localhost "${WP_VERSION}"
