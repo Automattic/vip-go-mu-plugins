4438d7d02ef840192f4e09e52e68d7c424f60589
Josh Betz <j@joshbetz.com>
2015-06-04 16:39:27 -0500

    Change Jetpack to a submodule
    Pinned to v3.5.3 for now (latest stable).

--- jetpack/json-endpoints/jetpack/class.jetpack-json-api-plugins-get-endpoint.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Jetpack_JSON_API_Plugins_Get_Endpoint extends Jetpack_JSON_API_Plugins_Endpoint {
-	// GET  /sites/%s/plugins/%s
-	protected $needed_capabilities = 'activate_plugins';
-}
