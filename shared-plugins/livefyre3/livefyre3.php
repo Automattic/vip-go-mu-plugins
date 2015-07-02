c8be59881070d1813576541a23a22fd23f63d329
Josh Betz <j@joshbetz.com>
2015-07-02 12:02:05 -0500

    Removing some old and unused plugins.

--- shared-plugins/livefyre3/livefyre3.php
+++
@@ -1,2 +0,0 @@
-<?php
-require( 'livefyre.php' ); // bootstrap to work with wpcom_vip_load_plugin()
--- shared-plugins/sem-frame-buster/sem-frame-buster-info.php
+++
@@ -1,4 +0,0 @@
-<?php
-__('Frame Buster', 'sem-frame-buster');
-__('Thwarts any attempt to load your site in a frame.', 'sem-frame-buster');
-?>
