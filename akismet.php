fd86d643414982f3a073b5d2359ca372a097cf9e
Nick Daugherty <ndaugherty987@gmail.com>
2015-07-07 12:29:58 -0600

    Bump Akismet plugin version to match the installed version
    See #40

--- akismet.php
+++ akismet.php
@@ -6,7 +6,7 @@
 Plugin Name: Akismet
 Plugin URI: http://akismet.com/
 Description: Used by millions, Akismet is quite possibly the best way in the world to <strong>protect your blog from comment and trackback spam</strong>. It keeps your site protected from spam even while you sleep. To get started: 1) Click the "Activate" link to the left of this description, 2) <a href="http://akismet.com/get/">Sign up for an Akismet API key</a>, and 3) Go to your Akismet configuration page, and save your API key.
-Version: 3.1.2
+Version: 3.1.3
 Author: Automattic
 Author URI: http://automattic.com/wordpress-plugins/
 License: GPLv2 or later
