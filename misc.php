48495b949f49d12aec30fd3fa9658dbc7aeae3e9
David Newman <dnewman@automattic.com>
2015-07-10 09:31:24 +1000

    Do not send the cache headers through to Varnish for 404s
    Do not send the cache headers, when the request results is a 404,
    otherwise it prevents Varnish from caching the 404 page according to
    the cache settings we have defined in the config.

--- misc.php
+++ misc.php
@@ -1,11 +1,23 @@
 <?php
 /*
-Plugin Name: VIP Hosting Miscellaneous
+Plugin Name: VIPv2 Hosting Miscellaneous
 Description: Handles CSS and JS concatenation, and Nginx compatibility
 Author: Automattic
 Version: 1.0
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
+
+// Ensure we do not send the cache headers through to Varnish,
+// so responses obey the cache settings we have configured.
+function check_for_404_and_remove_cache_headers( $headers ) {
+	if ( is_404() ) {
+		unset( $headers['Expires'] );
+		unset( $headers['Cache-Control'] );
+		unset( $headers['Pragma'] );
+	}
+	return $headers;
+}
+add_filter( 'nocache_headers', 'check_for_404_and_remove_cache_headers' );
 
 // Cleaner permalink options
 add_filter( 'got_url_rewrite', '__return_true' );
