767cccf33742b5c660ee867457aa3c4866b9effb
Josh Betz <j@joshbetz.com>
2015-07-17 14:33:36 -0500

    Remove plugins that depend on Batcache
    Cheeztest and wpcom-geo-uniques vary Batcache, which doesn't
    exist on the new platform

--- shared-plugins/wpcom-geo-uniques/wpcom-helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_action( 'wpcom_geo_uniques_locate_user', function( $location ) {
-	if ( function_exists( 'bump_stats_extras' ) ) {
-		bump_stats_extras( 'wpcom-geo-uniques-locate-user-by-theme', str_replace( '/', '-', get_stylesheet() ) );
-	}
-} );
-
