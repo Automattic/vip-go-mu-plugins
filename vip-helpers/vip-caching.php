18c91c3ab662033531b2b39506986c7e41648ad9
Nick Daugherty <ndaugherty987@gmail.com>
2015-09-02 12:20:30 -0600

    Add the taxonomy to the get_term_by call

--- vip-helpers/vip-caching.php
+++ vip-helpers/vip-caching.php
@@ -116,7 +116,7 @@ function wpcom_vip_term_exists( $term, $taxonomy = '', $parent = null ) {
  */
 add_action( 'delete_term', 'wp_flush_term_exists', 10, 4 );
 function wp_flush_term_exists( $term, $tt_id, $taxonomy, $deleted_term ){
-	$term = get_term_by( 'id', $term );
+	$term = get_term_by( 'id', $term, $taxonomy );
 	foreach( array( 'id', 'name', 'slug' ) as $field ) {
 		$cache_key = $term->$field . '|' . $taxonomy ;
 		$cache_group = 'term_exists';
