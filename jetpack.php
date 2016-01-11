7cd17eb14d565511f8a54c18b260cdbf74054a74
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-11 16:54:55 +0000

    Allow alternative Jetpack folders to be loaded
    Allow for a "standard" alternative, at `jetpack-beta`
    (expected to be `master` from the Jetpack repo, or
    similar).
    
    Allow for a specific alternative suffix to be specified,
    so a site can load a particular and specific version.
    
    Addresses #108

--- jetpack.php
+++ jetpack.php
@@ -14,5 +14,29 @@
  
 add_filter( 'jetpack_client_verify_ssl_certs', '__return_true' );
 
-require_once( __DIR__ . '/jetpack/jetpack.php' );
+$jetpack_to_load = __DIR__ . '/jetpack/jetpack.php';
+// If the VIP_JETPACK_ALT constant is defined, we should attempt to load
+// an alternative to the standard Jetpack
+if ( defined( 'VIP_JETPACK_ALT' ) && VIP_JETPACK_ALT ) {
+
+	// Set a default alternative Jetpack to load
+	$jp_alt_suffix = '-beta';
+	// Allow the alternative version of Jetpack to be specified on
+	// a site by site basis
+	if ( defined( 'VIP_JETPACK_ALT_SUFFIX' ) && VIP_JETPACK_ALT_SUFFIX ) {
+		$jp_alt_suffix = VIP_JETPACK_ALT_SUFFIX;
+	}
+
+	// Save the original Jetpack path, in case we need to revert
+	$jetpack_to_load_orig = $jetpack_to_load;
+	// Construct the new Jetpack path we want loaded, and test that it exists,
+	// falling back to the original if it does not.
+	$jetpack_to_load = __DIR__ . '/jetpack' . $jp_alt_suffix . '/jetpack.php';
+	if ( ! file_exists( $jetpack_to_load ) ) {
+		// We assume that the regular VIP Go Jetpack folder DOES exist
+		$jetpack_to_load = $jetpack_to_load_orig;
+	}
+}
+require_once( $jetpack_to_load );
+
 require_once( __DIR__ . '/vip-jetpack/vip-jetpack.php' );
