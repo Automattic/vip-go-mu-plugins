2ef773f9584bf663208e76b3624da23c30693df1
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-12 20:30:17 +0000

    Attempt to simplify the code flow around loading different Jetpacks
    * If no constant is specified, the Jetpack in `mu-plugins/jetpack/`
      is loaded
    * If VIP_JETPACK_ALT alone is specified, the Jetpack in
      `mu-plugins/jetpack-beta/` is loaded
    * If VIP_JETPACK_ALT and VIP_JETPACK_ALT_SUFFIX are specified,
      the Jetpack in `mu-plugins/jetpack-VIP_JETPACK_ALT_SUFFIX/` is loaded
    
    See: https://github.com/Automattic/vipv2-mu-plugins/pull/109#discussion_r49354680

--- jetpack.php
+++ jetpack.php
@@ -15,26 +15,31 @@
 add_filter( 'jetpack_client_verify_ssl_certs', '__return_true' );
 
 $jetpack_to_load = __DIR__ . '/jetpack/jetpack.php';
+
 // If the VIP_JETPACK_ALT constant is defined, we should attempt to load
 // an alternative to the standard Jetpack
+// Logic:
+// * If no constant is specified, the Jetpack in `mu-plugins/jetpack/`
+//   is loaded
+// * If VIP_JETPACK_ALT alone is specified, the Jetpack in
+//   `mu-plugins/jetpack-beta/` is loaded
+// * If VIP_JETPACK_ALT and VIP_JETPACK_ALT_SUFFIX are specified,
+//   the Jetpack in `mu-plugins/jetpack-VIP_JETPACK_ALT_SUFFIX/` is loaded
 if ( defined( 'VIP_JETPACK_ALT' ) && VIP_JETPACK_ALT ) {
 
-	// Set a default alternative Jetpack to load
-	$jp_alt_suffix = '-beta';
+	// Set the default alternative Jetpack
+	$jetpack_to_test = __DIR__ . '/jetpack-beta/jetpack.php';
+
 	// Allow the alternative version of Jetpack to be specified on
 	// a site by site basis
 	if ( defined( 'VIP_JETPACK_ALT_SUFFIX' ) && VIP_JETPACK_ALT_SUFFIX ) {
-		$jp_alt_suffix = VIP_JETPACK_ALT_SUFFIX;
+		// Set a specific alternative Jetpack
+		$jetpack_to_test = __DIR__ . '/jetpack' . VIP_JETPACK_ALT_SUFFIX . '/jetpack.php';
 	}
 
-	// Save the original Jetpack path, in case we need to revert
-	$jetpack_to_load_orig = $jetpack_to_load;
-	// Construct the new Jetpack path we want loaded, and test that it exists,
-	// falling back to the original if it does not.
-	$jetpack_to_load = __DIR__ . '/jetpack' . $jp_alt_suffix . '/jetpack.php';
-	if ( ! file_exists( $jetpack_to_load ) ) {
-		// We assume that the regular VIP Go Jetpack folder DOES exist
-		$jetpack_to_load = $jetpack_to_load_orig;
+	// Test that our proposed Jetpack exists, otherwise do not use it
+	if ( file_exists( $jetpack_to_test ) ) {
+		$jetpack_to_load = $jetpack_to_test;
 	}
 }
 require_once( $jetpack_to_load );
