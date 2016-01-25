8da061a9a95f190d0f657bd6c2083f617178e067
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-25 15:24:43 +0000

    Add some inline docs for the fake activation

--- query-monitor.php
+++ query-monitor.php
@@ -16,6 +16,8 @@ $wpcom_vip_qm_file = __DIR__ . '/query-monitor/query-monitor.php';
 
 require_once( $wpcom_vip_qm_file );
 
+// Because we're including Query Monitor as an MU plugin, we need to
+// manually call the `activate` method on `activation`.
 if ( 0 === get_option( 'wpcom_vip_qm_activated', 0 ) ) {
 	QueryMonitor::init( $wpcom_vip_qm_file )->activate( true );
 	update_option( 'wpcom_vip_qm_activated', 1, true );
