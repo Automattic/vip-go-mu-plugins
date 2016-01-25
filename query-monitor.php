7c7a963fab53c9d8c58fa95344697e84021b50bc
Simon Wheatley <simon@simonwheatley.co.uk>
2016-01-25 15:15:55 +0000

    Add Query Monitor
    Submodule the GitHub repo at the tag 2.8.1
    Add a "require" file with the QM plugin header in `mu-plugins`
    Fake out the activation hook, but only once

---
+++ query-monitor.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Query Monitor
+Description: Monitoring of database queries, hooks, conditionals and more.
+Version:     2.8.1
+Plugin URI:  https://querymonitor.com/
+Author:      John Blackbourn
+Author URI:  https://johnblackbourn.com/
+Text Domain: query-monitor
+Domain Path: /languages/
+License:     GPL v2 or later
+
+*/
+
+$wpcom_vip_qm_file = __DIR__ . '/query-monitor/query-monitor.php';
+
+require_once( $wpcom_vip_qm_file );
+
+if ( 0 === get_option( 'wpcom_vip_qm_activated', 0 ) ) {
+	QueryMonitor::init( $wpcom_vip_qm_file )->activate( true );
+	update_option( 'wpcom_vip_qm_activated', 1, true );
+}
