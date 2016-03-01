fafb9ea75fbedaa4c37a59f2f48a998c0e68d30c
Derrick Tennant <derrick.tennant@automattic.com>
2016-03-01 14:54:14 -0500

    Bringing wp-parsely up to date with WPCOM

--- shared-plugins/wp-parsely/parsely-parsely-page.php
+++ shared-plugins/wp-parsely/parsely-parsely-page.php
@@ -1,7 +1,11 @@
-<!-- wp-parsely Plugin Version <?php echo esc_html(Parsely::VERSION); ?> -->
+<!-- BEGIN wp-parsely Plugin Version <?php echo esc_html(Parsely::VERSION); ?> -->
 <meta name='wp-parsely_version' id='wp-parsely_version' content='<?php echo esc_html(Parsely::VERSION); ?>' />
-<?php if (!empty($parselyPage)) : ?>
-	<meta name='parsely-page' id='parsely-page' content='<?php echo json_encode($parselyPage, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT); ?>' />
+<?php if (!empty($parselyPage) && isset($parselyPage["headline"])) : ?>
+   <script type="application/ld+json">
+   <?php echo json_encode($parselyPage); ?>
+   </script>
 <?php else: ?>
-	<!-- parsleyPage is not defined / has no attributes.  What kind of page are you loading? -->
-<?php endif;
+    <!-- parsleyPage is not defined / has no attributes.  What kind of page are you loading? -->
+<?php endif; ?>
+<!-- END wp-parsely Plugin Version <?php echo esc_html(Parsely::VERSION); ?> -->
+<?php
