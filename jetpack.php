8c1bccb6e52bc279cac15e7740de2f92935c72b1
Erick Hitter <services@ethitter.com>
2016-03-10 14:49:15 -0800

    Jetpack 3.9.4
    See https://github.com/Automattic/jetpack/releases/tag/3.9.4

--- jetpack.php
+++ jetpack.php
@@ -5,13 +5,13 @@
  * Plugin URI: http://jetpack.me
  * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
  * Author: Automattic
- * Version: 3.9.2
+ * Version: 3.9.4
  * Author URI: http://jetpack.me
  * License: GPL2+
  * Text Domain: jetpack
  * Domain Path: /languages/
  */
- 
+
 add_filter( 'jetpack_client_verify_ssl_certs', '__return_true' );
 
 $jetpack_to_load = __DIR__ . '/jetpack/jetpack.php';
