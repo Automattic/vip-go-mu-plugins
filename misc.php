5e455d2a19d53bef7f52dab3ae9286c09d60e432
Simon Wheatley <simon@simonwheatley.co.uk>
2016-04-29 17:43:05 +0100

    Prevent concatenation outside VIP Go
    Closes #181

--- misc.php
+++ misc.php
@@ -22,13 +22,17 @@ add_filter( 'nocache_headers', 'wpcom_vip_check_for_404_and_remove_cache_headers
 // Cleaner permalink options
 add_filter( 'got_url_rewrite', '__return_true' );
 
-// Activate concatenation
-if ( ! isset( $_GET['concat_js'] ) || 'yes' === $_GET['concat_js'] ) {
-	require __DIR__ .'/http-concat/jsconcat.php';
-}
+// Checking for WPCOM_IS_VIP_ENV allows this code to work outside VIP Go environments,
+// albeit without concatenation of JS and CSS.
+if ( defined( 'WPCOM_IS_VIP_ENV' ) && WPCOM_IS_VIP_ENV ) {
+	// Activate concatenation
+	if ( ! isset( $_GET['concat_js'] ) || 'yes' === $_GET['concat_js'] ) {
+		require __DIR__ .'/http-concat/jsconcat.php';
+	}
 
-if ( ! isset( $_GET['concat_css'] ) || 'yes' === $_GET['concat_css'] ) {
-	require __DIR__ .'/http-concat/cssconcat.php';
+	if ( ! isset( $_GET['concat_css'] ) || 'yes' === $_GET['concat_css'] ) {
+		require __DIR__ .'/http-concat/cssconcat.php';
+	}
 }
 
 /**
