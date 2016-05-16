89503659295d28e737eb544b9cb45ebe23f84ac7
Simon Wheatley <simon@simonwheatley.co.uk>
2016-05-16 17:49:33 +0100

    Cat the content of the Git modules file

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -9,9 +9,11 @@ set -ex
 # We cannot checkout even public repositories via SSH unless we
 # are authenticated and authorised on that repo (and we aren't,
 # for the submodules here)
+cat ${TRAVIS_BUILD_DIR}/.gitmodules
 if [ -w ${TRAVIS_BUILD_DIR}/.gitmodules ]; then
     sed -i "s#https\?://github.com/#git@github.com:#" ${TRAVIS_BUILD_DIR}/.gitmodules
 fi;
+cat ${TRAVIS_BUILD_DIR}/.gitmodules
 
 # Install unit tests
 # ==================
