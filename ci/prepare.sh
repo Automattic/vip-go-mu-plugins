2beb392033248e7b12e3adcc97e8e85a84c7269b
Simon Wheatley <simon@simonwheatley.co.uk>
2016-05-16 16:23:20 +0100

    Switch to HTTPS URLs for the Git submodules

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -6,6 +6,13 @@
 # http://www.peterbe.com/plog/set-ex
 set -ex
 
+# We cannot checkout even public repositories via SSH unless we
+# are authenticated and authorised on that repo (and we aren't,
+# for the submodules here)
+if [ -w ${TRAVIS_BUILD_DIR}/.gitmodules ]; then
+    sed -i \"s#https\?://github.com/#git@github.com:#\" ${TRAVIS_BUILD_DIR}/.gitmodules
+fi;
+
 # Install unit tests
 # ==================
 
