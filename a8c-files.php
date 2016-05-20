Automattic/vip-go-mu-plugins

commit 989083f13b84b009c82a614debc1670c273bd571

Provide backwards compatibility by porting WP.com's function name
Rather than update existing code to account for how Go was disabling sizes, we can borrow WPcom's function name. The filter additions aren't dependent on the `A8C_Files` class, which makes this possible.

a8c-files.php
@@ -35,9 +35,7 @@ class A8C_Files {
 
 		add_filter( 'image_downsize', array( &$this, 'image_resize' ), 5, 3 ); // Ensure this runs before Jetpack, when Photon is active
 
-		// disable the automatic creation of intermediate image sizes
-		add_filter( 'intermediate_image_sizes',          '__return_empty_array' );
-		add_filter( 'intermediate_image_sizes_advanced', '__return_empty_array' );
+		// Automatic creation of intermediate image sizes is disabled via `wpcom_intermediate_sizes()`
 
 		// ensure we always upload with year month folder layouts
 		add_filter( 'pre_option_uploads_use_yearmonth_folders', function( $arg ) { return '1'; } );
@@ -589,6 +587,18 @@ function a8c_files_init() {
 	new A8C_Files();
 }
 
-if ( defined( 'FILES_CLIENT_SITE_ID' ) && defined( 'FILES_ACCESS_TOKEN' ) )
+/**
+ * Prevent WP from creating intermediate image sizes
+ *
+ * Function name parallels wpcom's implementation to accommodate existing code
+ */
+function wpcom_intermediate_sizes( $sizes ) {
+	__return_empty_array();
+}
+
+if ( defined( 'FILES_CLIENT_SITE_ID' ) && defined( 'FILES_ACCESS_TOKEN' ) ) {
 	add_action( 'init', 'a8c_files_init' );
+	add_filter( 'intermediate_image_sizes', 'wpcom_intermediate_sizes' );
+	add_filter( 'intermediate_image_sizes_advanced', 'wpcom_intermediate_sizes' );
+}
 

commit 1c5eb1c9d92d89297bf7130431ed3e00e415355e

One cannot simply call `__return_empty_array()` inside a function and walk away. One must return what's returned.
Since there was no return value, the effect was the same, but triggered a warning when WP later tried to iterate over a null when it expected an array.

I caused this with 989083f.

a8c-files.php
@@ -595,7 +595,7 @@ function a8c_files_init() {
  * Function name parallels wpcom's implementation to accommodate existing code
  */
 function wpcom_intermediate_sizes( $sizes ) {
-	__return_empty_array();
+	return __return_empty_array();
 }
 
 if ( defined( 'FILES_CLIENT_SITE_ID' ) && defined( 'FILES_ACCESS_TOKEN' ) ) {