c078494514480ecd260d905e2fc8423cb079ebd0
Erick Hitter <services@ethitter.com>
2016-06-01 20:43:02 -0700

    Suppress options page
    Also encapsulates everything in a class to dissuade unloading.

--- wp-cron-control.php
+++ wp-cron-control.php
@@ -10,23 +10,54 @@
 
 // TODO:
 // Global WP_CRON_CONTROL_SECRET
-// Remove menu - add_action( 'admin_menu', array( &$this, 'register_settings_page' ) ); WP_Cron_Control::instance
 
-/**
- * Enforce Platform-wide use of WP-Cron Control
- *
- * wpcroncontrol_settings: a:2:{s:6:"enable";s:1:"1";s:32:"enable_scheduled_post_validation";s:1:"1";}
- */
-function wpcom_vip_cron_control_options( $options ) {
-	return array(
-		'enable' =>                           '1',
-		'enable_scheduled_post_validation' => '1',
+class WPCom_VIP_Cron_Control {
+	/**
+	 *
+	 */
+	public function __construct() {
+		add_filter( 'pre_option_wpcroncontrol_settings', array( $this, 'set_options' ) );
+		add_action( 'admin_menu', array( $this, 'remove_menu_page' ), 99 );
+		add_action( 'admin_init', array( $this, 'block_admin_page' ) );
 
-	);
+		// Load plugin
+		require( __DIR__ . '/wp-cron-control/wp-cron-control.php' );
+	}
+
+	/**
+	 * Enforce Platform-wide use of WP-Cron Control
+	 *
+	 * wpcroncontrol_settings: a:2:{s:6:"enable";s:1:"1";s:32:"enable_scheduled_post_validation";s:1:"1";}
+	 */
+	public function set_options( $options ) {
+		return array(
+			'enable' =>                           '1',
+			'enable_scheduled_post_validation' => '1',
+
+		);
+	}
+
+	/**
+	 * Remove the menu page
+	 */
+	public function remove_menu_page() {
+		remove_submenu_page( 'options-general.php', 'wp-cron-control' ); // WP_Cron_Control::instance()->dashed_name
+	}
+
+	/**
+	 * Block access to the plugin's options page; everything is hardcoded
+	 */
+	public function block_admin_page() {
+		global $plugin_page;
+
+		if ( false !== stripos( $plugin_page, 'wp-cron-control' ) ) {
+			wp_die( 'This plugin\'s options are unavailable.<br /><br />Please open a support ticket with any questions.', 'Unauthorized', array(
+				'response' => 401,
+				'back_link' => true,
+			) );
+			// exit;
+		}
+	}
 }
-add_filter( 'pre_option_wpcroncontrol_settings', 'wpcom_vip_cron_control_options' );
 
-/**
- * Load plugin
- */
-require( __DIR__ . '/wp-cron-control/wp-cron-control.php' );
+new WPCom_VIP_Cron_Control;
