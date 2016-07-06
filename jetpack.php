7bfa02808ca1edc8a9bc3125dd13a4c88ed5334e
Erick Hitter <services@ethitter.com>
2016-07-06 10:16:56 -0700

    Jetpack 4.1
    See https://github.com/Automattic/jetpack/releases/tag/4.1.0

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://jetpack.me
  * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
  * Author: Automattic
- * Version: 4.0.4
+ * Version: 4.1
  * Author URI: http://jetpack.me
  * License: GPL2+
  * Text Domain: jetpack
