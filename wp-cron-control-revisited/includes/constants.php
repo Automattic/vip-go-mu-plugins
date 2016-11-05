5f0045c83744509f86477d15966201cd8a114fd3
Erick Hitter <services@ethitter.com>
2016-11-04 20:47:09 -0700

    Submodule the plugin now that it's its own repo

--- wp-cron-control-revisited/includes/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace WP_Cron_Control_Revisited;
-
-const REST_API_NAMESPACE     = 'wp-cron-control-revisited/v1';
-const REST_API_ENDPOINT_LIST = 'events';
-const REST_API_ENDPOINT_RUN  = 'event';
