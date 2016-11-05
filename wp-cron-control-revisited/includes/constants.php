5a1450b68cb43b54bb7b472649a6d915be07b475
Erick Hitter <services@ethitter.com>
2016-11-04 17:14:43 -0700

    Whitespace
    [skip ci]

--- wp-cron-control-revisited/includes/constants.php
+++ wp-cron-control-revisited/includes/constants.php
@@ -2,6 +2,6 @@
 
 namespace WP_Cron_Control_Revisited;
 
-const REST_API_NAMESPACE = 'wp-cron-control-revisited/v1';
+const REST_API_NAMESPACE     = 'wp-cron-control-revisited/v1';
 const REST_API_ENDPOINT_LIST = 'events';
-const REST_API_ENDPOINT_RUN = 'event';
+const REST_API_ENDPOINT_RUN  = 'event';
