6fc799296007d0c6bbf09775c92485f563457cfe
Simon Wheatley <simonw@codeforthepeople.com>
2016-12-02 15:20:17 +0000

    Explanatory comment around removing the conditional branch for 4.6.1 and below

--- vip-helpers/vip-syndication-cache.php
+++ vip-helpers/vip-syndication-cache.php
@@ -12,6 +12,9 @@ add_action( 'syn_after_setup_server', function() {
 			require_once( ABSPATH . WPINC . '/class-wp-simplepie-file.php' );
 			require_once( ABSPATH . WPINC . '/class-wp-simplepie-sanitize-kses.php' );
 		} else if ( file_exists( ABSPATH . WPINC . '/class-feed.php' ) ) {
+			// This branch of the conditional can be removed when we're
+			// no longer supporting 4.6.1 (the files moved to the above
+			// filenames in 4.7)
 			require_once( ABSPATH . WPINC . '/class-feed.php' );
 		}
 	}
