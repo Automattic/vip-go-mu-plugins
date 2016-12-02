06dfa8c33cfe9c57c5bb5e2f00713f0c284b67be
Simon Wheatley <simonw@codeforthepeople.com>
2016-12-02 13:08:52 +0000

    Require the required class files separately, if necessary
    Cribs from the core approach here:
    https://github.com/WordPress/WordPress/blob/493f76a3d2ef8c030ab5dcd4333f9a401208f534/wp-includes/feed.php#L672-L679
    
    Addresses #365

--- vip-helpers/vip-syndication-cache.php
+++ vip-helpers/vip-syndication-cache.php
@@ -2,6 +2,17 @@
 
 add_action( 'syn_after_setup_server', function() {
 	if ( ! class_exists( 'WP_Feed_Cache' ) ) {
+		if ( file_exists( ABSPATH . WPINC . '/class-wp-feed-cache.php' ) ) {
+			if ( ! class_exists( 'SimplePie', false ) ) {
+				require_once( ABSPATH . WPINC . '/class-simplepie.php' );
+			}
+
+			require_once( ABSPATH . WPINC . '/class-wp-feed-cache.php' );
+			require_once( ABSPATH . WPINC . '/class-wp-feed-cache-transient.php' );
+			require_once( ABSPATH . WPINC . '/class-wp-simplepie-file.php' );
+			require_once( ABSPATH . WPINC . '/class-wp-simplepie-sanitize-kses.php' );
+		} else if ( file_exists( ABSPATH . WPINC . '/class-feed.php' ) ) {
 			require_once( ABSPATH . WPINC . '/class-feed.php' );
+		}
 	}
 } );
