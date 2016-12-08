ce98ef31d2a1e3cfc82860af592bce5486eab6a8
Mohammad Jangda <batmoo@gmail.com>
2016-12-08 17:08:03 -0500

    Revert "Move the health check early"

--- vip-cache-manager/vip-cache-manager.php
+++ vip-cache-manager/vip-cache-manager.php
@@ -24,7 +24,15 @@ class WPCOM_VIP_Cache_Manager {
 	}
 
 	public function __construct() {
-		// N.B. The caching health check is run at the top of `000-vip-init.php`
+		// Execute the healthcheck as quickly as possible
+		if ( '/cache-healthcheck?' === $_SERVER['REQUEST_URI'] ) {
+			if ( function_exists( 'newrelic_end_transaction' ) ) {
+				# See: https://docs.newrelic.com/docs/agents/php-agent/configuration/php-agent-api#api-end-txn
+				newrelic_end_transaction( true );
+			}
+			die( 'ok' );
+		}
+
 		add_action( 'init', array( $this, 'init' ) );
 	}
 
