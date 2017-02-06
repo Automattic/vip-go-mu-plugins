d924342776139dc7a63dce707a59168de1cac642
Simon Wheatley <simonw@automattic.com>
2017-02-06 16:15:21 +0000

    Alright, don't ignore symlinks

--- ci/generate-docs.sh
+++ ci/generate-docs.sh
@@ -51,7 +51,7 @@ pwd
 composer require phpdocumentor/phpdocumentor
 # PHPDoc is really verbose, more than Travis can cope with,
 # so we send it to /dev/null
-vendor/phpdocumentor/phpdocumentor/bin/phpdoc --ignore-symlinks --verbose=2 --no-interaction --directory="${TRAVIS_BUILD_DIR}" --target="${VIP_DOCS_DIR}" --title="WordPress.com VIP – VIP Go Function Documentation" --template clean > /dev/null
+vendor/phpdocumentor/phpdocumentor/bin/phpdoc --verbose=2 --no-interaction --directory="${TRAVIS_BUILD_DIR}" --target="${VIP_DOCS_DIR}" --title="WordPress.com VIP – VIP Go Function Documentation" --template clean > /dev/null
 ls -alh $VIP_DOCS_DIR
 stat /home/travis/build/Automattic/vip-go-mu-plugins/advanced-post-cache.php
 
