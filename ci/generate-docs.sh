69cd074d3f3cacb449504df840bf7204189a7502
Simon Wheatley <simonw@automattic.com>
2017-02-06 15:42:16 +0000

    Quietly ignore symlinks

--- ci/generate-docs.sh
+++ ci/generate-docs.sh
@@ -49,7 +49,9 @@ cd $VIP_PHPDOC_DIR
 pwd
 
 composer require phpdocumentor/phpdocumentor
-vendor/phpdocumentor/phpdocumentor/bin/phpdoc --no-interaction --directory="${TRAVIS_BUILD_DIR}" --target="${VIP_DOCS_DIR}" --title="WordPress.com VIP – VIP Go Function Documentation" --template clean
+# PHPDoc is really verbose, more than Travis can cope with,
+# so we send it to /dev/null
+vendor/phpdocumentor/phpdocumentor/bin/phpdoc --ignore-symlinks --quiet --no-interaction -- --directory="${TRAVIS_BUILD_DIR}" --target="${VIP_DOCS_DIR}" --title="WordPress.com VIP – VIP Go Function Documentation" --template clean > /dev/null
 ls -alh $VIP_DOCS_DIR
 stat /home/travis/build/Automattic/vip-go-mu-plugins/advanced-post-cache.php
 
