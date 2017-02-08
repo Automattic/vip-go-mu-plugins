d6c765f2e6f8743bfb8852615ef7dd93f748092b
Mohammad Jangda <batmoo@gmail.com>
2017-02-08 11:46:23 -0500

    Rename test methods to reflect what they're testing

--- tests/test-lib-proxy-ip-forward.php
+++ tests/test-lib-proxy-ip-forward.php
@@ -24,7 +24,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		}
 	}
 
-	public function test__fix_remote_address__no_forwarded_for() {
+	public function test__fix_remote_address_from_ip_trail__no_forwarded_for() {
 		unset( $_SERVER['HTTP_X_FORWARDED_FOR'] );
 		$ip_trail = '1.2.3.4, 5.6.7.8';
 		$whitelist = [ '5.6.7.8' ];
@@ -35,7 +35,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__ip_trail_has_lt_2_ips() {
+	public function test__fix_remote_address_from_ip_trail__ip_trail_has_lt_2_ips() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4';
 		$whitelist = [ '5.6.7.8' ];
@@ -46,7 +46,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__ip_trail_has_gt_2_ips() {
+	public function test__fix_remote_address_from_ip_trail__ip_trail_has_gt_2_ips() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4, 9.0.21.0, 5.6.7.8';
 		$whitelist = [ '5.6.7.8' ];
@@ -57,7 +57,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__proxy_doesnt_match_forwarded_for() {
+	public function test__fix_remote_address_from_ip_trail__proxy_doesnt_match_forwarded_for() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.5.5.5';
 		$ip_trail = '1.2.3.4, 5.6.7.8';
 		$whitelist = [ '0.0.0.0' ];
@@ -68,7 +68,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__invalid_remote_ip() {
+	public function test__fix_remote_address_from_ip_trail__invalid_remote_ip() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4, 123456789';
 		$whitelist = [ '5.6.7.8' ];
@@ -79,7 +79,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__invalid_user_ip() {
+	public function test__fix_remote_address_from_ip_trail__invalid_user_ip() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = 'bad_ip, 5.6.7.8';
 		$whitelist = [ '5.6.7.8' ];
@@ -90,7 +90,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__ip_not_in_whitelist() {
+	public function test__fix_remote_address_from_ip_trail__ip_not_in_whitelist() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4, 5.6.7.8';
 		$whitelist = [ '0.0.0.0' ];
@@ -101,7 +101,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__ip_in_whitelist_ipv4() {
+	public function test__fix_remote_address_from_ip_trail__ip_in_whitelist_ipv4() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4, 5.6.7.8';
 		$whitelist = [ '5.6.7.8' ];
@@ -112,7 +112,7 @@ class IP_Foward_Test extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals( '1.2.3.4', $_SERVER['REMOTE_ADDR'] );
 	}
 
-	public function test__fix_remote_address__ip_in_whitelist_ipv6() {
+	public function test__fix_remote_address_from_ip_trail__ip_in_whitelist_ipv6() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '2001:db8::1234:ace:6006:1e, 5.6.7.8';
 		$whitelist = [ '5.6.7.8' ];
