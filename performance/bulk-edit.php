d7d94b95d0505c771787a2f841fd4e372d42fbd6
Erick Hitter <services@ethitter.com>
2017-02-21 10:32:37 -0800

    Basic limits on bulk actions based on number of posts to change

--- performance/bulk-edit.php
+++ performance/bulk-edit.php
@@ -11,3 +11,32 @@ add_action( 'load-edit.php', function() {
 		} );
 	}
 } );
+
+class Bulk_Edit {
+	/**
+	 * Generic callback to register class's hooks
+	 */
+	public static function register_hooks() {
+		add_action( 'bulk_actions-edit-post', array( __CLASS__, 'limit_bulk_edit' ) );
+	}
+
+	/**
+	 * Suppress bulk actions when too many posts would be affected
+	 *
+	 * Often causes database issues when too many posts are modified,
+	 * but since Core expects this to work with 20 posts, we limit to the same.
+	 */
+	public static function limit_bulk_edit( $bulk_actions ) {
+		$posts_per_page = get_query_var( 'posts_per_page' );
+
+		// Core defaults to 20 posts per page
+		// If requesting more, hide bulk actions
+		if ( $posts_per_page > 20 ) {
+			$bulk_actions = array();
+		}
+
+		return $bulk_actions;
+	}
+}
+
+Bulk_Edit::register_hooks();
