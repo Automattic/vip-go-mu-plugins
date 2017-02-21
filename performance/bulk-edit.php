e86f72f9cb19e175bbc9be6bf2291aa7380551b2
Erick Hitter <services@ethitter.com>
2017-02-21 12:08:12 -0800

    Cover all registered CPTs, not just those from Core
    Also handles a situation where Core doesn't set a limit, which we assume to be a case we don't want to support.

--- performance/bulk-edit.php
+++ performance/bulk-edit.php
@@ -8,7 +8,8 @@ class Bulk_Edit {
 	 */
 	public static function register_hooks() {
 		add_action( 'load-edit.php', array( __CLASS__, 'defer_term_counting' ) );
-		add_action( 'bulk_actions-edit-post', array( __CLASS__, 'limit_bulk_edit' ) );
+
+		add_action( 'wp_loaded', array( __CLASS__, 'limit_bulk_edit_for_registered_post_types' ) );
 	}
 
 	/**
@@ -24,15 +25,30 @@ class Bulk_Edit {
 	}
 
 	/**
+	 * Impose our bulk-edit limitations on all registered post types that provide an admin UI
+	 */
+	public static function limit_bulk_edit_for_registered_post_types() {
+		$types = get_post_types( array(
+			'show_ui' => true,
+		) );
+
+		foreach ( $types as $type ) {
+			add_action( 'bulk_actions-edit-' . $type, array( __CLASS__, 'limit_bulk_edit' ) );
+		}
+	}
+
+	/**
 	 * Suppress bulk actions when too many posts would be affected
 	 *
 	 * Often causes database issues when too many posts are modified,
 	 * but since Core expects this to work with 20 posts, we limit to the same.
 	 */
 	public static function limit_bulk_edit( $bulk_actions ) {
+		$per_page = get_query_var( 'posts_per_page' );
+
 		// Core defaults to 20 posts per page
-		// If requesting more, hide bulk actions
-		if ( get_query_var( 'posts_per_page' ) > 20 ) {
+		// If requesting more, or all entries, hide bulk actions
+		if ( $per_page > 20 || -1 === $per_page ) {
 			$bulk_actions = array();
 		}
 
