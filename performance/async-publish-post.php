aa38a5fcfd41a3c40edc61fef876a39630a8668c
Erick Hitter <services@ethitter.com>
2017-02-21 17:04:17 -0800

    Handle transitions from `publish`, per discussion in #469

--- performance/async-publish-post.php
+++ performance/async-publish-post.php
@@ -4,12 +4,15 @@ namespace Automattic\VIP\Performance;
 
 const EVENT = 'wpcom_async_publish_post';
 
-// TODO: should this be save_post to catch publish => not (e.g. trash)
-add_action( 'publish_post', 'queue_async_publish_post' );
+add_action( 'transition_post_status', 'queue_async_publish_post', 10, 3 );
 
-function queue_async_publish_post( $post ) {
-	$args = [ 'post_id' => $post->ID ]; // TODO: add before/after status?
-	if ( ! wp_next_scheduled( EVENT, $args ) ) {
-		wp_schedule_single_event( time(), EVENT, $args );  
+function queue_async_publish_post( $new_status, $old_status, $post ) {
+	// Pass only consistent data, allowing cron's duplicate handling to take effect
+	// For example, don't include post status here
+	// TODO: When clients can schedule their own events, include consistent name in $args to avoid duplicates
+	$args = [ 'post_id' => $post->ID ];
+
+	if ( in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( EVENT, $args ) ) {
+		wp_schedule_single_event( time(), EVENT, $args );
 	}
 }
