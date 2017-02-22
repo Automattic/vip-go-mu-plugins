fd1a1f9634c2fd13f51abc8f77fbbdf78fef71a7
Erick Hitter <services@ethitter.com>
2017-02-21 18:16:33 -0800

    Clarity
    Makes clear that functions are not for public consumption, and since we're in the general `Performance` namespace, the constant should have a meaningful name.

--- performance/async-publish-post.php
+++ performance/async-publish-post.php
@@ -2,7 +2,7 @@
 
 namespace Automattic\VIP\Performance;
 
-const EVENT = 'wpcom_async_publish_post';
+const ASYNC_PUBLISH_EVENT = 'wpcom_async_publish_post';
 
 /**
  * Queue async event when status was or is `publish`
@@ -11,27 +11,27 @@ const EVENT = 'wpcom_async_publish_post';
  * @param string $old_status
  * @param object $post
  */
-function queue_async_publish_post( $new_status, $old_status, $post ) {
+function _queue_async_publish_post( $new_status, $old_status, $post ) {
 	// Pass only consistent data, allowing cron's duplicate handling to take effect
 	// For example, don't include post status here
 	$args = [ 'post_id' => (int) $post->ID ];
 
-	if ( $new_status !== $old_status && in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( EVENT, $args ) ) {
-		wp_schedule_single_event( time(), EVENT, $args );
+	if ( $new_status !== $old_status && in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( ASYNC_PUBLISH_EVENT, $args ) ) {
+		wp_schedule_single_event( time(), ASYNC_PUBLISH_EVENT, $args );
 	}
 }
 
-add_action( 'transition_post_status', __NAMESPACE__ . '\queue_async_publish_post', 10, 3 );
+add_action( 'transition_post_status', __NAMESPACE__ . '\_queue_async_publish_post', 10, 3 );
 
 /**
  * Perform asynchronous tasks for a published post
  *
  * @param int $post_id
  */
-function wpcom_async_publish_post( $post_id ) {
+function _wpcom_do_async_publish_post( $post_id ) {
 	$post_object = get_post( $post_id );
 
 	do_action( 'async_publish_post', $post_id, $post_object );
 }
 
-add_action( EVENT, __NAMESPACE__ . '\wpcom_async_publish_post' );
+add_action( ASYNC_PUBLISH_EVENT, __NAMESPACE__ . '\_wpcom_do_async_publish_post' );
