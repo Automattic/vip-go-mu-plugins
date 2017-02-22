776e0c580f0c59ab92fc56224c49514d309ed7d8
Mohammad Jangda <batmoo@gmail.com>
2017-02-22 18:04:33 -0500

    Fix str_replace needle for dbDelta
    Core includes a key length for meta_key and our match was failing

--- schema.php
+++ schema.php
@@ -8,15 +8,22 @@ namespace Automattic\VIP\Schema;
 
 add_filter( 'dbdelta_create_queries', function( $queries ) {
 	global $wpdb;
-	
 	foreach( $queries as $k => $q ) {
-	
-		// Add meta_key_value index
+		// Replace meta_key index with one that indexes meta_value as well
 		if ( preg_match( "|CREATE TABLE ([^ ]*)|", $q, $matches ) && $wpdb->postmeta === $matches[1] ) {
-			$queries[$k] = str_replace( 'KEY meta_key (meta_key)', 'KEY meta_key (meta_key), KEY `meta_key_value` (`meta_key`(191), `meta_value`(20))', $q );
-			return $queries;
+			$queries[$k] = str_replace(
+				'KEY meta_key (meta_key(191))',
+				sprintf( 'KEY %s', get_postmeta_key_value_index() ),
+				$q
+			);
 		}
 	}
 
 	return $queries;
 });
+
+function get_postmeta_key_value_index() {
+	// 191 for meta_key is max set by core
+	// 20 for meta_value is arbitrary
+	return '`vip_meta_key_value` (`meta_key`(191), `meta_value`(20))';
+}
