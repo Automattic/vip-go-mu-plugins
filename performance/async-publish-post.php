cdce1183687e214454a3452c31af86a80e49509b
Erick Hitter <services@ethitter.com>
2017-02-21 17:57:51 -0800

    Simple, single-event implementation of an async action
    Questions:
    * Should this cover all post types?
    * Should we provide a filter to disable it for post types, statuses, etc?
    * What about discrete events for actions?
    * Might something be missed, since we don't requeue if there's already a scheduled event?

--- performance/async-publish-post.php
+++ performance/async-publish-post.php
@@ -4,15 +4,34 @@ namespace Automattic\VIP\Performance;
 
 const EVENT = 'wpcom_async_publish_post';
 
-add_action( 'transition_post_status', 'queue_async_publish_post', 10, 3 );
-
+/**
+ * Queue async event when status was or is `publish`
+ *
+ * @param string $new_status
+ * @param string $old_status
+ * @param object $post
+ */
 function queue_async_publish_post( $new_status, $old_status, $post ) {
 	// Pass only consistent data, allowing cron's duplicate handling to take effect
 	// For example, don't include post status here
-	// TODO: When clients can schedule their own events, include consistent name in $args to avoid duplicates
-	$args = [ 'post_id' => $post->ID ];
+	$args = [ 'post_id' => (int) $post->ID ];
 
-	if ( in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( EVENT, $args ) ) {
+	if ( $new_status !== $old_status && in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( EVENT, $args ) ) {
 		wp_schedule_single_event( time(), EVENT, $args );
 	}
 }
+
+add_action( 'transition_post_status', __NAMESPACE__ . '\queue_async_publish_post', 10, 3 );
+
+/**
+ * Perform asynchronous tasks for a published post
+ *
+ * @param int $post_id
+ */
+function wpcom_async_publish_post( $post_id ) {
+	$post_object = get_post( $post_id );
+
+	do_action( 'async_publish_post', $post_id, $post_object );
+}
+
+add_action( EVENT, __NAMESPACE__ . '\wpcom_async_publish_post' );
