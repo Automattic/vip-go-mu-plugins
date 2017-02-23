f12ef223f05def62bc478d39e9430e365d197596
Mohammad Jangda <batmoo@gmail.com>
2017-02-23 15:58:13 -0500

    Update test to match new meta_value length
    20 => 100

--- tests/test-schema.php
+++ tests/test-schema.php
@@ -12,6 +12,6 @@ class VIP_Go_Schema_Test extends WP_UnitTestCase {
 		$deltas = dbDelta( 'blog', false );
 		
 		$this->assertCount( 1, $deltas, 'More deltas than expected for blogs table' );
-		$this->assertEquals( $deltas[0], 'Added index wptests_postmeta KEY `vip_meta_key_value` (`meta_key`(191),`meta_value`(20))', 'Delta did not find vip_meta_key_value index or assertion needs updating.' );
+		$this->assertEquals( $deltas[0], 'Added index wptests_postmeta KEY `vip_meta_key_value` (`meta_key`(191),`meta_value`(100))', 'Delta did not find vip_meta_key_value index or assertion needs updating.' );
 	}
 }
