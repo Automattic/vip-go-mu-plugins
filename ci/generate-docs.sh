bd3cf5555e413a75879c7613529effc6647c246d
Simon Wheatley <simonw@automattic.com>
2017-02-24 17:26:57 +0000

    Fix things to only work on the proper branches, etc

--- ci/generate-docs.sh
+++ ci/generate-docs.sh
@@ -4,20 +4,15 @@
 
 set -ex
 
-#if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-#	echo "Not deploying pull requests."
-#	exit
-#fi
-#
-#if [[ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]]; then
-#	echo "Not on the '${DEPLOY_BRANCH}' branch."
-#	exit
-#fi
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
 
-#cd ${TRAVIS_BUILD_DIR}/../../
-##mkdir phpdoc
-##cd phpdocs
-#pwd
+if [[ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]]; then
+	echo "Not on the '${DEPLOY_BRANCH}' branch."
+	exit
+fi
 
 VIP_DOCS_DIR="$(readlink -m ${TRAVIS_BUILD_DIR}/../../phpdoc)"
 VIP_PHPDOC_DIR="/tmp/phpdoc/"
