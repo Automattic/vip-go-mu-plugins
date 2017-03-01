ea13331e57061bcbb04779c440e19ea8a5a17ce2
Josh Betz <j@joshbetz.com>
2017-03-01 17:21:20 -0600

    Add `wp vip migration dbdelta` command.

---
+++ wp-cli/vip-migrations.php
@@ -0,0 +1,34 @@
+<?php
+
+class VIP_Go_Migrations_Command extends WPCOM_VIP_CLI_Command {
+
+	/**
+	 * Run dbDelta() for the current site.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [<tables>]
+	 * : Which tables to update (all, blog, global, ms_global)
+	 */
+	function dbdelta( $args ) {
+		require_once( ABSPATH . 'wp-admin/include/upgrade.php' );
+
+		if ( in_array( $args[1], array( '', 'all', 'blog', 'global', 'ms_global' ), true ) {
+			$changes = dbDelta( $args[1] );
+		} else {
+			$changes = dbDelta();
+		}
+
+		if ( empty( $changes ) ) {
+			WP_CLI::success( 'No changes.' );
+		}
+
+		foreach( $changes as $change ) {
+			WP_CLI::line( $change );
+		}
+
+		WP_CLI::success( count( $changes ) . ' changes.' );
+	}
+}
+
+WP_CLI::add_command( 'vip migration', 'VIP_Go_Migrations_Command' );
