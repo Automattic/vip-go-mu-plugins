ee3a3756120fce63431fc36ba287b558f084429e
Josh Betz <j@joshbetz.com>
2017-03-02 10:23:40 -0600

    Fix include path

--- wp-cli/vip-migrations.php
+++ wp-cli/vip-migrations.php
@@ -11,7 +11,7 @@ class VIP_Go_Migrations_Command extends WPCOM_VIP_CLI_Command {
 	 * : Which tables to update (all, blog, global, ms_global)
 	 */
 	function dbdelta( $args ) {
-		require_once( ABSPATH . 'wp-admin/include/upgrade.php' );
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
 
 		if ( in_array( $args[1], array( '', 'all', 'blog', 'global', 'ms_global' ), true ) ) {
 			$changes = dbDelta( $args[1] );
@@ -21,6 +21,7 @@ class VIP_Go_Migrations_Command extends WPCOM_VIP_CLI_Command {
 
 		if ( empty( $changes ) ) {
 			WP_CLI::success( 'No changes.' );
+			return;
 		}
 
 		foreach( $changes as $change ) {
