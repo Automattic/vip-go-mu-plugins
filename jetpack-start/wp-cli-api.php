4ad90be7d1056f02f4c62f3e355ecaa1c8f0f040
Mohammad Jangda <batmoo@gmail.com>
2017-03-13 17:41:18 -0400

    jetpack keys command needs user set

--- jetpack-start/wp-cli-api.php
+++ jetpack-start/wp-cli-api.php
@@ -139,10 +139,11 @@ class Jetpack_Start_API_CLI_Command extends WP_CLI_Command {
 		$jetpack_secret = $data['jetpack_secret'] ?? '';
 		$jetpack_access_token = $data['jetpack_access_token'] ?? '';
 		$jetpack_result = WP_CLI::runcommand( sprintf(
-			'jetpack-start keys jetpack --jetpack_id=%s --jetpack_secret=%s --jetpack_access_token=%s',
+			'jetpack-start keys jetpack --jetpack_id=%s --jetpack_secret=%s --jetpack_access_token=%s --user=%s',
 			escapeshellarg( $jetpack_id ),
 			escapeshellarg( $jetpack_secret ),
-			escapeshellarg( $jetpack_access_token )
+			escapeshellarg( $jetpack_access_token ),
+			escapeshellarg( WPCOM_VIP_MACHINE_USER_LOGIN )
 		), $runcommand_args );
 	}
 
