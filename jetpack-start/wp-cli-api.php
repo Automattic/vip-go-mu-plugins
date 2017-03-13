4e5045f90054d2340824fbaa91826e86bcfdc3f5
Mohammad Jangda <batmoo@gmail.com>
2017-03-13 17:38:21 -0400

    Fix runcommand for jetpack

--- jetpack-start/wp-cli-api.php
+++ jetpack-start/wp-cli-api.php
@@ -139,7 +139,7 @@ class Jetpack_Start_API_CLI_Command extends WP_CLI_Command {
 		$jetpack_secret = $data['jetpack_secret'] ?? '';
 		$jetpack_access_token = $data['jetpack_access_token'] ?? '';
 		$jetpack_result = WP_CLI::runcommand( sprintf(
-			'jetpack-start keys vaultpress --jetpack_id=%s --jetpack_secret=%s --jetpack_access_token=%s',
+			'jetpack-start keys jetpack --jetpack_id=%s --jetpack_secret=%s --jetpack_access_token=%s',
 			escapeshellarg( $jetpack_id ),
 			escapeshellarg( $jetpack_secret ),
 			escapeshellarg( $jetpack_access_token )
