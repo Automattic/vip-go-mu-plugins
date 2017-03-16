0e353e1c8161178b84ff53ad85f57e350e2c826b
Erick Hitter <services@ethitter.com>
2017-03-16 11:41:07 -0700

    Review feedback: new home for secret-generation and checking

---
+++ vip-rest-api.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: REST API Enhancements
+Plugin URI: https://vip.wordpress.com/
+Description: Add custom REST API endpoints for VIP requests; these endpoints are private and only for the use of WordPress.com VIP
+Author: Erick Hitter, Automattic
+Version: 0.1
+*/
+
+/**
+ * Include customizations
+ */
+require_once __DIR__ . '/rest-api/vip-endpoints.php';
