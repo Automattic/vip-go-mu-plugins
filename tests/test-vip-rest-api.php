6ab56eef48e8394b56b4b165a22388958be2b466
Erick Hitter <services@ethitter.com>
2017-03-17 14:59:05 -0700

    Less repetition in tests

--- tests/test-vip-rest-api.php
+++ tests/test-vip-rest-api.php
@@ -4,6 +4,15 @@ namespace Automattic\VIP\Tests;
 
 class VIP_Go_REST_API_Test extends \WP_UnitTestCase {
 	/**
+	 * Let's reduce repetition
+	 */
+	const VALID_NAMESPACE   = 'vip/v1';
+	const INVALID_NAMESPACE = 'test/invalid';
+
+	const VALID_AUTH_MECHANISM   = 'VIP-MACHINE-TOKEN';
+	const INVALID_AUTH_MECHANISM = 'Basic';
+
+	/**
 	 * Test prep
 	 */
 	public function setUp() {
@@ -17,32 +26,32 @@ class VIP_Go_REST_API_Test extends \WP_UnitTestCase {
 	 * Test that a valid token verifies as expected
 	 */
 	public function test__valid_token_creation() {
-		$token = \wpcom_vip_generate_go_rest_api_request_token( 'test/valid' );
+		$token = \wpcom_vip_generate_go_rest_api_request_token( self::VALID_NAMESPACE );
 
-		$header = 'VIP-MACHINE-TOKEN ' . $token;
+		$header = self::VALID_AUTH_MECHANISM . ' ' . $token;
 
-		$this->assertTrue( \wpcom_vip_verify_go_rest_api_request_authorization( 'test/valid', $header ) );
+		$this->assertTrue( \wpcom_vip_verify_go_rest_api_request_authorization( self::VALID_NAMESPACE, $header ) );
 	}
 
 	/**
 	 * Test that a token doesn't verify for a different namespace
 	 */
 	public function test__invalid_token_creation() {
-		$token = \wpcom_vip_generate_go_rest_api_request_token( 'test/invalid' );
+		$token = \wpcom_vip_generate_go_rest_api_request_token( self::INVALID_NAMESPACE );
 
-		$header = 'VIP-MACHINE-TOKEN ' . $token;
+		$header = self::VALID_AUTH_MECHANISM . ' ' . $token;
 
-		$this->assertFalse( \wpcom_vip_verify_go_rest_api_request_authorization( 'test/valid', $header ) );
+		$this->assertFalse( \wpcom_vip_verify_go_rest_api_request_authorization( self::VALID_NAMESPACE, $header ) );
 	}
 
 	/**
 	 * Test that a valid token doesn't verify with an invalid header
 	 */
 	public function test__invalid_header() {
-		$token = \wpcom_vip_generate_go_rest_api_request_token( 'test/valid' );
+		$token = \wpcom_vip_generate_go_rest_api_request_token( self::VALID_NAMESPACE );
 
-		$header = 'Basic ' . $token;
+		$header = self::INVALID_AUTH_MECHANISM . ' ' . $token;
 
-		$this->assertFalse( \wpcom_vip_verify_go_rest_api_request_authorization( 'test/valid', $header ) );
+		$this->assertFalse( \wpcom_vip_verify_go_rest_api_request_authorization( self::VALID_NAMESPACE, $header ) );
 	}
 }
