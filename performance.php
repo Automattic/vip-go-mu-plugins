663df7b572b9ed84945bba498c079b88c90bc245
Mohammad Jangda <batmoo@gmail.com>
2017-03-29 18:51:31 -0400

    Prevent core from doing filname lookups
    For media search. This results in meta queries that are super slow on
    large sites and can take upwards of 30+ seconds for search.
    
    see https://core.trac.wordpress.org/ticket/39358

--- performance.php
+++ performance.php
@@ -4,4 +4,5 @@ require_once( __DIR__ . '/performance/lastpostmodified.php' );
 require_once( __DIR__ . '/performance/bulk-edit.php' );
 require_once( __DIR__ . '/performance/vip-tweaks.php' );
 require_once( __DIR__ . '/performance/do-pings.php' );
+require_once( __DIR__ . '/performance/media.php' );
 require_once( __DIR__ . '/performance/plugins.php' );
