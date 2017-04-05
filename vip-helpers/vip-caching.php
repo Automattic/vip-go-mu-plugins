4a42d504f764cb099571be302ffbcacfc77c5e04
David Binovec <david.binovec@automattic.com>
2017-04-05 12:06:45 +0000

    Run the `wpcom_vip_set_old_slug_redirect_cache` callback as the very last one on the `old_slug_redirect_url` filter
    This should allow clients to filter the `old_slug_redirect_url` at default priority and still have the filtered URL properly cached

--- vip-helpers/vip-caching.php
+++ vip-helpers/vip-caching.php
@@ -660,7 +660,7 @@ function wpcom_vip_wp_old_slug_redirect() {
 		$redirect = wp_cache_get( 'old_slug' . $wp_query->query_vars['name'] );
 
 		if ( false === $redirect ) {
-			add_filter( 'old_slug_redirect_url', 'wpcom_vip_set_old_slug_redirect_cache' );
+			add_filter( 'old_slug_redirect_url', 'wpcom_vip_set_old_slug_redirect_cache', 9999, 1 );
 			// If an old slug is not found the function returns early and does not apply the old_slug_redirect_url filter. so we will set the cache for not found and if it is found it will be overwritten later in wpcom_vip_set_old_slug_redirect_cache()
 			wp_cache_set( 'old_slug' . $wp_query->query_vars['name'], 'not_found', 'default', 12 * HOUR_IN_SECONDS + mt_rand( 0, 12 * HOUR_IN_SECONDS ) );
 		} elseif ( 'not_found' === $redirect ) {
