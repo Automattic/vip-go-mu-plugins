c118968580bdc2c783d7330cebc8812ab2c466ff
Mohammad Jangda <batmoo@gmail.com>
2017-04-11 18:36:11 -0400

    Fix typo in filter name
    Causing the filter to not be removed correctly.

--- performance/media.php
+++ performance/media.php
@@ -3,5 +3,5 @@
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
 add_action( 'pre_get_posts', function() {
-	remove_filter( 'post_clauses', '_filter_query_attachment_filenames' );
+	remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
 } );
