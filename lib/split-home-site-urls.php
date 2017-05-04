48fcaea55c92363634242debac65447a9944847b
Erick Hitter <git-contrib@ethitter.com>
2017-05-03 22:43:17 -0700

    Enforce canonical redirect for logged-out users

--- lib/split-home-site-urls.php
+++ lib/split-home-site-urls.php
@@ -60,3 +60,33 @@ function add_home_logged_in_cookie( $logged_in_cookie, $expire, $expiration, $us
 add_action( 'set_logged_in_cookie', __NAMESPACE__ . '\add_home_logged_in_cookie', 10, 5 );
 
 // TODO: clear custom cookies on clear_auth_cookie
+
+/**
+ * Redirect logged-out users to the canonical (home) URL
+ */
+function enforce_logged_out_canonical_redirect() {
+	if ( ! isset( $_SERVER['HTTP_HOST'] ) ) {
+		return;
+	}
+
+	if ( is_user_logged_in() ) {
+		return;
+	}
+
+	$requested_proto = is_ssl() ? 'https://' : 'http://';
+	$requested_host  = $_SERVER['HTTP_HOST'];
+	$requested_uri   = $_SERVER['REQUEST_URI'];
+
+	$home_url_host = parse_url( home_url( '/' ), PHP_URL_HOST );
+	$site_url_host = parse_url( site_url( '/' ), PHP_URL_HOST );
+
+	if ( $requested_host === $site_url_host ) {
+		$redirect_url = $requested_proto . $home_url_host . $requested_uri;
+
+		$status_code = apply_filters( 'wpcom_vip_split_url_canonical_redirect_status_code', 301 );
+
+		wp_redirect( $redirect_url, $status_code );
+		exit;
+	}
+}
+add_action( 'parse_request', __NAMESPACE__ . '\enforce_logged_out_canonical_redirect' );
