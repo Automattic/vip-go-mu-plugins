8f9a77c3823f4793df98275b20777f1f4cb9a7d9
Erick Hitter <git-contrib@ethitter.com>
2017-05-05 16:06:45 -0700

    Fix REST API requests from the admin

--- lib/split-home-site-urls/misc.php
+++ lib/split-home-site-urls/misc.php
@@ -5,9 +5,18 @@ namespace Automattic\VIP\Split_Home_Site_URLs;
 /**
 * Ensure preview URLs are served over SSL
 */
-function fix_preview_link_host( $link ) {
+function fix_url_host( $url ) {
 	$search = get_home_url( null, '/', is_ssl() ? 'https' : 'http' );
 
-	return str_replace( $search, site_url( '/' ), $link );
+	return str_replace( $search, site_url( '/' ), $url );
 }
-add_filter( 'preview_post_link', __NAMESPACE__ . '\fix_preview_link_host' );
+add_filter( 'preview_post_link', __NAMESPACE__ . '\fix_url_host' );
+
+/**
+ * Ensure REST API requests are secure and respect cookies
+ *
+ * REST API uses home_url() by default
+ */
+if ( is_admin() ) {
+	add_filter( 'rest_url', __NAMESPACE__ . '\fix_url_host', 99 );
+}
