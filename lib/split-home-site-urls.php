632eec03e2d1c07f4341191336ec385e813d7f2a
Erick Hitter <git-contrib@ethitter.com>
2017-05-04 17:27:25 -0700

    Ensure wp-login redirects to wp-admin use the proper host
    Since cookies aren't set for `home` right now, visitor will end up in what appears to be a login loop.

--- lib/split-home-site-urls.php
+++ lib/split-home-site-urls.php
@@ -56,3 +56,44 @@ function enforce_logged_out_canonical_redirect() {
 	}
 }
 add_action( 'parse_request', __NAMESPACE__ . '\enforce_logged_out_canonical_redirect' );
+
+/**
+ * Fix login redirect URLs that point to home URL instead of site URL
+ */
+function correct_login_redirect( $login_url, $redirect_to ) {
+	// Nothing to correct if it's not a wp-admin link
+	if ( empty( $redirect_to ) || false === stripos( $redirect_to, 'wp-admin' ) ) {
+		return $login_url;
+	}
+
+	// Grab the path relative to wp-admin
+	preg_match( '#/wp-admin(/)?(.+?)$#i', $redirect_to, $admin_paths );
+
+	// Build a proper admin URL
+	if ( is_array( $admin_paths ) && isset( $admin_paths[2] ) ) {
+		$redirect_to = admin_url( $admin_paths[2] );
+	} else {
+		$redirect_to = admin_url( '/' );
+	}
+
+	// Replace the query string wp_login_url() produced, maintaining encoding
+	$login_url = remove_query_arg( 'redirect_to', $login_url );
+	$login_url = add_query_arg( 'redirect_to', urlencode( $redirect_to ), $login_url );
+
+	return $login_url;
+}
+add_filter( 'login_url', __NAMESPACE__ . '\correct_login_redirect', 10, 2 );
+
+/**
+ * Ensure that both home and site URL are valid redirect hosts
+ */
+function filter_allowed_redirect_hosts( $hosts ) {
+	$hosts[] = parse_url( site_url( '/' ), PHP_URL_HOST );
+
+	// In case something else already added the site_url()
+	// Core doesn't, but other plugins may
+	$hosts = array_unique( $hosts );
+
+	return $hosts;
+}
+add_filter( 'allowed_redirect_hosts', __NAMESPACE__ . '\filter_allowed_redirect_hosts' );
