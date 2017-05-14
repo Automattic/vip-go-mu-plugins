fe39a1bc9961f072d1797fa15f8306fe2f6cd41d
Mohammad Jangda <batmoo@gmail.com>
2017-05-14 15:02:30 -0700

    Add search-replace command from wp-cli
    Small mods to the loader and filenames since we don't have composer +
    autoloading.
    
    https://github.com/wp-cli/search-replace-command

---
+++ wp-cli/search-replace.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Adapted from https://github.com/wp-cli/search-replace-command
+ */
+
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+require_once( __DIR__ . '/search-replace/command.php' );
+require_once( __DIR__ . '/search-replace/search-replacer.php' );
+
+WP_CLI::add_command( 'search-replace', 'Search_Replace_Command' );
