46bafb546ed1592bbd911277b03c03120dc77563
Mohammad Jangda <batmoo@gmail.com>
2017-05-16 03:23:26 -0400

    New migration commands
    scrub: cleans out sensitive (unsyncable) options for services like
    Jetpack
    search-replace-url: a utility command for replacing one URL with another
    that relies on a whitelist of table columns (since not all columns will
    have URLs).

--- wp-cli/vip-migrations.php
+++ wp-cli/vip-migrations.php
@@ -5,6 +5,152 @@ use \WP_CLI\Utils;
 class VIP_Go_Migrations_Command extends WPCOM_VIP_CLI_Command {
 
 	/**
+	 * Deletes senstive options
+	 *
+	 * @subcommand scrub-site
+	 */
+	public function clear_sensitive_options( $args, $assoc_args ) {
+		WP_CLI::log( 'Clearing sensitive options (like Jetpack connection).' );
+
+		// TODO: if called from preprod site with production values, does it disconnect the preprod site?
+//		WP_CLI::log( '- Disconnecting Jetpack' );
+//		Jetpack::disconnect();
+
+		WP_CLI::log( '- Deleting Options' );
+		$options = [
+			'jetpack_options',
+			'jetpack_private_options',
+			'vaultpress',
+			'wordpress_api_key',
+		];
+
+		foreach ( $options as $option_name ) {
+			WP_CLI::log( '-- ' . $option_name );
+			delete_option( $option_name );
+		}
+	}
+
+	/**
+	 * Update URLs across a whitelist of table columns
+	 *
+	 * ## OPTIONS
+	 *
+	 * --from=<from_url>
+	 * : The URL to search for.
+	 *
+	 * --to=<to_url>
+	 * : The URL to replace with.
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp vip migration search-replace-url 'http://example.com' 'http://example.go-vip.co'
+	 *
+	 * @subcommand search-replace-url
+	 */
+	public function search_replace_url( $args, $assoc_args ) {
+		$from_url = $assoc_args['from'] ?? false;
+		$to_url = $assoc_args['to'] ?? false;
+
+		$from_url_parsed = parse_url( $from_url );
+		if ( empty( $from_url ) || ! $from_url_parsed ) {
+			WP_CLI::error( sprintf( 'Please provide a valid `--from` URL (current: %s)', $from_url ) );
+			return;
+		}
+
+		$to_url_parsed = parse_url( $to_url );
+		if ( empty( $to_url ) || ! $to_url_parsed ) {
+			WP_CLI::error( sprintf( 'Please provide a valid `--to` URL (current: %s)', $to_url ) );
+			return;
+		}
+
+		// TODO: should we have a flag to exclude home/siteurl option?
+
+		$tables_and_columns = [
+			'wp_commentmeta' => [
+				'meta_key',
+				'meta_value',
+			],
+
+			'wp_comments' => [
+				'comment_author',
+				'comment_author_url',
+				'comment_content',
+				'comment_agent',
+			],
+
+			'wp_links' => [
+				'link_url',
+				'link_name',
+				'link_image',
+				'link_description',
+				'link_notes',
+				'link_rss',
+			],
+
+			'wp_options' => [
+				'option_name',
+				'option_value',
+			],
+
+			'wp_postmeta' => [
+				'meta_key',
+				'meta_value',
+			],
+
+			'wp_posts' => [
+				'post_content',
+				'post_title',
+				'post_excerpt',
+				'post_name',
+				'post_content_filtered',
+				'guid',
+			],
+
+			'wp_term_taxonomy' => [
+				'taxonomy',
+				'description',
+			],
+
+			'wp_termmeta' => [
+				'meta_key',
+				'meta_value',
+			],
+
+			'wp_terms' => [
+				'name',
+			],
+
+			'wp_usermeta' => [
+				'meta_key',
+				'meta_value',
+			],
+
+			'wp_users' => [
+				'user_url',
+				'display_name',
+			],
+		];
+
+		$runcommand_args = [
+			'exit_error' => false,
+		];
+
+		foreach ( $tables_and_columns as $table => $columns ) {
+			$command = sprintf(
+				'vip search-replace %1$s %2$s %3$s --include-columns=%4$s --verbose',
+				escapeshellarg( $from_url ),
+				escapeshellarg( $to_url ),
+				escapeshellarg( $table ),
+				escapeshellarg( implode( ',', $columns ) )
+			);
+
+			WP_CLI::log( 'Running command: ' . $command );
+			WP_CLI::runcommand( $command, $runcommand_args );
+			WP_CLI::log( '---' );
+		}
+	}
+
+	/**
 	 * Run dbDelta() for the current site.
 	 *
 	 * [--network]
