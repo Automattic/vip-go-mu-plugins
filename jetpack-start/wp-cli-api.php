69f316811772f115d4b13032994f5f49dcb98d39
Daniel Walmsley <goldsounds@gmail.com>
2017-05-31 20:57:50 -0700

    Rudimentary conversion to Jetpack Start v2

--- jetpack-start/wp-cli-api.php
+++ jetpack-start/wp-cli-api.php
@@ -1,34 +1,27 @@
 <?php
 
+/** 
+ * Note: this file requires that WPCOM_VIP_JP_START_API_CLIENT_ID and WPCOM_VIP_JP_START_CLIENT_SECRET are set
+ */
+
 class Jetpack_Start_CLI_Command extends WP_CLI_Command {
 	const API_ERROR_EXISTING_SUBSCRIPTION = 'Failed to provision WPCOM store subscription';
 	const API_ERROR_USER_PERMISSIONS = 'User does not have permission to administer the given site.';
 
 	/**
-	 * Cancel a Jetpack Start subscription
+	 * Cancel a Jetpack Start subscription for the current site
 	 *
 	 * ## OPTIONS
-	 *
-	 * [--jetpack_site_id=<site_id>]
-	 * : WP.com site ID for the shadow site. If not specified, grabs the site ID from Jetpack.
 	 *
 	 * ## EXAMPLES
 	 *
 	 *     wp jetpack-start api cancel
-	 *     wp jetpack-start api cancel --jetpack_site_id=999999999
 	 *
 	 */
 	public function cancel( $args, $assoc_args ) {
-		$site_id = WP_CLI\Utils\get_flag_value( $assoc_args, 'jetpack_site_id', false );
 
-		if ( empty( $site_id ) ) {
-			$site_id = Jetpack_Options::get_option( 'id' );
-			if ( empty( $site_id ) ) {
-				WP_CLI::error( 'Could not get WP.com blog_id from the local Jetpack install. Please specify the `--jetpack_site_id` of the Jetpack Shadow site to cancel the subscription. You can get this from the Jetpack Debugger or using WP.com Network Admin.' );
-			}
-		}
-
-		$data = $this->api_cancel_subscription( $site_id );
+		$data = $this->run_jetpack_bin( 'partner-cancel.sh' );
+		
 		if ( is_wp_error( $data ) ) {
 			WP_CLI::error( $data->get_error_message() );
 		}
@@ -58,8 +51,8 @@ class Jetpack_Start_CLI_Command extends WP_CLI_Command {
 	 *
 	 */
 	public function connect( $args, $assoc_args ) {
-		if ( ! defined( 'WPCOM_VIP_JP_START_API_BASE' ) || ! defined( 'WPCOM_VIP_JP_START_API_TOKEN' ) ) {
-			WP_CLI::error( 'Jetpack Start API constants (`WPCOM_VIP_JP_START_API_BASE` and/or `WPCOM_VIP_JP_START_API_TOKEN`) are not defined. Please check the `vip-secrets` file to confirm they\'re there, accessible, and valid.' );
+		if ( ! defined( 'WPCOM_VIP_JP_START_API_CLIENT_ID' ) || ! defined( 'WPCOM_VIP_JP_START_API_CLIENT_SECRET' ) ) {
+			WP_CLI::error( 'Jetpack Start API constants (`WPCOM_VIP_JP_START_API_CLIENT_ID` and/or `WPCOM_VIP_JP_START_API_CLIENT_SECRET`) are not defined. Please check the `vip-secrets` file to confirm they\'re there, accessible, and valid.' );
 		}
 
 		if ( defined( 'JETPACK_DEV_DEBUG' ) && true === JETPACK_DEV_DEBUG ) {
@@ -130,26 +123,13 @@ class Jetpack_Start_CLI_Command extends WP_CLI_Command {
 			return false;
 		}
 
-		WP_CLI::line( '-- Fetching keys from Jetpack Start API' );
-		$data = $this->api_fetch_keys( $user );
+		WP_CLI::line( '-- Provisioning via Jetpack Start API' );
+		$data = $this->run_jetpack_bin( 'partner-provision.sh', array( 'user_id' => $user->ID, 'wpcom_user_id' => 22573739, 'plan' => 'premium' ) );
+		
 		if ( is_wp_error( $data ) ) {
-			$message = $data->get_error_message();
-			// Use strpos because our API method appends stuff to the error message
-			if ( false !== strpos( $message, self::API_ERROR_EXISTING_SUBSCRIPTION ) ) {
-				$message = 'There is an existing Jetpack Start subscription for this site. Please disconnect using the `cancel` subcommand and try again.';
-			} elseif ( false !== strpos( $message, self::API_ERROR_USER_PERMISSIONS ) ) {
-                                $jetpack_shadow_site_id = Jetpack_Options::get_option( 'id' );
-                                if ( false === $jetpack_shadow_site_id ) {
-                                        $jetpack_shadow_site_id = '%JETPACK_SHADOW_SITE_ID%';
-                                }
-                                $message = sprintf( 'This site already has an existing Jetpack shadow site but the `%s` is not an administrator on it.' . PHP_EOL . 'From within `wp shell` on your WPCom sandbox: `add_user_to_blog( %d, get_user_by( "login", "wpcomvip" )->ID, "administrator" ); `', WPCOM_VIP_MACHINE_USER_LOGIN, $jetpack_shadow_site_id );
-			}
-			WP_CLI::warning( 'Failed to fetch keys from Jetpack Start: ' . $message );
+			WP_CLI::warning( $data->get_error_message() );
 			return false;
 		}
-
-		WP_CLI::line( '-- Adding keys to site' );
-		$this->install_keys( $data );
 
 		update_option( 'vip_jetpack_start_connected_on', time(), false );
 
@@ -195,107 +175,18 @@ class Jetpack_Start_CLI_Command extends WP_CLI_Command {
 		return $user;
 	}
 
-	private function api_request( $url, $args ) {
-		// get_api_auth_header returns an associative array.
-		// The merge will make it easier if we have to add more headers in the future.
-		$headers = array_merge( [], $this->get_api_auth_header() );
-		$request = wp_remote_post( $url, [
-			'body'    => $args,
-			'headers' => $headers,
-			'timeout' => 60, // jetpack start can be a bit slow so give it time
-		] );
+	private function run_jetpack_bin( $script, $args = null ) {
+		$script_path = JETPACK__PLUGIN_DIR . 'bin/' . $script;
+		exec( sprintf( '%s --partner_id=%d --partner_secret=%s', $script_path, WPCOM_VIP_JP_START_API_CLIENT_ID, WPCOM_VIP_JP_START_API_CLIENT_SECRET ), $script_output, $script_result );
+		$script_output_json = json_decode( end( $script_output ) );
 
-		if ( is_wp_error( $request ) ) {
-			return $request;
+		if ( ! $script_output_json ) {
+			return new WP_Error( 'invalid_output', 'Could not parse script output: ' . $script_output );
+		} elseif ( isset( $script_output_json->error_code ) ) {
+			return new WP_Error( $script_output_json->error_code, $script_output_json->error_message );
+		} else {
+			return $script_output_json;
 		}
-
-		$response_code = wp_remote_retrieve_response_code( $request );
-		$body = wp_remote_retrieve_body( $request );
-		if ( 200 !== $response_code ) {
-			return new WP_Error( 'api-request-fail-non-200', sprintf( 'got non-200 response from jetpack start api (code: %d | body: %s)', $response_code, $body ) );
-		}
-
-		$data = json_decode( $body, true );
-		if ( ! $data || ! $data['success'] ) {
-			return new WP_Error( 'api-request-fail-invalid-response', sprintf( 'got invalid response from jetpack start api (body: %s)', $body ) );
-		}
-
-		return $data;
-	}
-
-	private function api_cancel_subscription( $site_id ) {
-		$url = $this->get_api_url( '/cancel-subscription' );
-		$args = [
-			'site_id' => $site_id,
-		];
-
-		return $this->api_request( $url, $args );
-	}
-
-	private function api_fetch_keys( $user ) {
-		$url = $this->get_api_url( '/product-keys' );
-		$args = [
-			'site_url'  => get_site_url(),
-			'site_name' => get_bloginfo( 'name' ),
-			'site_lang' => get_locale(),
-			'user_id'   => $user->ID,
-			'user_role' => WPCOM_VIP_MACHINE_USER_ROLE,
-		];
-
-		return $this->api_request( $url, $args );
-	}
-
-	private function install_keys( $data ) {
-		$site_url = get_site_url();
-
-		$runcommand_args = [
-			'exit_error' => false,
-		];
-
-		$akismet_key = $data['akismet_api_key'] ?? '';
-		WP_CLI::line( '---' );
-		WP_CLI::line( sprintf( 'Got Akismet key: %s', $akismet_key ) );
-		$akismet_result = WP_CLI::runcommand( sprintf(
-			'jetpack-keys akismet --akismet_key=%s --url=%s',
-			escapeshellarg( $akismet_key ),
-			escapeshellarg( $site_url )
-		), $runcommand_args );
-		WP_CLI::line( '' );
-
-		$vaultpress_key = $data['vaultpress_registration_key'] ?? '';
-		WP_CLI::line( '---' );
-		WP_CLI::line( sprintf( 'Got VaultPress key: %s', $vaultpress_key ) );
-		$vaultpress_result = WP_CLI::runcommand( sprintf(
-			'jetpack-keys vaultpress --vaultpress_key=%s --url=%s',
-			escapeshellarg( $vaultpress_key ),
-			escapeshellarg( $site_url )
-		), $runcommand_args );
-		WP_CLI::line( '' );
-
-		$jetpack_id = $data['jetpack_id'] ?? '';
-		$jetpack_secret = $data['jetpack_secret'] ?? '';
-		$jetpack_access_token = $data['jetpack_access_token'] ?? '';
-		WP_CLI::line( '---' );
-		WP_CLI::line( sprintf( 'Got Jetpack ID: %s', $jetpack_id ) );
-		$jetpack_result = WP_CLI::runcommand( sprintf(
-			'jetpack-keys jetpack --jetpack_id=%s --jetpack_secret=%s --jetpack_access_token=%s --user=%s --url=%s',
-			escapeshellarg( $jetpack_id ),
-			escapeshellarg( $jetpack_secret ),
-			escapeshellarg( $jetpack_access_token ),
-			escapeshellarg( WPCOM_VIP_MACHINE_USER_LOGIN ),
-			escapeshellarg( $site_url )
-		), $runcommand_args );
-		WP_CLI::line( '' );
-	}
-
-	private function get_api_auth_header() {
-		return [
-			'Authorization' => 'Bearer ' . WPCOM_VIP_JP_START_API_TOKEN,
-		];
-	}
-
-	private function get_api_url( $endpoint ) {
-		return WPCOM_VIP_JP_START_API_BASE . $endpoint;
 	}
 }
 
