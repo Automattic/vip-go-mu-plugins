f0a82354c943fef642f84ff03af583624631733b
Derrick Tennant <emrikol@users.noreply.github.com>
2017-06-13 13:29:46 -0400

    Add "validate-attachments" CLI command.
    This command will iterate over attachments in the database and create a CSV of any missing (or other non-200 status) files.  This can be useful to determine if a media import has went wrong.

--- wp-cli/vip-migrations.php
+++ wp-cli/vip-migrations.php
@@ -80,6 +80,72 @@ class VIP_Go_Migrations_Command extends WPCOM_VIP_CLI_Command {
 		$count = count( $changes );
 		WP_CLI::success( _n( '%s change', '%s changes', $count ), number_format_i18n( $count ) );
 	}
+
+	/**
+	 * Iterate over attachments and check to see if they actually exist.
+	 *
+	 * @subcommand validate-attachments
+	 * @synopsis --output=<csv-filename> [--log-found]
+	 */
+	public function validate_attachments( $args, $assoc_args ) {
+		$attachment_count = array_sum( (array) wp_count_posts( 'attachment' ) );
+
+		if ( isset( $args['log-found'] ) ) {
+			$log_found = true;
+		} else {
+			$log_found = false;
+		}
+
+		$output_file = $assoc_args['output'];
+
+		$posts_per_page = 500;
+		$paged = 1;
+		$count = 0;
+		$output = array();
+
+		$progress = \WP_CLI\Utils\make_progress_bar( 'Checking ' . number_format( $attachment_count ) . ' attachments', $attachment_count );
+		$file_descriptor = fopen( $output_file, 'w' );
+		do {
+			$attachments = get_posts( array(
+				'post_type' => 'attachment',
+				'posts_per_page' => $posts_per_page,
+				'paged' => $paged,
+			) );
+
+			foreach ( $attachments as $attachment ) {
+				$url = $attachment->guid;
+				$request = wp_remote_head( $url );
+				if ( 200 !== $request['response']['code'] ) {
+					$output[] = array(
+						$url,
+						$request['response']['code'],
+						$request['response']['message'],
+					);
+				} else {
+					if ( $log_found ) {
+						$output[] = array(
+							$url,
+							$request['response']['code'],
+							$request['response']['message'],
+						);
+					}
+				}
+
+				$progress->tick();
+				$count++;
+			}
+
+			// Pause.
+			sleep( 1 );
+
+			// Free up memory.
+			$this->stop_the_insanity();
+			$paged++;
+		} while ( count( $attachments ) );
+		$progress->finish();
+		WP_CLI\Utils\write_csv( $file_descriptor, $output );
+		fclose( $file_descriptor );
+	}
 }
 
 WP_CLI::add_command( 'vip migration', 'VIP_Go_Migrations_Command' );
