cc6dc99c2ca41cfd604126549f0f70ea1b1fd3d6
Mohammad Jangda <batmoo@gmail.com>
2017-06-14 17:14:20 -0400

    Re-add loader plugins

---
+++ vip-support.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:  WordPress.com VIP Support
+ * Description:  Manages the WordPress.com Support Users on your site
+ * Version:      3.0.0
+ * Author:       <a href="http://automattic.com">Automattic</a>
+ * License:      GPLv2 or later
+ */
+
+require_once( __DIR__ . '/vip-support/vip-support.php' );
