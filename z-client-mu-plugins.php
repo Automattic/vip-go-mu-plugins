c1110aa61ae39ffb92ed1fefe6b33300b27c80da
Mohammad Jangda <batmoo@gmail.com>
2017-06-20 01:04:02 -0400

    Add support for additional mu-plugins (#611)
    Sites can add their own which are loaded after the VIP Go mu-plugins are
    loaded ("enforced" by the filename prefix).

---
+++ z-client-mu-plugins.php
@@ -0,0 +1,50 @@
+<?php
+
+// Note: This file is prefixed with `z-` for load order
+
+define( 'WPCOM_VIP_CLIENT_MU_PLUGIN_DIR', WP_CONTENT_DIR . '/client-mu-plugins' );
+
+function wpcom_vip_load_client_mu_plugins() {
+	static $loaded = false;
+
+	// Prevent running this multiple times
+	if ( $loaded ) {
+		return;
+	}
+
+	$loaded = true;
+
+	// Code below is adapted from wp_get_mu_plugins()
+	$client_mu_plugins = [];
+
+	if ( ! is_dir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR ) ) {
+		return;
+	}
+
+	$dh = opendir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR );
+	if ( ! $dh ) {
+		return;
+	}
+
+	do {
+		$plugin = readdir( $dh );
+		if ( false === $plugin ) {
+			break;
+		}
+
+		if ( substr( $plugin, -4 ) === '.php' ) {
+			$client_mu_plugins[] = WPCOM_VIP_CLIENT_MU_PLUGIN_DIR . '/' . $plugin;
+		}
+	} while ( false !== $plugin );
+
+	closedir( $dh );
+
+	// Make sure plugins load in a consistent, predictable order
+	sort( $client_mu_plugins );
+
+	foreach ( $client_mu_plugins as $plugin ) {
+		include_once( $plugin );
+	}
+}
+
+wpcom_vip_load_client_mu_plugins();
