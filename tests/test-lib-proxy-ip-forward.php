5e7073ac3d320096a621685d59e2b2a6053f9dc7
Simon Wheatley <simonw@automattic.com>
2017-07-10 17:27:11 +0100

    Test get_ip_addresses_from_ip_trail()

--- tests/test-lib-proxy-ip-forward.php
+++ tests/test-lib-proxy-ip-forward.php
@@ -30,6 +30,84 @@ abstract class IP_Forward_Test_Base extends \PHPUnit_Framework_TestCase {
 	}
 }
 
+class IP_Forward__Get_Ip_Addresses_From_Ip_Trail__Tests extends IP_Forward_Test_Base {
+
+	// fix_remote_address_from_ip_trail
+	public function test__get_ip_addresses_from_ip_trail__no_forwarded_for() {
+		unset( $_SERVER['HTTP_X_FORWARDED_FOR'] );
+		$ip_trail = '1.2.3.4, 5.6.7.8';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__get_ip_addresses_from_ip_trail__ip_trail_has_lt_2_ips() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = '1.2.3.4';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__get_ip_addresses_from_ip_trail__ip_trail_has_gt_2_ips() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = '1.2.3.4, 9.0.21.0, 5.6.7.8';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__get_ip_addresses_from_ip_trail__proxy_doesnt_match_forwarded_for() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.5.5.5';
+		$ip_trail = '1.2.3.4, 5.6.7.8';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__fix_remote_address_from_ip_trail__invalid_remote_ip() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = '1.2.3.4, 123456789';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__fix_remote_address_from_ip_trail__invalid_user_ip() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = 'bad_ip, 5.6.7.8';
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail );
+
+		$this->assertFalse( $result );
+	}
+
+	public function test__fix_remote_address_from_ip_trail__valid_ip_trail_ipv4() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = '1.2.3.4, 5.6.7.8';
+		$expected_ip_addresses = [ '1.2.3.4', '5.6.7.8' ];
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail, $whitelist );
+
+		$this->assertEquals( $expected_ip_addresses, $result );
+	}
+
+	public function test__fix_remote_address_from_ip_trail__valid_ip_trail_ipv6() {
+		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
+		$ip_trail = '2001:db8::1234:ace:6006:1e, 5.6.7.8';
+		$expected_ip_addresses = [ '2001:db8::1234:ace:6006:1e', '5.6.7.8' ];
+
+		$result = get_ip_addresses_from_ip_trail( $ip_trail, $whitelist );
+
+		$this->assertEquals( $expected_ip_addresses, $result );
+	}
+}
+
 class IP_Forward_Tests extends IP_Forward_Test_Base {
 
 	// is_valid_ip
