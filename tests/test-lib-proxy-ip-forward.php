b89c211b44e4a8b37a7e030f61f5f701f9590379
Simon Wheatley <simonw@automattic.com>
2017-07-11 10:58:41 +0100

    Remove some tests against `fix_remote_address_from_ip_trail()` (functionality is tested against `get_ip_addresses_from_ip_trail()`

--- tests/test-lib-proxy-ip-forward.php
+++ tests/test-lib-proxy-ip-forward.php
@@ -192,73 +192,6 @@ class IP_Forward_Tests extends IP_Forward_Test_Base {
 		$this->assertEquals( '2001:db8::1234:ace:6006:1e', $_SERVER['REMOTE_ADDR'] );
 	}
 
-	// fix_remote_address_from_ip_trail
-	public function test__fix_remote_address_from_ip_trail__no_forwarded_for() {
-		unset( $_SERVER['HTTP_X_FORWARDED_FOR'] );
-		$ip_trail = '1.2.3.4, 5.6.7.8';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__ip_trail_has_lt_2_ips() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
-		$ip_trail = '1.2.3.4';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__ip_trail_has_gt_2_ips() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
-		$ip_trail = '1.2.3.4, 9.0.21.0, 5.6.7.8';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__proxy_doesnt_match_forwarded_for() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.5.5.5';
-		$ip_trail = '1.2.3.4, 5.6.7.8';
-		$whitelist = [ '0.0.0.0' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__invalid_remote_ip() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
-		$ip_trail = '1.2.3.4, 123456789';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__invalid_user_ip() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
-		$ip_trail = 'bad_ip, 5.6.7.8';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertFalse( $result );
-		$this->assertEquals( self::DEFAULT_REMOTE_ADDR, $_SERVER['REMOTE_ADDR'] );
-	}
-
 	public function test__fix_remote_address_from_ip_trail__ip_not_in_whitelist() {
 		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
 		$ip_trail = '1.2.3.4, 5.6.7.8';
@@ -279,17 +212,6 @@ class IP_Forward_Tests extends IP_Forward_Test_Base {
 
 		$this->assertTrue( $result );
 		$this->assertEquals( '1.2.3.4', $_SERVER['REMOTE_ADDR'] );
-	}
-
-	public function test__fix_remote_address_from_ip_trail__ip_in_whitelist_ipv6() {
-		$_SERVER['HTTP_X_FORWARDED_FOR'] = '5.6.7.8';
-		$ip_trail = '2001:db8::1234:ace:6006:1e, 5.6.7.8';
-		$whitelist = [ '5.6.7.8' ];
-
-		$result = fix_remote_address_from_ip_trail( $ip_trail, $whitelist );
-
-		$this->assertTrue( $result );
-		$this->assertEquals( '2001:db8::1234:ace:6006:1e', $_SERVER['REMOTE_ADDR'] );
 	}
 }
 
