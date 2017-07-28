b96876d9cb9a259d54d6d987e70974da313e4017
Mohammad Jangda <batmoo@gmail.com>
2017-07-27 23:48:07 -0400

    Add plugin headers for wp-cli

--- wp-cli.php
+++ wp-cli.php
@@ -1,9 +1,15 @@
 <?php
 
+/**
+ * Plugin Name: WP-CLI for VIP Go
+ * Description: Scripts for VIP Go
+ * Author: Automattic
+ */
+
 if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
 	return;
 }
 
-foreach( glob( __DIR__ . '/wp-cli/*.php' ) as $command ) {
+foreach ( glob( __DIR__ . '/wp-cli/*.php' ) as $command ) {
 	require( $command );
 }
