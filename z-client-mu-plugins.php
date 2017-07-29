08446205a6095e4fedf1fb662131bfaad98d355a
Mohammad Jangda <batmoo@gmail.com>
2017-07-28 20:05:12 -0400

    Fix scoping issues for client-mu-plugins
    Don't load them inside a function. Instead, load them in global scope
    inside the mu-plugin loader so the the loaded client-mu-plugins also
    have global scope.
    
    h/t fklein-lu

--- z-client-mu-plugins.php
+++ z-client-mu-plugins.php
@@ -1,79 +1,48 @@
 <?php
 
+/**
+ * Plugin Name: VIP Client mu-plugins
+ * Description: Helper plugin to load custom mu-plugins in the `client-mu-plugins` dir.
+ * Author: Automattic
+ */
+
 // Note: This file is prefixed with `z-` for load order
 
-function wpcom_vip_load_client_mu_plugins() {
-	static $loaded = false;
-
-	// Prevent running this multiple times
-	if ( $loaded ) {
-		return;
-	}
-
-	$loaded = true;
-
-	// Code below is adapted from wp_get_mu_plugins()
+/**
+ * Gets PHP files in the client-mu-plugins folder.
+ *
+ * The code for this function is adapted from `wp_get_mu_plugins()`
+ */
+function wpcom_vip_get_client_mu_plugins() {
 	$client_mu_plugins = [];
 
 	if ( ! is_dir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR ) ) {
 		return;
 	}
 
-	$dh = opendir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR );
-	if ( ! $dh ) {
+	$dir_handle = opendir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR );
+	if ( ! $dir_handle ) {
 		return;
 	}
 
 	do {
-		$plugin = readdir( $dh );
+		$plugin = readdir( $dir_handle );
 		if ( false === $plugin ) {
 			break;
 		}
 
-		if ( substr( $plugin, -4 ) === '.php' ) {
+		$is_php_file = substr( $plugin, -4 ) === '.php';
+		if ( $is_php_file ) {
 			$client_mu_plugins[] = WPCOM_VIP_CLIENT_MU_PLUGIN_DIR . '/' . $plugin;
 		}
 	} while ( false !== $plugin );
 
-	closedir( $dh );
+	closedir( $dir_handle );
 
 	// Make sure plugins load in a consistent, predictable order
 	sort( $client_mu_plugins );
 
-	foreach ( $client_mu_plugins as $plugin ) {
-		include_once( $plugin );
-	}
-}
-
-// Load the plugins
-// TODO: move out of function scope to avoid issues with globals not being properly set
-wpcom_vip_load_client_mu_plugins();
-
-function wpcom_vip_get_client_mu_plugins() {
-	$wp_plugins = array();
-	$plugin_files = array();
-
-	if ( ! is_dir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR ) ) {
-		return $wp_plugins;
-	}
-
-	if ( $plugins_dir = @opendir( WPCOM_VIP_CLIENT_MU_PLUGIN_DIR ) ) {
-		while ( ( $file = readdir( $plugins_dir ) ) !== false ) {
-			if ( substr( $file, -4 ) === '.php' ) {
-				$plugin_files[] = $file;
-			}
-		}
-	} else {
-		return $wp_plugins;
-	}
-
-	@closedir( $plugins_dir );
-
-	if ( empty( $plugin_files ) ) {
-		return $wp_plugins;
-	}
-
-	return $plugin_files;
+	return $client_mu_plugins;
 }
 
 function wpcom_vip_get_client_mu_plugins_data() {
@@ -105,3 +74,9 @@ function wpcom_vip_get_client_mu_plugins_data() {
 
 	return $wp_plugins;
 }
+
+// Let's load the plugins
+foreach ( wpcom_vip_get_client_mu_plugins() as $client_mu_plugin ) {
+	include_once( $client_mu_plugin );
+}
+unset( $client_mu_plugin );
