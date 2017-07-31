98111ca8f27713f7b0cf47f19665c30ca7cdbcc2
Erick Hitter <git-contrib@ethitter.com>
2017-07-31 15:24:23 -0700

    `str_replace()` is faster

--- shared-plugins/split-home-site-urls/split-home-site-urls.php
+++ shared-plugins/split-home-site-urls/split-home-site-urls.php
@@ -30,7 +30,7 @@ namespace Automattic\VIP\Split_Home_Site_URLs;
  *
  * URLs must differ by more than just protocol for this plugin to be useful
  */
-if ( preg_replace( '#https?://#i', '', home_url( '/' ) ) === preg_replace( '#https?://#i', '', site_url( '/' ) ) ) {
+if ( str_replace( array( 'http://', 'https://', ), '', home_url( '/' ) ) === str_replace( array( 'http://', 'https://', ), '', site_url( '/' ) ) ) {
 	_doing_it_wrong( __NAMESPACE__, 'Called with identical site and home URls. Plugin functionality not loaded.', '1.0' );
 	return;
 }
