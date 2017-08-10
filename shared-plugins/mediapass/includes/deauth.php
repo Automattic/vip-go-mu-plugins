16559d4e874d8013c7efd1e338a6bcdaa0a0cc00
Scott Evans <git@scott.ee>
2017-08-10 11:24:32 +0100

    Remove MediaPass

--- shared-plugins/mediapass/includes/deauth.php
+++
@@ -1,6 +0,0 @@
-<div class="mp-wrap">
-	<h2 class="header"><img width="24" height="24" src="<?php echo plugins_url('/images/logo-icon.png', dirname(__FILE__)) ?>" class="mp-icon"> De-Authorize Plugin</h2>
-	<p>Are you sure you want to de-authorize this plugin?</p>
-	<p><a href="<?php echo menu_page_url('mediapass_deauth',false)?>&deauth=true">Click here to de-authorize this plugin and unlink your MediaPass account.</a></p>
-</div>
-		
--- shared-plugins/mediapass/uninstall.php
+++
@@ -1,8 +0,0 @@
-<?php
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-    exit();
-
-require_once( dirname(__FILE__) . "/mediapass_plugin.php");
-
-MediaPass_Plugin::delete_all_options();
-?>
