574c60fa1111e1784e23c8880eec4dd428eaa0d5
Scott Evans <git@scott.ee>
2017-08-10 11:25:13 +0100

    Remove NewsCred

--- shared-plugins/newscred/application/views/messages/active_mu_sites.php
+++
@@ -1,4 +0,0 @@
-<div class="update-nag" style="margin-top: 5px">
-    <?php $active_url =esc_url( wp_nonce_url(  NC_PLUGIN_ACTIVE_URL, 'myfeed_active_plugin_nonce')); ?>
-    Please activate <strong><a href="<?php echo $active_url;?>" > NewsCred Content Feeds Plugin</a></strong> for this blog .
-</div>
--- shared-plugins/newscred/static/js/backbone/models/Tag.js
+++
@@ -1,7 +0,0 @@
-ncApp.Tag = Backbone.Model.extend( {
-    defaults:{
-        name:null,
-        guid:null,
-        type:null
-    }
-} );
