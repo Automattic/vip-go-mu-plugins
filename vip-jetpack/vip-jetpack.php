aad9dbecd3bc7c3aa6eee8af289867f18b250e6a
Mohammad Jangda <batmoo@gmail.com>
2017-08-16 04:07:18 +0000

    Don't kill Jetpack version update for new sites
    More specifically, when the old version is empty. That's usually an
    indication that we're in a new install or the Jetpack data was just
    reset in which case we may accidentally kill processes we don't want to
    like Jetpack Start.

--- vip-jetpack/vip-jetpack.php
+++ vip-jetpack/vip-jetpack.php
@@ -28,9 +28,14 @@ add_filter( 'jetpack_get_available_modules', function( $modules ) {
 
 // Prevent Jetpack version ping-pong when a sandbox has an old version of stacks
 if ( true === WPCOM_SANDBOXED ) {
-	add_action( 'updating_jetpack_version', function() {
+	add_action( 'updating_jetpack_version', function( $new_version, $old_version ) {
+		// This is a brand new site with no Jetpack data
+		if ( empty( $old_version ) ) {
+			return;
+		}
+
 		wp_die( '😱😱😱 Oh no! Looks like your sandbox is trying to change the version of Jetpack. This is probably not a good idea. As a precaution, we\'re killing this request to prevent this from happening (this === 💥💥💥). Please run `vip stacks update` on your sandbox before doing anything else.', 400 );
-	}, 0 ); // No need to wait till priority 10 since we're going to die anyway
+	}, 0, 2 ); // No need to wait till priority 10 since we're going to die anyway
 }
 
 function wpcom_vip_did_jetpack_search_query( $query ) {
