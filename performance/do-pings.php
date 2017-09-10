49d90d3ac4ddebc08c1a0840a4bfaccff1d7913c
Erick Hitter <git-contrib@ethitter.com>
2017-09-10 10:12:19 -0700

    Don't try to get a property of a non-object

--- performance/do-pings.php
+++ performance/do-pings.php
@@ -4,6 +4,11 @@ namespace Automattic\VIP\Performance;
 
 // Disable pings by default
 function disable_pings( $event ) {
+	// Already blocked, carry on
+	if ( ! is_object( $event ) ) {
+		return $event;
+	}
+
 	if ( 'do_pings' === $event->hook ) {
 		return false;
 	}
