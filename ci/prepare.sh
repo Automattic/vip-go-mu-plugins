6202ca5ca08ea2b4c865e73b0636683b515a145e
Erick Hitter <git-contrib@ethitter.com>
2017-09-11 13:39:21 -0700

    Only install WP when we have a version to install

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -8,18 +8,18 @@ set -ex
 
 cd $TRAVIS_BUILD_DIR
 
-# Convert the URLs in the superproject .gitmodules file,
-# then init those submodules
-# Done only when running WP tests, as we don't want to run PHPCS on submodules
+# Prepare for PHPUnit tests
 if [[ ! -z "$WP_VERSION" ]] ; then
+	# Convert the URLs in the superproject .gitmodules file,
+	# then init those submodules
 	sed -i -e "s|git@\([^:]*\):|https://\1/|" .gitmodules
 	git submodule update --init
 	# Now recurse over all the contained submodules,
 	# sub-submodules, etc, to do the same
 	date; git submodule foreach --recursive 'if [ -w .gitmodules ]; then sed -i -e "s|git@\([^:]*\):|https://\1/|" "$toplevel/$path/.gitmodules"; fi; git submodule update --init "$toplevel/$path";'; date;
+
+	# Install unit tests
+	# ==================
+
+	bash bin/install-wp-tests.sh wordpress_test root '' localhost "${WP_VERSION}"
 fi
-
-# Install unit tests
-# ==================
-
-bash bin/install-wp-tests.sh wordpress_test root '' localhost "${WP_VERSION}"
