1950925bc05448f9c84139161f322fa25d8fbb0f
Erick Hitter <git-contrib@ethitter.com>
2017-09-18 14:29:58 -0700

    Reorganize to allow bypass

--- performance/async-publish-post.php
+++ performance/async-publish-post.php
@@ -5,32 +5,9 @@ namespace Automattic\VIP\Performance;
 const ASYNC_PUBLISH_EVENT = 'wpcom_async_publish_post';
 
 /**
- * Queue async event when status was or is `publish`
- *
- * @param string $new_status
- * @param string $old_status
- * @param object $post
- */
-function _queue_async_publish_post( $new_status, $old_status, $post ) {
-	if ( 'auto-draft' === $post->post_status ) {
-		return;
-	}
-
-	// Pass only consistent data, allowing cron's duplicate handling to take effect
-	// For example, don't include post status here
-	$args = [ 'post_id' => (int) $post->ID ];
-
-	if ( $new_status !== $old_status && in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( ASYNC_PUBLISH_EVENT, $args ) ) {
-		wp_schedule_single_event( time(), ASYNC_PUBLISH_EVENT, $args );
-	}
-}
-
-add_action( 'transition_post_status', __NAMESPACE__ . '\_queue_async_publish_post', 10, 3 );
-
-/**
  * Perform asynchronous tasks for a published post
  *
- * @param int $post_id
+ * @param int $post_id Post ID.
  */
 function _wpcom_do_async_publish_post( $post_id ) {
 	$post_object = get_post( $post_id );
@@ -41,7 +18,30 @@ function _wpcom_do_async_publish_post( $post_id ) {
 add_action( ASYNC_PUBLISH_EVENT, __NAMESPACE__ . '\_wpcom_do_async_publish_post' );
 
 /**
+ * Queue async event when status was or is `publish`
+ *
+ * @param string $new_status New post status.
+ * @param string $old_status Old post status.
+ * @param object $post \WP_Post object.
+ */
+function _queue_async_publish_post( $new_status, $old_status, $post ) {
+	if ( 'auto-draft' === $post->post_status ) {
+		return;
+	}
+
+	// Pass only consistent data, allowing cron's duplicate handling to take effect.
+	// For example, don't include post status here.
+	$args = [ 'post_id' => (int) $post->ID ];
+
+	if ( $new_status !== $old_status && in_array( 'publish', [ $new_status, $old_status ], true ) && false === wp_next_scheduled( ASYNC_PUBLISH_EVENT, $args ) ) {
+		wp_schedule_single_event( time(), ASYNC_PUBLISH_EVENT, $args );
+	}
+}
+
+add_action( 'transition_post_status', __NAMESPACE__ . '\_queue_async_publish_post', 10, 3 );
+
+/**
  * Offload ping- and enclosure-related events
  */
-remove_action( 'publish_post', '_publish_post_hook', 5, 1 );
-add_action( 'async_publish_post', '_publish_post_hook', 5, 1 );
+remove_action( 'publish_post', '\_publish_post_hook', 5 );
+add_action( 'async_publish_post', '\_publish_post_hook', 5, 1 );
