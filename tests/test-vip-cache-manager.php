5c1e4700460252283c9f32702274473255dce366
Mohammad Jangda <batmoo@gmail.com>
2017-09-25 13:37:51 -0400

    Split valid and invalid tests
    So we can more cleanly test for exceptions/warnings

--- tests/test-vip-cache-manager.php
+++ tests/test-vip-cache-manager.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class VIP_Go_Cache_Manager_Test extends WP_UnitTestCase {
@@ -11,39 +10,56 @@ class VIP_Go_Cache_Manager_Test extends WP_UnitTestCase {
 		$this->cache_manager->clear_queued_purge_urls();
 	}
 
-	public function get_data_for_queue_purge_url() {
+	public function get_data_for_valid_queue_purge_url_test() {
 		return [
 			// 1: input URL
-			// 2: expected response from method
-			// 3: expected purge_urls list
+			// 2: array of expected purge_urls list
 
 			'normal_url' => [
 				'https://example.com/path/to/file?query',
-				true,
 				[ 'https://example.com/path/to/file?query' ],
-			],
-
-			'invalid_url' => [
-				'badscheme://example.com/path',
-				false,
-				[],
 			],
 
 			'strip_fragment_from_url' => [
 				'https://example.com/post#fragment',
-				true,
 				[ 'https://example.com/post' ],
 			],
 		];
 	}
 
+	public function get_data_for_invalid_queue_purge_url_test() {
+		return [
+			'invalid_scheme' => [
+				'badscheme://example.com/path',
+			],
+		];
+	}
+
 	/**
- 	 * @dataProvider get_data_for_queue_purge_url
- 	 */ 
-	public function test__queue_purge_url( $queue_url, $expected_output, $expected_urls ) {
+	 * Tests valid URL inputs for `queue_purge_url`
+	 *
+	 * @dataProvider get_data_for_valid_queue_purge_url_test
+	 */
+	public function test__valid__queue_purge_url( $queue_url, $expected_urls ) {
 		$actual_output = $this->cache_manager->queue_purge_url( $queue_url );
 
-		$this->assertEquals( $expected_output, $actual_output, 'Return value from `queue_purge_url` does not match.' );
+		$this->assertTrue( $actual_output, 'Return value from `queue_purge_url` does not match.' );
 		$this->assertEquals( $expected_urls, $this->cache_manager->get_queued_purge_urls(), 'List of queued purge urls do not match' );
+	}
+
+	/**
+	 * Tests invalid URL inputs for `queue_purge_url`
+	 *
+	 * They are all expected to return false, queue nothing, and throw a warning.
+	 *
+	 * @dataProvider get_data_for_invalid_queue_purge_url_test
+	 */
+	public function test__invalid__queue_purge_url( $queue_url ) {
+		$this->setExpectedExceptionRegExp( 'PHPUnit_Framework_Error_Warning' );
+
+		$actual_output = $this->cache_manager->queue_purge_url( $queue_url );
+
+		$this->assertFalse( $actual_output, 'Return value from `queue_purge_url` should be false.' );
+		$this->assertEmpty( $this->cache_manager->get_queued_purge_urls(), 'List of queued purge urls should be empty' );
 	}
 }
