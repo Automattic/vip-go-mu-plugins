795e7b05778b74188421602b233acbe286fe0b3c
Erick Hitter <git-contrib@ethitter.com>
2017-09-27 19:58:41 -0700

    Revise existing tests to focus on one thing

--- tests/test-performance-async-publish-actions.php
+++ tests/test-performance-async-publish-actions.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace Automattic\VIP\Performance;
+use WP_UnitTestCase;
 
-class async_publish_actions_Test extends \WP_UnitTestCase {
+class async_publish_actions_Test extends WP_UnitTestCase {
 	/**
 	 * Prepare test environment
 	 */
@@ -24,9 +25,9 @@ class async_publish_actions_Test extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Test that offload is scheduled when post is published
+	 * Confirm that events aren't scheduled for non-published posts
 	 */
-	function test_schedule_offload() {
+	function test_event_not_scheduled_for_draft() {
 		$post = [
 			'post_title'   => 'Tommy Tutone - 867-5309/Jenny',
 			'post_content' => 'https://www.youtube.com/watch?v=6WTdTwcmxyo',
@@ -35,20 +36,30 @@ class async_publish_actions_Test extends \WP_UnitTestCase {
 
 		$pid = wp_insert_post( $post, true );
 
-		// Insert succeeded.
-		$this->assertFalse( is_wp_error( $pid ) );
-
 		$args = [
-			'post_id'    => (int) $pid,
+			'post_id' => (int) $pid,
 			'new_status' => 'draft',
 			'old_status' => 'new',
 		];
 
-		$next_transition    = wp_next_scheduled( ASYNC_TRANSITION_EVENT, $args );
+		$next_transition = wp_next_scheduled( ASYNC_TRANSITION_EVENT, $args );
 
 		// No async event for a draft post.
 		$this->assertFalse( $next_transition );
 
+	}
+
+	/**
+	 * Confirm that an event is scheduled for a published post.
+	 */
+	function test_event_is_scheduled_for_publish() {
+		$post = [
+			'post_title'   => 'Toto - Africa',
+			'post_content' => 'https://www.youtube.com/watch?v=FTQbiNvZqaY',
+			'post_status'  => 'draft',
+		];
+
+		$pid = wp_insert_post( $post, true );
 		wp_publish_post( $pid );
 
 		$args = [
@@ -57,9 +68,9 @@ class async_publish_actions_Test extends \WP_UnitTestCase {
 			'old_status' => 'draft',
 		];
 
-		$next_transition    = wp_next_scheduled( ASYNC_TRANSITION_EVENT, $args );
+		$next_transition = wp_next_scheduled( ASYNC_TRANSITION_EVENT, $args );
 
 		// Event scheduled after publish.
-		$this->assertTrue( is_int( $next_transition ) );
+		$this->assertInternalType( 'int',  $next_transition );
 	}
 }
