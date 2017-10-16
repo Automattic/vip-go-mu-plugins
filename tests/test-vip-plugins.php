b3825a77b671609a8d2c79aef09a8dca4aab5458
Scott Evans <git@scott.ee>
2017-10-16 13:00:51 +0100

    Complete multisite test

--- tests/test-vip-plugins.php
+++ tests/test-vip-plugins.php
@@ -102,29 +102,37 @@ class VIP_Go_Plugins_Test extends \WP_UnitTestCase {
 		}
 
 		/**
+		 * Clear global of code activated plugins from previous test
+		 */
+		global $vip_loaded_plugins;
+		$vip_loaded_plugins = array();
+
+		/**
 		 * Set active_sitewide_plugins to empty to start the test
 		 */
-		update_option( 'active_sitewide_plugins', [] );
+		update_site_option( 'active_sitewide_plugins', [] );
 
 		/**
 		 * Ensure the values are indeed empty
 		 */
-		$this->assertEmpty( get_option( 'active_sitewide_plugins' ) );
+		$this->assertEmpty( get_site_option( 'active_sitewide_plugins' ) );
 
 		/**
 		 * Update the option with our list of active network plugins
 		 */
-		update_option( 'active_sitewide_plugins' , $this->option_active_sitewide_plugins );
+		update_site_option( 'active_sitewide_plugins' , $this->option_active_sitewide_plugins );
 
 		/**
 		 * Check that option is not empty
 		 */
-		$this->assertNotEmpty( get_option( 'active_sitewide_plugins' ) );
+		$this->assertNotEmpty( get_site_option( 'active_sitewide_plugins' ) );
 
 		/**
 		 * Setup the code activated plugins
 		 */
-		// skipped as global still setup from previous test
+		foreach ( $this->code_activated_plugins as $plugin ) {
+			wpcom_vip_add_loaded_plugin( $plugin );
+		}
 
 		/**
 		 * Check that list of code activated plugins matches the mocked data
@@ -135,27 +143,22 @@ class VIP_Go_Plugins_Test extends \WP_UnitTestCase {
 		 * Check that the returned option matches a merge of the filtered loaded plugins and active plugins
 		 */
 		$merged_plugins = array_merge( wpcom_vip_get_network_filtered_loaded_plugins(), $this->option_active_sitewide_plugins );
-		// FAILS HERE - due to filters not loading
-		//print_r( wpcom_vip_get_network_filtered_loaded_plugins() );
-		//print_r( $this->option_active_sitewide_plugins );
-		//print_r( $merged_plugins );
-		//print_r( get_option( 'active_sitewide_plugins' ) );
-		$this->assertEquals( $merged_plugins, get_option( 'active_sitewide_plugins' ) );
+		$this->assertEquals( $merged_plugins, get_site_option( 'active_sitewide_plugins' ) );
 
 		/**
 		 * Check that updating the option is OK, add an extra plugin
 		 */
-		/*$plugin_change = array_merge( $this->option_active_plugins, array( 'amp-wp/amp.php' ) );
-		$option_update = update_option( 'active_plugins', $plugin_change );
-		$this->assertTrue( $option_update );*/
+		$plugin_change = array_merge( $this->option_active_sitewide_plugins, array( 'amp-wp/amp.php' => 1507904134 ) );
+		$option_update = update_site_option( 'active_sitewide_plugins', $plugin_change );
+		$this->assertTrue( $option_update );
 
 		/**
 		 * Check that the option still makes sense again
 		 */
-		/*// emulates update
-		$track_plugin_change = array_diff( $plugin_change, wpcom_vip_get_filtered_loaded_plugins() );
+		// emulates update
+		$track_plugin_change = array_diff( $plugin_change, wpcom_vip_get_network_filtered_loaded_plugins() );
 		// emulates get
-		$saved_plugins = array_merge( wpcom_vip_get_filtered_loaded_plugins(), $track_plugin_change );
-		$this->assertEquals( $saved_plugins, get_option( 'active_plugins' ) );*/
+		$saved_plugins = array_merge( wpcom_vip_get_network_filtered_loaded_plugins(), $track_plugin_change );
+		$this->assertEquals( $saved_plugins, get_site_option( 'active_sitewide_plugins' ) );
 	}
 }
