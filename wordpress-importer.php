8a72452fac99d86e0c5d1bd23e74038a7e780562
Josh Betz <j@joshbetz.com>
2017-10-30 17:10:27 -0500

    Add WP_IMPORTING check before suspending cache invalidation

--- wordpress-importer.php
+++ wordpress-importer.php
@@ -14,12 +14,22 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 require( __DIR__ . '/wordpress-importer/wordpress-importer.php' );
 
 add_action( 'import_start', function() {
+	if ( ! defined( 'WP_IMPORTING' ) || ! WP_IMPORTING ) {
+		// Safety check: Don't suspend cache invalidation if we're not importing
+		return;
+	}
+
 	wp_suspend_cache_addition( true );
 	wp_suspend_cache_invalidation( true );
 	wp_cache_flush();
 });
 
 add_action( 'import_end', function() {
+	if ( ! defined( 'WP_IMPORTING' ) || ! WP_IMPORTING ) {
+		// Safety check: Don't suspend cache invalidation if we're not importing
+		return;
+	}
+
 	wp_suspend_cache_addition( false );
 	wp_suspend_cache_invalidation( false );
 	wp_cache_flush();
