464651ccca609953a1a4143da969e626f5ddda46
Simon Wheatley <simonw@automattic.com>
2017-11-29 12:53:59 +0000

    Switch `Plugin URI` to `https` in plugin header for Jetpack

--- jetpack.php
+++ jetpack.php
@@ -2,7 +2,7 @@
 
 /*
  * Plugin Name: Jetpack by WordPress.com
- * Plugin URI: http://jetpack.com
+ * Plugin URI: https://jetpack.com
  * Description: Bring the power of the WordPress.com cloud to your self-hosted WordPress. Jetpack enables you to connect your blog to a WordPress.com account to use the powerful features normally only available to WordPress.com users.
  * Author: Automattic
  * Version: 5.5.1
