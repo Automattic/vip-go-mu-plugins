2dc8f7f3a603f4a0be7d1dc97ce10e6f5211f645
Nick Daugherty <ndaugherty987@gmail.com>
2018-02-08 16:58:01 -0700

    Trigger a warning when jp/vp connection is called outside of WP CLI

--- vip-helpers/vip-migrations.php
+++ vip-helpers/vip-migrations.php
@@ -53,11 +53,15 @@ function delete_db_transients() {
 function connect_jetpack() {
 	if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 		\WP_CLI::runcommand( sprintf( 'jetpack-start connect --url=%s', home_url() ) );
+	} else {
+		trigger_error( 'Cannot connect JP outside of a WP_CLI context, skipping', E_USER_WARNING );
 	}
 }
 
 function connect_vaultpress() {
 	if ( defined( 'WP_CLI' ) && WP_CLI && class_exists( 'WP_CLI' ) ) {
 		\WP_CLI::runcommand( sprintf( 'vaultpress register_via_jetpack --url=%s', home_url() ) );
+	} else {
+		trigger_error( 'Cannot connect VaultPress outside of a WP_CLI context, skipping', E_USER_WARNING );
 	}
 }
