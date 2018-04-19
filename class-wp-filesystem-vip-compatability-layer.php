97063e46341b7dd02042fa342715484a6d4f2426
Stéphane <stephane.boisvert@automattic.com>
2018-04-19 15:21:45 -0400

    WP_Filesystem compatability layer
    We currently don't have a good way to interact with the file system on VIP Go. This commit allows us to use some of the functions provided by WP_Filesystem.
    The first layer is a compatability layer that detects if we're trying to do an operation on the /tmp folder or in the uploads folder. Everything else will trigger a E_USER_ERROR. It also handled doing things like copying from tmp to uploads and vice versa.
    For the tmp folder we use WP_Filesystem_Direct() and for the VIP Go we use WP_Filesystem_VIP_GO() which in turn calls the VIP GO files API.

---
+++ class-wp-filesystem-vip-compatability-layer.php
@@ -0,0 +1,347 @@
+<?php
+
+class WP_Filesystem_VIP_Compatability_Layer extends WP_Filesystem_Base {
+
+	private $api;
+	private $direct;
+
+	public function __construct( ) {
+
+		$this->method = 'vip_compatability_layer';
+		$this->errors = new WP_Error();
+
+
+		$this->api = new WP_Filesystem_VIP_GO( ); //this might change
+		$this->direct = new WP_Filesystem_Direct( null );
+
+	}
+
+	private function get_appropriate_transport( $filename ){
+		if ( $this->is_file_in_valid_vip_go_uploads_folder( $filename ) ){
+			return $this->api;
+		}else if( $this->is_temporary_folder( $filename ) ){
+			return $this->direct;
+		}
+		//This is the usual way to do errors, we'll use it but also trigger a PHP E_USER_ERROR to ensure users see this.
+		$this->errors->add( 'filepath_not_supported', 'No appropriate transport found for filename: '. $filename );
+		//TODO: Do we want to just trigger_error in some circumstances? maybe only when environement != production?
+		trigger_error('Files can only be modified either in the temporary folder or in the uploads folder. Please see our documentation here:', E_USER_ERROR );
+	}
+
+	private function is_temporary_folder( $filename ){
+		// TODO: Should we check against sys_get_temp_dir()?
+		if ( strpos( $filename, '/tmp' ) ){
+			return true;
+		}
+		return false;
+	}
+
+	private function is_file_in_valid_vip_go_uploads_folder( $filename ){
+		$upload_path = trim( get_option( 'upload_path' ) );
+		if ( empty( $upload_path ) ){
+			$upload_path = 'wp-content/uploads';
+		}
+		//TODO: Do we want to ensure the folder exists? This could flag false positives.
+		if ( false === strpos( $filename, $upload_path ) ){
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Reads entire file into a string
+	 *
+	 * @param string $file Name of the file to read.
+	 * @return string|bool The function returns the read data or false on failure.
+	 */
+	public function get_contents( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->get_contents( $file );
+	}
+
+	/**
+	 * Reads entire file into an array
+	 *
+	 * @param string $file Path to the file.
+	 * @return array|bool the file contents in an array or false on failure.
+	 */
+	public function get_contents_array( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->get_contents_array( $file );
+	}
+
+	/**
+	 * Write a string to a file
+	 *
+	 * @param string $file     Remote path to the file where to write the data.
+	 * @param string $contents The data to write.
+	 * @param int    $mode     Optional. The file permissions as octal number, usually 0644.
+	 *                         Default false. - Unimplemented
+	 * @return bool False upon failure, true otherwise.
+	 */
+	public function put_contents( $file, $contents, $mode = false ) {
+		$transport = $this->get_appropriate_transport( $file );
+		$transport->put_contents( $file, $contents, $mode = false );
+		return true;
+	}
+
+	/**
+	 * @param string $source
+	 * @param string $destination
+	 * @param bool   $overwrite
+	 * @param int    $mode - Unimplemented
+	 * @return bool
+	 */
+	public function copy( $source, $destination, $overwrite = false, $mode = false ) {
+		$source_transport = $this->get_appropriate_transport( $source );
+		$destination_transport = $this->get_appropriate_transport( $destination );
+
+		if ( ! $overwrite && $destination_transport->exists( $destination ) ){
+			return false;
+		}
+
+		$file_content = $source_transport->get_contents( $source );
+		$destination_transport->put_contents( $destination, $file_content );
+	}
+
+	/**
+	 * @param string $source
+	 * @param string $destination
+	 * @param bool $overwrite
+	 * @return bool
+	 */
+	public function move( $source, $destination, $overwrite = false ) {
+		$copy_results = $this->copy( $source, $destination, $overwrite );
+		if ( false == $copy_results ){
+			return false;
+		}
+
+		$this->delete( $source );
+
+		return true;//TODO: What if delete fails?
+	}
+
+	/**
+	 * @param string $file
+	 * @param bool $recursive - Unimplemented
+	 * @param string $type - Unimplemented
+	 * @return bool
+	 */
+	public function delete( $file, $recursive = false, $type = false ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->delete( $file );
+	}
+
+	/**
+	 * @param string $file
+	 * @return int
+	 */
+	public function size( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->size( $file );
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function exists( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->exists( $file );
+	}
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_file( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->is_file( $file );
+	}
+	/**
+	 * @param string $path
+	 * @return bool
+	 */
+	public function is_dir( $path ) {
+		$transport = $this->get_appropriate_transport( $path );
+		return $transport->is_dir( $path );
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_readable( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->is_readable( $file );
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_writable( $file ) {
+		$transport = $this->get_appropriate_transport( $file );
+		return $transport->is_writable( $file );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return int
+	 */
+	public function atime( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return int
+	 */
+	public function mtime( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @param int $time
+	 * @param int $atime
+	 * @return bool
+	 */
+	public function touch( $file, $time = 0, $atime = 0 ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param mixed  $chmod
+	 * @param mixed  $chown
+	 * @param mixed  $chgrp
+	 * @return bool
+	 */
+	public function mkdir( $path, $chmod = false, $chown = false, $chgrp = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param bool $recursive
+	 * @return bool
+	 */
+	public function rmdir( $path, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param bool $include_hidden
+	 * @param bool $recursive
+	 * @return bool|array
+	 */
+	public function dirlist( $path, $include_hidden = true, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+
+	/**
+	 * Unimplemented - Gets the current working directory
+	 *
+	 * @return string|bool the current working directory on success, or false on failure.
+	 */
+	public function cwd() {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Change directory
+	 *
+	 * @param string $dir The new current directory.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chdir( $dir ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes file group
+	 *
+	 * @param string $file      Path to the file.
+	 * @param mixed  $group     A group name or number.
+	 * @param bool   $recursive Optional. If set True changes file group recursively. Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chgrp( $file, $group, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes filesystem permissions
+	 *
+	 * @param string $file      Path to the file.
+	 * @param int    $mode      Optional. The permissions as octal number, usually 0644 for files,
+	 *                          0755 for dirs. Default false.
+	 * @param bool   $recursive Optional. If set True changes file group recursively. Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chmod( $file, $mode = false, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes file owner
+	 *
+	 * @param string $file      Path to the file.
+	 * @param mixed  $owner     A user name or number.
+	 * @param bool   $recursive Optional. If set True changes file owner recursively.
+	 *                          Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chown( $file, $owner, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Gets file owner
+	 *
+	 * @param string $file Path to the file.
+	 * @return string|bool Username of the user or false on error.
+	 */
+	public function owner( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Gets file permissions
+	 *
+	 * FIXME does not handle errors in fileperms()
+	 *
+	 * @param string $file Path to the file.
+	 * @return string Mode of the file (last 3 digits).
+	 */
+	public function getchmod( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return string|false
+	 */
+	public function group( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+}
---
+++ class-wp-filesystem-vip-go.php
@@ -0,0 +1,304 @@
+<?php
+
+class WP_Filesystem_VIP_GO extends WP_Filesystem_Base {
+
+
+	public function __construct( ) {
+		$this->method = 'vip_go';
+		$this->errors = new WP_Error();
+	}
+
+	/**
+	 * Reads entire file into a string
+	 *
+	 * @param string $file Name of the file to read.
+	 * @return string|bool The function returns the read data or false on failure.
+	 */
+	public function get_contents( $file ) {
+		//TODO: Caching for remote gets? Static single request cache vs memcache?
+	}
+
+	/**
+	 * Reads entire file into an array
+	 *
+	 * @param string $file Path to the file.
+	 * @return array|bool the file contents in an array or false on failure.
+	 */
+	public function get_contents_array( $file ) {
+
+	}
+
+	/**
+	 * Write a string to a file
+	 *
+	 * @param string $file     Remote path to the file where to write the data.
+	 * @param string $contents The data to write.
+	 * @param int    $mode     Optional. The file permissions as octal number, usually 0644.
+	 *                         Default false. - Unimplemented
+	 * @return bool False upon failure, true otherwise.
+	 */
+	public function put_contents( $file, $contents, $mode = false ) {
+
+	}
+
+
+
+	/**
+	 * @param string $file
+	 * @param bool $recursive - Unimplemented
+	 * @param string $type - Unimplemented
+	 * @return bool
+	 */
+	public function delete( $file, $recursive = false, $type = false ) {
+
+	}
+
+	/**
+	 * @param string $file
+	 * @return int
+	 */
+	public function size( $file ) {
+
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function exists( $file ) {
+
+	}
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_file( $file ) {
+
+	}
+	/**
+	 * @param string $path
+	 * @return bool
+	 */
+	public function is_dir( $path ) {
+
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_readable( $file ) {
+
+	}
+
+	/**
+	 * @param string $file
+	 * @return bool
+	 */
+	public function is_writable( $file ) {
+
+	}
+
+	/**
+	 *
+	 * Unimplemented, this is handled at the WP_Filesystem_VIP_Compatability_Layer layer so that we can do from /tmp/ to uploads and vice versa
+	 *
+	 * @param string $source
+	 * @param string $destination
+	 * @param bool   $overwrite
+	 * @param int    $mode -
+	 * @return bool
+	 */
+	public function copy( $source, $destination, $overwrite = false, $mode = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 *
+	 * Unimplemented, this is handled at the WP_Filesystem_VIP_Compatability_Layer layer so that we can do from /tmp/ to uploads and vice versa
+	 *
+	 * @param string $source
+	 * @param string $destination
+	 * @param bool $overwrite
+	 * @return bool
+	 */
+	public function move( $source, $destination, $overwrite = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return int
+	 */
+	public function atime( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return int
+	 */
+	public function mtime( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @param int $time
+	 * @param int $atime
+	 * @return bool
+	 */
+	public function touch( $file, $time = 0, $atime = 0 ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param mixed  $chmod
+	 * @param mixed  $chown
+	 * @param mixed  $chgrp
+	 * @return bool
+	 */
+	public function mkdir( $path, $chmod = false, $chown = false, $chgrp = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param bool $recursive
+	 * @return bool
+	 */
+	public function rmdir( $path, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $path
+	 * @param bool $include_hidden
+	 * @param bool $recursive
+	 * @return bool|array
+	 */
+	public function dirlist( $path, $include_hidden = true, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+
+	/**
+	 * Unimplemented - Gets the current working directory
+	 *
+	 * @return string|bool the current working directory on success, or false on failure.
+	 */
+	public function cwd() {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Change directory
+	 *
+	 * @param string $dir The new current directory.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chdir( $dir ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes file group
+	 *
+	 * @param string $file      Path to the file.
+	 * @param mixed  $group     A group name or number.
+	 * @param bool   $recursive Optional. If set True changes file group recursively. Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chgrp( $file, $group, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes filesystem permissions
+	 *
+	 * @param string $file      Path to the file.
+	 * @param int    $mode      Optional. The permissions as octal number, usually 0644 for files,
+	 *                          0755 for dirs. Default false.
+	 * @param bool   $recursive Optional. If set True changes file group recursively. Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chmod( $file, $mode = false, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Changes file owner
+	 *
+	 * @param string $file      Path to the file.
+	 * @param mixed  $owner     A user name or number.
+	 * @param bool   $recursive Optional. If set True changes file owner recursively.
+	 *                          Default false.
+	 * @return bool Returns true on success or false on failure.
+	 */
+	public function chown( $file, $owner, $recursive = false ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Gets file owner
+	 *
+	 * @param string $file Path to the file.
+	 * @return string|bool Username of the user or false on error.
+	 */
+	public function owner( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented - Gets file permissions
+	 *
+	 * FIXME does not handle errors in fileperms()
+	 *
+	 * @param string $file Path to the file.
+	 * @return string Mode of the file (last 3 digits).
+	 */
+	public function getchmod( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+	/**
+	 * Unimplemented
+	 *
+	 * @param string $file
+	 * @return string|false
+	 */
+	public function group( $file ) {
+		trigger_error('This function is currently unimplemented', E_USER_ERROR );
+	}
+
+
+
+	// get_contents
+	// move (files only)
+	// unlink (files only)
+	// exists (files only)
+	// size
+
+	// TODO: caching for remote GETs?
+
+	// TODO: how is BB using these and how do we handle them?
+	// mkdir
+	// rmdir
+	// is_dir
+	// dirlist
+}
