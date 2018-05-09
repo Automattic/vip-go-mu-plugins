0edc2e984ae279b6037433eaa1d3e410e4d0438b
Rebecca Hum <rebeccahum@gmail.com>
2018-05-09 13:36:49 -0600

    Disable email sharing when recaptcha is not set-up (#886)
    * Disable email sharing when recapatcha is not set-up

--- vip-jetpack/vip-jetpack.php
+++ vip-jetpack/vip-jetpack.php
@@ -110,3 +110,22 @@ function wpcom_vip_disable_jetpack_sync_for_frontend_get_requests( $should_load 
 
 }
 add_filter( 'jetpack_sync_listener_should_load', 'wpcom_vip_disable_jetpack_sync_for_frontend_get_requests' );
+
+/**
+ * Disable Email Sharing if Recaptcha is not setup.
+ *
+ * To prevent spam and abuse, we should only allow sharing via e-mail when reCAPTCHA is enabled.
+ *
+ * @see https://jetpack.com/support/sharing/#captcha Instructions on how to set up reCAPTCHA for your site
+ *
+ * @param  bool $is_enabled Current value.
+ * @return bool              Whether (true) or not (false) email sharing is enabled.
+ */
+function wpcom_vip_disable_jetpack_email_no_recaptcha( $is_enabled ) {
+	if ( ! $is_enabled ) {
+		return $is_enabled;
+	}
+
+	return defined( 'RECAPTCHA_PUBLIC_KEY' ) && defined( 'RECAPTCHA_PRIVATE_KEY' );
+}
+add_filter( 'sharing_services_email', 'wpcom_vip_disable_jetpack_email_no_recaptcha' );
