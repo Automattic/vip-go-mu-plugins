79dc0fe45a86c1ed1dd37433f9f171fcc284f520
Mohammad Jangda <mo@automattic.com>
2018-05-14 09:29:00 -0700

    Add inline reference to upstream AMP PR

--- plugin-fixes.php
+++ plugin-fixes.php
@@ -1,7 +1,7 @@
 <?php
 /*
-Plugin Name: VIP GO Plugin Fixes
-Description: Handles Plugin Specific Fixes for VIP GO Environments
+Plugin Name: VIP Go Plugin Fixes
+Description: A collection of fixes for various plugins.
 Author: Automattic
 Version: 1.0
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -9,14 +9,13 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 
 // Make sure the `amp` query var has an explicit value.
 // Avoids issues when filtering the deprecated `query_string` hook.
+// Copy of upstream fix: https://github.com/Automattic/amp-wp/pull/910
 function vip_go_amp_force_query_var_value( $query_vars ) {
 
 	if ( isset( $query_vars[ AMP_QUERY_VAR ] ) ) {
 		if ( '' === $query_vars[ AMP_QUERY_VAR ] ) {
 			$query_vars[ AMP_QUERY_VAR ] = 1;
-
-			// Allow for some fuzziness on string/integer type matching.
-		} else if ( '1' !== $query_vars[ AMP_QUERY_VAR ] && 1 !== $query_vars[ AMP_QUERY_VAR ] ) {
+		} else if ( '1' !== $query_vars[ AMP_QUERY_VAR ] && 1 !== $query_vars[ AMP_QUERY_VAR ] ) { // Allow for some fuzziness on string/integer type matching.
 			global $wp_rewrite;
 
 			// If there is something after /amp/, pretend we didn't hit that rewrite rule.
@@ -33,8 +32,7 @@ function vip_go_amp_force_query_var_value( $query_vars ) {
 			}
 		}
 	}
+
 	return $query_vars;
 }
-
-
 add_filter( 'request', 'vip_go_amp_force_query_var_value', 9, 1 );
