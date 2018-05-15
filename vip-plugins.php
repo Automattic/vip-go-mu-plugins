f1551473dd5da434ae01f8feff0327c5bdd6dc61
Matt Perry <mattoperry@gmail.com>
2018-05-15 11:02:13 -0700

    add Gutenberg loading functionality to Go
    Submodule for the Gutenberg Ramp plugin.
    
    Provide a wraper for `ramp_for_gutenberg_load_gutenberg` called `wpcom_vip_load_gutenberg`
    
    Turn off Ramp UI by default

--- vip-plugins.php
+++ vip-plugins.php
@@ -10,3 +10,23 @@
  */
 
 require_once( __DIR__ . '/vip-plugins/vip-plugins.php' );
+
+/** loading helper and behaviors for Gutenberg on VIP Go **/
+
+/** load Gutenberg Ramp **/
+if ( file_exists( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' ) ) {
+	require_once( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' );
+}
+
+/** Turn off the UI for Ramp **/
+add_action( 'plugins_loaded', function() {
+	remove_action( 'admin_init', 'ramp_for_gutenberg_initialize_admin_ui' );
+} );
+
+/** Loading helper **/
+function wpcom_vip_load_gutenberg( $criteria ) {
+	if ( !function_exists( 'ramp_for_gutenberg_load_gutenberg' ) ) {
+		return;
+	}
+	ramp_for_gutenberg_load_gutenberg( $criteria );
+}
