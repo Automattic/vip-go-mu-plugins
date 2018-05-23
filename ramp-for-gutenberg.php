88d72438f0fda761857b4d1ffa55aa6ac889f6a9
Fränk Klein <frank@humanmade.co.uk>
2018-05-23 09:46:05 +0200

    Move Gutenberg Ramp loading into a function.
    This allows developers to choose whether they want to use Gutenberg Ramp for loading Gutenberg on their sites.

--- ramp-for-gutenberg.php
+++ ramp-for-gutenberg.php
@@ -2,30 +2,31 @@
 
 /**
  * Plugin Name: Gutenberg Ramp
- * Description: Allows theme authors to control the circumstances under which the Gutenberg editor loads. Options include "load" (1 loads all the time, 0 loads never) "post_ids" (load for particular posts) "post_types" (load for particular posts types.)
- * Version:     0.1
+ * Description: Control the circumstances under which the Gutenberg editor loads in code, using Gutenberg Ramp.
+ * Version:     0.2
  * Author:      Automattic, Inc.
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * Text Domain: ramp-for-gutenberg
  */
 
-// This file loads Ramp, and modifies behaviors for Gutenberg on VIP Go
-
-/** load Gutenberg Ramp **/
-if ( file_exists( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' ) ) {
-	require_once( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' );
-}
-
-/** Turn off the UI for Ramp **/
-add_action( 'plugins_loaded', function() {
-	remove_action( 'admin_init', 'ramp_for_gutenberg_initialize_admin_ui' );
-} );
-
-/** Loading helper **/
+/**
+ * Load Gutenberg via the Gutenberg Ramp plugin.
+ *
+ * @param array $criteria Use [ 'load' => 1 ] to always load, [ 'load' => 0 ] to never load, [ 'post_ids' => [] ] to load for particular posts, and [ 'post_types' => [] ] to load for particular post types.
+ */
 function wpcom_vip_load_gutenberg( $criteria ) {
+	// Load Gutenberg Ramp.
+	if ( file_exists( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' ) ) {
+		require_once( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' );
+	}
+
+	// Turn off the UI for Gutenberg Ramp.
+	remove_action( 'admin_init', 'ramp_for_gutenberg_initialize_admin_ui' );
+
 	if ( ! function_exists( 'ramp_for_gutenberg_load_gutenberg' ) ) {
 		return;
 	}
+
 	ramp_for_gutenberg_load_gutenberg( $criteria );
 }
