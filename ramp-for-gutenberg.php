f9c575946b2cc4885358ec7e2a59bfddba4e677e
Fränk Klein <frank@humanmade.co.uk>
2018-05-23 17:12:50 +0200

    Use a constant to opt out from using Gutenberg Ramp.

--- ramp-for-gutenberg.php
+++ ramp-for-gutenberg.php
@@ -10,23 +10,30 @@
  * Text Domain: ramp-for-gutenberg
  */
 
+// This file loads Ramp, and modifies behaviors for Gutenberg on VIP Go
+
+if ( defined( 'VIP_GO_DISABLE_RAMP' ) && VIP_GO_DISABLE_RAMP ) {
+	return;
+}
+
+/** load Gutenberg Ramp **/
+if ( file_exists( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' ) ) {
+	require_once( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' );
+}
+
+/** Turn off the UI for Ramp **/
+add_action( 'plugins_loaded', function() {
+	remove_action( 'admin_init', 'ramp_for_gutenberg_initialize_admin_ui' );
+} );
+
 /**
  * Load Gutenberg via the Gutenberg Ramp plugin.
  *
  * @param array|false $criteria Use `false` or [ 'load' => 1 ] to always load, [ 'load' => 0 ] to never load, [ 'post_ids' => [] ] to load for particular posts, and [ 'post_types' => [] ] to load for particular post types.
  */
 function wpcom_vip_load_gutenberg( $criteria = false ) {
-	// Load Gutenberg Ramp.
-	if ( file_exists( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' ) ) {
-		require_once( __DIR__ . '/ramp-for-gutenberg/ramp-for-gutenberg.php' );
-	}
-
-	// Turn off the UI for Gutenberg Ramp.
-	remove_action( 'admin_init', 'ramp_for_gutenberg_initialize_admin_ui' );
-
 	if ( ! function_exists( 'ramp_for_gutenberg_load_gutenberg' ) ) {
 		return;
 	}
-
 	ramp_for_gutenberg_load_gutenberg( $criteria );
 }
