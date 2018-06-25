cc2e5287312a7e6d495bce4af7a908e07a3f7468
Mohammad Jangda <batmoo@gmail.com>
2018-06-25 02:59:59 -0400

    Add more tests for Context

--- tests/lib/utils/context.php
+++ tests/lib/utils/context.php
@@ -34,7 +34,7 @@ class Context_Test extends \PHPUnit_Framework_TestCase {
 
 		$actual_result = Context::is_maintenance_mode();
 
-		$this->assertFalse( $actual_result );
+		$this->assertFalse( $actual_result, '`WPCOM_VIP_SITE_MAINTENANCE_MODE` constant should not be defined for this test' );
 	}
 
 	/**
@@ -95,5 +95,149 @@ class Context_Test extends \PHPUnit_Framework_TestCase {
 		$actual_result = Context::is_web_request();
 
 		$this->assertTrue( $actual_result, 'Test failed; either something is actually broken or one of the constants being checked is being unintentionally defined in our test environment.' );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_wp_cli__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_wp_cli();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_wp_cli__yep() {
+		define( 'WP_CLI', true );
+
+		$actual_result = Context::is_wp_cli();
+
+		$this->assertTrue( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_rest_api__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_rest_api();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_rest_api__yep() {
+		define( 'REST_REQUEST', true );
+
+		$actual_result = Context::is_rest_api();
+
+		$this->assertTrue( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_cron__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_cron();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_cron__yep() {
+		define( 'DOING_CRON', true );
+
+		$actual_result = Context::is_cron();
+
+		$this->assertTrue( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_xmlrpc_api__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_xmlrpc_api();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_xmlrpc_api__yep() {
+		define( 'XMLRPC_REQUEST', true );
+
+		$actual_result = Context::is_xmlrpc_api();
+
+		$this->assertTrue( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_ajax__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_ajax();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_ajax__yep() {
+		define( 'DOING_AJAX', true );
+
+		$actual_result = Context::is_ajax();
+
+		$this->assertTrue( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_installing__nope() {
+		// Note: constant should not be defined here
+
+		$actual_result = Context::is_installing();
+
+		$this->assertFalse( $actual_result );
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function test__is_installing__yep() {
+		define( 'WP_INSTALLING', true );
+
+		$actual_result = Context::is_installing();
+
+		$this->assertTrue( $actual_result );
 	}
 }
