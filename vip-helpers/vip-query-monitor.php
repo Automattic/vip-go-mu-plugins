a6c1ced57ec63181e4774fefdff248842c5b04d1
Norris <im@justnorris.com>
2018-07-03 16:29:22 +0300

    Fix Query Monitor 3.0.1 notices

--- vip-helpers/vip-query-monitor.php
+++ vip-helpers/vip-query-monitor.php
@@ -4,11 +4,17 @@ class WPCOM_VIP_QM_Collector_DB_Queries extends QM_Collector_DB_Queries
 {
 	public function process_db_object($id, wpdb $db)
 	{
+		global $wp_the_query;
+		
 		$rows = array();
 		$types = array();
 		$total_time = 0;
 		$has_result = false;
 		$has_trace = false;
+		$request    = trim( $wp_the_query->request );
+		if ( method_exists( $db, 'remove_placeholder_escape' ) ) {
+			$request = $db->remove_placeholder_escape( $request );
+		}
 
 		foreach ((array)$db->queries as $query) {
 
@@ -65,7 +71,9 @@ class WPCOM_VIP_QM_Collector_DB_Queries extends QM_Collector_DB_Queries
 				$types[ $type ]['callers'][ $caller ]++;
 			}
 
-			$row = compact('caller', 'caller_name', 'stack', 'sql', 'ltime', 'result', 'type', 'component', 'trace');
+
+			$is_main_query = ( $request === $sql && ( false !== strpos( $stack, ' WP->main,' ) ) );
+			$row = compact('caller', 'caller_name', 'stack', 'sql', 'ltime', 'result', 'type', 'component', 'trace', 'has_main_query', 'is_main_query');
 
 			if (is_wp_error($result)) {
 				$this->data['errors'][] = $row;
@@ -83,10 +91,14 @@ class WPCOM_VIP_QM_Collector_DB_Queries extends QM_Collector_DB_Queries
 
 		$this->data['total_qs'] += $total_qs;
 		$this->data['total_time'] += $total_time;
+		
+		$has_main_query = wp_list_filter( $rows, array(
+			'is_main_query' => true,
+		) );
 
 		# @TODO put errors in here too:
 		# @TODO proper class instead of (object)
-		$this->data['dbs'][ $id ] = (object)compact('rows', 'types', 'has_result', 'has_trace', 'total_time', 'total_qs');
+		$this->data['dbs'][ $id ] = (object)compact('rows', 'types', 'has_result', 'has_trace', 'total_time', 'total_qs', 'has_main_query');
 
 	}
 }
