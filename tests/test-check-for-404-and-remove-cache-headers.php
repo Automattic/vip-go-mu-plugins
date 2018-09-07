9400c00043f2825dc04217b7f6cf01323331b395
Shantanu <shantanu2846@gmail.com>
2018-09-07 12:20:06 +0530

    Make the test file specific to check_for_404_and_remove_cache_headers() and add tearDown() for cleanup

--- tests/test-check-for-404-and-remove-cache-headers.php
+++ tests/test-check-for-404-and-remove-cache-headers.php
@@ -1,10 +1,15 @@
 <?php
 
-class VIP_Go_Misc_Test extends WP_UnitTestCase {
+class VIP_Go_Test_Check_For_404_And_Remove_Cache_Headers extends WP_UnitTestCase {
 
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
 		require_once( __DIR__ . '/../misc.php' );
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+		wp_reset_query();
 	}
 
 	/**
@@ -14,7 +19,7 @@ class VIP_Go_Misc_Test extends WP_UnitTestCase {
 	 * @param bool $is_wp_query_set Is the global $wp_query object set for a particular test case.
 	 * @param bool $is_404_set Is this page a 404.
 	 */
-	function test_check_for_404_and_remove_cache_headers( $is_wp_query_set, $is_404_set ) {
+	function test_check_for_404_and_remove_cache_headers( $is_wp_query_set, $is_404_set, $expected_headers ) {
 		global $wp_query;
 		$headers = array(
 			'Expires'       => 1,
@@ -22,20 +27,16 @@ class VIP_Go_Misc_Test extends WP_UnitTestCase {
 			'Pragma'        => 1,
 		);
 
-		$temp = null;
 		if ( $is_404_set ) {
 			$wp_query->set_404();
 		}
 		if ( ! $is_wp_query_set ) {
-			$temp     = $wp_query;
 			$wp_query = null;
 		}
 
 		$result = wpcom_vip_check_for_404_and_remove_cache_headers( $headers );
-		$this->assertEquals( empty( $result ), ( $is_404_set && $is_wp_query_set ) );
-		if ( ! isset( $wp_query ) ) {
-			$wp_query = $temp;
-		}
+		$this->assertEquals( $result, $expected_headers );
+
 	}
 
 	/**
@@ -45,21 +46,37 @@ class VIP_Go_Misc_Test extends WP_UnitTestCase {
 	 */
 	function data_provider() {
 		return array(
-			array(
+			'wp_query-set-and-is_404'         => array(
 				true,
 				true,
+				array(),
 			),
-			array(
+			'wp_query-set-and-not-is_404'     => array(
 				true,
 				false,
+				array(
+					'Expires'       => 1,
+					'Cache-Control' => 1,
+					'Pragma'        => 1,
+				),
 			),
-			array(
+			'wp_query-not-set-and-not-is_404' => array(
 				false,
 				false,
+				array(
+					'Expires'       => 1,
+					'Cache-Control' => 1,
+					'Pragma'        => 1,
+				),
 			),
-			array(
+			'wp_query-not-set-and-is_404'     => array(
 				false,
 				true,
+				array(
+					'Expires'       => 1,
+					'Cache-Control' => 1,
+					'Pragma'        => 1,
+				),
 			),
 		);
 	}
