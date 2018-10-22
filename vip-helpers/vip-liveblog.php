c9ec86afd7c839f47a390b90079bc8313573a3c5
Philip John <phil@philipjohn.me.uk>
2018-10-22 17:24:11 +0100

    Ensure we include the priority and arguments parameters.

--- vip-helpers/vip-liveblog.php
+++ vip-helpers/vip-liveblog.php
@@ -26,4 +26,4 @@ function wpcom_vip_liveblog_purge_on_new_entries( int $comment_id, int $post_id 
 	wpcom_vip_purge_edge_cache_for_url( $permalink );
 
 }
-add_action( 'liveblog_insert_entry', 'wpcom_vip_liveblog_purge_on_new_entries' );
+add_action( 'liveblog_insert_entry', 'wpcom_vip_liveblog_purge_on_new_entries', 10, 2 );
