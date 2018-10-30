4b44ea219e8c73b4cc331e347621735b3bedfebe
Daryl Chymko <dchymko@infometrix.ca>
2018-10-30 12:34:43 -0700

    Changed group metric calculation  (#998)
    * -Changed group metric calculation to place more emphasis on average group size relative to number of scripts in total
    
    * Handle individual do_item groups
    
    * Fix array key name

--- stats.php
+++ stats.php
@@ -61,19 +61,22 @@ class Concat_Metrics {
 	}
 
 	private function calculate_efficiency_ratio( $scripts ) {
-		$total_scripts = count( $scripts );
 
-		$concats_multiple = array_filter( $scripts, function ( $var ) {
+		$groups = array_reduce( $scripts, function ( $groups, $var ) {
 			if ( 'concat' === $var['type'] ) {
-				return count( $var['paths'] ) > 1;
+				$num_scripts = count( $var['paths'] );
+				$groups['total'] += $num_scripts;
+				array_push( $groups['size'], $num_scripts );
+			} elseif ( 'do_item' === $var['type'] ) {
+				// do_item are individual scripts
+				$groups['total'] += 1;
+				array_push( $groups['size'], 1 );
 			}
+			return $groups;
+		}, ['total' => 0, 'size' => []] );
 
-			return false;
-		});
 
-		$total_concats_multiple = count( $concats_multiple );
-
-		return ( $total_concats_multiple / $total_scripts );
+		return ( array_sum( $groups['size'] ) / count( $groups['size'] ) ) / $groups['total'];
 	}
 
 	private function send_efficiency_stat( $ratio ) {
