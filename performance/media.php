455f9ade4ef07563c195b9b5ac95936f37f99d03
Matthew Denton <matt@mdbitz.com>
2018-10-30 15:17:21 -0400

    Replace constant with named function that clients can remove.

--- performance/media.php
+++ performance/media.php
@@ -2,8 +2,8 @@
 
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
-if( ! ( defined( 'VIP_GO_ENABLE_FILENAMES_SEARCH' ) && VIP_GO_ENABLE_FILENAMES_SEARCH ) ) {
-	add_action( 'pre_get_posts', function() {
-		remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
-	} );
+function vip_go_filter_query_attachment_filenanes ( ){
+    remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
 }
+
+add_action( 'pre_get_posts', 'vip_go_filter_query_attachment_filenanes' );
