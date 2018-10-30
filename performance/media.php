33e1711928ee3048229373cb95e4393f4165fb94
Matthew Denton <matt@mdbitz.com>
2018-10-30 11:38:20 -0400

    Constant to enable filename media searhes

--- performance/media.php
+++ performance/media.php
@@ -2,6 +2,8 @@
 
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
-add_action( 'pre_get_posts', function() {
-	remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
-} );
+if( ! ( defined( 'VIP_GO_ENABLE_FILENAMES_SEARCH' ) && VIP_GO_ENABLE_FILENAMES_SEARCH ) ) {
+	add_action( 'pre_get_posts', function() {
+		remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
+	} );
+}
