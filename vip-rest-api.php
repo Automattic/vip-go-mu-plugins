7fff9d58ca2380c5a5c6441cf95ed852d0daa7b1
Josh Betz <j@joshbetz.com>
2018-10-31 14:01:19 -0500

    Add support for basic auth to internal API endpoints
    Use case: We need to use the vip/v1/sites endpoint to get information
    about multisites in the migration CLI. Rather than hardcoding a shared
    secret in the CLI, we can create a user on the site and use that
    username and password to authenticate for the API endpoints.
    
    This uses the basic auth plugin (https://github.com/WP-API/Basic-Auth)
    to populte the current user based on HTTP basic auth. We can then use
    that current user to decide whether to authenticate the current request
    for our internal endpoints.
    
    Note: We're also checking `current_user_can( 'vip_support' )` here
    because VIP Support users only have `read`, `subscriber`, and
    `vip_support` in this context. We may just want to fix that instead.
    
    Testing:
    
    1. Load the PR on your sandbox
    2. Create support user on a test site
    3. curl -u 'username:password' https://<domain>/wp-json/vip/v1/sites

--- vip-rest-api.php
+++ vip-rest-api.php
@@ -68,7 +68,11 @@ function wpcom_vip_verify_go_rest_api_request_authorization( $namespace, $auth_h
  * @param  string $namespace RESET API route's namespace
  * @return bool
  */
-function wpcom_vip_go_rest_api_request_allowed( $namespace ) {
+function wpcom_vip_go_rest_api_request_allowed( $namespace, $cap = 'manage_options' ) {
+	if ( get_current_user_id() > 0 && ( current_user_can( 'vip_support' ) || current_user_can( $cap ) ) ) {
+		return true;
+	}
+
 	// Do we have a header to check?
 	if ( ! isset( $_SERVER['HTTP_AUTHORIZATION'] ) || empty( $_SERVER['HTTP_AUTHORIZATION'] ) ) {
 		return false;
@@ -76,6 +80,55 @@ function wpcom_vip_go_rest_api_request_allowed( $namespace ) {
 
 	return wpcom_vip_verify_go_rest_api_request_authorization( $namespace, $_SERVER['HTTP_AUTHORIZATION'] );
 }
+
+// https://github.com/WP-API/Basic-Auth
+function json_basic_auth_handler( $user ) {
+	global $wp_json_basic_auth_error;
+	$wp_json_basic_auth_error = null;
+
+	// Don't authenticate twice
+	if ( ! empty( $user ) ) {
+		return $user;
+	}
+
+	// Check that we're trying to authenticate
+	if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+		return $user;
+	}
+
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$password = $_SERVER['PHP_AUTH_PW'];
+
+	/**
+	 * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
+	 * get_currentuserinfo which in turn calls the determine_current_user filter. This leads to infinite
+	 * recursion and a stack overflow unless the current function is removed from the determine_current_user
+	 * filter during authentication.
+	 */
+	remove_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+	$user = wp_authenticate( $username, $password );
+	add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+
+	if ( is_wp_error( $user ) ) {
+		$wp_json_basic_auth_error = $user;
+		return null;
+	}
+
+	$wp_json_basic_auth_error = true;
+	return $user->ID;
+}
+add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
+
+function json_basic_auth_error( $error ) {
+	// Passthrough other errors
+	if ( ! empty( $error ) ) {
+		return $error;
+	}
+
+	global $wp_json_basic_auth_error;
+	return $wp_json_basic_auth_error;
+}
+add_filter( 'rest_authentication_errors', 'json_basic_auth_error' );
 
 /**
  * Include customizations
