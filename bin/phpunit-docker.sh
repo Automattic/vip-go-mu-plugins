9c27bf9e9d05d19014e0f066b8d7d12e33db3946
Josh Betz <j@joshbetz.com>
2018-11-05 10:01:41 -0600

    Add PHPUnit test script
    ```
    usage: ./bin/phpunit-docker.sh [wp-version]
    ```
    
    Adds a new script that uses docker to create a temporary database
    container, installs the WP Unit Test dependencies, and runs the tests
    in a phpunit container.
    
    We're also splitting the existing install-wp-tests.sh script into two parts
    so the applicable parts are reusable for this. The part that sets up a
    local database is unnecessary here since we're using Docker for that.
    
    The existing script still references the new download-wp-tests.sh script
    internally, so that should continue to function the same as before.
    
    Fixes #1017

---
+++ bin/phpunit-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+WP_VERSION=${1-latest}
+
+MYSQL_ROOT_PASSWORD='wordpress'
+db=$(docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d mariadb)
+function cleanup() {
+	docker rm -f $db
+}
+trap cleanup EXIT
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+WP_VERSION=$($DIR/download-wp-tests.sh wordpress_test root $MYSQL_ROOT_PASSWORD 127.0.0.1 $WP_VERSION)
+
+## Ensure there's a database connection for the rest of the steps
+until docker exec -it $db mysql -u root --password="wordpress" -e 'CREATE DATABASE wordpress_test' > /dev/null; do
+	echo "Waiting for database connection..."
+	sleep 5
+done
+
+docker run \
+	-v $(pwd):/app \
+	-v /tmp/wordpress-tests-lib-$WP_VERSION:/tmp/wordpress-tests-lib \
+	-v /tmp/wordpress-$WP_VERSION:/tmp/wordpress \
+	--network host \
+	--rm phpunit/phpunit \
+	--bootstrap /app/tests/bootstrap.php
