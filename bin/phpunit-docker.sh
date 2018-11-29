f5c418e2cf7d6727d90a47da25a1e221177eb217
Daryl Chymko <dchymko@infometrix.ca>
2018-11-29 15:45:03 +0000

    Use docker network to avoid port collisions
    Creates a special docker network for tests to avoid port 3306 collisions on the host network
    
    Co-Authored-By: joshbetz <j@joshbetz.com>

--- bin/phpunit-docker.sh
+++ bin/phpunit-docker.sh
@@ -3,22 +3,25 @@
 WP_VERSION=${1-latest}
 
 MYSQL_ROOT_PASSWORD='wordpress'
-db=$(docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d mariadb)
+docker network create tests
+db=$(docker run -p 3306:3306 --network tests --name db -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD -d mariadb)
 function cleanup() {
 	docker rm -f $db
+	docker network rm tests
 }
 trap cleanup EXIT
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-WP_VERSION=$($DIR/download-wp-tests.sh wordpress_test root "$MYSQL_ROOT_PASSWORD" "127.0.0.1" "$WP_VERSION")
+WP_VERSION=$($DIR/download-wp-tests.sh wordpress_test root "$MYSQL_ROOT_PASSWORD" "db" "$WP_VERSION")
 
 ## Ensure there's a database connection for the rest of the steps
-until docker exec -it $db mysql -u root --password="wordpress" -e 'CREATE DATABASE wordpress_test' > /dev/null; do
+until docker exec -it $db mysql -u root -h db --password="wordpress" -e 'CREATE DATABASE wordpress_test' > /dev/null; do
 	echo "Waiting for database connection..."
 	sleep 5
 done
 
 docker run \
+ 	--network tests \
 	-v $(pwd):/app \
 	-v /tmp/wordpress-tests-lib-$WP_VERSION:/tmp/wordpress-tests-lib \
 	-v /tmp/wordpress-$WP_VERSION:/tmp/wordpress \
