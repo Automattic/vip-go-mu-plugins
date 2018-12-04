7ee148164dad96fb133450ffe6ac97329c778c1e
Mohammad Jangda <mo@automattic.com>
2018-12-04 00:04:23 -0500

    Fix function formatting

--- performance/media.php
+++ performance/media.php
@@ -2,7 +2,7 @@
 
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
-function vip_filter_query_attachment_filenames ( ){
+function vip_filter_query_attachment_filenames() {
     remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
 }
 
