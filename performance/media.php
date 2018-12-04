b535e8b2c4e2dd49d7e399b32ec0662c0a639713
Mohammad Jangda <mo@automattic.com>
2018-12-03 23:39:12 -0500

    Fix typo in function name

--- performance/media.php
+++ performance/media.php
@@ -2,8 +2,8 @@
 
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
-function vip_go_filter_query_attachment_filenanes ( ){
+function vip_filter_query_attachment_filenames ( ){
     remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
 }
 
-add_action( 'pre_get_posts', 'vip_go_filter_query_attachment_filenanes' );
+add_action( 'pre_get_posts', 'vip_filter_query_attachment_filenames' );
