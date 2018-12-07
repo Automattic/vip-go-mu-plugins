01b12e0e0efd7054dcf0cc9020040276be1b15bc
Mohammad Jangda <batmoo@gmail.com>
2018-12-06 22:34:26 -0500

    Modify the 5.0 about page to be more VIP-specific (#1039)
    * Modify the 5.0 about page to be more VIP-specific
    
    Remove Classic Editor references since we have Ramp in place. Add links
    and references to VIP docs and resources instead.
    
    * Add callout for support

--- gutenberg-ramp.php
+++ gutenberg-ramp.php
@@ -41,3 +41,63 @@ function wpcom_vip_load_gutenberg( $criteria = true ) {
 
 	gutenberg_ramp_load_gutenberg( $criteria );
 }
+
+// Modify the the post-upgrade screen (about.php) to be more VIP-specific
+// Don't mind that hacky CSS, please :)
+add_action( 'all_admin_notices', function() {
+	global $pagenow;
+	if ( 'about.php' !== $pagenow ) {
+		return;
+	}
+
+	// Only on 5.0+
+	$db_version = absint( get_option( 'db_version' ) );
+	if ( $db_version < 43764 ) {
+		return;
+	}
+
+	$is_using_gutenberg = false;
+	if ( class_exists( 'Gutenberg_Ramp' ) ) {
+		$is_using_gutenberg = Gutenberg_Ramp::get_instance()->active;
+	}
+
+?>
+<style>
+#classic-editor,
+#classic-editor + div.full-width,
+#classic-editor + div.full-width + div.feature-section {
+	display: none;
+}
+
+.about-text {
+	display: none;
+}
+
+#vip-upgrade-message {
+	margin: 20px 200px 20px 0;
+	padding: 0;
+
+	background: none;
+	border: none;
+	box-shadow: none;
+}
+
+#vip-upgrade-message p {
+	font-weight: 400;
+    line-height: 1.6em;
+    font-size: 19px;
+}
+</style>
+<div id="vip-upgrade-message" class="notice notice-success" style="display: block !important">
+	<p>Thank you for updating to the latest version! WordPress 5.0 introduces a robust new content creation experience.</p>
+
+	<p><strong>Your editor of choice has not been changed</strong>.</p>
+
+	<p>Read more about the new editor below, learn about <a href="https://vip.wordpress.com/documentation/gutenberg-at-vip/" rel="noopener noreferrer" target="_blank">how it's configured on VIP</a>, or <a href="https://testgutenberg.com/" rel="noopener noreferrer" target="_blank">try it out in your browser</a>.</p>
+
+	<?php if ( ! $is_using_gutenberg ) : ?>
+		<p>Need help planning your transition? The <a href="<?php echo admin_url( 'admin.php?page=vip-dashboard' ); ?>">VIP Support team is ready to help</a>!</p>
+	<?php endif; ?>
+</div>
+<?php
+} );
