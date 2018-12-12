673ba75b95a6351cce344a973a16cb2134e3a547
Mohammad Jangda <batmoo@gmail.com>
2018-12-12 05:51:56 +0000

    Retain < 5.0 behaviour for tinyMCE scripts.
    These used to be output individually and bypass concat. 5.0 registers
    them as scripts which gets them picked up by our concat.
    
    However, tinymce.min.js (root) does not handle that situation well.
    It ends up initializing with the baseURL set to that of the script.
    
    Which then leads to problems loading additional dependencies and
    everything breaks.
    
    By skipping concat, we restore earlier behaviour and keep sites working.

--- http-concat.php
+++ http-concat.php
@@ -12,6 +12,20 @@ if ( true === WPCOM_IS_VIP_ENV ) {
 	// Activate concatenation
 	if ( ! isset( $_GET['concat_js'] ) || 'yes' === $_GET['concat_js'] ) {
 		require __DIR__ .'/http-concat/jsconcat.php';
+
+		add_filter( 'js_do_concat', function( $do_concat, $handle ) {
+			// Retain < 5.0 behaviour for tinyMCE scripts.
+			// These used to be output individually and bypass concat.
+			// 5.0 registers them as scripts which gets them picked up by out concat.
+			// However, tinymce.min.js (root) does not handle that situation well.
+			// It ends up initializing with the baseURL set to that of the script.
+			// Which then leads to problems loading additional dependencies.
+			if ( 'wp-tinymce-root' === $handle || 'wp-tinymce' === $handle ) {
+				$do_concat = false;
+			}
+
+			return $do_concat;
+		}, 10, 2 );
 	}
 
 	if ( ! isset( $_GET['concat_css'] ) || 'yes' === $_GET['concat_css'] ) {
