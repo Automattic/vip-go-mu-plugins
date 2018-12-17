3c4697607498df7bcd31d7b4af1114bd427c84bb
Shantanu <shantanu2846@gmail.com>
2018-12-17 20:59:32 +0530

    Enable Query Monitor in Debug Mode

--- 000-debug/0-load.php
+++ 000-debug/0-load.php
@@ -8,21 +8,33 @@ function toggle_debug_mode() {
     if ( isset( $_GET['a8c-debug'] )
         && 'true' === $_GET['a8c-debug']
         && \is_proxied_request() ) {
-            setcookie( 'vip-go-cb', '1', time() + 2 * HOUR_IN_SECONDS );
+			setcookie( 'vip-go-cb', '1', time() + 2 * HOUR_IN_SECONDS );
 			nocache_headers();
-			// Redirect to the same page without the Debug Mode activation handler.
-			add_action( 'init', function(){
+
+			// The priority is 0 as the cookie needs to be set before the check in wpcom_vip_qm_enable(), even if the validation takes place much later.
+			add_action( 'plugins_loaded', function() {
+				$user = get_user_by( 'slug', 'wpcomvip' );
+				// The cookie value is generated using the same parameters that QM uses internally in QM_Dispatcher_Html::ajax_on() -> query-monitor/dispatchers/Html.php .
+				$cookie = \wp_generate_auth_cookie( $user->ID, time() + ( 2 * DAY_IN_SECONDS ), 'logged_in' );
+				setcookie( 'query_monitor_' . COOKIEHASH, $cookie, time() + 2 * HOUR_IN_SECONDS, COOKIEPATH, COOKIE_DOMAIN );
+			}, 0 );
+
+			add_action( 'init', function() {
+				// Redirect to the same page without the activation handler.
 				\wp_safe_redirect( remove_query_arg( 'a8c-debug' ) );
-			} );
+				exit;
+			});
     } elseif ( isset( $_GET['a8c-debug'] )
             && 'false' === $_GET['a8c-debug'] ) {
 				setcookie( 'vip-go-cb', '', time() - 2 * HOUR_IN_SECONDS );
+
 				add_action( 'init', function() {
+					setcookie( 'query_monitor_' . COOKIEHASH, time() - 2 * HOUR_IN_SECONDS );
 					// Redirect to the same page without the deactivation handler.
 					\wp_safe_redirect( remove_query_arg( 'a8c-debug' ) );
 					exit;
 				});
-    }
+	}
 }
 
 function debug_bar() {
@@ -70,7 +82,6 @@ function debug_bar() {
 		    }
             </style>
             <?php
-            // @todo Enable Query Monitor and Debug Bar.
         }
 
 }
