c4418ee065897207d6cd2b68fa23b4c51b9de37c
Mohammad Jangda <batmoo@gmail.com>
2019-02-05 23:28:15 -0500

    Coding standards cleanup

--- cache/class-vary-cache.php
+++ cache/class-vary-cache.php
@@ -2,35 +2,32 @@
 
 namespace Automattic\VIP\Cache;
 
-//use WP_Error;
+use WP_Error;
 
-
-class Vary_Cache
-{
-	private static $PREFIX_NO_CACHE = "vip-go-cb";
-	private static $PREFIX_SEGMENT = "vip-go-seg";
-	private static $PREFIX_AUTH = "vip-go-auth";
+class Vary_Cache {
+	private static $PREFIX_NO_CACHE = 'vip-go-cb';
+	private static $PREFIX_SEGMENT = 'vip-go-seg';
+	private static $PREFIX_AUTH = 'vip-go-auth';
 
 	private static $encryption_enabled = false;
 
-	public function __construct(  )
-	{
-	}
-
 	/* nocache */
-	static function set_no_cache_for_user( ) {
-		setcookie(static::$PREFIX_NO_CACHE, 1);
-		self::trackAction( 'no_cache' );
+	static function set_no_cache_for_user() {
+		// TODO: need to scope cookie domain/path + TTL
+		setcookie( static::$PREFIX_NO_CACHE, 1 );
+
+		self::track_action( 'no_cache' );
 	}
 
-	static function remove_no_cache_for_user( ) {
-		if(isset($_COOKIE[static::$PREFIX_NO_CACHE])){
+	static function remove_no_cache_for_user() {
+		if ( isset( $_COOKIE[ static::$PREFIX_NO_CACHE ] ) ) {
 			setcookie( static::$PREFIX_NO_CACHE, '', time() - 3600 );
 		}
 	}
 
 	/* Grouping */
 	// will set the group cookie to the added group to indicate Varnish to cache it for those groups
+	// TODO: group + value to allow multiple groups
 	static function set_group_for_user( $group ) {
 		//TODO: make sure headers aren't already sent
 		//TODO: only send header if we added or changed things
@@ -38,22 +35,25 @@ class Vary_Cache
 		// validate, process $group, etc.
 		if ( self::is_encryption_enabled() ) {
 			self::set_group_cookie_encrypted( $group );
-			self::trackAction( 'set_user_group' );
+
+			self::track_action( 'set_user_group_encrypted' );
 		} else {
 			self::set_group_cookie_plaintext( $group );
-			self::trackAction( 'set_user_group_encrypted' );
+
+			self::track_action( 'set_user_group' );
 		}
 	}
 
 
 	static function is_user_in_group( $group ) {
-		return (isset($_COOKIE[ static::$PREFIX_SEGMENT ] ) && $_COOKIE[ static::$PREFIX_SEGMENT ] === $group );
+		return isset( $_COOKIE[ static::$PREFIX_SEGMENT ] ) && $_COOKIE[ static::$PREFIX_SEGMENT ] === $group;
 	}
 
-	static function get_user_groups( $group ) {
+	static function get_user_groups() {
+		// TODO
 	}
 
-	static function set_encryption($value = true) {
+	static function set_encryption( $value = true ) {
 		static::$encryption_enabled = $value;
 	}
 
@@ -61,42 +61,42 @@ class Vary_Cache
 		return static::$encryption_enabled;
 	}
 
-
 	static private function set_group_cookie_encrypted( $value, $ttl = null ) {
-		//validate that we have the secret values
+		// Validate that we have the secret values
 		if ( ! defined( 'VIP_GO_AUTH_COOKIE_KEY' ) || ! defined( 'VIP_GO_AUTH_COOKIE_IV' ) ) {
-			error_log('Secrets not defined for encrypted vary cookies ');
+			// TODO: check that values are not empty
+			trigger_error( 'Secrets not defined for encrypted vary cookies', E_USER_WARNING );
 			return;
 		}
+
 		$client_key = constant( 'VIP_GO_AUTH_COOKIE_KEY' );
 		$client_iv = constant( 'VIP_GO_AUTH_COOKIE_IV' );
-		$cookie_value = random_bytpes(32) + "|" . $value . "|".time() + $ttl;
+		$cookie_value = random_bytpes( 32 ) + '|' . $value . '|' . time() + $ttl;
 		$cipher_cookie = openssl_encrypt( $cookie_value, 'aes-128-cbc', $client_key, 0, $client_iv );
-		setcookie(static::$PREFIX_AUTH, $cipher_cookie);
+
+		// TODO: need to scope cookie domain/path + TTL
+		setcookie( static::$PREFIX_AUTH, $cipher_cookie );
 	}
 
 	static private function set_group_cookie_plaintext( $value, $ttl = null ) {
-		setcookie(static::$PREFIX_SEGMENT, $value);
+		// TODO: need to scope cookie domain/path + TTL
+		setcookie( static::$PREFIX_SEGMENT, $value );
 	}
 
-	//send action for tracking purposes
-	static private function trackAction( $action )
-	{
-		if ( defined( 'VIP_GO_APP_ID' ) ){
-			add_action('vipgo_did_vary_cache', $action, constant( 'VIP_GO_APP_ID') );
+	// Send action for tracking purposes
+	static private function track_action( $action ) {
+		if ( defined( 'VIP_GO_APP_ID' ) ) {
+			add_action( 'vipgo_did_vary_cache', $action, constant( 'VIP_GO_APP_ID' ) );
 		}
 	}
 
-	//Hook to send the Vary header
+	// Hook to send the Vary header
 	static function add_vary_headers() {
 		if( ! headers_sent() ) {
 			header( 'X-Vary2: abc' );
 		}
 	}
-
-
 }
 
-
+// TODO: move
 add_action( 'send_headers', 'Automattic\Vip\Cache\Vary_Cache::add_vary_headers' );
-
