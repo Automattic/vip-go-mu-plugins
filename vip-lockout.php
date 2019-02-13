b491723c91c2e95d03f4c2fc61e25315614351b4
Hanif Norman <hanifnorman@gmail.com>
2019-02-13 16:50:28 +0800

    Fix identation

--- vip-lockout.php
+++ vip-lockout.php
@@ -8,15 +8,15 @@
 
 class VIP_Lockout {
 
-    const USER_SEEN_WARNING_KEY = 'seen_lockout_warning';
+	const USER_SEEN_WARNING_KEY = 'seen_lockout_warning';
 
-    const USER_SEEN_WARNING_TIME_KEY = 'seen_lockout_warning_time';
+	const USER_SEEN_WARNING_TIME_KEY = 'seen_lockout_warning_time';
 
 	/**
 	 * VIP_Lockout constructor.
 	 */
 	public function __construct() {
-	    add_action( 'admin_notices', [ $this, 'add_admin_notice' ], 1 );
+		add_action( 'admin_notices', [ $this, 'add_admin_notice' ], 1 );
 		add_action( 'user_admin_notices', [ $this, 'add_admin_notice' ], 1 );
 
 		add_filter( 'user_has_cap', [ $this, 'filter_user_has_cap' ], PHP_INT_MAX, 4 );
@@ -31,13 +31,13 @@ class VIP_Lockout {
 				case 'warning':
 					$user = wp_get_current_user();
 
-                    $this->render_warning_notice( $user );
+					$this->render_warning_notice( $user );
 
-                    $this->user_seen_warning( $user );
-                    break;
+					$this->user_seen_warning( $user );
+					break;
 
 				case 'locked':
-				    $this->render_locked_notice();
+					$this->render_locked_notice();
 					break;
 			}
 		}
@@ -49,13 +49,13 @@ class VIP_Lockout {
 	 * @param WP_User $user
 	 */
 	protected function user_seen_warning( WP_User $user ) {
-        $seen_warning = get_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true );
+		$seen_warning = get_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true );
 
-        if ( ! $seen_warning ) {
-            add_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true, true );
-            add_user_meta( $user->ID, self::USER_SEEN_WARNING_TIME_KEY, date('Y-m-d H:i:s'), true );
-        }
-    }
+		if ( ! $seen_warning ) {
+			add_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true, true );
+			add_user_meta( $user->ID, self::USER_SEEN_WARNING_TIME_KEY, date('Y-m-d H:i:s'), true );
+		}
+	}
 
 	protected function render_warning_notice( WP_User $user ) {
 		if ( ! $user->has_cap( 'manage_options' ) ) {
@@ -64,22 +64,22 @@ class VIP_Lockout {
 
 		?>
 		<div id="lockout-warning" class="notice-warning wrap clearfix" style="align-items: center;background: #ffffff;border-left-width:4px;border-left-style:solid;border-radius: 6px;display: flex;margin-top: 30px;padding: 30px;" >
-            <div class="dashicons dashicons-warning" style="display:flex;float:left;margin-right:2rem;font-size:38px;align-items:center;margin-left:-20px;color:#ffb900;"></div>
-            <div class="vp-message" style="display: flex;align-items: center;" >
-                <h3><?php _e( VIP_LOCKOUT_MESSAGE ); ?></h3>
-            </div>
+			<div class="dashicons dashicons-warning" style="display:flex;float:left;margin-right:2rem;font-size:38px;align-items:center;margin-left:-20px;color:#ffb900;"></div>
+			<div class="vp-message" style="display: flex;align-items: center;" >
+				<h3><?php _e( VIP_LOCKOUT_MESSAGE ); ?></h3>
+			</div>
 		</div>
 		<?php
 	}
 
 	protected function render_locked_notice() {
 		?>
-        <div id="lockout-warning" class="notice-error wrap clearfix" style="align-items: center;background: #ffffff;border-left-width:4px;border-left-style:solid;border-radius: 6px;display: flex;margin-top: 30px;padding: 30px;" >
-            <div class="dashicons dashicons-warning" style="display:flex;float:left;margin-right:2rem;font-size:38px;align-items:center;margin-left:-20px;color:#dc3232;"></div>
-            <div class="vp-message" style="display: flex;align-items: center;" >
-                <h3><?php _e( VIP_LOCKOUT_MESSAGE ); ?></h3>
-            </div>
-        </div>
+		<div id="lockout-warning" class="notice-error wrap clearfix" style="align-items: center;background: #ffffff;border-left-width:4px;border-left-style:solid;border-radius: 6px;display: flex;margin-top: 30px;padding: 30px;" >
+			<div class="dashicons dashicons-warning" style="display:flex;float:left;margin-right:2rem;font-size:38px;align-items:center;margin-left:-20px;color:#dc3232;"></div>
+			<div class="vp-message" style="display: flex;align-items: center;" >
+				<h3><?php _e( VIP_LOCKOUT_MESSAGE ); ?></h3>
+			</div>
+		</div>
 		<?php
 	}
 
@@ -96,14 +96,14 @@ class VIP_Lockout {
 	 * @return array
 	 */
 	public function filter_user_has_cap( array $user_caps, array $caps, array $args, WP_User $user ) {
-        if ( defined( 'VIP_LOCKOUT_STATE' ) && 'locked' === VIP_LOCKOUT_STATE ) {
-            $subscriber = get_role( 'subscriber' );
+		if ( defined( 'VIP_LOCKOUT_STATE' ) && 'locked' === VIP_LOCKOUT_STATE ) {
+			$subscriber = get_role( 'subscriber' );
 
-            return array_intersect_key( $user_caps, (array) $subscriber->capabilities );
-        }
+			return array_intersect_key( $user_caps, (array) $subscriber->capabilities );
+		}
 
-        return $user_caps;
-    }
+		return $user_caps;
+	}
 }
 
 new VIP_Lockout();
