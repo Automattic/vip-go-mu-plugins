e662e87ac1624eab4e950c6f8ebf5b77ae6dcf47
Hanif Norman <hanifnorman@gmail.com>
2019-02-13 16:33:22 +0800

    Add user seen warning meta
    For auditing purposes

--- vip-lockout.php
+++ vip-lockout.php
@@ -8,6 +8,10 @@
 
 class VIP_Lockout {
 
+    const USER_SEEN_WARNING_KEY = 'seen_lockout_warning';
+
+    const USER_SEEN_WARNING_TIME_KEY = 'seen_lockout_warning_time';
+
 	/**
 	 * VIP_Lockout constructor.
 	 */
@@ -18,6 +22,9 @@ class VIP_Lockout {
 		add_filter( 'user_has_cap', [ $this, 'filter_user_has_cap' ], PHP_INT_MAX, 4 );
 	}
 
+	/**
+	 * Add warnings to admin page
+	 */
 	public function add_admin_notice() {
 		if ( defined( 'VIP_LOCKOUT_STATE' ) ) {
 			switch ( VIP_LOCKOUT_STATE ) {
@@ -25,6 +32,8 @@ class VIP_Lockout {
 					$user = wp_get_current_user();
 
                     $this->render_warning_notice( $user );
+
+                    $this->user_seen_warning( $user );
                     break;
 
 				case 'locked':
@@ -33,6 +42,20 @@ class VIP_Lockout {
 			}
 		}
 	}
+
+	/**
+     * Mark that user has seen warning
+     *
+	 * @param WP_User $user
+	 */
+	protected function user_seen_warning( WP_User $user ) {
+        $seen_warning = get_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true );
+
+        if ( ! $seen_warning ) {
+            add_user_meta( $user->ID, self::USER_SEEN_WARNING_KEY, true, true );
+            add_user_meta( $user->ID, self::USER_SEEN_WARNING_TIME_KEY, date('Y-m-d H:i:s'), true );
+        }
+    }
 
 	protected function render_warning_notice( WP_User $user ) {
 		if ( ! $user->has_cap( 'manage_options' ) ) {
@@ -60,6 +83,18 @@ class VIP_Lockout {
 		<?php
 	}
 
+	/**
+     * Filter the result of user capability check
+     *
+     * If site is in lockout mode then all user will only have capabilities of a subscriber.
+     *
+	 * @param array $user_caps
+	 * @param array $caps
+	 * @param array $args
+	 * @param WP_User $user
+	 *
+	 * @return array
+	 */
 	public function filter_user_has_cap( array $user_caps, array $caps, array $args, WP_User $user ) {
         if ( defined( 'VIP_LOCKOUT_STATE' ) && 'locked' === VIP_LOCKOUT_STATE ) {
             $subscriber = get_role( 'subscriber' );
