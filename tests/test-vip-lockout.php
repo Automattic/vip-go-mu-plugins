e05b3b95825d69d904567fe984bd382fc00e9370
Hanif Norman <hanifnorman@gmail.com>
2019-02-15 16:59:55 +0800

    Fix typo in comments

--- tests/test-vip-lockout.php
+++ tests/test-vip-lockout.php
@@ -8,7 +8,7 @@ class VIP_Lockout_Test extends WP_UnitTestCase {
 	private $lockout;
 
 	/**
-	 * Make tests run in separate processes and don't reserve global state so
+	 * Make tests run in separate processes and don't preserve global state so
 	 * that constants set in tests won't affect one another.
 	 */
 	protected $preserveGlobalState = FALSE;
