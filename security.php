56f367f35b8a025a6c62685faffa5ce51892390d
Mohammad Jangda <batmoo@gmail.com>
2019-02-27 19:04:19 +0000

    Return a 429 code instead of 503
    As it's a more appropriate response, props @seanosh

--- security.php
+++ security.php
@@ -107,7 +107,7 @@ function wpcom_vip_login_limit_xmlrpc_error( $error, $user ) {
 	}
 
 	if ( is_wp_error( $login_limit_error ) ) {
-		return new IXR_Error( 503, $login_limit_error->get_error_message() );
+		return new IXR_Error( 429, $login_limit_error->get_error_message() );
 	}
 
 	return $error;
