e0c1a0a4fb6f5f18b673ab8958ce98f90cb1e553
Josh Betz <j@joshbetz.com>
2019-05-17 12:10:50 -0500

    Add two_factor_providers field
    Get the two factor methods that each user has enabled and return a list of the labels using the `get_label()` method of each one.

--- wp-cli/vip-two-factor.php
+++ wp-cli/vip-two-factor.php
@@ -3,20 +3,29 @@
 class VIP_Two_Factor_Command extends WPCOM_VIP_CLI_Command {
 	function report() {
 		$fields = array( 'ID', 'display_name' );
-		$users = get_users( array( 'fields' => $fields ) );
+		$users = get_users();
+		$providers = Two_Factor_Core::get_providers();
 
 		$cap = apply_filters( 'wpcom_vip_two_factor_enforcement_cap', 'edit_posts' );
 		foreach ( $users as $user ) {
+			$user->two_factor_enabled = 'false';
+			$user->two_factor_providers = '';
+
 			if ( Two_Factor_Core::is_user_using_two_factor( $user->ID ) ) {
 				$user->two_factor_enabled = 'true';
+
+				$user_providers = Two_Factor_Core::get_enabled_providers_for_user( $user );
+				$user_providers = array_map( function( $provider ) use ( $providers ) {
+					return $providers[ $provider ]->get_label();
+				}, $user_providers );
+				$user->two_factor_providers = implode( ', ', $user_providers );
 			} else if ( ! user_can( $user->ID, $cap ) ) {
 				$user->two_factor_enabled = 'n/a';
-			} else {
-				$user->two_factor_enabled = 'false';
 			}
 		}
 
 		$fields[] = 'two_factor_enabled';
+		$fields[] = 'two_factor_providers';
 		WP_CLI\Utils\format_items( 'table', $users, $fields );
 	}
 }
