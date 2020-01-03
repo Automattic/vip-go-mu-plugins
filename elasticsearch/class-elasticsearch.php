eb12d760aea5b35a0bdc82e5203a97d4dc0c82a7
Hanif Norman <hanifnorman@gmail.com>
2020-01-03 14:44:03 +0800

    Add ES init class

---
+++ elasticsearch/class-elasticsearch.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Automattic\VIP\Elasticsearch;
+
+use \WP_CLI;
+
+class Elasticsearch {
+
+	/**
+	 * Initialize the VIP Elasticsearch plugin
+	 */
+	public function init() {
+		if ( defined( 'USE_VIP_ELASTICSEARCH' ) && USE_VIP_ELASTICSEARCH ) {
+			$this->load_dependencies();
+			$this->add_hooks();
+			$this->load_commands();
+		}
+	}
+
+	protected function load_dependencies() {
+		/**
+		 * Load ES Health command class
+		 */
+		require_once 'class-health-command.php';
+	}
+
+	protected function add_hooks() {
+		// Add filters and action hooks here
+	}
+
+	protected function load_commands() {
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			WP_CLI::add_command( 'vip-es health', __NAMESPACE__ . '\Health_Command' );
+		}
+	}
+}
