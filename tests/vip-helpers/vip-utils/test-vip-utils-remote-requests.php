d80ecdafd3f3129523a4378dc75ec04cae962cfc
Nick Daugherty <ndaugherty987@gmail.com>
2020-01-08 12:21:20 -0700

    Split up vip-utils tests into 2 separate files

--- tests/vip-helpers/vip-utils/test-vip-utils-remote-requests.php
+++ tests/vip-helpers/vip-utils/test-vip-utils-remote-requests.php
@@ -1,34 +1,12 @@
 <?php
 
-class WPCOM_VIP_Get_Resized_Attachment_Url_Test extends \WP_UnitTestCase {
+class WPCOM_VIP_Utils_Remote_Requests_Test extends \WP_UnitTestCase {
 	public function mock_http_response( $mocked_response, $response_time = 1 ) {
 		add_filter( 'pre_http_request', function( $response, $args, $url ) use ( $mocked_response, $response_time ) {
 			usleep( $response_time * 1000000 );
 
 			return $mocked_response;
 		}, 10, 3 );
-	}
-
-	public function test__invalid_attachment() {
-		$attachment_id = 99999999;
-
-		$actual_url = wpcom_vip_get_resized_attachment_url( $attachment_id, 100, 101 );
-
-		$this->assertFalse( $actual_url );
-	}
-
-	public function test__valid_attachment() {
-		$expected_end_of_url = '/image.jpg?w=100&h=101';
-
-		$attachment_id = $this->factory->attachment->create_object( [
-			'file' => 'image.jpg',
-		] );
-
-		$actual_url = wpcom_vip_get_resized_attachment_url( $attachment_id, 100, 101 );
-
-		$actual_end_of_url = substr( $actual_url, strrpos( $actual_url, '/' ) );
-
-		$this->assertEquals( $expected_end_of_url, $actual_end_of_url );
 	}
 
 	/**
