7eb7b49b2d439d860f590c6b16e7e792ac4d6d81
Nick Daugherty <ndaugherty987@gmail.com>
2020-01-24 15:57:02 -0700

    Put new error logging behind existing constant
    So it can be disabled

--- vip-helpers/vip-utils.php
+++ vip-helpers/vip-utils.php
@@ -763,7 +763,9 @@ function vip_safe_wp_remote_request( $url, $fallback_value='', $threshold=3, $ti
 	// check if the timeout was hit and obey the option and return the fallback value
 	if ( false !== $option && time() - $option['time'] < $retry ) {
 		if ( $option['hits'] >= $threshold ) {
-			trigger_error( "vip_safe_wp_remote_request: Blog ID {$blog_id}: Requesting $url with method {$parsed_args[ 'method' ]} has been throttled after {$option['hits']} attempts. Not reattempting until after $retry seconds", E_USER_WARNING);
+			if ( ! defined( 'WPCOM_VIP_DISABLE_REMOTE_REQUEST_ERROR_REPORTING' ) || ! WPCOM_VIP_DISABLE_REMOTE_REQUEST_ERROR_REPORTING ) {
+				trigger_error( "vip_safe_wp_remote_request: Blog ID {$blog_id}: Requesting $url with method {$parsed_args[ 'method' ]} has been throttled after {$option['hits']} attempts. Not reattempting until after $retry seconds", E_USER_WARNING);
+			}
 
 			return ( $fallback_value ) ? $fallback_value : new WP_Error('remote_request_disabled', 'Remote requests disabled: ' . maybe_serialize( $option ) );
 		}
