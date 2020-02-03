a6388aaf552b2c452548b98ace1000e2a1bbc5e3
Gabriel Sambarino <gabriel.sambarino@gmail.com>
2020-02-03 14:51:55 +0100

    Fix version number
    Co-Authored-By: Gary Jones <gary.jones@automattic.com>

--- elasticsearch/elasticsearch.php
+++ elasticsearch/elasticsearch.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: VIP Search
  * Description: Power your site search and other queries with Elasticsearch
- * Version:     0.1
+ * Version:     0.1.0
  * Author:      Automattic VIP
  * Author URI:  https://wpvip.com
  * License:     GPLv2 or later
