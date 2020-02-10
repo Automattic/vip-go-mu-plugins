206cf54df044cb809910669c0f56d804233fb3ef
Josh Betz <j@joshbetz.com>
2020-02-10 15:13:08 -0600

    Clean up PoC code

--- elasticsearch/includes/classes/class-elasticsearch-integration.php
+++ elasticsearch/includes/classes/class-elasticsearch-integration.php
@@ -1,13 +1,31 @@
 <?php
 
-add_filter( 'ep_skip_query_integration', function() {
-	if ( $_GET[ 'es' ] ) {
-		return false;
+namespace Automattic\VIP\Elasticsearch;
+
+class Integration {
+
+	function __construct() {
+		add_filter( 'ep_skip_query_integration', array( __CLASS__, 'ep_skip_query_integration' ) );
 	}
 
-	if ( true === get_option( 'vip_enable_elasticsearch'  ) ) {
-		return false;
-	}
+	/**
+	 * Separate plugin enabled and querying the index
+	 *
+	 * The index can be tested at any time by setting an `es` query argument.
+	 * When we're ready to use the index in production, the `vip_enable_elasticsearch`
+	 * option will be set to `true`, which will enable querying for everyone.
+	 */
+	function ep_skip_query_integration() {
+		if ( isset( $_GET[ 'es' ] ) ) {
+			return false;
+		}
 
-	return true;
-} );
+		if ( true === get_option( 'vip_enable_elasticsearch' ) ) {
+			return true;
+		}
+
+		return true;
+	}
+}
+
+new Integration;
