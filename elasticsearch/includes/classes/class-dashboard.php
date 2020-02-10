7de15bf0d3dd38a992946259a63ad8c0feeacaa0
Josh Betz <j@joshbetz.com>
2020-02-10 15:12:33 -0600

    Clean up PoC code

--- elasticsearch/includes/classes/class-dashboard.php
+++ elasticsearch/includes/classes/class-dashboard.php
@@ -1,5 +1,19 @@
 <?php
 
-add_action( 'admin_init', function() {
-	remove_menu_page( 'elasticpress' );
-} );
+namespace Automattic\VIP\Elasticsearch;
+
+class Dashboard {
+
+	function __construct() {
+		add_action( 'admin_init', array( __CLASS__, 'admin_init' ) );
+	}
+
+	/**
+	 * Disable the Elasticpress dashboard
+	 */
+	function admin_init() {
+		remove_menu_page( 'elasticpress' );
+	}
+}
+
+new Dashboard;
