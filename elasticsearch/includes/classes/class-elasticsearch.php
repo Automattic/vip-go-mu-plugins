316dec32c2f53eb2f0f06397e40497bd94c68a1f
Gabriel Sambarino <gabriel.sambarino@automattic.com>
2020-02-12 10:57:23 +0100

    Load debug bar

--- elasticsearch/includes/classes/class-elasticsearch.php
+++ elasticsearch/includes/classes/class-elasticsearch.php
@@ -25,6 +25,8 @@ class Elasticsearch {
 		}
 		// Load ElasticPress
 		require_once __DIR__ . '/../../elasticpress/elasticpress.php';
+		// Load ElasticPress Debug Bar
+		require_once __DIR__ . '/../../debug-bar-elasticpress/debug-bar-elasticpress.php';
 
 		// Load health check cron job
 		require_once __DIR__ . '/class-health-job.php';
