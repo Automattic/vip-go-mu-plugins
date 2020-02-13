86eb9bbb0f4477179ccec7bfc40aa36b41878746
Josh Betz <j@joshbetz.com>
2020-02-13 11:51:23 -0600

    Allow additional query skipping
    It may be desirable to skip elasticsearch integration for specific
    queries.
    
    Running our filter slightly earlier allows other plugins to override
    our defaults by hooking on the default priority, for example.
    
    Additionally, we're honoring any plugins that opt-in to query skipping
    even if they run earlier than ours.
    
    The net result is that we disable query integration by default and other
    plugins can introduce further cases where the integration is disabled.

--- elasticsearch/includes/classes/class-query-integration.php
+++ elasticsearch/includes/classes/class-query-integration.php
@@ -5,7 +5,7 @@ namespace Automattic\VIP\Elasticsearch;
 class Query_Integration {
 
 	function __construct() {
-		add_filter( 'ep_skip_query_integration', array( __CLASS__, 'ep_skip_query_integration' ) );
+		add_filter( 'ep_skip_query_integration', array( __CLASS__, 'ep_skip_query_integration' ), 5 );
 	}
 
 	/**
@@ -15,9 +15,21 @@ class Query_Integration {
 	 * When we're ready to use the index in production, the `vip_enable_elasticsearch`
 	 * option will be set to `true`, which will enable querying for everyone.
 	 */
-	function ep_skip_query_integration() {
+	function ep_skip_query_integration( $skip ) {
 		if ( isset( $_GET[ 'es' ] ) ) {
 			return false;
+		}
+
+		/**
+		 * Honor filters that skip query integration
+		 *
+		 * It may be desirable to skip query integration for specific
+		 * queries. We should honor those other filters. Since this
+		 * defaults to false, it will only kick in if someone specifically
+		 * wants to bypass ES in addition to what we're doing here.
+		 */
+		if ( $skip ) {
+			return true;
 		}
 
 		return ! ( defined( 'VIP_ENABLE_ELASTICSEARCH_QUERY_INTEGRATION' )
