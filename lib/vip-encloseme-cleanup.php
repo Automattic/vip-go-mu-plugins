78dab83b7f7fb53556ffac17036c496d0277e7c3
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2020-02-21 13:14:55 -0700

    Feedback changes from Mo
    * failsafe DISABLE_VIP_CLEANUP_ENV constant added
    * logging to IRC
    * recurring job

--- lib/vip-encloseme-cleanup.php
+++ lib/vip-encloseme-cleanup.php
@@ -3,16 +3,36 @@
 class VIP_Encloseme_Cleanup {
     const OPTION_NAME = 'vip_encloseme_cleanup';
     const CRON_HOOK = 'vip_encloseme_cleanup_hook';
+    const CRON_INTERVAL = 'vip_cleanup_interval';
 
     public static function init() {
+        add_filter( 'cron_schedules', [ __CLASS__, 'add_cron_schedule' ] );
         add_action( 'init', [ __CLASS__, 'schedule_cleanup' ], 99999 );
         add_action( self::CRON_HOOK, [ __CLASS__, 'cleanup_encloseme_meta' ] );
     }
 
+    public static function add_cron_schedule( $schedule ) {
+        if ( isset( $schedule[ self::CRON_INTERVAL ] ) ) {
+            return $schedule;
+        }
+
+        $schedule[ self::CRON_INTERVAL ] = [
+			'interval' => 1200,
+			'display' => __( 'Once every twenty minutes.' ),
+        ];
+        
+        return $schedule;
+    }
+
     public static function schedule_cleanup() {
         $already_ran = get_option( self::OPTION_NAME );
+
+        if ( $already_ran && wp_next_scheduled( self::CRON_HOOK ) ) {
+            wp_clear_scheduled_hook( self::CRON_HOOK );
+        }
+
         if ( false === $already_ran && ! wp_next_scheduled( self::CRON_HOOK ) ) {
-            wp_schedule_single_event( time() + mt_rand( 15, 30 ), self::CRON_HOOK );
+            wp_schedule_event( time(), self::CRON_INTERVAL, self::CRON_HOOK );
         }
     }
 
@@ -22,13 +42,16 @@ class VIP_Encloseme_Cleanup {
         }
 
         global $wpdb;
-        $find_meta_query = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_encloseme' LIMIT 1"; // We use this instead of count because of the risk of the SQL server going away if there's :alot: of rows. 
+        $find_meta_query = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_encloseme' LIMIT 1"; // We use this instead of count to mitigate the risk of the SQL server going away. 
         $meta_exists = $wpdb->get_var( $find_meta_query ); 
 
         if ( is_null( $meta_exists ) ) {
             update_option( self::OPTION_NAME, time() );
+            wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'No _encloseme meta cleanup needed for %s!', get_site_url() ) );
             return;
         }
+
+        wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'Starting _encloseme meta cleanup for %s.', get_site_url() ) );
 
         do {
             $pids = $wpdb->get_results(
@@ -38,22 +61,22 @@ class VIP_Encloseme_Cleanup {
             $pids = array_map( function( $pid ) {
                 return $pid[0];
             }, $pids );
+            
             foreach( $pids as $pid ) {
                 delete_post_meta( $pid, '_encloseme' );
+                if ( defined( 'DISABLE_VIP_CLEANUP_ENV' ) && true === DISABLE_VIP_CLEANUP_ENV ) {
+                    break 2;
+                }
             }
     
             sleep( 3 );
 
             $meta_exists = $wpdb->get_var( $find_meta_query );
-        } while ( ! is_null( $meta_exists ) );
+        } while ( ! is_null( $meta_exists ) && false === defined( 'DISABLE_VIP_CLEANUP_ENV' ) );
+
+}
         update_option( self::OPTION_NAME, time() );
+
+        wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'Finished _encloseme meta cleanup for %s.', get_site_url() ) );
     }
-}
-
-if ( false === defined( 'VIP_CLEANUP_ENV' ) && defined( 'VIP_GO_ENV' ) && 'preprod' === VIP_GO_ENV ) { // Automatically enable for certain environments.
-    define( 'VIP_CLEANUP_ENV', true );
-}
-
-if ( defined( 'VIP_CLEANUP_ENV' ) && true === VIP_CLEANUP_ENV ) {
-    add_action( 'init', [ 'VIP_Encloseme_Cleanup', 'init' ] );
 }
