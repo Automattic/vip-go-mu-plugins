409caa88b1815abb7bc7fbf8062d2358a5951c95
Loo3 <jyl2171@columbia.edu>
2020-02-28 14:51:45 -0800

    Test format_sms_message() to check that the SMS message is formatted correctly

--- tests/wpcom-vip-two-factor/sms-provider.php
+++ tests/wpcom-vip-two-factor/sms-provider.php
@@ -2,7 +2,10 @@
 
 class Two_Factor_SMS__Test extends \WP_UnitTestCase {
  public function test__formatting() {
-  //test code
+  $token = '123456'
+  $expected = $token . " is your " . $domain . " verification code.\r\n" . "@" . $domain . " #" . $token;
+  $actual = format_sms_message( $token );
+
   $this->assertEquals( $expected, $actual );
  }
 }
