f77b77e0e9fe5b80293e362453c966c391ab48b4
Loo3 <jyl2171@columbia.edu>
2020-03-01 22:05:37 -0800

    Mock the class object to check if generate_and_send_token() was called

--- tests/wpcom-vip-two-factor/sms-provider.php
+++ tests/wpcom-vip-two-factor/sms-provider.php
@@ -1,11 +1,22 @@
 <?php
 
 class Two_Factor_SMS__Test extends \WP_UnitTestCase {
- public function test__formatting() {
-  $token = '123456'
-  $expected = $token . " is your " . $domain . " verification code.\r\n" . "@" . $domain . " #" . $token;
-  $actual = format_sms_message( $token );
+ public function setUp() {
+		require_once __DIR__ . '/../wpcom-vip-two-factor/sms-provider.php';
+	}
 
-  $this->assertEquals( $expected, $actual );
+ public function test__two_factor_sms_formatting() {
+
+  $mock = $this->createMock( Two_Factor_SMS::class )
+						->setMethods( [ 'generate_and_send_token' ] )
+						->getMock();
+
+  $user = new stdClass;
+  $user->name = 'fake-user';
+
+  $mock->expects( $this->exactly( 1 ) )
+       ->method( 'generate_and_send_token' )
+       ->with( $user )
+       ->will( $this->returnValue( null ) );
  }
 }
