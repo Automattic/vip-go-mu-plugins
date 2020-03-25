618a333ca333c83d0eb7dbb188f0ddb7fce6350d
pandah3 <joanne.lee@automattic.com>
2020-03-25 15:06:25 -0700

    Fix tab spacing error
    Co-Authored-By: Mohammad Jangda <mo@automattic.com>

--- 000-debug/0-load.php
+++ 000-debug/0-load.php
@@ -6,7 +6,7 @@ require_once( __DIR__ . '/logger.php' );
 require_once( __DIR__ . '/debug-mode.php' );
 
 function remove_background_updates_test( $tests ) {
- unset( $tests['async']['background_updates'] );
- return $tests;
+	unset( $tests['async']['background_updates'] );
+	return $tests;
 }
 add_filter( 'site_status_tests', 'remove_background_updates_test' );
