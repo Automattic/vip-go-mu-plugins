1b8add0a2e21a1c3407c62c590fb558d6f06dfd0
Nick Daugherty <ndaugherty987@gmail.com>
2020-03-27 14:12:22 -0600

    Initial commit of StatsD library

---
+++ lib/statsd/class-statsd.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace Automattic\VIP;
+
+/**
+ * Sends statistics to the stats daemon over UDP
+ *
+ * Internal use only!
+ */
+
+class StatsD {
+	/**
+	 * Sets one or more timing values
+	 *
+	 * @param string|array $stats The metric(s) to set.
+	 * @param float $time The elapsed time (ms) to log
+	 **/
+	public static function timing( $stats, $time ) {
+		StatsD::updateStats( $stats, $time, 1, 'ms' );
+	}
+
+	/**
+	 * Sets one or more gauges to a value
+	 *
+	 * @param string|array $stats The metric(s) to set.
+	 * @param float $value The value for the stats.
+	 **/
+	public static function gauge( $stats, $value ) {
+		StatsD::updateStats( $stats, $value, 1, 'g' );
+	}
+
+	/**
+	 * A "Set" is a count of unique events.
+	 * This data type acts like a counter, but supports counting
+	 * of unique occurences of values between flushes. The backend
+	 * receives the number of unique events that happened since
+	 * the last flush.
+	 *
+	 * The reference use case involved tracking the number of active
+	 * and logged in users by sending the current userId of a user
+	 * with each request with a key of "uniques" (or similar).
+	 *
+	 * @param string|array $stats The metric(s) to set.
+	 * @param float $value The value for the stats.
+	 **/
+	public static function set( $stats, $value ) {
+		StatsD::updateStats( $stats, $value, 1, 's' );
+	}
+
+	/**
+	 * Increments one or more stats counters
+	 *
+	 * @param string|array $stats The metric(s) to increment.
+	 * @param float|1 $sampleRate the rate (0-1) for sampling.
+	 * @return boolean
+	 **/
+	public static function increment( $stats, $sampleRate = 1 ) {
+		StatsD::updateStats( $stats, 1, $sampleRate, 'c' );
+	}
+
+	/**
+	 * Decrements one or more stats counters.
+	 *
+	 * @param string|array $stats The metric(s) to decrement.
+	 * @param float|1 $sampleRate the rate (0-1) for sampling.
+	 * @return boolean
+	 **/
+	public static function decrement( $stats, $sampleRate = 1 ) {
+		StatsD::updateStats( $stats, -1, $sampleRate, 'c' );
+	}
+
+	/**
+	 * Updates one or more stats.
+	 *
+	 * @param string|array $stats The metric(s) to update. Should be either a string or array of metrics.
+	 * @param int|1 $delta The amount to increment/decrement each metric by.
+	 * @param float|1 $sampleRate the rate (0-1) for sampling.
+	 * @param string|c $metric The metric type ("c" for count, "ms" for timing, "g" for gauge, "s" for set)
+	 * @return boolean
+	 **/
+	public static function updateStats( $stats, $delta = 1, $sampleRate = 1, $metric = 'c' ) {
+		if ( ! is_array( $stats ) ) {
+			$stats = array( $stats ); 
+		}
+
+		$data = array();
+
+		foreach( $stats as $stat ) {
+			$data[ $stat ] = "$delta|$metric";
+		}
+
+		StatsD::send( $data, $sampleRate );
+	}
+
+	/*
+	 * Send the metrics over UDP
+	 **/
+	public static function send( $data, $sampleRate = 1 ) {
+		// Disables StatsD logging for test environments
+		if ( defined( 'VIP_DISABLE_STATSD' ) && VIP_DISABLE_STATSD ) {
+			return;
+		}
+
+		// If we don't have server info defined, abort
+		if ( ! defined( 'VIP_STATSD_HOST' ) || ! VIP_STATSD_HOST || ! defined( 'VIP_STATSD_PORT' ) || ! VIP_STATSD_PORT ) {
+			return;
+		}
+
+		// sampling
+		$sampledData = array();
+
+		if ( $sampleRate < 1 ) {
+			foreach ( $data as $stat => $value ) {
+				if ( ( mt_rand() / mt_getrandmax() ) <= $sampleRate ) {
+					$sampledData[ $stat ] = "$value|@$sampleRate";
+				}
+			}
+		} else {
+			$sampledData = $data;
+		}
+
+		if ( empty( $sampledData ) ) {
+			return;
+		}
+
+		// Wrap this in a try/catch - failures in any of this should be silently ignored
+		try {
+			$host = VIP_STATSD_HOST;
+			$port = VIP_STATDS_PORT;
+
+			$fp = fsockopen( "udp://$host", $port, $errno, $errstr );
+
+			if ( ! $fp ) {
+				return;
+			}
+
+			foreach ( $sampledData as $stat => $value ) {
+				fwrite( $fp, "$stat:$value" );
+			}
+
+			fclose( $fp );
+		} catch (Exception $e) {
+
+		}
+	}
+}
