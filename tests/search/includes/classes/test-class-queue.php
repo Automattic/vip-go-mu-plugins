d7ddb96a9c7010d797ba9b16261a35e7fe5a77f2
Nick Daugherty <ndaugherty987@gmail.com>
2020-04-03 16:58:43 -0600

    Add test for rate limiting

--- tests/search/includes/classes/test-class-queue.php
+++ tests/search/includes/classes/test-class-queue.php
@@ -58,4 +58,48 @@ class Queue_Test extends \WP_UnitTestCase {
 			$this->assertCount( 1, $results );
 		}
 	}
+
+	public function test_rate_limiting_of_repeat_indexing() {
+		global $wpdb;
+
+		$table_name = $this->queue->schema->get_table_name();
+
+		$objects = array(
+			array(
+				'id' => 1,
+				'type' => 'post',
+			),
+			array(
+				'id' => 1,
+				'type' => 'user',
+			),
+		);
+
+		foreach ( $objects as $object ) {
+			$now = time();
+
+			// Insert a job, and set it to running
+			$this->queue->queue_object( $object['id'], $object['type'] );
+			$this->queue->update_job( $object['id'], array( 'status' => 'running' ) );
+			$this->queue->set_last_index_time( $object['id'], $object['type'], $now );
+			
+			// Requeue the job
+			$this->queue->queue_object( $object['id'], $object['type'] );
+
+			// Since it was already running, we should now have a new queued entry with a start_time
+			// that is now() + min interval - this is the rate limit
+
+			$row = $wpdb->get_row( 
+				$wpdb->prepare( 
+					"SELECT `start_time` FROM `{$table_name}` WHERE `object_id` = %d AND `object_type` = %s AND `status` = 'queued'",
+					$object['id'],
+					$object['type']
+				)
+			);
+
+			$expected_start_time = date( 'Y-m-d H:i:s', $now + $this->queue->get_index_interval_time( $object['id'], $object['type'] ) );
+
+			$this->assertEquals( $expected_start_time, $row->start_time );
+		}
+	}
 }
