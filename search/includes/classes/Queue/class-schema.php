57421b567520788444a81a23ab300311ded1987a
Nick Daugherty <ndaugherty987@gmail.com>
2020-04-10 11:39:31 -0600

    Revert "Allow schema create/update on either admin or WP_CLI"
    This reverts commit a5d0cc46a61c11e0e620e95e09a91227c62134da.

--- search/includes/classes/Queue/class-schema.php
+++ search/includes/classes/Queue/class-schema.php
@@ -79,7 +79,7 @@ class Schema {
 	 * Does not include front-end requests
 	 */
 	public function maybe_create_table_on_shutdown() {
-		if ( ! is_admin() || ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
+		if ( ! is_admin() && ! ( defined( 'WP_CLI' ) && WP_CLI ) ) {
 			return;
 		}
 
