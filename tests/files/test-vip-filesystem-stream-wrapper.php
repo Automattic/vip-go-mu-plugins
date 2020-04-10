Automattic/vip-go-mu-plugins

commit d3c995e74e53e4c94f02a4fc0b3fbd2d7868ef94

Fix OOM errors on large video uploads (#1397)
* Add new curl download streamer class

* Switch API client to return a stream

* Fix typo

* Use WP_Http stream options instead

Reduces complexity but not requiring the Curl_Download_Streamer class anymore

* Fix requests args being overwritten in `call_api`

* Fix api client tests

Make sure `mock_http_response()` takes into account requests with `stream` set to `true`

* Remove curl download streamer class

* Revert changes to curl streamer class

* Fix file mode errors

* Update vip uploads class to use updated client

Added a new method to API client that should work with the VIP Uploads class

* Move temp file creation into its own method

Co-authored-by: Nick Daugherty <ndaugherty987@gmail.com>

files/class-api-cache.php
@@ -62,7 +62,7 @@ class API_Cache {
 		$file_name = basename( $filepath );
 
 		if ( isset( $this->files[ $file_name ] ) ) {
-			return file_get_contents( $this->files[ $file_name ] );
+			return $this->files[ $file_name ];
 		}
 
 		return false;
@@ -78,18 +78,10 @@ class API_Cache {
 		return false;
 	}
 
-	public function cache_file( $filepath, $data ) {
+	public function cache_file( $filepath, $local_file ) {
 		$file_name = basename( $filepath );
 
-		if ( ! isset( $this->files[ $file_name ] ) ) {
-			// create file with unique filename
-			$tmp_file = tempnam( $this->tmp_dir, 'vip' );
-
-			$this->files[ $file_name ] = $tmp_file;
-		}
-
-		// This will overwrite existing file if any
-		file_put_contents( $this->files[ $file_name ], $data );
+		$this->files[ $file_name ] = $local_file;
 	}
 
 	public function cache_file_stats( $filepath, $info ) {
@@ -104,7 +96,7 @@ class API_Cache {
 
 		if ( ! isset( $this->files[ $file_name ] ) ) {
 			// create file with unique filename
-			$tmp_file = tempnam( $this->tmp_dir, 'vip' );
+			$tmp_file = $this->create_tmp_file();
 
 			$this->files[ $file_name ] = $tmp_file;
 		}
@@ -130,5 +122,9 @@ class API_Cache {
 
 		// Remove cached stats if any
 		unset( $this->file_stats[ $file_name ] );
+	}
+
+	public function create_tmp_file() {
+		return tempnam( $this->tmp_dir, 'vip' );
 	}
 }

files/class-api-client.php
@@ -68,11 +68,11 @@ class API_Client {
 
 		$timeout = $request_args['timeout'] ?? self::DEFAULT_REQUEST_TIMEOUT;
 
-		$request_args = [
+		$request_args = array_merge( $request_args, [
 			'method' => $method,
 			'headers' => $headers,
 			'timeout' => $timeout,
-		];
+		] );
 
 		$response = wp_remote_request( $request_url, $request_args );
 
@@ -155,13 +155,21 @@ class API_Client {
 
 	public function get_file( $file_path ) {
 		// check in cache first
-		$file_content = $this->cache->get_file( $file_path );
-		if ( $file_content ) {
-			return $file_content;
+		$file = $this->cache->get_file( $file_path );
+		if ( $file ) {
+			return $file;
 		}
 
+		$tmp_file = $this->cache->create_tmp_file();
+
+		// Request args for wp_remote_request()
+		$request_args = [
+			'stream' => true,
+			'filename' => $tmp_file,
+		];
+
 		// not in cache so get from API
-		$response = $this->call_api( $file_path, 'GET' );
+		$response = $this->call_api( $file_path, 'GET', $request_args );
 
 		if ( is_wp_error( $response ) ) {
 			return $response;
@@ -176,12 +184,16 @@ class API_Client {
 			return new WP_Error( 'get_file-failed', sprintf( __( 'Failed to get file `%1$s` (response code: %2$d)' ), $file_path, $response_code ) );
 		}
 
-		$body = wp_remote_retrieve_body( $response );
-
 		// save to cache
-		$this->cache->cache_file( $file_path, $body );
+		$this->cache->cache_file( $file_path, $tmp_file );
 
-		return $body;
+		return $tmp_file;
+	}
+
+	public function get_file_content( $file_path ) {
+		$file = $this->get_file( $file_path );
+
+		return file_get_contents( $file );
 	}
 
 	public function delete_file( $file_path ) {

files/class-vip-filesystem-stream-wrapper.php
@@ -162,11 +162,10 @@ class VIP_Filesystem_Stream_Wrapper {
 				}
 
 				// File doesn't exist on File service so create new file
-				$result = '';
+				$file = $this->string_to_resource( '', $mode );
+			} else {
+				$file = fopen( $result, $mode );
 			}
-
-			// Converts file contents into stream resource
-			$file = $this->string_to_resource( $result, $mode );
 
 			// Get meta data
 			$meta           = stream_get_meta_data( $file );

files/class-wp-filesystem-vip-uploads.php
@@ -57,15 +57,14 @@ class WP_Filesystem_VIP_Uploads extends \WP_Filesystem_Base {
 	public function get_contents( $file ) {
 		$uploads_path = $this->sanitize_uploads_path( $file );
 
-		// TODO: Caching for remote gets? Static single request cache vs memcache?
-		$contents = $this->api->get_file( $uploads_path );
-		if ( is_wp_error( $contents ) ) {
-			$this->errors = $contents;
+		$content = $this->api->get_file_content( $uploads_path );
+		if ( is_wp_error( $content ) ) {
+			$this->errors = $content;
 
 			return false;
 		}
 
-		return $contents;
+		return $content;
 	}
 
 	/**

tests/files/test-api-client.php
@@ -43,6 +43,14 @@ class API_Client_Test extends \WP_UnitTestCase {
 				'args' => $args,
 			];
 
+			if ( $args[ 'stream' ] && 
+				! is_wp_error( $mocked_response ) && 
+				isset( $mocked_response[ 'response' ] ) && 
+				$mocked_response[ 'response' ][ 'code' ] === 200 ) {
+				// Handle streamed requests
+				file_put_contents( $args[ 'filename' ], $mocked_response[ 'body' ] );
+			}
+
 			return $mocked_response;
 		}, 10, 3 );
 	}
@@ -283,7 +291,7 @@ class API_Client_Test extends \WP_UnitTestCase {
 					],
 					'body' => null,
 				],
-				new WP_Error( 'file-not-found', 'The requested file `/wp-content/uploads/file.jpg` does not exist (response code: 404)' ),
+				new WP_Error( 'file-not-found', 'The requested file `/wp-content/uploads/get_file.jpg` does not exist (response code: 404)' ),
 			],
 
 			'other-bad-status' => [
@@ -293,7 +301,7 @@ class API_Client_Test extends \WP_UnitTestCase {
 					],
 					'body' => null,
 				],
-				new WP_Error( 'get_file-failed', 'Failed to get file `/wp-content/uploads/file.jpg` (response code: 500)' ),
+				new WP_Error( 'get_file-failed', 'Failed to get file `/wp-content/uploads/get_file.jpg` (response code: 500)' ),
 			],
 
 			'file-exists' => [
@@ -314,7 +322,14 @@ class API_Client_Test extends \WP_UnitTestCase {
 	public function test__get_file( $mocked_response, $expected_result ) {
 		$this->mock_http_response( $mocked_response );
 
-		$actual_result = $this->api_client->get_file( '/wp-content/uploads/file.jpg' );
+		$file = $this->api_client->get_file( '/wp-content/uploads/get_file.jpg' );
+		
+		if ( is_wp_error( $file ) ) {
+			$actual_result = $file;
+		} else {
+			$actual_result = file_get_contents( $file );
+		}
+
 		$this->assertEquals( $expected_result, $actual_result );
 	}
 

tests/files/test-vip-filesystem-api-cache.php
@@ -78,7 +78,7 @@ class API_Cache_Test extends \WP_UnitTestCase {
 
 		$actual = $this->cache->get_file( 'test.jpg' );
 
-		$this->assertEquals( $expected, $actual );
+		$this->assertEquals( $expected, file_get_contents( $actual ) );
 	}
 
 	public function test__get_file__invalid_file() {
@@ -107,7 +107,9 @@ class API_Cache_Test extends \WP_UnitTestCase {
 	public function test__cache_file() {
 		$prop = self::get_property( $this->cache, 'files' );
 
-		$this->cache->cache_file( 'test.txt', 'test data' );
+		$file = tempnam( sys_get_temp_dir(), 'test' );
+
+		$this->cache->cache_file( 'test.txt', $file );
 
 		$files = $prop->getValue( $this->cache );
 
@@ -124,7 +126,11 @@ class API_Cache_Test extends \WP_UnitTestCase {
 
 		$expected = 'updated data';
 
-		$this->cache->cache_file( 'test.jpg', $expected );
+		$updated_file = tempnam( sys_get_temp_dir(), 'test' );
+
+		file_put_contents( $updated_file, $expected );
+
+		$this->cache->cache_file( 'test.jpg', $updated_file );
 
 		$files = $prop->getValue( $this->cache );
 

tests/files/test-wp-filesystem-vip-uploads.php
@@ -76,7 +76,7 @@ class WP_Filesystem_VIP_Uploads_Test extends \WP_UnitTestCase {
 
 	public function test__get_contents__error() {
 		$this->api_client_mock
-			->method( 'get_file' )
+			->method( 'get_file_content' )
 			->willReturn( new WP_Error( 'oh-no', 'Oh no!' ) );
 
 		$expected_error_code = 'oh-no';
@@ -91,7 +91,7 @@ class WP_Filesystem_VIP_Uploads_Test extends \WP_UnitTestCase {
 
 	public function test__get_contents__success() {
 		$this->api_client_mock
-			->method( 'get_file' )
+			->method( 'get_file_content' )
 			->with( '/wp-content/uploads/file.txt' )
 			->willReturn( 'Hello World!' );
 
@@ -104,7 +104,7 @@ class WP_Filesystem_VIP_Uploads_Test extends \WP_UnitTestCase {
 
 	public function test__get_contents_array__error() {
 		$this->api_client_mock
-			->method( 'get_file' )
+			->method( 'get_file_content' )
 			->willReturn( new WP_Error( 'oh-no', 'Oh no!' ) );
 
 		$actual_contents = $this->filesystem->get_contents_array( 'file.txt' );
@@ -148,7 +148,7 @@ class WP_Filesystem_VIP_Uploads_Test extends \WP_UnitTestCase {
 	 */
 	public function test__get_contents_array__success( $api_response, $expected_contents ) {
 		$this->api_client_mock
-			->method( 'get_file' )
+			->method( 'get_file_content' )
 			->with( '/wp-content/uploads/file.txt' )
 			->willReturn( $api_response );
 

commit 251748901b07830c951d254787e5d3804c6530b8

Add test for Stream Wrapper rename() method
Which caught the regression introduced in #1397

tests/files/test-vip-filesystem-stream-wrapper.php
@@ -1,0 +1,84 @@
+<?php
+
+namespace Automattic\VIP\Files;
+
+class VIP_Filesystem_Stream_Wrapper_Test extends \WP_UnitTestCase {
+	private $stream_wrapper;
+
+	private $api_client_mock;
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once( __DIR__ . '/../../files/class-vip-filesystem-stream-wrapper.php' );
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->api_client_mock = $this->createMock( Api_Client::class );
+
+		$this->stream_wrapper = new VIP_Filesystem_Stream_Wrapper( $this->api_client_mock ); 
+	}
+
+	public function tearDown() {
+		$this->stream_wrapper = null;
+		$this->api_client_mock = null;
+
+		parent::tearDown();
+	}
+
+	/**
+	 * Helper function for accessing protected methods.
+	 */
+	protected static function get_method( $name ) {
+		$class = new \ReflectionClass( __NAMESPACE__ . '\VIP_Filesystem_Stream_Wrapper' );
+		$method = $class->getMethod( $name );
+		$method->setAccessible( true );
+		return $method;
+	}
+
+	public function test__rename__same_path() {
+		$path_from = 'vip://wp-content/uploads/file.txt';
+		$path_to = 'vip://wp-content/uploads/file.txt';
+
+		// We bail early so Api_Client should not be touched. 
+		$this->api_client_mock
+			->expects( $this->never() )
+			->method( $this->anything() );
+
+		$actual_result = $this->stream_wrapper->rename( $path_from, $path_to );
+
+		$this->assertTrue( $actual_result, 'Return value from rename() was not true' );
+	}
+
+	public function test__rename__sucess() {
+		$path_from = 'vip://wp-content/uploads/old.txt';
+		$path_to = 'vip://wp-content/uploads/new.txt';
+
+		$tmp_file = tempnam( sys_get_temp_dir(), 'phpunit' );
+
+		$this->api_client_mock
+			->expects( $this->once() )
+			->method( 'get_file' )
+			->with( 'wp-content/uploads/old.txt' )
+			->willReturn( $tmp_file );
+
+		$this->api_client_mock
+			->expects( $this->once() )
+			->method( 'upload_file' )
+			->with( $tmp_file, 'wp-content/uploads/new.txt' ) 
+			->willReturn( '/wp-content/uploads/new.txt' );
+
+		$this->api_client_mock
+			->expects( $this->once() )
+			->method( 'delete_file' )
+			->with( 'wp-content/uploads/old.txt' ) 
+			->willReturn( true );
+
+		$actual_result = $this->stream_wrapper->rename( $path_from, $path_to );
+
+		$this->assertTrue( $actual_result );
+	}
+
+}