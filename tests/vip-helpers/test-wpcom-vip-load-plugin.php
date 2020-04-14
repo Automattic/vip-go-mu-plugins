e643af45ded90a8f9a862d11f31265ff48a2ada4
Brandon Skinner <brandon.skinner@automattic.com>
2020-04-14 09:28:16 -0600

    Lint fix

--- tests/vip-helpers/test-wpcom-vip-load-plugin.php
+++ tests/vip-helpers/test-wpcom-vip-load-plugin.php
@@ -3,7 +3,7 @@
 namespace Automattic\VIP\Tests;
 
 class WPCOM_VIP_Load_Plugin_Test extends \WP_UnitTestCase {
-	function test__warning_on_late_load() {
+	public function test__warning_on_late_load() {
 		$this->setExpectedIncorrectUsage( 'wpcom_vip_load_plugin' );
 
 		// This should have been fired already but just in case
