812e6cdcc30d4d31366fece23174e5b454ddbafc
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2020-04-22 15:45:44 -0600

    Update option and constant name and remove infinite loop

--- lib/class-vip-encloseme-cleanup.php
+++ lib/class-vip-encloseme-cleanup.php
@@ -5,14 +5,17 @@ if ( ! defined( 'VIP_ENCLOSEME_LIMIT_SIZE' ) ) {
 }
 
 class VIP_Encloseme_Cleanup {
-	const OPTION_NAME = 'vip_encloseme_cleanup';
+	const OPTION_NAME = 'vip_encloseme_cleanup_complete';
 	const CRON_HOOK = 'vip_encloseme_cleanup_hook';
 	const CRON_INTERVAL = 'vip_encloseme_cleanup_interval';
 
 	public static function init() {
-		add_filter( 'cron_schedules', [ __CLASS__, 'add_cron_schedule' ] );
-		add_action( 'init', [ __CLASS__, 'schedule_cleanup' ], 99999 );
-		add_action( self::CRON_HOOK, [ __CLASS__, 'cleanup_encloseme_meta' ] );
+
+		if ( defined( 'ENABLE_VIP_ENCLOSEME_CLEANUP_ENV' ) && true === ENABLE_VIP_ENCLOSEME_CLEANUP_ENV ) {
+			add_filter( 'cron_schedules', [ __CLASS__, 'add_cron_schedule' ] );
+			add_action( 'init', [ __CLASS__, 'schedule_cleanup' ], 99999 );
+			add_action( self::CRON_HOOK, [ __CLASS__, 'cleanup_encloseme_meta' ] );
+		}
 	}
 
 	public static function add_cron_schedule( $schedule ) {
@@ -29,14 +32,14 @@ class VIP_Encloseme_Cleanup {
 	}
 
 	public static function schedule_cleanup() {
-		$already_ran = get_option( self::OPTION_NAME );
+		$completed = get_option( self::OPTION_NAME );
 
-		if ( $already_ran && wp_next_scheduled( self::CRON_HOOK ) ) {
+		if ( $completed && wp_next_scheduled( self::CRON_HOOK ) ) {
 			wp_clear_scheduled_hook( self::CRON_HOOK );
 			return;
 		}
 
-		if ( false === $already_ran && ! wp_next_scheduled( self::CRON_HOOK ) ) {
+		if ( false === $completed && ! wp_next_scheduled( self::CRON_HOOK ) ) {
 			wp_schedule_event( time(), self::CRON_INTERVAL, self::CRON_HOOK );
 		}
 	}
@@ -46,23 +49,23 @@ class VIP_Encloseme_Cleanup {
 			return;
 		}
 
-		if ( defined( 'DISABLE_VIP_CLEANUP_ENV' ) && true === DISABLE_VIP_CLEANUP_ENV ) {
-			return;
-		}
-
 		global $wpdb;
-		$find_meta_query = "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_encloseme' LIMIT 1"; // We use this instead of count to mitigate the risk of the SQL server going away. 
-		$meta_exists = $wpdb->get_var( $find_meta_query ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
+		// We use this query instead of count to mitigate the risk of the SQL server going away for large DBs. 
+		$meta_exists = $wpdb->get_var( "SELECT post_id FROM $wpdb->postmeta WHERE meta_key = '_encloseme' LIMIT 1" ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
 
 		if ( is_null( $meta_exists ) ) {
 			update_option( self::OPTION_NAME, time() );
-			wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'No _encloseme meta cleanup needed for %s!', get_site_url() ) );
+			wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( '_encloseme meta cleanup completed for %s!', get_site_url() ) );
 			return;
 		}
 
 		wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'Starting _encloseme meta cleanup for %s.', get_site_url() ) );
 
-		do {
+		for ( $count = 0; $count < 10; $count++ ) {
+			if ( ! defined( 'ENABLE_VIP_ENCLOSEME_CLEANUP_ENV' ) ) { // Constant not set, bail.
+				break;
+			}
+
 			$pids = $wpdb->get_results(
 				$wpdb->prepare(
 					"SELECT DISTINCT post_id FROM $wpdb->postmeta WHERE meta_key = '_encloseme' LIMIT %d",
@@ -84,11 +87,6 @@ class VIP_Encloseme_Cleanup {
 			}
 	
 			sleep( 3 );
-
-			$meta_exists = $wpdb->get_var( $find_meta_query ); // phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared
-		} while ( ! is_null( $meta_exists ) );
-
-		update_option( self::OPTION_NAME, time() );
-		wpcom_vip_irc( '#vip-go-encloseme-meta-cleanup', sprintf( 'Finished _encloseme meta cleanup for %s.', get_site_url() ) );
+		} 
 	}
 }
