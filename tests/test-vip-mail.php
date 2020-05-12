6f307998058241f0157ae208b4fa07db122732ba
Mohammad Jangda <batmoo@gmail.com>
2020-05-11 23:52:43 -0400

    Add filter to allow VIP SMTP Bypass
    By default, all VIP environments are automatically configured to send
    transactional emails (e.g. password reset, new comment) generated by
    WordPreess using VIP SMTP servers.
    
    For cases, where a third party provider needs to be used for all emails
    from the site, including both marketing and transaction emails, the new
    filter (`vip_smtp_enabled`) can be use to disable loading of the VIP
    SMTP mailer and loading a custom one.

--- tests/test-vip-mail.php
+++ tests/test-vip-mail.php
@@ -6,6 +6,21 @@ class VIP_Mail_Test extends \WP_UnitTestCase {
 		reset_phpmailer_instance();
 	}
 
+	public function test__disable_vip_smtp() {
+		$GLOBALS['all_smtp_servers'] = [ 'server1', 'server2' ];
+
+		add_filter( 'vip_smtp_enabled', '__return_false' );
+
+		wp_mail( 'test@example.com', 'Test', 'Test' );
+		$mailer = tests_retrieve_phpmailer_instance();
+
+		// Expect defaults to be unchanged
+		// phpcs:disable WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$this->assertEquals( 'mail', $mailer->Mailer );
+		$this->assertEquals( 'localhost', $mailer->Host );
+		// phpcs:enable
+	}
+
 	public function test__all_smtp_servers__not_array() {
 		$GLOBALS['all_smtp_servers'] = false;
 
