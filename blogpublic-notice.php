7c0d23abac4f862d3d89c8a1ed706ed4c021b9c7
Kailey Lampert <kailey.lampert@automattic.com>
2020-05-15 14:59:23 -0700

    fix typo

--- blogpublic-notice.php
+++ blogpublic-notice.php
@@ -24,7 +24,7 @@ function notice( $wp_admin_bar ) {
 	}
 
 	printf(
-		'<div id="blogpublic-notice" class="notice notice-warning is-dismissible"><p>Your site may be discoverable by search engines. You can change this in <a href="%s">Reading Setings</a>, under <strong>Search Engine Visibility</strong>.</p></div>',
+		'<div id="blogpublic-notice" class="notice notice-warning is-dismissible"><p>Your site may be discoverable by search engines. You can change this in <a href="%s">Reading Settings</a>, under <strong>Search Engine Visibility</strong>.</p></div>',
 		admin_url('options-reading.php')
 	);
 	add_action( 'admin_footer', __NAMESPACE__ . '\dismiss_handler' );
