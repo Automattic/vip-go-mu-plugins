1428eec32bd79b2c06356a8a797fbc419b2f00eb
Nick Daugherty <ndaugherty987@gmail.com>
2020-07-13 14:54:02 -0600

    Switch to admin_menu hook
    Per docs, menu changes should be done on the admin_menu hook

--- search/includes/classes/class-dashboard.php
+++ search/includes/classes/class-dashboard.php
@@ -5,13 +5,13 @@ namespace Automattic\VIP\Search;
 class Dashboard {
 
 	function __construct() {
-		add_action( 'admin_init', array( __CLASS__, 'admin_init' ) );
+		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
 	}
 
 	/**
 	 * Disable the Elasticpress dashboard
 	 */
-	static function admin_init() {
+	static function admin_menu() {
 		if ( ! is_automattician() ) {
 			remove_menu_page( 'elasticpress' );
 		}
