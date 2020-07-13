4e0a9ef1da8e0a3899fa16c765831230ad7deaa9
Nick Daugherty <ndaugherty987@gmail.com>
2020-07-13 15:01:24 -0600

    Linting

--- search/includes/classes/class-dashboard.php
+++ search/includes/classes/class-dashboard.php
@@ -3,19 +3,18 @@
 namespace Automattic\VIP\Search;
 
 class Dashboard {
-
-	function __construct() {
+	public function __construct() {
 		add_action( 'admin_menu', array( __CLASS__, 'admin_menu' ) );
 	}
 
 	/**
 	 * Disable the Elasticpress dashboard
 	 */
-	static function admin_menu() {
+	public static function admin_menu() {
 		if ( ! is_automattician() ) {
 			remove_menu_page( 'elasticpress' );
 		}
 	}
 }
 
-new Dashboard;
+new Dashboard();
