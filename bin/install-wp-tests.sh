fc19bfaa7526536d10cc43be2d83b44f4dc24b7f
Brandon Skinner <brandon.skinner@automattic.com>
2020-07-27 16:08:57 -0600

    Add nightly builds for CI tests

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -9,7 +9,12 @@ DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 DB_HOST=${4-localhost}
-WP_VERSION=${5-latest}
+
+if [[ $5 == 'nightly' ]]; then
+	WP_VERSION='nightly'
+else
+	WP_VERSION=${5-latest}
+fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 $DIR/download-wp-tests.sh "$DB_NAME" "$DB_USER" "$DB_PASS" "$DB_HOST" "$WP_VERSION"
