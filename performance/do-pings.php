3312a152df38beb5e7b1c630ac039e2c0fa3a6be
Hanif Norman <hanifnorman@gmail.com>
2020-08-12 16:31:10 +0800

    Fix identation

--- performance/do-pings.php
+++ performance/do-pings.php
@@ -19,10 +19,10 @@ add_action( 'schedule_event', __NAMESPACE__ . '\disable_pings' );
 
 // Don't allow new _encloseme metas
 function block_encloseme_metadata_filter( $should_update, $object_id, $meta_key, $meta_value, $unique ) {
-    if ( '_encloseme' === $meta_key ) {
-        $should_update = false;
-    }
+	if ( '_encloseme' === $meta_key ) {
+		$should_update = false;
+	}
 
-    return $should_update;
+	return $should_update;
 }
 add_filter( 'add_post_metadata', __NAMESPACE__ . '\block_encloseme_metadata_filter', 10, 5 );
