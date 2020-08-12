2d6fc297eececa56b86f2feb3563dddd052284b9
Hanif Norman <hanifnorman@gmail.com>
2020-08-12 16:26:35 +0800

    Block new `_encloseme` metas
    Block the addition of new `_encloseme` post metas

--- performance/do-pings.php
+++ performance/do-pings.php
@@ -16,3 +16,13 @@ function disable_pings( $event ) {
 	return $event;
 }
 add_action( 'schedule_event', __NAMESPACE__ . '\disable_pings' );
+
+// Don't allow new _encloseme metas
+function block_encloseme_metadata_filter( $should_update, $object_id, $meta_key, $meta_value, $unique ) {
+    if ( '_encloseme' === $meta_key ) {
+        $should_update = false;
+    }
+
+    return $should_update;
+}
+add_filter( 'add_post_metadata', __NAMESPACE__ . '\block_encloseme_metadata_filter', 10, 5 );
