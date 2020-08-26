1509ea0ec0411049066d3fd1ef00c30ad5fb0c4e
Nick Daugherty <ndaugherty987@gmail.com>
2020-08-26 16:21:25 -0600

    Use a site_id() function, so we can check for FILES_CLIENT_SITE_ID before using it
    Not necessarily defined, such as in custom local dev environments, so can't use it directly...needs to be wrapped

--- lib/feature/class-feature.php
+++ lib/feature/class-feature.php
@@ -5,7 +5,7 @@ namespace Automattic\VIP;
 class Feature {
 	public static $feature_percentages = array();
 
-	public static $site_id = FILES_CLIENT_SITE_ID;
+	public static $site_id = false;
 
 	public static function is_enabled( $feature ) {
 		return static::is_enabled_by_percentage( $feature );
@@ -20,11 +20,19 @@ class Feature {
 
 		// Which bucket is the site in - 100 possibilites, one for each percentage. We run this through crc32 with
 		// the feature name so that the same sites aren't always the canaries
-		$bucket = crc32( $feature . '-' . static::$site_id ) % 100;
+		$bucket = crc32( $feature . '-' . static::site_id() ) % 100;
 
 		// Is the bucket enabled?
 		$threshold = $percentage * 100; // $percentage is decimal
 
 		return $bucket < $threshold; // If our 0-based bucket is inside our threshold, it's enabled
+	}
+
+	public static function site_id() {
+		if ( static::$site_id ) {
+			return static::$site_id;
+		}
+
+		return defined( 'FILES_CLIENT_SITE_ID' ) ? FILES_CLIENT_SITE_ID : 0;
 	}
 }
