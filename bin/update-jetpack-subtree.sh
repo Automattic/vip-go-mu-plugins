a18fb4dd57f0fa7dbd388ca59fc4014c36117566
Suso Bravo-Alvarez <suso@automattic.com>
2020-10-01 15:01:27 +0200

    Add helper scripts to create and update jetpack subtrees

---
+++ bin/update-jetpack-subtree.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# -lt 2 ]; then
+  echo "Syntax: update-jetpack-subtree.sh <tree_version> <jetpack_tag>"
+  echo
+  echo "Example: update-jetpack-subtree.sh 8.9 8.9.2"
+  echo "This will update the subtree jetpack-8.9 to the tag 8.9.2"
+  exit 1
+fi
+
+tree_version=$1
+jetpack_tag=$2
+
+tree_dir="jetpack-${tree_version}"
+if [ ! -d "$tree_dir" ]; then
+  echo "Subtree directory $tree_dir doesn't exist, cannot update it"
+  exit 1
+fi
+
+echo "Updating jetpack subtree $tree_dir to the jetpack tag $jetpack_tag"
+
+git subtree pull --squash -P $tree_dir https://github.com/Automattic/jetpack $jetpack_tag -m "Update jetpack $tree_version subtree to tag $jetpack_tag"
