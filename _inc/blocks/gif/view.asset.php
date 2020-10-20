1da20c7410978c627774cd220158d64cb123f204
Pavel Schoffer <xschof00@gmail.com>
2020-10-20 15:50:33 +0200

    Squashed 'jetpack-9.0/' content from commit 1e4231fe05
    git-subtree-dir: jetpack-9.0
    git-subtree-split: 1e4231fe0542f42a199024f9be87fc8751f64784

---
+++ _inc/blocks/gif/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '03fa8bcb20ecc2004d716cd33ad787b1');
---
+++ _inc/blocks/image-compare/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-dom-ready', 'wp-polyfill'), 'version' => '78567c211d9dd80bfee5fab8efb69122');
