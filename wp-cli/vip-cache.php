914479f88cbf8c0d1bee1de840d1a984766ddc7f
Rinat Khaziev <rinat.khaziev@gmail.com>
2020-10-23 21:57:32 -0500

    Add wp vip cache purge-url subcommand

--- wp-cli/vip-cache.php
+++ wp-cli/vip-cache.php
@@ -2,9 +2,9 @@
 
 class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 	/**
-	 * Purges the site's varnish cache.
+	 * Purges the site's Varnish cache.
 	 */
-	function purge( $args, $assoc_args ) {
+	public function purge( $args, $assoc_args ) {
 		// This sets up the URL/regex to be banned.  There's no need to manually
 		// execute a purge since it is handled automatically on the `shutdown` hook.
 		WPCOM_VIP_Cache_Manager::instance()->purge_site_cache();
@@ -12,6 +12,32 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 			WP_CLI::line( 'Because you are sandboxed this has only purged the Varnish cache on your sandbox host, un-sandbox and use the button in the site admin area if you need to purge the VIP Go edge cache.' );
 		}
 		WP_CLI::success( 'Varnish cache purged!' );
+	}
+
+	/**
+	 * Purges a specific URL from Varnish
+	 *
+	 * ## OPTIONS
+	 *
+	 * <url>
+	 * : URL to purge
+	 *
+	 * ## EXAMPLES
+	 *
+	 * wp vip cache purge-url https://example.com/full-url/?with-query-args
+	 * @subcommand purge-url
+	 */
+	public function purge_url( $args, $assoc_args ) {
+		if ( ! wp_http_validate_url( $args[0] ) ) {
+			WP_CLI::error( 'Invalid URL:' . $args[0] );
+		}
+
+		// Queue a specific URL purge
+		wpcom_vip_purge_edge_cache_for_url( $args[0] );
+		if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED ) {
+			WP_CLI::line( 'Because you are sandboxed this has only purged the Varnish cache on your sandbox host, un-sandbox and use the button in the site admin area if you need to purge the VIP Go edge cache.' );
+		}
+		WP_CLI::success( 'URL purged from Varnish!' );
 	}
 }
 
