1cda27ee98ef2817ca093e1a5b0e04fb89595227
Mohammad Jangda <mo@automattic.com>
2020-10-27 11:37:34 -0400

    Tweak wording for sandbox message
    Co-authored-by: Caleb Burks <caleb.burks@automattic.com>

--- wp-cli/vip-cache.php
+++ wp-cli/vip-cache.php
@@ -35,7 +35,7 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 		// Queue a specific URL purge
 		wpcom_vip_purge_edge_cache_for_url( $args[0] );
 		if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED ) {
-			WP_CLI::line( 'Because you are sandboxed this has only purged the Varnish cache on your sandbox host, un-sandbox and use the button in the site admin area if you need to purge the VIP Go edge cache.' );
+			WP_CLI::line( 'This has only purged the Varnish cache on your sandbox. If you need to purge the production caches, please un-sandbox first and then run this command again..' );
 		}
 		WP_CLI::success( 'URL purged from Varnish!' );
 	}
