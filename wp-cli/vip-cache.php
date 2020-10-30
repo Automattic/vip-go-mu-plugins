d369f8a52ee824d9eedbb7191df2d70ea88b6616
Mohammad Jangda <mo@automattic.com>
2020-10-29 23:57:39 -0400

    Replace "Varnish" with "Page Cache"

--- wp-cli/vip-cache.php
+++ wp-cli/vip-cache.php
@@ -2,20 +2,22 @@
 
 class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 	/**
-	 * Purges the site's Varnish cache.
+	 * Purges the VIP Page Cache.
+	 *
+	 * Warning: on large and high traffic sites, this can severely impact site performance and stability. Use with caution.
 	 */
 	public function purge( $args, $assoc_args ) {
 		// This sets up the URL/regex to be banned.  There's no need to manually
 		// execute a purge since it is handled automatically on the `shutdown` hook.
 		WPCOM_VIP_Cache_Manager::instance()->purge_site_cache();
 		if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED ) {
-			WP_CLI::line( 'Because you are sandboxed this has only purged the Varnish cache on your sandbox host, un-sandbox and use the button in the site admin area if you need to purge the VIP Go edge cache.' );
+			WP_CLI::line( 'Because you are sandboxed this has only purged the Page Cache on your sandbox host, un-sandbox and use the button in the site admin area if you need to purge the VIP Go edge cache.' );
 		}
-		WP_CLI::success( 'Varnish cache purged!' );
+		WP_CLI::success( 'VIP Page Cache purged!' );
 	}
 
 	/**
-	 * Purges a specific URL from Varnish
+	 * Purges a specific URL from the VIP Page Cache
 	 *
 	 * ## OPTIONS
 	 *
@@ -25,6 +27,7 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 	 * ## EXAMPLES
 	 *
 	 * wp vip cache purge-url https://example.com/full-url/?with-query-args
+	 *
 	 * @subcommand purge-url
 	 */
 	public function purge_url( $args, $assoc_args ) {
@@ -35,7 +38,7 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 		// Queue a specific URL purge
 		wpcom_vip_purge_edge_cache_for_url( $args[0] );
 		if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED ) {
-			WP_CLI::line( 'This has only purged the Varnish cache on your sandbox. If you need to purge the production caches, please un-sandbox first and then run this command again..' );
+			WP_CLI::line( 'This has only purged the Page Cache on your sandbox. If you need to purge the production caches, please un-sandbox first and then run this command again..' );
 		}
 		WP_CLI::success( 'URL purged from the VIP page cache.' );
 	}
