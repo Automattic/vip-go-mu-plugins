dca451d98c0d517bfc1e0b5641d20c81a5348291
Pavel Schoffer <xschof00@gmail.com>
2020-11-10 09:26:56 +0100

    Apply suggestions from code review
    Co-authored-by: Gary Jones <gary.jones@automattic.com>

--- admin-notice.php
+++ admin-notice.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Admin Notice
- * Description: Adds dismisable notice in admin area
+ * Description: Adds dismissible notice in admin area
  * Version:     0.1.0
  * Author:      Automattic VIP
  * Author URI:  https://wpvip.com
