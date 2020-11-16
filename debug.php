20c4fbdc6cdbdea394c3ae3b3db505327e89ae9f
Pavel Schoffer <xschof00@gmail.com>
2020-11-16 11:06:59 +0100

    Sets default to test

--- debug.php
+++ debug.php
@@ -15,18 +15,31 @@ $has_test_jquery_constant = defined( 'VIP_ENABLE_TEST_JQUERY_UPDATES' ) && true 
 if ( $has_test_jquery_param || $has_test_jquery_constant ) {
 	$base_folder = __DIR__ . '/debug/test-jquery-updates';
 	$entrypoint_script = 'wp-jquery-update-test.php';
+	$version = 'unkown';
+	$settings_key = 'wp-jquery-test-settings';
+	$default_settings = [];
 
-	$avaiable_options = array_diff( scandir( $base_folder ), [ '.', '..' ] );
-
-	$best_fit = '';
-	foreach ( $avaiable_options as $candidate ) {
-		if ( version_compare( $GLOBALS['wp_version'], $candidate, '>=' ) ) {
-			if ( ! $best_fit || version_compare( $candidate, $best_fit, '>' ) ) {
-				$best_fit = $candidate;
-			}
-		}
+	if ( version_compare( $GLOBALS['wp_version'], '5.6', '<' ) ) {
+		$version = '5.5';
+		$default_settings = [
+			'version'   => '3.5.1',
+			'migrate'   => 'enable',
+			'uiversion' => '1.12.1',
+		];
+	} else {
+		$version = '5.6';
+		$default_settings = [
+			'version'   => '1.12.4',
+			'migrate'   => 'enable',
+		];
 	}
-	require_once( "$base_folder/$best_fit/$entrypoint_script" );
+
+	require_once( "$base_folder/$version/$entrypoint_script" );
+
+	$existing_settings = get_site_option( $settings_key );
+	if ( ! $existing_settings ) {
+		update_site_option( 'wp-jquery-test-settings', $default_settings );
+	}
 }
 
 unset( $has_test_jquery_param, $has_test_jquery_constant );
