3ee21a45c7aa746348e41893493f7344aa8de146
Rinat Khaziev <rinat.khaziev@gmail.com>
2020-11-16 15:51:36 -0600

    CLI: vip cache purge
    1. Introduce a confirmation prompt for purge command and the corresponding --skip-confirm flag
    Silently passthrough to purge-url from purge the argument if a valid URL

--- wp-cli/vip-cache.php
+++ wp-cli/vip-cache.php
@@ -5,8 +5,20 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 	 * Purges the VIP Page Cache.
 	 *
 	 * Warning: on large and high traffic sites, this can severely impact site performance and stability. Use with caution.
+	 *
+	 * ## OPTIONS
+	 *
+	 * [--skip-confirm] force purge without confirmation.
 	 */
 	public function purge( $args, $assoc_args ) {
+		if ( isset( $args[0] ) && wp_http_validate_url( $args[0] ) ) {
+			return $this->purge_url( $args, $assoc_args );
+		}
+
+		if ( ! isset( $assoc_args['skip-confirm'] ) ) {
+			WP_CLI::confirm( "⚠️ You're about to invalidate Page Cache for the whole site, this can severely impact performance and stability for large sites. Are you sure?" );
+		}
+
 		// This sets up the URL/regex to be banned.  There's no need to manually
 		// execute a purge since it is handled automatically on the `shutdown` hook.
 		WPCOM_VIP_Cache_Manager::instance()->purge_site_cache();
@@ -35,7 +47,7 @@ class VIP_Cache_CLI extends WPCOM_VIP_CLI_Command {
 			WP_CLI::error( 'Invalid URL:' . $args[0] );
 		}
 
-		// Queue a specific URL purge
+		// Queue a specific URL purge.
 		wpcom_vip_purge_edge_cache_for_url( $args[0] );
 		if ( defined( 'WPCOM_SANDBOXED' ) && WPCOM_SANDBOXED ) {
 			WP_CLI::line( 'This has only purged the Page Cache on your sandbox. If you need to purge the production caches, please un-sandbox first and then run this command again..' );
