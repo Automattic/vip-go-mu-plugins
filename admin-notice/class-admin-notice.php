09136c5afb221b06383f285846467b305231ef13
Nick Daugherty <ndaugherty987@gmail.com>
2020-11-17 18:07:33 -0700

    Revert "Add admin notice for WP 5.6 RC1"

--- admin-notice/class-admin-notice.php
+++ admin-notice/class-admin-notice.php
@@ -29,15 +29,8 @@ class Admin_Notice {
 			$notice_class .= ' is-dismissible';
 		}
 
-		$allowed_html = array(
-			'a' => array(
-				'href' => array(),
-				'target' => array(),
-			),
-		);
-
 		printf( '<div %s="%s" class="%s">', esc_html( self::DISMISS_DATA_ATTRIBUTE ), esc_attr( $this->dismiss_identifier ), esc_attr( $notice_class ) );
-		printf( '<p>%s</p>', wp_kses( $this->message, $allowed_html ) );
+		printf( '<p>%s</p>', esc_html( $this->message ) );
 		printf( '</div>' );
 	}
 
