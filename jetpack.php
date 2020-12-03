270e880b8219d4071f105b74f9a57c4b3ed7b3a2
Suso Bravo-Alvarez <suso@automattic.com>
2020-12-03 18:33:50 +0100

    Ensure that a proper Jetpack version is loaded depending on WordPress version

--- jetpack.php
+++ jetpack.php
@@ -13,7 +13,11 @@
  */
 
 if ( ! defined( 'VIP_JETPACK_DEFAULT_VERSION' ) ) {
-	define( 'VIP_JETPACK_DEFAULT_VERSION', '9.2' );
+	if ( version_compare( $wp_version, '5.5', '<' ) ) {
+		define( 'VIP_JETPACK_DEFAULT_VERSION', '9.1' );
+	} else {
+		define( 'VIP_JETPACK_DEFAULT_VERSION', '9.2' );
+	}
 }
 
 // Bump up the batch size to reduce the number of queries run to build a Jetpack sitemap.
