4c5d320b2b0d2d3d284528d200507537e658a193
Rinat Khaziev <rinat.khaziev@gmail.com>
2020-12-09 20:05:03 -0500

    Address send_pixel feedback: make keys more specific, don't track vip-go-cache-manager-action and vip-go-cache-manager-url-purge-by-site for unsuccessful requests

--- vip-cache-manager/vip-cache-manager.php
+++ vip-cache-manager/vip-cache-manager.php
@@ -116,18 +116,14 @@ class WPCOM_VIP_Cache_Manager {
 
 		if ( json_last_error() ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-action' => 'user-url-purge',
-				'vip-go-cache-manager-site'   => VIP_GO_APP_ID,
-				'vip-go-cache-manager-status' => 'bad-payload',
+				'vip-go-cache-manager-url-purge-status' => 'bad-payload',
 			] );
 			wp_send_json_error( [ 'error' => 'Malformed payload' ], 400 );
 		}
 
 		if ( ! ( current_user_can( 'manage_options' ) && isset( $req->nonce ) && wp_verify_nonce( $req->nonce, 'purge-page' ) ) ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-action' => 'user-url-purge',
-				'vip-go-cache-manager-site'   => VIP_GO_APP_ID,
-				'vip-go-cache-manager-status' => 'deny',
+				'vip-go-cache-manager-url-purge-status' => 'deny',
 			] );
 
 			wp_send_json_error( [ 'error' => 'Unauthorized' ], 403 );
@@ -142,8 +138,8 @@ class WPCOM_VIP_Cache_Manager {
 
 		\Automattic\VIP\Stats\send_pixel( [
 			'vip-go-cache-manager-action' => 'user-url-purge',
-			'vip-go-cache-manager-site'   => VIP_GO_APP_ID,
-			'vip-go-cache-manager-status' => 'success',
+			'vip-go-cache-manager-url-purge-by-site'   => VIP_GO_APP_ID,
+			'vip-go-cache-manager-url-purge-status' => 'success',
 		] );
 
 		// Optimistically tell that the operation is successful and bail.
