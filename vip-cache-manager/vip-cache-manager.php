1c5e69bfbcfb75de9f61e194d10e73a2025962a2
Rinat Khaziev <rinat.khaziev@gmail.com>
2020-12-21 13:02:27 -0500

    Cache Manager: Remove '-manager' prefix from stats - it makes the key too long, leading to truncation of the stat name

--- vip-cache-manager/vip-cache-manager.php
+++ vip-cache-manager/vip-cache-manager.php
@@ -115,14 +115,14 @@ class WPCOM_VIP_Cache_Manager {
 
 		if ( json_last_error() ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-url-purge-status' => 'bad-payload',
+				'vip-go-cache-url-purge-status' => 'bad-payload',
 			] );
 			wp_send_json_error( [ 'error' => 'Malformed payload' ], 400 );
 		}
 
 		if ( ! current_user_can( 'manage_options' ) ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-url-purge-status' => 'deny-permissions',
+				'vip-go-cache-url-purge-status' => 'deny-permissions',
 			] );
 
 			wp_send_json_error( [ 'error' => 'Unauthorized' ], 403 );
@@ -130,7 +130,7 @@ class WPCOM_VIP_Cache_Manager {
 
 		if ( ! ( isset( $req->nonce ) && wp_verify_nonce( $req->nonce, 'purge-page' ) ) ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-url-purge-status' => 'deny-nonce',
+				'vip-go-cache-url-purge-status' => 'deny-nonce',
 			] );
 
 			wp_send_json_error( [ 'error' => 'Unauthorized' ], 403 );
@@ -140,7 +140,7 @@ class WPCOM_VIP_Cache_Manager {
 
 		if ( empty( $urls ) ) {
 			\Automattic\VIP\Stats\send_pixel( [
-				'vip-go-cache-manager-url-purge-status' => 'deny-no-urls',
+				'vip-go-cache-url-purge-status' => 'deny-no-urls',
 			] );
 
 			wp_send_json_error( [ 'error' => 'No URLs' ], 400 );
@@ -152,9 +152,9 @@ class WPCOM_VIP_Cache_Manager {
 		}
 
 		\Automattic\VIP\Stats\send_pixel( [
-			'vip-go-cache-manager-action' => 'user-url-purge',
-			'vip-go-cache-manager-url-purge-by-site'   => VIP_GO_APP_ID,
-			'vip-go-cache-manager-url-purge-status' => 'success',
+			'vip-go-cache-action' => 'user-url-purge',
+			'vip-go-cache-url-purge-by-site'   => VIP_GO_APP_ID,
+			'vip-go-cache-url-purge-status' => 'success',
 		] );
 
 		// Optimistically tell that the operation is successful and bail.
