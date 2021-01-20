dd7714cf18e61bb6e2cbedb0e8b6294741d6e102
Mohammad Jangda <batmoo@gmail.com>
2021-01-20 12:44:22 -0500

    Add tests for restrict-all-files

---
+++ tests/files/acl/test-restrict-all-files.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Automattic\VIP\Files\Acl\Restrict_All_Files;
+
+use WP_Error;
+
+class VIP_Files_Acl_Restrict_All_Files_Test extends \WP_UnitTestCase {
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once( __DIR__ . '/../../../files/acl.php' );
+		require_once( __DIR__ . '/../../../files/acl/restrict-all-files.php' );
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->original_current_user = get_current_user_id();
+	}
+
+	public function tearDown() {
+		wp_set_current_user( $this->original_current_user );
+
+		parent::tearDown();
+	}
+
+	public function test__check_file_visibility__not_logged_in() {
+		$expected_file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PRIVATE_AND_DENIED;
+
+		$file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PUBLIC;
+		$file_path = '2021/01/kittens.jpg';
+
+		$actual_file_visibility = check_file_visibility( $file_visibility, $file_path );
+
+		$this->assertEquals( $expected_file_visibility, $actual_file_visibility );
+	}
+
+	public function test__check_file_visibility__logged_in_without_permissions() {
+		$expected_file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PRIVATE_AND_DENIED;
+
+		$file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PUBLIC;
+		$file_path = '2021/01/kittens.jpg';
+
+		$test_user_id = $this->factory->user->create();
+		$user = new \WP_User( $test_user_id );
+		$user->remove_role( 'subscriber' );
+		wp_set_current_user( $test_user_id );
+
+		$actual_file_visibility = check_file_visibility( $file_visibility, $file_path );
+
+		$this->assertEquals( $expected_file_visibility, $actual_file_visibility );
+	}
+
+	public function test__check_file_visibility__logged_in_with_permissions() {
+		$expected_file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PRIVATE_AND_ALLOWED;
+
+		$file_visibility = \Automattic\VIP\Files\Acl\FILE_IS_PUBLIC;
+		$file_path = '2021/01/kittens.jpg';
+
+		$test_user_id = $this->factory->user->create( array( 'role' => 'editor' ) );
+		$user = new \WP_User( $test_user_id );
+		wp_set_current_user( $test_user_id );
+
+		$actual_file_visibility = check_file_visibility( $file_visibility, $file_path );
+
+		$this->assertEquals( $expected_file_visibility, $actual_file_visibility );
+	}
+}
+
