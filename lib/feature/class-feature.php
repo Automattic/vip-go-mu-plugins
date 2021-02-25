2f08f796610669a836aef97a196a3e99cb9a94d4
Rinat Khaziev <rinat.khaziev@gmail.com>
2021-02-25 11:25:28 -0600

    oFeature lib updates: - Add usage examples - Add PHPDoc for $feature_percentages - Change $feature_percentages visibitily to protected to avoid unintended usage

--- lib/feature/class-feature.php
+++ lib/feature/class-feature.php
@@ -1,9 +1,25 @@
 <?php
 
 namespace Automattic\VIP;
-
+/**
+ * Feature provides a simple interface to gate the functionality by the Go Site Id
+ * 
+ * To register a feature add it to the $feature_percentages array (per example below)
+ * 
+ * To check whether a feature is enabled: \Automattic\VIP::is_enabled( 'feature-flag' )
+ */
 class Feature {
-	public static $feature_percentages = array();
+	/**
+	 * Holds feature slug and corresponding percentages as a float. E.g.
+	 * // Enable feature for roughly half of the websites
+	 * // 'feature-flag' => 0.5,
+	 *
+	 * @var array
+	 */
+	protected static $feature_percentages = array(
+		// https://github.com/Automattic/vip-go-mu-plugins/tree/master/vip-jetpack/connection-pilot
+		'jetpack-cxn-pilot' => 0.25,
+	);
 
 	public static $site_id = false;
 
