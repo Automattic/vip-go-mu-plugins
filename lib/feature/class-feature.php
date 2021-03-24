da58b9f1cff1e1b104da67b1e74316d0d747983f
Pavel Schoffer <xschof00@gmail.com>
2021-03-24 11:23:28 +0100

    adds feature check

--- lib/feature/class-feature.php
+++ lib/feature/class-feature.php
@@ -20,6 +20,7 @@ class Feature {
 		// https://github.com/Automattic/vip-go-mu-plugins/tree/master/vip-jetpack/connection-pilot
 		'jetpack-cxn-pilot' => 0.25,
 		'search_indexable_settings_auto_heal' => 0,
+		'search_content_validation_and_auto_heal_cron_job' => 0.1,
 	);
 
 	public static $site_id = false;
