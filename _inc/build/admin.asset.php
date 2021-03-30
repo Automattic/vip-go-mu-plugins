f869b94f641f3f664e746d59b2d9abb3ef7e5562
Suso Bravo-Alvarez <suso@automattic.com>
2021-03-30 18:48:35 +0200

    Squashed 'jetpack-9.6/' content from commit a522d5189
    git-subtree-dir: jetpack-9.6
    git-subtree-split: a522d518935a333054cde96a8d04d773267d3bb8

---
+++ _inc/build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'react-dom', 'wp-components', 'wp-date', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'f5481e54b62b529b2b450ad1252af77d');
---
+++ _inc/build/instant-search/jp-search-main.bundle.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '065855dd595ab7cdc6340cd4769e533a');
