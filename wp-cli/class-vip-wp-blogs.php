56cb1247a80d896c3d8dd256115a1de807661df4
Nejc Lovrencic <nejc.lovrencic@gmail.com>
2021-04-01 12:15:49 +0200

    Fix vip-wp-blogs related lint issues

--- wp-cli/class-vip-wp-blogs.php
+++ wp-cli/class-vip-wp-blogs.php
@@ -10,12 +10,12 @@ class VIP_Subsites_CLI_Command extends \WPCOM_VIP_CLI_Command {
 	 * <blog-id>
 	 * : Which blog id we're updating the domain for
 	 *
-     * <domain>
-     * : New domain for subsite
-     *
-     * [--subsite-path=<path>]
-     * : Subsite path, e.g. for subsite www.example.com/subsite1 use subsite1 as path
-     *
+	 * <domain>
+	 * : New domain for subsite
+	 *
+	 * [--subsite-path=<path>]
+	 * : Subsite path, e.g. for subsite www.example.com/subsite1 use subsite1 as path
+	 *
 	 * ## EXAMPLES
 	 *     wp vip subsites update_wp_blogs 2 www.example.com subsite1
 	 *
@@ -23,11 +23,11 @@ class VIP_Subsites_CLI_Command extends \WPCOM_VIP_CLI_Command {
 	 * @sypnosis [--subsite-path=<path>]
 	 */
 	public function update_wp_blogs( $args, $assoc_args ) {
-        global $wpdb;
+		global $wpdb;
 
 		WP_CLI::line( 'Updating wp_blogs...' );
 
-		$path = WP_CLI\Utils\get_flag_value( $assoc_args, 'subsite-path');
+		$path = WP_CLI\Utils\get_flag_value( $assoc_args, 'subsite-path' );
 
 		$blog_id = (int) sanitize_key( $args[0] );
 		$domain = $args[1];
@@ -37,27 +37,27 @@ class VIP_Subsites_CLI_Command extends \WPCOM_VIP_CLI_Command {
 		WP_CLI::line( '* blog ID: ' . $blog_id );
 		WP_CLI::line( '* domain: ' . $domain );
 		WP_CLI::line( '* path: ' . ( $path ? $path : 'N/A' ) );
-        WP_CLI::line( '* new subsite URL: ' . $domain . '/' . $path );
+		WP_CLI::line( '* new subsite URL: ' . $domain . '/' . $path );
 		WP_CLI::line( '' );
 
 		$date_now = new \DateTime( 'now', new \DateTimeZone( 'UTC' ) );
-        $blogs_update = $wpdb->update(
-            'wp_blogs',
-            array(
-                'domain' => $domain,
-                'path' => $path,
-                'last_updated' => $date_now->format('Y-m-d H:i:s')
-            ),
-            array( 'blog_id' => $blog_id )
-        );
+		$blogs_update = $wpdb->update(
+			'wp_blogs',
+			array(
+				'domain' => $domain,
+				'path' => $path,
+				'last_updated' => $date_now->format( 'Y-m-d H:i:s' ),
+			),
+			array( 'blog_id' => $blog_id )
+		);
 
-        if ( $blogs_update ) {
-            WP_CLI::success( 'WP blogs update complete!' );
-        } else if ( $wpdb->last_error ) {
-            WP_CLI::error( $wpdb->last_error );
-        } else {
-            WP_CLI::warning( 'No changes detected' );
-        }
+		if ( $blogs_update ) {
+			WP_CLI::success( 'WP blogs update complete!' );
+		} else if ( $wpdb->last_error ) {
+			WP_CLI::error( $wpdb->last_error );
+		} else {
+			WP_CLI::warning( 'No changes detected' );
+		}
 	}
 }
 
