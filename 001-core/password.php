baed1e78227a90b01c7d9357a29fca8e77d131ae
Pau Argelaguet <pau@outlook.com>
2021-04-08 12:57:54 +0200

    Lintng and adding tests

--- 001-core/password.php
+++ 001-core/password.php
@@ -1,54 +1,61 @@
 <?php
 
-add_action( 'user_profile_update_errors', 'validate_current_password' );
+add_action( 'user_profile_update_errors', 'validate_current_password', 1, 3 );
 
-function validate_current_password(&$errors) {
-    $new_pass = $_POST['pass1'];
-    if (isset($new_pass) && $new_pass) {
-        $current_pass = $_POST['current_pass'];
-        $user = wp_get_current_user();
-        $error = wp_authenticate($user->user_login, $current_pass);
-        if (is_wp_error($error)) {
-            $errors->add('current_pass', "<strong>ERROR</strong>: Current password is not correct.");
-        }
-    }
-    // we are not changing the password
+function validate_current_password( $errors, $update, $user ) {
+	if ( ! isset( $_POST['pass1'] ) || empty( $_POST['pass1'] ) || ! $update ) {
+		return;
+	}
+
+	if ( ! isset( $_POST['current_pass'] ) || empty( $_POST['current_pass'] ) ) {
+		$errors->add( 'wrong_current_password', __( '<strong>ERROR</strong>: Please enter your current password.' ), array( 'form-field' => 'current_pass' ) );
+		return;
+	}
+
+	$error = wp_authenticate( $user->user_login, $_POST['current_pass'] );
+	if ( is_wp_error( $error ) ) {
+		$errors->add( 'wrong_current_password', __( '<strong>ERROR</strong>: The entered current password is not correct.' ), array( 'form-field' => 'current_pass' ) );
+	}
 }
 
-add_action( 'show_user_profile', 'add_current_password_field');
+add_action( 'show_user_profile', 'add_current_password_field' );
 
 function add_current_password_field() { ?>
-    <table id="nojs-current-pass" class="form-table" role="presentation">
-        <tr>
-            <th><label for="current_pass"><?php _e('Current Password') ?></label></th>
-            <td>
-                <div id="current-password-confirm">
-                    <input type="password" name="current_pass" id="current_pass" class="regular-text" value="" autocomplete="off" />
-                    <p class="description">
-                        <?php _e('Please, type your current password if you want to change it for a new one') ?>.
-                    </p>
-                </div>
+	<table id="nojs-current-pass" class="form-table" role="presentation">
+		<tr>
+			<th><label for="current_pass"><?php _e( 'Current Password' ); ?></label></th>
+			<td>
+				<div id="current-password-confirm">
+					<input type="password" name="current_pass" id="current_pass" class="regular-text" value="" autocomplete="off" />
+					<p class="description">
+						<?php _e( 'Please, type your <strong>current password</strong> to change it for a new one' ); ?>.
+					</p>
+				</div>
 
-            </td>
-        </tr>
-    </table>
-<?php }
+			</td>
+		</tr>
+	</table>
+	<?php 
+}
 
 add_action( 'admin_head', 'reposition_current_password_field' );
 
 function reposition_current_password_field() {
-    $screen = get_current_screen();
-    if ($screen->id != "profile") {
-        return;
-    } ?>
+	$screen = get_current_screen();
+	if ( 'profile' != $screen->id ) {
+		return;
+	} 
+	?>
 
-    <script type="text/javascript">
-        (function($) {
-            $(document).ready(function() {
-                $element = $('#current-password-confirm');
-                $element.insertAfter($('#pass-strength-result'));
-                $("#nojs-current-pass").hide();
-            })
-        })(jQuery);
-    </script>
-<?php }
+	<script type="text/javascript">
+		(function($) {
+			$(document).ready(function() {
+				$element = $('#current-password-confirm');
+				$element.insertBefore($('.password-input-wrapper'));
+				$element.css('margin-bottom', '1em');
+				$("#nojs-current-pass").hide();
+			})
+		})(jQuery);
+	</script>
+	<?php 
+}
---
+++ tests/001-core/test-password.php
@@ -0,0 +1,69 @@
+<?php
+
+class Current_Password_Change extends WP_UnitTestCase {
+
+	public static function setUpBeforeClass() {
+		parent::setUpBeforeClass();
+
+		require_once __DIR__ . '/../../001-core/password.php';
+	}
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->factory->user->create( [
+			'user_login' => 'john',
+			'user_email' => 'john@example.com',
+			'user_pass'  => 'secret1',
+		] );
+	}
+
+	public function test__should_return_if_no_pass_update() {
+		$errors = new WP_Error();
+		$user   = get_user_by( 'login', 'john' );
+		do_action_ref_array( 'user_profile_update_errors', array( &$errors, true, $user ) );
+
+		$this->assertFalse( $errors->has_errors() );
+	}
+
+	public function test__should_return_error_if_no_current_pass() {
+		$_POST  = [
+			'pass1' => 'somepassword',
+		];
+		$errors = new WP_Error();
+		$user   = get_user_by( 'login', 'john' );
+		do_action_ref_array( 'user_profile_update_errors', array( &$errors, true, $user ) );
+
+		$expected_error = '<strong>ERROR</strong>: Please enter your current password.';
+
+		$this->assertTrue( $errors->has_errors() );
+		$this->assertEquals( $expected_error, $errors->get_error_message( 0 ) );
+	}
+
+	public function test__should_return_error_if_current_pass_incorrect() {
+		$_POST  = [
+			'pass1'        => 'somepassword',
+			'current_pass' => 'incorrect',
+		];
+		$errors = new WP_Error();
+		$user   = get_user_by( 'login', 'john' );
+		do_action_ref_array( 'user_profile_update_errors', array( &$errors, true, $user ) );
+
+		$expected_error = '<strong>ERROR</strong>: The entered current password is not correct.';
+
+		$this->assertTrue( $errors->has_errors() );
+		$this->assertEquals( $expected_error, $errors->get_error_message( 0 ) );
+	}
+
+	public function test__should_succeed_with_correct_password() {
+		$_POST  = [
+			'pass1'        => 'somepassword',
+			'current_pass' => 'secret1',
+		];
+		$errors = new WP_Error();
+		$user   = get_user_by( 'login', 'john' );
+		do_action_ref_array( 'user_profile_update_errors', array( &$errors, true, $user ) );
+
+		$this->assertFalse( $errors->has_errors() );
+	}
+}
