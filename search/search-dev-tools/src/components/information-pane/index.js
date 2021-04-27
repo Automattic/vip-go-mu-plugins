89782fab230c9c3cfeca94b9f2196ccc373746f1
Rinat Khaziev <rinat.khaziev@gmail.com>
2021-04-26 20:57:56 -0500

    More cleanup/CS and docs

--- search/search-dev-tools/src/components/information-pane/index.js
+++ search/search-dev-tools/src/components/information-pane/index.js
@@ -6,14 +6,15 @@ import cx from 'classnames';
 import { SearchContext } from '../../context';
 import style from './style.scss';
 
+// More than this number of elements in the list will be hidden in the collapsible.
 const EXPAND_THRESHOLD = 2;
 
 /**
  *
- * @returns {Preact.Component} General useful debug info.
+ * @returns {preact.FunctionComponent} General useful debug info.
  */
 export const GeneralInformation = () => {
-	const { status, queries, information } = useContext( SearchContext );
+	const { queries, information } = useContext( SearchContext );
 	return ( <div>
 		<h2 className={`vip-h2 ${ style.query_count }`}>{pluralize( 'query', queries.length, true )} { pluralize( 'was', queries.length ) } run on this page</h2>
 
@@ -27,7 +28,7 @@ export const GeneralInformation = () => {
  * Represents a single collaplsible info block.
  *
  * @param {Object} props
- * @returns {Preact.Component} A collapsible block of information.
+ * @returns {preact.FunctionComponent} A collapsible block of information.
  */
 export const InfoBlock = ( { label, value, options = { collapsible: false } } ) => {
 	const [ collapsed, setCollapsed ] = useState( true );
@@ -42,23 +43,25 @@ export const InfoBlock = ( { label, value, options = { collapsible: false } } ) 
 	const valueLength = value.length;
 	const hasMore = isArrayValue && valueLength > EXPAND_THRESHOLD ? `+ ${ pluralize( 'other', valueLength - EXPAND_THRESHOLD, true ) }` : '';
 
-	// This is getting a bit unwieldy
 	return ( <div className={cx( {
 		[ style.info_block ]: true,
 		[ style.info_block_collapsible ]: options.collapsible || false,
-		[ style.info_block_collapsed ]: collapsed } )}>
-		<h5 className={style.info_label} onClick={ toggleCollapsed }> {label} </h5>
-		{ options.collapsible ? (
-			<Fragment>
-				<div className={cx( { [ style.info_block_inner ]: true } )}>
-					{
-						isArrayValue ? value.map( val => <span key={val} className={style.info_block_item}>{val}</span> ) : <span>{value}</span>
+		[ style.info_block_collapsed ]: collapsed,
+	} )}>
+		<h5 className={style.info_label} onClick={ toggleCollapsed }>{ label }</h5>
+		{ options.collapsible
+			? (
+				<Fragment>
+					<div className={cx( { [ style.info_block_inner ]: true } )}>
+						{
+							isArrayValue ? value.map( val => <span key={val} className={ style.info_block_item }>{ val }</span> ) : <span>{ value }</span>
+						}
+					</div>
+					<span className={ style.info_block_teaser } onClick={ toggleCollapsed }>{
+						isArrayValue ? ` ${ value.slice( 0, EXPAND_THRESHOLD ).join( ', ' ) } ${ hasMore }` : 'Click to show'
 					}
-				</div>
-				<span style="color: var(--vip-brand-60); cursor: pointer;" onClick={toggleCollapsed}>{
-					isArrayValue ? ` ${ value.slice( 0, EXPAND_THRESHOLD ).join( ', ' ) } ${ hasMore }` : 'Click to show'
-				}
-				</span>
-			</Fragment> ) : ( <span>{ isArrayValue ? value.join( ', ' ) : value }</span> ) }
+					</span>
+				</Fragment> )
+			: ( <span>{ isArrayValue ? value.join( ', ' ) : value }</span> ) }
 	</div> );
 };
