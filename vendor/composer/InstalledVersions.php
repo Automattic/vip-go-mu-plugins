da034a0ca39617f160c8b1b6e77f62b52cc3dc63
Pau Argelaguet <pau@outlook.com>
2021-04-29 12:10:43 +0200

    Squashed 'jetpack-9.7/' changes from 762295acd..9600cd807
    9600cd807 Prepare changelog for 9.7 Beta 2
    3837d4528 [not verified] Standardize text prompting to connect/authenticate the user to wordpress.com (#19678)
    
    git-subtree-dir: jetpack-9.7
    git-subtree-split: 9600cd807a0402fefdd20f1456ef43b2aab2293d

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -20,6 +20,8 @@ use Composer\Semver\VersionParser;
 
 
 
+
+
 class InstalledVersions
 {
 private static $installed = array (
@@ -315,7 +317,6 @@ foreach (self::getInstalled() as $installed) {
 $packages[] = array_keys($installed['versions']);
 }
 
-
 if (1 === \count($packages)) {
 return $packages[0];
 }
@@ -507,6 +508,7 @@ public static function reload($data)
 self::$installed = $data;
 self::$installedByVendor = array();
 }
+
 
 
 
