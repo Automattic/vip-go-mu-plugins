c3c90b4312ac5cb779e3140cbb3a4c1f156a78a4
mikeyarce <mikeyarce@gmail.com>
2021-06-01 12:34:44 -0700

    Change hover logic from JS to CSS
    We can use CSS’s `:hover` instead of jQuery.
    `:hover` has good support: https://caniuse.com/mdn-css_selectors_hover

--- vip-plugins/js/plugins-ui.js
+++ vip-plugins/js/plugins-ui.js
@@ -1,6 +1,5 @@
 ( function( $ ) {
 	function init() {
-		bindActions();
 		protectCodeActivatedPlugins();
 	}
 
@@ -10,28 +9,7 @@
 		$row.find( 'input[type="checkbox"]' ).hide();
 	}
 
-	function bindActions() {
-		$( '.plugin' ).hover(
-			function() {
-				var plugin = $( this );
-				pluginHover( plugin );
-			},
-			function() {
-				var plugin = $( this );
-				pluginReset( plugin );
-			}
-		);
-	}
 
-	function pluginHover( plugin ) {
-		plugin.addClass( 'hovered' ),
-		plugin.find( '.fp-overlay' ).addClass( 'visible' );
-	}
-
-	function pluginReset( plugin ) {
-		plugin.removeClass( 'hovered' ),
-		plugin.find( '.fp-overlay' ).removeClass( 'visible' );
-	}
 
 	$( function() {
 		init();
