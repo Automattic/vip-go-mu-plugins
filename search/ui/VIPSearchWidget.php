17d7cfe81641f1f4d95b1225cbe91d8c670dd5ea
Pau Argelaguet <pau@outlook.com>
2021-06-18 16:51:50 +0200

    Adding Widget

---
+++ search/ui/VIPSearchWidget.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Automattic\VIP\Search\UI;
+
+class VIPSearchWidget extends \WP_Widget {
+	function __construct() {
+		parent::__construct(
+			'VIPSearchWidget',
+			'Search (VIP Enterprise)',
+			array( 'description' => 'UI for VIP Enterprise Search', )
+		);
+	}
+
+	// Creating widget front-end
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		// before and after widget arguments are defined by themes
+		echo $args['before_widget'];
+		if ( ! empty( $title ) )
+			echo $args['before_title'] . $title . $args['after_title'];
+
+		// This is where you run the code and display the output
+		echo __( 'Hello, World!', 'wpb_widget_domain' );
+		echo $args['after_widget'];
+	}
+
+	// Widget Backend
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ];
+		}
+		else {
+			$title = __( 'New title', 'wpb_widget_domain' );
+		}
+		// Widget admin form
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
+	}
+
+	// Updating widget replacing old instances with new
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
+	}
+}
