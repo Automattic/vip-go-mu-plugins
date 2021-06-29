75332e29579caaf865bca8a8a6e225d2a23fcdd7
Pau Argelaguet <pau@outlook.com>
2021-06-29 11:29:21 +0200

    Allow to test all files with specific WP version

--- bin/phpunit-docker.sh
+++ bin/phpunit-docker.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
 # Note: you can pass in additional phpunit args
+# Test with explicit WP version and Multisite config: ./bin/phpunit-docker.sh 5.4.4 0
+# Test with explicit WP version and Multisite config and file: ./bin/phpunit-docker.sh tests/path/to/test 5.4.4 0
 # Test a specific file: ./bin/phpunit-docker.sh tests/path/to/test.php
 # Stop on failures: ./bin/phpunit-docker.sh --stop-on-failure
 # etc.
 
-WP_VERSION=${2-latest}
-WP_MULTISITE=${3-0}
+if [ $# -eq 2 ]; then
+	WP_VERSION=${1-latest}
+  WP_MULTISITE=${2-0}
+else
+  PATH_TO_TEST=${1}
+  WP_VERSION=${2-latest}
+  WP_MULTISITE=${3-0}
+fi
 
 echo "--------------"
 echo "Will test with WP_VERSION=$WP_VERSION and WP_MULTISITE=$WP_MULTISITE"
+if [ -z "$PATH_TO_TEST" ]; then
+  echo "Will test ALL FILES"
+else
+  echo "Will test FILE $PATH_TO_TEST"
+fi
 echo "--------------"
 echo
 
@@ -45,4 +58,4 @@ docker run \
 	-v /tmp/wordpress-tests-lib-$WP_VERSION:/tmp/wordpress-tests-lib \
 	-v /tmp/wordpress-$WP_VERSION:/tmp/wordpress \
 	--rm ghcr.io/automattic/phpunit-docker/phpunit:latest \
-	--bootstrap /app/tests/bootstrap.php "$@"
+	--bootstrap /app/tests/bootstrap.php "${PATH_TO_TEST}"
