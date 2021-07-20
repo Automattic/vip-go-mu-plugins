d11ac52dbb6b19893d360ac94614ba6159c7f59b
Jeff Bowen <jblz@users.noreply.github.com>
2021-07-20 16:16:58 -0400

    Pre-set the _apikey_ portion of empty _parsely_ site option to site_url if loaded

--- wp-parsely.php
+++ wp-parsely.php
@@ -29,6 +29,8 @@ function wpvip_load_wp_parsely_plugin() {
 		return;
 	}
 
+	add_filter( 'option_parsely', 'wpvip_override_parsely_option_if_empty' );
+
 	/**
 	 * Allows specifying a major version of the plugin per-site.
 	 * If the version is invalid, the default version will be used.
@@ -46,3 +48,20 @@ function wpvip_load_wp_parsely_plugin() {
 	require 'wp-parsely-' . $major_version . '/wp-parsely.php';
 }
 add_action( 'after_setup_theme', 'wpvip_load_wp_parsely_plugin' );
+
+/**
+ * Pre-sets the "apikey" portion of the `parsely` site option hash map to the site_url IF it's empty.
+ * This is only hooked into `option_parsely` if we're loading the plugin from this repo.
+ *
+ * @param array $parsely_settings Settings from the database
+ * @return array Settings with the 'apikey' value overridden
+ */
+function wpvip_override_parsely_option_if_empty( $parsely_settings ) {
+	if ( ! empty( $parsely_settings ) ) {
+		return $parsely_settings;
+	}
+
+	return [
+		'apikey' => preg_replace( '/^http:\/\//', '', site_url( '', 'http' ) ),
+	];
+}
