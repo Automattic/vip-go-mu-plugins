20fb672ac960d2521c2a14e6803249c511267366
Suso Bravo-Alvarez <suso@automattic.com>
2021-08-05 23:41:44 +0200

    Fix the url for the Site Details service

--- config/class-site-details-index.php
+++ config/class-site-details-index.php
@@ -203,7 +203,7 @@ class Site_Details_Index {
 		$site_details = $this->get_site_details();
 
 		if ( defined( 'SERVICES_API_URL' ) && defined( 'SERVICES_AUTH_TOKEN' ) && ! empty( SERVICES_AUTH_TOKEN ) ) {
-			$url = rtrim( SERVICES_API_URL, '/' ) . '/siteinfo/sites';
+			$url = rtrim( SERVICES_API_URL, '/' ) . '/sitedetails/sites';
 
 			$args = array(
 				'method' => 'PUT',
