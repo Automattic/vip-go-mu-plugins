206843e5b4b33d6b37e3e901598b6cb887c7fb04
Pau Argelaguet <pau@outlook.com>
2021-08-09 16:51:51 +0200

    Moving composer update

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -21,7 +21,4 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 $DIR/download-wp-tests.sh "$DB_NAME" "$DB_USER" "$DB_PASS" "$DB_HOST" "$WP_VERSION"
 
-composer update
-composer install
-
 install_db "$DB_NAME" "$DB_USER" "$DB_PASS" "$DB_HOST"
