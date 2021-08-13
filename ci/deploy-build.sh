fa5ad98b0cb191553780c54dfc2f23630d20c086
Pavel Schoffer <xschof00@gmail.com>
2021-08-13 11:13:21 +0200

    test key

--- ci/deploy-build.sh
+++ ci/deploy-build.sh
@@ -8,70 +8,74 @@ DEPLOY_BUILD_DIR="/tmp/deploy_build/"
 
 # Anyone can read the build log, so it MUST NOT contain any sensitive data,
 # turn off script echoing for a bit
-set +x
+# set +x
 
 # Nuke the existing SSH key
-rm -fv ~/.ssh/id_rsa
+# rm -fv ~/.ssh/id_rsa
 
-# The private portion of the deploy key is in a Travis repository
-# setting called VIP_GITHUB_BUILD_REPO_DEPLOY_KEY. Before pasting
-# it into the setting, I replaced newlines with \n and surrounded
-# it with double quotes, e.g. "KEY\nHERE\n".
-echo -e $VIP_GITHUB_BUILD_REPO_DEPLOY_KEY > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
+# ls -alh ~/.ssh
 
-# # Restore script echoing now we've done the private things
+# The private portion of the deploy key is in a CircleCI env variable
+# VIP_GITHUB_BUILD_REPO_DEPLOY_KEY.
+# echo -e $VIP_GITHUB_BUILD_REPO_DEPLOY_KEY > ~/.ssh/id_rsa
+# chmod 600 ~/.ssh/id_rsa
+
+# Restore script echoing now we've done the private things
 # set -x
 
-# cp ${TRAVIS_BUILD_DIR}/ci/known_hosts ~/.ssh/known_hosts
+# less ~/.ssh/id_rsa
 
-# git clone git@github.com:Automattic/vip-go-mu-plugins-built.git /tmp/target
+cp ./ci/known_hosts ~/.ssh/known_hosts
 
-# mkdir -p ${DEPLOY_BUILD_DIR}
+# git clone  git@github.com:Automattic/vip-go-mu-plugins-built.git /tmp/target
+git clone  --branch pavel_test --single-branch git@github.com:Automattic/vip-go-mu-plugins-built.git /tmp/target
 
-# # Copy the files into the build dir
-# cp -pr ${TRAVIS_BUILD_DIR}/* ${DEPLOY_BUILD_DIR}
-# cp -prv ${TRAVIS_BUILD_DIR}/.[a-zA-Z0-9]* ${DEPLOY_BUILD_DIR}
+mkdir -p ${DEPLOY_BUILD_DIR}
 
-# ls -alh ${TRAVIS_BUILD_DIR}
-# ls -alh ${DEPLOY_BUILD_DIR}
+# Copy the files into the build dir
+cp -pr ./* ${DEPLOY_BUILD_DIR}
+cp -prv ./.[a-zA-Z0-9]* ${DEPLOY_BUILD_DIR}
 
-# # Some of the commands below may fail
-# set +e
+ls -alh .
+ls -alh ${DEPLOY_BUILD_DIR}
 
-# # Remove VCS and CI config
-# find ${DEPLOY_BUILD_DIR} -name ".svn" -exec rm -rfv {} \; 2> /dev/null
-# find ${DEPLOY_BUILD_DIR} -name ".git*" -not -name ".github" -exec rm -rfv {} \; 2> /dev/null
-# find ${DEPLOY_BUILD_DIR} -name ".travis.yml" -exec rm -rfv {} \; 2> /dev/null
+# Some of the commands below may fail
+set +e
 
-# # Remove everything unnecessary to running this (tests, deploy scripts, etc)
-# rm -v ${DEPLOY_BUILD_DIR}/README.md
-# mv -v ${DEPLOY_BUILD_DIR}/README-PUBLIC.md ${DEPLOY_BUILD_DIR}/README.md
+# Remove VCS and CI config
+find ${DEPLOY_BUILD_DIR} -name ".svn" -exec rm -rfv {} \; 2> /dev/null
+find ${DEPLOY_BUILD_DIR} -name ".git*" -not -name ".github" -exec rm -rfv {} \; 2> /dev/null
+find ${DEPLOY_BUILD_DIR} -name ".travis.yml" -exec rm -rfv {} \; 2> /dev/null
 
-# # Update the composer file to the distribution version
-# rm -v ${DEPLOY_BUILD_DIR}/composer.json
-# mv -v ${DEPLOY_BUILD_DIR}/ci/templates/composer.json ${DEPLOY_BUILD_DIR}/composer.json
+# Remove everything unnecessary to running this (tests, deploy scripts, etc)
+rm -v ${DEPLOY_BUILD_DIR}/README.md
+mv -v ${DEPLOY_BUILD_DIR}/README-PUBLIC.md ${DEPLOY_BUILD_DIR}/README.md
 
-# # @FIXME: We will need to replace this ci dir with one which can run tests on the public repo
-# # BUT this public ci dir cannot include our insecure key (and does not need to)
-# rm -rfv ${DEPLOY_BUILD_DIR}/ci/
-# rm -v ${DEPLOY_BUILD_DIR}/.travis.yml
+# Update the composer file to the distribution version
+rm -v ${DEPLOY_BUILD_DIR}/composer.json
+mv -v ${DEPLOY_BUILD_DIR}/ci/templates/composer.json ${DEPLOY_BUILD_DIR}/composer.json
 
-# # We've finished with commands which may fail, so return to exiting the script
-# # if any command exits with a non-zero
-# set -e
+# @FIXME: We will need to replace this ci dir with one which can run tests on the public repo
+# BUT this public ci dir cannot include our insecure key (and does not need to)
+rm -rfv ${DEPLOY_BUILD_DIR}/ci/
+rm -v ${DEPLOY_BUILD_DIR}/.travis.yml
 
-# # Copy the Git config into place
-# mv /tmp/target/.git ${DEPLOY_BUILD_DIR}
+# We've finished with commands which may fail, so return to exiting the script
+# if any command exits with a non-zero
+set -e
 
-# cd ${DEPLOY_BUILD_DIR}
+# Copy the Git config into place
+mv /tmp/target/.git ${DEPLOY_BUILD_DIR}
 
-# git config user.name "Travis CI"
-# git config user.email "travis@travis-ci.com"
-# git config push.default "current"
+cd ${DEPLOY_BUILD_DIR}
 
-# git add -A .
-# # If you get an SSH prompt to enter a passphrase, you likely encrypted then
-# # key against the wrong repository
-# git commit -am "Built from ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}"
+git config user.name "Travis CI"
+git config user.email "travis@travis-ci.com"
+git config push.default "current"
+
+git add -A .
+# If you get an SSH prompt to enter a passphrase, you likely encrypted then
+# key against the wrong repository
+git commit -am "Built from ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}"
 # git push
+git status
