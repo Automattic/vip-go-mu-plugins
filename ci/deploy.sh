3155dfced478a28a8483803a8613c4f02bc2d4b8
Pavel Schoffer <xschof00@gmail.com>
2021-08-13 11:27:11 +0200

    deploy works

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -4,47 +4,17 @@
 
 set -ex
 
-if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
-	echo "Not deploying pull requests."
-	exit
-fi
-
-if [[ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]]; then
-	echo "Not on the '${DEPLOY_BRANCH}' branch."
-	exit
-fi
-
 DEPLOY_BUILD_DIR="/tmp/deploy_build/"
 
-# Anyone can read the build log, so it MUST NOT contain any sensitive data,
-# turn off script echoing for a bit
-set +x
+cp ./ci/known_hosts ~/.ssh/known_hosts
 
-# Nuke the existing SSH key
-rm -fv ~/.ssh/id_rsa
-
-# The private portion of the deploy key is in a Travis repository
-# setting called VIP_GITHUB_BUILD_REPO_DEPLOY_KEY. Before pasting
-# it into the setting, I replaced newlines with \n and surrounded
-# it with double quotes, e.g. "KEY\nHERE\n".
-echo -e $VIP_GITHUB_BUILD_REPO_DEPLOY_KEY > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-
-# Restore script echoing now we've done the private things
-set -x
-
-cp ${TRAVIS_BUILD_DIR}/ci/known_hosts ~/.ssh/known_hosts
-
-git clone git@github.com:Automattic/vip-go-mu-plugins-built.git /tmp/target
+git clone  git@github.com:Automattic/vip-go-mu-plugins-built.git /tmp/target
 
 mkdir -p ${DEPLOY_BUILD_DIR}
 
 # Copy the files into the build dir
-cp -pr ${TRAVIS_BUILD_DIR}/* ${DEPLOY_BUILD_DIR}
-cp -prv ${TRAVIS_BUILD_DIR}/.[a-zA-Z0-9]* ${DEPLOY_BUILD_DIR}
-
-ls -alh ${TRAVIS_BUILD_DIR}
-ls -alh ${DEPLOY_BUILD_DIR}
+cp -pr ./* ${DEPLOY_BUILD_DIR}
+cp -prv ./.[a-zA-Z0-9]* ${DEPLOY_BUILD_DIR}
 
 # Some of the commands below may fail
 set +e
