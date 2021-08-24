016780db48f1b8ebc9827b7e6de1ea59d9bf8c52
Pau Argelaguet <pau@outlook.com>
2021-08-24 07:15:05 +0200

    Removing commented code

--- search/search.php
+++ search/search.php
@@ -35,11 +35,5 @@ if ( \Automattic\VIP\Search\Search::are_es_constants_defined() ) {
 		add_filter( 'jetpack_search_should_handle_query', '__return_false', PHP_INT_MAX );
 	}
 
-	// Temporarily disabling
-	// add_action( 'widgets_init', function () {
-	//	require_once __DIR__ . '/ui/class-vip-search-widget.php';
-	//	register_widget( 'Automattic\VIP\Search\UI\VIP_Search_Widget' );
-	// } );
-
 	do_action( 'vip_search_loaded' );
 }
