31d81eb442ea2b4178373af59fbe17c058283b25
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2021-09-08 21:09:53 +0300

    Use lint-staged to run php linting in a pre-commit hook

--- bin/php-lint.sh
+++ bin/php-lint.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/bin/sh
 
-find . -name \*.php -not -path "*/vendor/*" -print0 | xargs -0 -n 1 -P 4 php -d display_errors=stderr -l > /dev/null
+npx phplint '**/*.php' '!vendor/**' '!node_modules/**' > /dev/null
