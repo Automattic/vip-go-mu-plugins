00461d588c0151e813eaf2b7d440140759dca986
Pau Argelaguet <pau@outlook.com>
2021-09-23 11:26:26 +0200

    Checking for hook

--- tests/parsely/test-mu-parsely-integration.php
+++ tests/parsely/test-mu-parsely-integration.php
@@ -66,6 +66,7 @@ class MU_Parsely_Integration_Test extends \WP_UnitTestCase {
 		$this->assertSame( $expected, has_action( 'admin_menu', array( $parsely, 'add_settings_sub_menu' ) ) );
 		$this->assertSame( $expected, has_action( 'admin_footer', array( $parsely, 'display_admin_warning' ) ) );
 		$this->assertSame( $expected, has_action( 'widgets_init', 'parsely_recommended_widget_register' ) );
+		$this->assertSame( $expected, has_action( 'option_parsely', 'Automattic\VIP\WP_Parsely_Integration\alter_option_use_repeated_metas' ) );
 
 		/*
 			TODO: put these in when landing the quick links
