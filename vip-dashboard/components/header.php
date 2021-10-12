79d9bfc9c8de7a97b8c910b36af57a3962a95cf6
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2021-10-12 23:41:39 +0300

    VIP Dashboard: fix code standards compliance issues

--- vip-dashboard/components/header.php
+++ vip-dashboard/components/header.php
@@ -7,7 +7,7 @@ function render_vip_dashboard_header() {
 		<h1>
 			<a href="https://wpvip.com" target="_blank">
 				<img src="<?php echo esc_url( plugin_dir_url( __FILE__ ) . '../assets/img/wpcom-vip-logo.svg' ); ?>"
-					 alt="WordPress VIP" class="top-header__logo"/>
+					alt="WordPress VIP" class="top-header__logo"/>
 			</a>
 		</h1>
 	</div>
