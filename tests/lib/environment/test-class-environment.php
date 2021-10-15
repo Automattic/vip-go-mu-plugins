7eb30549f95a9ae0e65a2027c0eb7136a08d4078
jesse-greathouse <jesse.greathouse@gmail.com>
2021-10-15 10:55:34 -0600

    Add Environment::get_var implementation
    * Adds method Automattic\VIP\Environment::get_var
    * First, checks to see if the VIP_ENV_VAR constant exists and returns it
    * If constant doesn't exist it falls back to const without the prefix
    * If neither constant exists it returns the $default_value parameter

--- tests/lib/environment/test-class-environment.php
+++ tests/lib/environment/test-class-environment.php
@@ -6,7 +6,58 @@ class Environment_Test extends \PHPUnit_Framework_TestCase {
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
 
-		require_once( __DIR__ . '/../../../lib/environment/class-environment.php' );
+		require_once __DIR__ . '/../../../lib/environment/class-environment.php';
+	}
+
+	public function get_var_standard_env() {
+		define( 'VIP_ENV_VAR_MY_VAR', 'VIP_ENV_VAR_MY_VAR' );
+	}
+
+	public function get_var_legacy_env() {
+		define( 'MY_VAR', 'MY_VAR' );
+	}
+
+	// tests the use-case where $key parameter is not found
+	public function test_get_default_var() {
+		\PHPUnit_Framework_Error_Notice::$enabled = false;
+
+		$val = Environment::get_var( 'MY_VAR', 'default_value' );
+		$this->assertEquals( $val, 'default_value' );
+	}
+
+	// tests the use-case where $key parameter does not have the prefix
+	public function test_get_var_legacy_key() {
+		\PHPUnit_Framework_Error_Notice::$enabled = false;
+
+		$this->get_var_legacy_env();
+		$val = Environment::get_var( 'MY_VAR', 'default_value' );
+		$this->assertEquals( $val, 'MY_VAR' );
+	}
+
+	// tests the use-case where $key parameter is lower case
+	public function test_get_var_lower_key() {
+		\PHPUnit_Framework_Error_Notice::$enabled = false;
+
+		$this->get_var_standard_env();
+		$val = Environment::get_var( 'vip_env_var_my_var', 'default_value' );
+		$this->assertEquals( $val, 'VIP_ENV_VAR_MY_VAR' );
+	}
+
+	// tests the use-case where $key parameter is ''
+	public function test_get_var_empty_key() {
+		\PHPUnit_Framework_Error_Notice::$enabled = false;
+
+		$this->get_var_standard_env();
+		$val = Environment::get_var( '', 'default_value' );
+		$this->assertEquals( $val, 'default_value' );
+	}
+
+	public function test_get_var() {
+		\PHPUnit_Framework_Error_Notice::$enabled = false;
+
+		$this->get_var_standard_env();
+		$val = Environment::get_var( 'MY_VAR', 'default_value' );
+		$this->assertEquals( $val, 'VIP_ENV_VAR_MY_VAR' );
 	}
 
 	public function is_sandbox_container_data() {
