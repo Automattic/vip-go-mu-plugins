a5c916d62a9fc4341d4c4e247d8b4e3657de1785
Jesse Greathouse <46167019+jesse-greathouse@users.noreply.github.com>
2021-10-20 10:36:25 -0600

    Update/jetpack 10.2.0 to jetpack 10.2.1 (#2526)
    * Squashed 'jetpack-10.2/' changes from efcf1067b..67f83ab5c
    
    67f83ab5c Pin down connection-ui package
    6ad40e116 Update packages to latest stable for release
    5b65b0ea5 Update version numbers for 10.2.1 release
    563ce4642 Update changelog for point release
    36e9e0b90 [not verified] Connection: fixing the bug with connect button not appearing for secondary users (#21450)
    71a4a3bf8 [not verified] Connection: fix connection screen for non-admins (#21425)
    3cbe16d29 [not verified] VideoPress : Fix semicolon displayed on the rendered page (#21431)
    f3940c652 [not verified] Identity Crisis: move initialization to the Config package (#21287)
    089b23206 [not verified] Identity Crisis: Move the check for an idc to the identity-crisis package (#21267)
    5bdb6b447 [not verified] Jetpack Sync :: Reduce overflow to Sync Queue (#21296)
    9a3ed498b [not verified] Use distinct query for Terms and Term Relationship tables. (#21398)
    2c33243d3 [not verified] Search: fix translations for the new search customizer and lazy loaded payload (#21367)
    5e28c393c [not verified] Support unparsed apiFetch responses for VideoPress uploads (#21406)
    fe6615462 [not verified] Convert text to latin1 vs utf8 when performing checksums, as this is the charset used on WP.com (#21350)
    736af827e [not verified] Videopress display error messages in Media library and Gutenberg block (#21253)
    ed7a85b44 [not verified] Explicitly ignore locale if same as site setting (#21305)
    
    git-subtree-dir: jetpack-10.2
    git-subtree-split: 67f83ab5c65bd47ef40ed5108ad5cae323b3f564
    
    * Update Jetpack from 10.2.0 to 10.2.1
    
    * corrected jetpack default version
    
    Co-authored-by: jesse-greathouse <jesse.greathouse@gmail.com>
    Co-authored-by: Pau Argelaguet <pauarge@users.noreply.github.com>

--- jetpack-10.2/vendor/jetpack-autoloader/class-hook-manager.php
+++ jetpack-10.2/vendor/jetpack-autoloader/class-hook-manager.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ10_2;
+namespace Automattic\Jetpack\Autoloader\jpf11009ded9fc4592b6a05b61ce272b3c_jetpackⓥ10_2_1;
 
  // phpcs:ignore
 
