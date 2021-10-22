c3f42392fb6ebfc372b56308c6d1708a88e6dc28
Pau Argelaguet <pau@outlook.com>
2021-10-22 18:25:32 +0200

    Fixing linting issues

--- tests/vip-parsely/Telemetry/Tracks/test-class-tracks.php
+++ tests/vip-parsely/Telemetry/Tracks/test-class-tracks.php
@@ -11,19 +11,19 @@ require_once __DIR__ . '/../../../../vip-parsely/Telemetry/Tracks/class-tracks.p
 
 class Tracks_Test extends WP_UnitTestCase {
 	public function test__should_normalize_invalid_name() {
-		$normalize_event_name = self::get_method('normalize_event_name');
+		$normalize_event_name = self::get_method( 'normalize_event_name' );
 
 		$tracks = new Tracks();
 		$normalized_name = $normalize_event_name->invokeArgs( $tracks, array( 'invalid' ) );
-		$this->assertEquals( 'wpparsely_invalid', $normalized_name);
+		$this->assertEquals( 'wpparsely_invalid', $normalized_name );
 	}
 
 	public function test__should_not_normalize_valid_name() {
-		$normalize_event_name = self::get_method('normalize_event_name');
+		$normalize_event_name = self::get_method( 'normalize_event_name' );
 
 		$tracks = new Tracks();
 		$normalized_name = $normalize_event_name->invokeArgs( $tracks, array( 'wpparsely_valid' ) );
-		$this->assertEquals( 'wpparsely_valid', $normalized_name);
+		$this->assertEquals( 'wpparsely_valid', $normalized_name );
 	}
 
 	/**
