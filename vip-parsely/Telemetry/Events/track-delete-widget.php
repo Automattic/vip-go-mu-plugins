79637f7f234db3b843bd553af139d1a51790220c
Pau Argelaguet <pau@outlook.com>
2021-10-27 16:45:11 +0200

    Adding phpdoc

--- vip-parsely/Telemetry/Events/track-delete-widget.php
+++ vip-parsely/Telemetry/Events/track-delete-widget.php
@@ -17,9 +17,18 @@ namespace Automattic\VIP\Parsely\Telemetry;
  */
 const WP_PARSELY_RECOMMENDED_WIDGET_BASE_ID = 'parsely_recommended_widget';
 
-function track_delete_widget( $widget_id, $sidebar_id, $id_base, Telemetry_System $telemetry_system ): void {
+/**
+ * Records an event using the given Telemetry System whenever a `parsely_recommended_widget` instance is deleted.
+ *
+ * @param string $widget_id ID of the widget marked for deletion.
+ * @param string $sidebar_id ID of the sidebar the widget was deleted from.
+ * @param string $id_base ID base for the widget.
+ * @param Telemetry_System $telemetry_system
+ * @return void
+ */
+function track_delete_widget( string $widget_id, string $sidebar_id, string $id_base, Telemetry_System $telemetry_system ): void {
 	if ( WP_PARSELY_RECOMMENDED_WIDGET_BASE_ID !== $id_base ) {
 		return;
 	}
-	$telemetry_system->record_event( 'vip_wpparsely_delete_widget', compact( 'id_base' ) );
+	$telemetry_system->record_event( 'wpparsely_delete_widget', compact( 'id_base' ) );
 }
