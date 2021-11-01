279656d8f51099b248358af7f253f86941092002
jesse-greathouse <jesse.greathouse@gmail.com>
2021-11-01 13:17:41 -0600

    Requested Changes and code quality fixes   * Fixed typo in the word: pertaining in lib/helpers/environment.php   * Added require_once to lib/helpers/environment.php   * Added missing signature arguments to function in lib/helpers/environment.php   * Changed env var values in testing to make a clear distiction between values and variable names

--- lib/helpers/environment.php
+++ lib/helpers/environment.php
@@ -1,12 +1,17 @@
 <?php
 /**
- * Helper functions pretaining to the environment domain
+ * Helper functions pertaining to the environment domain
 */
+
+require_once __DIR__ . '/../environment/class-environment.php';
 
 use Automattic\VIP\Environment;
 
 /**
  * A wrapper for Automattic\VIP\Environment::get_var
+ *
+ * @param string $key The name of the environment variable.
+ * @param string $default_value The value to return if no environment variable matches the key 
  */
 function vip_get_env_var( $key, $default_value = '' ) {
 	return Environment::get_var( $key, $default_value );
--- tests/test-core.php
+++ tests/test-core.php
@@ -2,84 +2,10 @@
 use Yoast\PHPUnitPolyfills\Polyfills\ExpectPHPException;
 
 // phpcs:disable PEAR.NamingConventions.ValidClassName.Invalid
-// phpcs:disable WordPress.PHP.DiscouragedPHPFunctions.runtime_configuration_error_reporting
 
 class VIP_Go__Core__Disable_Update_Caps_Test extends WP_UnitTestCase {
 	public function setUp(): void {
 		wpcom_vip_init_core_restrictions();
-		$this->error_reporting = error_reporting();
-	}
-
-	public function tearDown(): void {
-		error_reporting( $this->error_reporting );
-		parent::tearDown();
-	}
-
-	public function get_var_standard_env() {
-		define( 'VIP_ENV_VAR_MY_VAR', 'VIP_ENV_VAR_MY_VAR' );
-	}
-
-	public function get_var_legacy_env() {
-		define( 'MY_VAR', 'MY_VAR' );
-	}
-
-	// tests the use-case where $key parameter is not found
-	public function test_get_default_var() {
-		error_reporting( $this->error_reporting & ~E_USER_NOTICE );
-
-		$val = vip_get_env_var( 'MY_VAR', 'default_value' );
-		$this->assertEquals( 'default_value', $val );
-	}
-
-	/**
-	 * tests the use-case where $key parameter does not have the prefix
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
-	public function test_get_var_legacy_key() {
-		error_reporting( $this->error_reporting & ~E_USER_NOTICE );
-
-		$this->get_var_legacy_env();
-		$val = vip_get_env_var( 'MY_VAR', 'default_value' );
-		$this->assertEquals( 'MY_VAR', $val );
-	}
-
-	/**
-	 * tests the use-case where $key parameter is lower case
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
-	public function test_get_var_lower_key() {
-		error_reporting( $this->error_reporting & ~E_USER_NOTICE );
-
-		$this->get_var_standard_env();
-		$val = vip_get_env_var( 'vip_env_var_my_var', 'default_value' );
-		$this->assertEquals( 'VIP_ENV_VAR_MY_VAR', $val );
-	}
-
-	/**
-	 * tests the use-case where $key parameter is ''
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
-	public function test_get_var_empty_key() {
-		error_reporting( $this->error_reporting & ~E_USER_NOTICE );
-
-		$this->get_var_standard_env();
-		$val = vip_get_env_var( '', 'default_value' );
-		$this->assertEquals( 'default_value', $val );
-	}
-
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
-	public function test_get_var() {
-		error_reporting( $this->error_reporting & ~E_USER_NOTICE );
-
-		$this->get_var_standard_env();
-		$val = vip_get_env_var( 'MY_VAR', 'default_value' );
-		$this->assertEquals( 'VIP_ENV_VAR_MY_VAR', $val );
 	}
 
 	public function test__super_admin_should_not_have_update_core_cap() {
