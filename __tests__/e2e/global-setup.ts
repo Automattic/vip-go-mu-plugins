5b67ffee0aff17aa047b0d19ff728e0f222791f2
Brent Sessions <brent.sessions@automattic.com>
2021-11-19 08:52:04 -0500

    Update setup

--- __tests__/e2e/global-setup.ts
+++ __tests__/e2e/global-setup.ts
@@ -9,8 +9,10 @@ async function globalSetup( config: FullConfig ) {
   // Log in to wp-admin
   await page.goto( baseURL + '/wp-login.php' );
   const loginPage = new LoginPage( page );
-  await page.screenshot( { path: 'screenshot.png' } );
-  await loginPage.login( 'e2e_tester', 'aut0matedTe$ter' );
+  await Promise.all( [
+    page.waitForNavigation(),
+    loginPage.login( 'e2e_tester', 'aut0matedTe$ter' )
+  ] );
 
   // Save signed-in state
   await page.context().storageState( { path: storageState as string } );
