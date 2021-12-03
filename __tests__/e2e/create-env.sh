a1975cf1d78661e76280cf99f987c83da1c76fe9
Brent Sessions <brent.sessions@automattic.com>
2021-12-03 09:29:42 -0500

    fixing some lines

--- __tests__/e2e/create-env.sh
+++ __tests__/e2e/create-env.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-git submodule init
-git submodule update
-npm ci
-npm install --prefix=$HOME/.local -g @automattic/vip
 echo $'\t' | vip --slug=e2e-test-site dev-env create --title="E2E Testing site" --phpmyadmin --mu-plugins="./" --wordpress="5.8.1" --multisite=false
+vip --slug=e2e-test-site dev-env start
