aadcc2f9596789f146249ca52d6e78ca5975478b
Brent Sessions <brent.sessions@automattic.com>
2021-12-03 09:44:14 -0500

    use skeleton

--- __tests__/e2e/create-env.sh
+++ __tests__/e2e/create-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $'\t' | vip --slug=e2e-test-site dev-env create --title="E2E Testing site" --phpmyadmin --mu-plugins="./" --wordpress="5.8.1" --multisite=false
+vip --slug=e2e-test-site dev-env create --title="E2E Testing site" --phpmyadmin --mu-plugins="/code" --wordpress="5.8.1" --multisite=false --client-code="/vip-go-skeleton"
 vip --slug=e2e-test-site dev-env start
