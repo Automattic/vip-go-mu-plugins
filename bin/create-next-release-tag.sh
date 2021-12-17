6b28d3c4fd5d91a413366fd1c686aaa350a4473e
Pavel Schoffer <pavel.schoffer@automattic.com>
2021-12-17 11:28:13 +0100

    testingOutputs

---
+++ bin/create-next-release-tag.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+current_date=$(date '+v%Y%m%d.')
+minor_version=0
+
+while [ $minor_version -le 8 ]
+do
+  tag="$current_date$minor_version"
+  if git rev-parse "$tag" >/dev/null 2>&1
+  then
+    if [ $minor_version -ge 8 ]
+    then
+      echo "Could not create a release. Please attempt it manually."
+      exit
+    fi
+    minor_version=$(( minor_version + 1 ))
+  else
+    break
+  fi
+done
+
+echo $tag
