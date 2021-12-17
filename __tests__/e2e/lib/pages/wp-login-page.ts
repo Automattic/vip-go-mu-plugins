ed800d00d9965fb8aa08c8a36e5fedee0521be7f
Brent Sessions <31110506+bsessions85@users.noreply.github.com>
2021-12-17 11:56:06 -0500

    e2e tests: Initial framework, tests, and CI integration (#2682)
    * Add e2e tests
    
    * Update setup
    
    * tweaks for stability
    
    * update package-lock
    
    * Setup docker for CircleCI
    
    * Fix CI issues
    
    * try root user
    
    * try getting startup script to run
    
    * try moving script
    
    * try setting user
    
    * another try
    
    * Try copying when in CI
    
    * disable caching
    
    * change script permissions
    
    * trying to restart
    
    * try changing owner to get script to run
    
    * another try
    
    * wasn't paying attention
    
    * change permissions after copy
    
    * try different docker version... volume mounting is putting in wrong directory
    
    * need a supported version
    
    * Change the way we copy scripts and mount volumes
    
    * update submodules and store artifacts
    
    * forgot to update submodules
    
    * fix yml
    
    * Try using dev env
    
    * fix config
    
    * install vip globally
    
    * Get global install working
    
    * remove stray slash
    
    * Try different images and user
    
    * Try another way
    
    * copy to container then exec
    
    * try all in separate container
    
    * use different image
    
    * keep container running
    
    * fixing some lines
    
    * use skeleton
    
    * fix paths
    
    * try using script and separate container to run tests
    
    * troubleshooting
    
    * update timeout
    
    * fix creation URL and copy back artifacts
    
    * fix artifacts
    
    * Update to get tests passing
    
    * trying GH actions
    
    * move config
    
    * fix job name
    
    * missed node setup
    
    * fix yml and working dir
    
    * fix var
    
    * fix paths
    
    * fix checkout
    
    * figuring out paths
    
    * update url
    
    * Add classic editor tests and edit tests, update gh actions inputs
    
    * fix workflow
    
    * install classic editor plugin
    
    * bad paste
    
    * try on 4 workers
    
    * try single test and new reporter
    
    * get the artifacts
    
    * trying to get it to recognize the welcome
    
    * Making more configurable.
    
    * Update how notice is dismissed
    
    * missed an await
    
    * change back dismiss
    
    * Trying another way to dismiss the welcome tour
    
    * Specify worker count
    
    * making some adjustments
    
    * try longer timeout
    
    * try waiting for page to settle
    
    * Make the page wait
    
    * debugging
    
    * change order of editor setup
    
    * Accounting for missing classic editor
    
    * revert circle config
    
    * Code cleanup
    
    * update package lock
    
    * Add some gh action configuration options
    
    * Add readme and update variables
    
    * Clean up readme
    
    * Clean up readme, and force linting
    
    * had to use breaks for spacing and fix linting
    
    * Clean up files
    
    * more cleanup
    
    * add wait
    
    Co-authored-by: Volodymyr Kolesnykov <sjinks@users.noreply.github.com>

---
+++ __tests__/e2e/lib/pages/wp-login-page.ts
@@ -0,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { Page } from '@playwright/test';
+
+const selectors = {
+    userField: '#user_login',
+    passwordField: '#user_pass',
+    submitButton: '#wp-submit',
+};
+
+export class LoginPage {
+    private page: Page;
+
+    /**
+     *  Constructs an instance of the component.
+     *
+     * @param { Page } page The underlying page
+     */
+    constructor( page: Page ) {
+        this.page = page;
+    }
+
+    /**
+     * Navigate to login page
+     *
+     */
+    async visit(): Promise<void> {
+        await this.page.goto( '/wp-login.php' );
+    }
+
+    /**
+     * Logs in to account with specified username and password
+     *
+     * @param {string} username Username to login as
+     * @param {string} password Password for account
+     */
+    async login( username: string, password: string ): Promise<void> {
+        await this.page.fill( selectors.userField, username );
+        await this.page.fill( selectors.passwordField, password );
+        await Promise.all( [ this.page.waitForNavigation(), this.page.click( selectors.submitButton ) ] );
+    }
+}
