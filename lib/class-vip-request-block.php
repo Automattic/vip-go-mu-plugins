5f29dc54f613768d50290cd487c4796340888744
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2021-12-27 15:18:17 +0200

    Make the class more testable and rewrite tests

--- lib/class-vip-request-block.php
+++ lib/class-vip-request-block.php
@@ -23,20 +23,23 @@ class VIP_Request_Block {
 	 * 🛑 ALWAYS: use `whois {IP}` to look up the IP before making the changes.
 	 *
 	 * @param string $value target IP address to be blocked.
+	 * @return bool|void
 	 */
 	public static function ip( string $value ) {
 		// Don't try to block if the passed value is not a valid IP.
 		if ( ! filter_var( $value, FILTER_VALIDATE_IP ) ) {
-			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-			error_log( 'VIP Request Block: The value passed is not a correct IP address: ' . $value );
-			return;
+			if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
+				// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+				error_log( 'VIP Request Block: The value passed is not a correct IP address: ' . $value );
+			}
+
+			return false;
 		}
 
 		// This is explicit because we only want to try x-forwarded-for if the true-client-ip is not set.
 		// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
 		if ( isset( $_SERVER['HTTP_TRUE_CLIENT_IP'] ) && $value === $_SERVER['HTTP_TRUE_CLIENT_IP'] ) {
-			self::block_and_log( $value, 'true-client-ip' );
-			return;
+			return self::block_and_log( $value, 'true-client-ip' );
 		}
 
 		// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
@@ -44,22 +47,26 @@ class VIP_Request_Block {
 			// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 			$ips = array_map( 'trim', explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) );
 			if ( in_array( $value, $ips, true ) ) {
-				self::block_and_log( $value, 'x-forwarded-for' );
+				return self::block_and_log( $value, 'x-forwarded-for' );
 			}
 		}
+
+		return false;
 	}
 
 	/**
 	 * Block by exact match of the user agent header
 	 *
 	 * @param string $user_agent target user agent to be blocked.
-	 * @return void
+	 * @return void|bool
 	 */
 	public static function ua( string $user_agent ) {
 		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPressVIPMinimum.Variables.RestrictedVariables.cache_constraints___SERVER__HTTP_USER_AGENT__
 		if ( $user_agent === $_SERVER['HTTP_USER_AGENT'] ) {
-			self::block_and_log( $user_agent, 'user-agent' );
+			return self::block_and_log( $user_agent, 'user-agent' );
 		}
+
+		return false;
 	}
 
 	/**
@@ -67,7 +74,7 @@ class VIP_Request_Block {
 	 *
 	 * @param string $header HTTP header.
 	 * @param string $value header value.
-	 * @return void
+	 * @return void|bool
 	 */
 	public static function header( string $header, string $value ) {
 		// Normalize the header to what PHP exposes in $_SERVER:
@@ -76,8 +83,10 @@ class VIP_Request_Block {
 		$key = sprintf( 'HTTP_%s', str_ireplace( 'HTTP_', '', $key ) );
 
 		if ( isset( $_SERVER[ $key ] ) && $value === $_SERVER[ $key ] ) {
-			self::block_and_log( $value, $header );
+			return self::block_and_log( $value, $header );
 		}
+
+		return false;
 	}
 
 	/**
@@ -85,16 +94,20 @@ class VIP_Request_Block {
 	 *
 	 * @param string $value value of the header for a block.
 	 * @param string $criteria header field used for a block.
-	 * @return void
+	 * @return true|void
 	 */
 	public static function block_and_log( string $value, string $criteria ) {
-		http_response_code( 403 );
-		header( 'Expires: Wed, 11 Jan 1984 05:00:00 GMT' );
-		header( 'Cache-Control: no-cache, must-revalidate, max-age=0' );
+		if ( ! defined( 'WP_TESTS_DOMAIN' ) ) {
+			http_response_code( 403 );
+			header( 'Expires: Wed, 11 Jan 1984 05:00:00 GMT' );
+			header( 'Cache-Control: no-cache, must-revalidate, max-age=0' );
 
-		fastcgi_finish_request();
-		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-		error_log( sprintf( 'VIP Request Block: request was blocked based on "%s" with value of "%s"', $criteria, $value ) );
-		exit;
+			fastcgi_finish_request();
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			error_log( sprintf( 'VIP Request Block: request was blocked based on "%s" with value of "%s"', $criteria, $value ) );
+			exit;
+		}
+
+		return true;
 	}
 }
