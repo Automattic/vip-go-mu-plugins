2c289452b3b154efb9ef1e5c7dba1c78d6603667
jesse-greathouse <jessegreathouse@gmail.com>
2022-01-05 12:22:21 -0700

    Added Jetpack 10.5-beta subtree Added backward compatibility logic for dealing with jetpack 10.5

--- jetpack.php
+++ jetpack.php
@@ -22,6 +22,8 @@ if ( ! defined( 'VIP_JETPACK_DEFAULT_VERSION' ) ) {
 		define( 'VIP_JETPACK_DEFAULT_VERSION', '9.4' );
 	} elseif ( version_compare( $wp_version, '5.7', '<' ) ) {
 		define( 'VIP_JETPACK_DEFAULT_VERSION', '9.8' );
+	} elseif ( version_compare( $wp_version, '5.8', '<' ) ) {
+		define( 'VIP_JETPACK_DEFAULT_VERSION', '10.4' );
 	} else {
 		define( 'VIP_JETPACK_DEFAULT_VERSION', '10.4' );
 	}
