4301ffdc7c938bd21e8b8d50954d3b1b359f1ec7
Pavel Schoffer <pavel.schoffer@automattic.com>
2022-01-10 07:51:29 +0100

    fix indend

--- admin-notice/admin-notice.php
+++ admin-notice/admin-notice.php
@@ -26,3 +26,19 @@ add_action(
 		do_action( 'vip_admin_notice_init', $admin_notice_controller );
 	}
 );
+
+add_action(
+	'vip_admin_notice_init',
+	function( $admin_notice_controller ) {
+		$admin_notice_controller->add(
+			new Admin_Notice(
+				'WordPress 5.9 is scheduled to be released on Tuesday, January 25th',
+				[
+					new Date_Condition( '2022-01-01', '2022-02-01' ),
+					new WP_Version_Condition( '5.8', '5.9' ),
+				],
+				'wp-5.9-test'
+			)
+		);
+	}
+);
