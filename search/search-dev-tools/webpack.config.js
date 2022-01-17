90881530c7f3cb60979c85ed0ab55543f4c80c5e
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-01-17 15:09:49 +0200

    Bump mini-css-extract-plugin to 2.5.1

--- search/search-dev-tools/webpack.config.js
+++ search/search-dev-tools/webpack.config.js
@@ -1,6 +1,6 @@
 const { resolve, join } = require( 'path' );
 const webpack = require( 'webpack' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' ).default;
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const CssMinimizerPlugin = require( 'css-minimizer-webpack-plugin' );
 const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
