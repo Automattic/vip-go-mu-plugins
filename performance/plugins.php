fed5754f4cf21d63674b3a3b66693c6a94027527
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-01-28 14:32:33 -0700

    Performance: Add CAP as default filter
    https://lobby.vip.wordpress.com/2017/11/07/co-authors-plus-global-filter/

--- performance/plugins.php
+++ performance/plugins.php
@@ -9,3 +9,13 @@ namespace Automattic\VIP\Performance;
 add_action( 'amp_extract_image_dimensions_callbacks_registered', function() {
 	remove_filter( 'amp_extract_image_dimensions', array( 'AMP_Image_Dimension_Extractor', 'extract_from_attachment_metadata' ) );
 } );
+
+// CAP: By default, co-authors will query in a sort of compatibility mode.
+// Making the SQL queries work for posts with the old author style (as in the built-in post author
+// or by using the co-authors taxonomy.
+add_action( 'plugins_loaded', __NAMESPACE__ . '\vip_coauthors_plus_should_query_post_author' );
+function vip_coauthors_plus_should_query_post_author() {
+	if ( class_exists( 'CoAuthors_Plus' ) ) {
+		add_filter( 'coauthors_plus_should_query_post_author', '__return_false' );
+	}
+}
