ef2a0d5ee457576d4ed5e5babaabba13a492c6b4
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-01-31 08:52:56 -0700

    Use Docblock and rewrite description

--- performance/plugins.php
+++ performance/plugins.php
@@ -10,12 +10,19 @@ add_action( 'amp_extract_image_dimensions_callbacks_registered', function() {
 	remove_filter( 'amp_extract_image_dimensions', array( 'AMP_Image_Dimension_Extractor', 'extract_from_attachment_metadata' ) );
 } );
 
-// CAP: By default, co-authors will query in a sort of compatibility mode.
-// Making the SQL queries work for posts with the old author style (as in the built-in post author
-// or by using the co-authors taxonomy.
-add_action( 'plugins_loaded', __NAMESPACE__ . '\vip_coauthors_plus_should_query_post_author' );
+/**
+ * By default, the Co-Authors Plus plugin will query in a compatibility mode where it uses an expensive JOIN for post_author in it.
+ * This filter disables it and uses a simpler, taxonomy-only query.
+ * Run `wp co-authors-plus create-terms-for-posts` to ensure all authors have an author taxonomy attached to them.
+ * 
+ * @see https://lobby.vip.wordpress.com/2017/11/07/co-authors-plus-global-filter/
+ * 
+ * @return void
+ */
 function vip_coauthors_plus_should_query_post_author() {
 	if ( class_exists( 'CoAuthors_Plus' ) ) {
 		add_filter( 'coauthors_plus_should_query_post_author', '__return_false' );
 	}
 }
+
+add_action( 'plugins_loaded', __NAMESPACE__ . '\vip_coauthors_plus_should_query_post_author' );
