ade9b1797ec692978476a1bc551dd3f7ef1c435d
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-02-09 12:26:10 -0700

    Feature: Add enabling or disabling by ID (#2887)
    * Add is_enabled_by_ids and is_disabled_by_ids to rollout feature
    
    * Add tests
    
    * Use constant directly and for tests, use Constant_Mocker
    
    * Change format of $feature_ids
    
    Co-authored-by: Rinat K <rinat.khaziev@automattic.com>

--- lib/feature/class-feature.php
+++ lib/feature/class-feature.php
@@ -19,12 +19,46 @@ class Feature {
 	 */
 	public static $feature_percentages = [];
 
-	public static $site_id = false;
+	/**
+	 * Holds feature slug and then, key of ids with bool value to enable E.g.
+	 * // Enable feature for sites 123 and 345 and disable 567
+	 * // 'feature-flag' => [ 123 => true, 345 => true, 567 => false ],
+	 *
+	 * @var array
+	 */
+	public static $feature_ids = [];
 
 	public static function is_enabled( $feature ) {
 		return static::is_enabled_by_percentage( $feature );
 	}
 
+	/**
+	 * Selectively enable or disable feature by certain IDs.
+	 * 
+	 * @param string $feature The feature we are targeting.
+	 * @param mixed $default Default return value if ID is not on list.
+	 * 
+	 * @return mixed Returns bool if on list and if not, $default value.  
+	 */
+	public static function is_enabled_by_ids( $feature, $default = false ) {
+		if ( ! isset( static::$feature_ids[ $feature ] ) ) {
+			return false;
+		}
+
+		if ( array_key_exists( constant( 'FILES_CLIENT_SITE_ID' ), static::$feature_ids[ $feature ] ) ) {
+			return static::$feature_ids[ $feature ][ constant( 'FILES_CLIENT_SITE_ID' ) ];
+		}
+
+		return $default;
+	}
+
+	/**
+	 * Roll out based on percentage.
+	 * 
+	 * @param string $feature The feature we are targeting.
+	 * 
+	 * @return bool Whether it is enabled or not. 
+	 */
 	public static function is_enabled_by_percentage( $feature ) {
 		if ( ! isset( static::$feature_percentages[ $feature ] ) ) {
 			return false;
@@ -34,20 +68,11 @@ class Feature {
 
 		// Which bucket is the site in - 100 possibilites, one for each percentage. We run this through crc32 with
 		// the feature name so that the same sites aren't always the canaries
-		$bucket = crc32( $feature . '-' . static::site_id() ) % 100;
+		$bucket = crc32( $feature . '-' . constant( 'FILES_CLIENT_SITE_ID' ) ) % 100;
 
 		// Is the bucket enabled?
 		$threshold = $percentage * 100; // $percentage is decimal
 
 		return $bucket < $threshold; // If our 0-based bucket is inside our threshold, it's enabled
-	}
-
-	public static function site_id() {
-		// This is used in tests, so the site id can be easily changed
-		if ( static::$site_id ) {
-			return static::$site_id;
-		}
-
-		return defined( 'FILES_CLIENT_SITE_ID' ) ? FILES_CLIENT_SITE_ID : 0;
 	}
 }
