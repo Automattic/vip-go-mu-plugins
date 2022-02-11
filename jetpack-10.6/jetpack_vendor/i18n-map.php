ace8283010e0d89ce61a9860a936cb4ed1174674
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-02-11 10:36:15 -0700

    Upgrade Jetpack 10.6 to 10.6.1 (#2896)

--- jetpack-10.6/jetpack_vendor/i18n-map.php
+++ jetpack-10.6/jetpack_vendor/i18n-map.php
@@ -28,13 +28,13 @@ return array(
       'path' => 'jetpack_vendor/automattic/jetpack-identity-crisis',
       'ver' => '0.6.4',
     ),
-    'jetpack-sync' => array(
-      'path' => 'jetpack_vendor/automattic/jetpack-sync',
-      'ver' => '1.29.0',
-    ),
     'jetpack-backup-pkg' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-backup',
       'ver' => '1.2.3',
+    ),
+    'jetpack-sync' => array(
+      'path' => 'jetpack_vendor/automattic/jetpack-sync',
+      'ver' => '1.29.0',
     ),
     'jetpack-config' => array(
       'path' => 'jetpack_vendor/automattic/jetpack-config',
