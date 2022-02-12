ae301935e73abf05ac612f14d920f74db5263b52
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-02-12 17:49:05 +0200

    Only update CREATE TABLE if the index exists and is correct

--- schema.php
+++ schema.php
@@ -17,16 +17,19 @@ add_filter( 'dbdelta_create_queries', function( $queries ) {
 			$search = 'KEY meta_key (meta_key(191))';
 			if ( false !== strpos( $q, $search ) ) {
 				$status = _get_table_status( $wpdb->postmeta );
-				// For InnoDB, Data_length is the approximate amount of space allocated for the clustered index, in bytes.
-				// Each InnoDB table has a special index called the clustered index that stores row data.
-				if ( defined( 'WP_TESTS_DOMAIN' ) || ( isset( $status['Data_length'] ) && $status['Data_length'] < 52428800 ) ) {
-					$queries[ $k ] = str_replace(
-						$search,
-						sprintf( 'KEY %s', get_postmeta_key_value_index() ),
-						$q
-					);
+				/* If the table does not exists, simply replace the index definition */
+				if ( null === $status ) {
+					$queries[ $k ] = str_replace( $search, get_postmeta_key_value_index( 'meta_key' ), $q );
 				} else {
-					trigger_error( sprintf( 'Skip meta_key index modification because Data_length is %s', esc_html( $status['Data_length'] ?? 'N/A' ) ), E_USER_WARNING );
+					/* The table exists, we need to check the index */
+					$indices = _get_table_indices( $wpdb->postmeta );
+					if ( isset( $indices['meta_key'] ) ) {
+						/* If meta_key index covers both meta_key and meta_value, modify meta_key index definition to match */
+						if ( get_postmeta_key_value_index( 'meta_key' ) === $indices['meta_key'] ) { // NOSONAR
+							$queries[ $k ] = str_replace( $search, get_postmeta_key_value_index( 'meta_key' ), $q );
+						}
+						/* Otherwise, do nothing, rely upon a cron job to fix this */
+					}
 				}
 			}
 		}
@@ -35,15 +38,56 @@ add_filter( 'dbdelta_create_queries', function( $queries ) {
 	return $queries;
 });
 
-function _get_table_status( string $table ): array {
+function _get_table_status( string $table ): ?array {
 	global $wpdb;
 
 	// phpcs:ignore WordPress.DB.DirectDatabaseQuery
-	return $wpdb->get_results( $wpdb->prepare( 'SHOW TABLE STATUS WHERE Name = %s', $table ), ARRAY_A );
+	return $wpdb->get_row( $wpdb->prepare( 'SHOW TABLE STATUS WHERE Name = %s', $table ), ARRAY_A );
 }
 
-function get_postmeta_key_value_index() {
+function _get_table_indices( string $table ): array {
+	global $wpdb;
+	return _parse_indices( $wpdb->get_results( "SHOW INDEX FROM {$table}", ARRAY_A ) ); // phpcs:ignore WordPress.DB
+}
+
+function _parse_indices( array $rows ): array {
+	$result = [];
+	$flags  = [];
+	$keys   = [];
+	foreach ( $rows as $row ) {
+		$is_primary  = 'PRIMARY' === $row['Key_name'];
+		$is_unique   = ! $is_primary && '0' === $row['Non_unique'];
+		$is_fulltext = 'FULLTEXT' === $row['Index_type'];
+		$index_name  = $row['Key_name'];
+		$column      = $row['Column_name'];
+		$sequence    = (int) $row['Seq_in_index'];
+		$prefix      = null === $row['Sub_part'] ? null : (int) $row['Sub_part'];
+
+		if ( isset( $keys[ $index_name ] ) ) {
+			$keys[ $index_name ][ $sequence ] = sprintf( '`%s`%s', $column, null === $prefix ? '' : "({$prefix})" );
+		} else {
+			$keys[ $index_name ] = [ 
+				$sequence => sprintf( '`%s`%s', $column, null === $prefix ? '' : "({$prefix})" ),
+			];
+
+			$flags[ $index_name ] = $is_primary ? 'PRIMARY KEY' : ( $is_unique ? 'UNIQUE KEY' : ( $is_fulltext ? 'FULLTEXT KEY' : 'KEY' ) ); // NOSONAR
+		}
+	}
+
+	foreach ( $keys as $name => $columns ) {
+		$columns = join( ', ', $columns );
+		if ( 'PRIMARY' === $name ) {
+			$result[ $name ] = 'PRIMARY KEY  (' . $columns . ')';
+		} else {
+			$result[ $name ] = sprintf( '%s `%s` (%s)', $flags[ $name ], $name, $columns );
+		}
+	}
+
+	return $result;
+}
+
+function get_postmeta_key_value_index( string $index_name ) {
 	// 191 for meta_key is max set by core.
 	// 100 for meta_value is arbitrary-ish.
-	return '`vip_meta_key_value` (`meta_key`(191), `meta_value`(100))';
+	return sprintf( 'KEY `%s` (`meta_key`(191), `meta_value`(100))', $index_name );
 }
