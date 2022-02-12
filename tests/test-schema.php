90f1876a1edadbebeb42e887a13bd298b9e95383
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-02-12 17:57:58 +0200

    Fix tests

--- tests/test-schema.php
+++ tests/test-schema.php
@@ -8,8 +8,7 @@ class VIP_Go_Schema_Test extends WP_UnitTestCase {
 	public function test__dbDelta__verify_blog_tables() {
 		$deltas = dbDelta( 'blog', false ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.dbDelta_dbdelta
 		
-		$this->assertCount( 1, $deltas, 'More deltas than expected for blogs table' );
-		$this->assertEquals( $deltas[0], 'Added index wptests_postmeta KEY `vip_meta_key_value` (`meta_key`(191),`meta_value`(100))', 'Delta did not find vip_meta_key_value index or assertion needs updating.' );
+		$this->assertCount( 0, $deltas );
 	}
 
 	public function test__get_table_indices(): void {
