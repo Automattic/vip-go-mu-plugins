6c6ee3c4ba873ccdaa11654f8cc92752f49d6e35
Derrick Tennant <emrikol@users.noreply.github.com>
2022-02-14 16:03:31 -0500

    Do not use Jetpack Photon when Jetpack disabled (#2895)
    When a site is disabling Jetpack according to [our documentation](https://docs.wpvip.com/technical-references/restricting-site-access/controlling-content-distribution-via-jetpack/#h-preventing-jetpack-from-loading), it will cause `A8C_Files::init_jetpack_photon_filters()` to continually throw the warning `Cannot initialize Photon filters as the Jetpack_Photon class is not loaded. Please verify that Jetpack is loaded and active to restore this functionality.`
    
    This will skip attempting to use Jetpack Photon without having to have the site add an additional constant  `define( 'WPCOM_VIP_USE_JETPACK_PHOTON', false )`

--- a8c-files.php
+++ a8c-files.php
@@ -172,6 +172,10 @@ class A8C_Files {
 	}
 
 	private function use_jetpack_photon() {
+		if ( defined( 'VIP_JETPACK_SKIP_LOAD' ) && true === VIP_JETPACK_SKIP_LOAD ) {
+			return false;
+		}
+		
 		if ( defined( 'WPCOM_VIP_USE_JETPACK_PHOTON' ) && true === WPCOM_VIP_USE_JETPACK_PHOTON ) {
 			return true;
 		}
