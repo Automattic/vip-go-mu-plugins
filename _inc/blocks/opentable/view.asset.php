571cc115f95c85a1c3adaa3f291788713710b4b5
jesse-greathouse <jessegreathouse@gmail.com>
2022-03-01 08:56:19 -0700

    Squashed 'jetpack-10.7/' content from commit ff3b66d3a3
    git-subtree-dir: jetpack-10.7
    git-subtree-split: ff3b66d3a31bfbc9fade5c5628a8d6600e544301

---
+++ _inc/blocks/opentable/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => '71df11eaf886abf467c90fcbc2f94ac0');
---
+++ _inc/build/widgets/milestone/admin.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e3b23e706709d247c6ce6f9faf892126');
