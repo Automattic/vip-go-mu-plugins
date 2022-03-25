bb4efc47022a1bc887b3e8a0e2358d260096399b
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-03-25 15:57:03 -0600

    Use simpler registry that is cross-compatible

---
+++ vip-feed-cache/class-vip-go-feed-cache.php
@@ -0,0 +1,17 @@
+<?php
+
+class VIP_Go_Feed_Cache extends SimplePie_Cache {
+	/**
+	 * Creates a new SimplePie_Cache object.
+	 *
+	 * @access public
+	 *
+	 * @param string $location  URL location (scheme is used to determine handler).
+	 * @param string $filename  Unique identifier for cache object.
+	 * @param string $extension 'spi' or 'spc'.
+	 * @return VIP_Go_Feed_Cache_Transient Feed cache handler object that uses transients and normalizes SimplePie Build number.
+	 */
+	public static function get_handler( $location, $filename, $extension ) {
+		return new VIP_Go_Feed_Cache_Transient( $location, $filename, $extension );
+	}
+}
