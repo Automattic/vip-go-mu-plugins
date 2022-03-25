7d11970402e04af659f8f17275715dccee65ad82
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-03-25 16:14:04 -0600

    Account for different transient name in versions below 5.9

--- tests/vip-feed-cache/test-vip-feed-cache.php
+++ tests/vip-feed-cache/test-vip-feed-cache.php
@@ -27,17 +27,21 @@ class VIP_Feed_Cache_Test extends WP_UnitTestCase {
 	}
 
 	/**
-	 * This mocks get_cache_filename in class-simplepie.php
+	 * This mocks get_cache_filename in class-simplepie.php for WP versions 5.9+
 	 *
+	 * @see https://core.trac.wordpress.org/changeset/52393
 	 * @param $url string
 	 * @return $url string
 	 */
 	private function get_cache_filename( $url ) {
-		$options                    = [];
-		$options[ CURLOPT_TIMEOUT ] = 3;
-		if ( ! empty( $options ) ) {
-			ksort( $options );
-			$url .= '#' . urlencode( var_export( $options, true ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_export
+		global $wp_version;
+		if ( $wp_version >= '5.9' ) {
+			$options                    = [];
+			$options[ CURLOPT_TIMEOUT ] = 3;
+			if ( ! empty( $options ) ) {
+				ksort( $options );
+				$url .= '#' . urlencode( var_export( $options, true ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_export
+			}
 		}
 		return $url;
 	}
