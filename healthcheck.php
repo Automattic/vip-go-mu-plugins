1ce7ebb84901ae73f8d3da64bbe0d521662a3ef2
Rinat Khaziev <rinat@automattic.com>
2022-04-07 13:17:16 -0500

    Second pass at healthcheck: instead of a boolean signifying whether the app is healthy we introduced an string[] $errors property, an empty array would mean the app is healthy. response code are as follows: 200 for healthy, 500 for not healthy. Additionally we list the reasons in the response body

--- healthcheck.php
+++ healthcheck.php
@@ -1,36 +1,74 @@
 <?php
 namespace Automattic\VIP;
 
-use Automattic\VIP\Utils\Context;
 /**
- * Utility Healthcheck class that runs further than cache-healthcheck and allow to verify the app code is working.
+ * Utility Healthcheck class that runs further than cache-healthcheck,
+ * and allows to verify the app code is working as expected.
  */
 class Healthcheck {
 	/**
-	 * By default the site is considered healthy unless filter value returns falsy value
+	 * By default, an app is considered healthy unless filter value returns a non-empty array
 	 *
-	 * @var boolean
+	 * @var array
 	 */
-	protected bool $healthy = true;
+	protected array $errors = [];
+
 	public function __construct() {
 		$this->check();
 	}
+
+	/**
+	 * Apply the filters to and set the `$errors` property accordingly.
+	 *
+	 * @return void
+	 */
 	public function check() {
-		$this->healthy = (bool) apply_filters( 'vip_site_healthcheck', $this->healthy );
+		/**
+		 * Filters the errors that would indicate the app is unhealthy.
+		 *
+		 * @param string[]     $errors either empty array (no errors) or array of strings: [ 'an error has occurred', 'another error' ]
+		 */
+		$this->errors = apply_filters( 'vip_site_healthcheck_errors_array', $this->errors );
+		if ( ! is_array( $this->errors ) ) {
+			$this->errors = [ 'Unexpected value in Healthcheck->errors' ];
+		}
 	}
 
+	/**
+	 * Check if the app is healthy.
+	 * @return boolean true
+	 */
 	public function is_healthy() {
-		return $this->healthy;
+		return is_array( $this->errors ) && empty( $this->errors );
+	}
+
+	/**
+	 * Render the response:
+	 * it should always be uncached and result either in
+	 * 200 or 500 with the reasons listed in the response body.
+	 *
+	 * @return void
+	 */
+	public function render() {
+		$is_healthy = $this->is_healthy();
+
+		nocache_headers();
+		header( 'Content-Type: text/plain' );
+		http_response_code( $is_healthy ? 200 : 500 );
+
+		foreach ( $this->errors as $error ) {
+			echo "{$error}\n"; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- we're using text/plain content-type
+		}
+
+		exit;
 	}
 }
 
 /**
- * It's safe to assume that by the time of `template_redirect` the codebase is loaded fully.
+ * `parse_request` provides a good balance between making sure the codebase is loaded and not running the main query.
  */
 if ( isset( $_SERVER['REQUEST_URI'] ) && '/vip-healthcheck' === $_SERVER['REQUEST_URI'] ) {
-	add_action( 'template_redirect', function() {
-		nocache_headers();
-		$is_healthy = ( new Healthcheck() )->is_healthy();
-		exit( $is_healthy ? 'Ok' : 'Not ok' );
-	}, PHP_INT_MIN );
+	add_action( 'parse_request', fn( $wp ) =>
+		( new Healthcheck() )->render(),
+	PHP_INT_MIN );
 }
