b65e9ac2d1b50ac886b3c41d400608b567395c02
Jeff Bowen <jblz@users.noreply.github.com>
2022-04-30 23:43:25 -0400

    Work toward testability

--- lib/helpers/wp-cli-db.php
+++ lib/helpers/wp-cli-db.php
@@ -1,98 +1,146 @@
 <?php
-
 /**
  * This file is sourced in wp-config.php so we can limit the critical path to the specific `wp db` command.
  */
 
-if ( defined( 'WPINC' ) ) {
-	// Don't do anything when WordPress is loaded.
-	return;
+namespace Automattic\VIP\Helpers;
+
+use Exception;
+use WP_CLI;
+
+class Config {
+	private bool $allow_writes = false;
+
+	public function __construct() {
+		$this->allow_writes = defined( 'WPVIP_WP_DB_ALLOW_WRITES' ) && WPVIP_WP_DB_ALLOW_WRITES;
+	}
+
+	public function allow_writes(): bool {
+		return $this->allow_writes;
+	}
 }
 
-if ( ! class_exists( 'WP_CLI' ) ) {
-	return;
+class DB_Server {
+	private $host;
+	private $user;
+	private $password;
+	private $name;
+	private $read_priority;
+	private $write_priority;
+
+	function __construct( string $host, string $user, string $password, string $name, int $read_priority, int $write_priority ) {
+		$this->host = $host;
+		$this->user = $user;
+		$this->password = $password;
+		$this->name = $name;
+		$this->read_priority = $read_priority;
+		$this->write_priority = $write_priority;
+	}
+
+	function define_variables() {
+		if ( ! defined( 'DB_HOST' ) ) {
+			define( 'DB_HOST', $this->host );
+		}
+		if ( ! defined( 'DB_USER' ) ) {
+			define( 'DB_USER', $this->user );
+		}
+		if ( ! defined( 'DB_PASSWORD' ) ) {
+			define( 'DB_PASSWORD', $this->password );
+		}
+		if ( ! defined( 'DB_NAME' ) ) {
+			define( 'DB_NAME', $this->name );
+		}
+	}
+
+	function can_read() {
+		return $this->read_priority > 0;
+	}
+
+	function can_write() {
+		return $this->write_priority > 0;
+	}
+
+	function read_priority() {
+		return $this->read_priority;
+	}
+
+	function write_priority() {
+		return $this->write_priority;
+	}
 }
 
-WP_CLI::add_hook( 'before_run_command', function ( $command ) {
-	global $db_servers;
-
-	if ( ! ( isset( $command[0] ) && 'db' === $command[0] ) ) {
-		// Don't do anything for any command other than `db`
+function init() {
+	if ( defined( 'WPINC' ) ) {
+		// Don't do anything when WordPress is loaded.
 		return;
 	}
 
+	if ( ! class_exists( 'WP_CLI' ) ) {
+		return;
+	}
+
+	add_db_before_run_command( new Config() );
+}
+
+function get_database_server( Config $config ): DB_Server {
+	global $db_servers;
+
 	if ( ! is_array( $db_servers ) ) {
-		echo "Error: No database servers are configured for this environment.\n";
-		exit( 10 );
+		throw new Exception( 'The database configuration is missing.' );
 	}
 
-	$allow_writes = defined( 'WPVIP_WP_DB_ALLOW_WRITES' ) && WPVIP_WP_DB_ALLOW_WRITES;
-
-	$write_specific_subcommands = [
-		'clean',
-		'create',
-		'drop',
-		'import',
-		'optimize',
-		'repair',
-		'reset',
-	];
-
-	$subcommand = $command[1];
-	if ( ! $allow_writes && in_array( $subcommand, $write_specific_subcommands ) ) {
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-		echo "Error: The 'wp db $subcommand' subcommand is not currently allowed for this site.\n.";
-		exit( 20 );
+	if ( empty( $db_servers ) ) {
+		throw new Exception( 'The database configuration is empty.' );
 	}
 
-	$_db_servers = array_filter( $db_servers, function ( $candidate ) use ( $allow_writes ) {
-		if ( ! ( is_array( $candidate ) && 6 === count( $candidate ) ) ) {
-			// This value isn't correctly formed. Remove.
-			return false;
-		}
+	$server_objects = array_map(
+		fn ( $server_tuple ) => new DB_Server( ...$server_tuple ),
+		$db_servers
+	);
 
-		if ( $candidate[4] < 1 ) {
-			// This server cannot read. Remove.
-			return false;
-		}
-
-		if ( ! $allow_writes && $candidate[5] > 0 ) {
-			// This server can write and it's not allowed. Remove.
-			return false;
-		}
-
-		// Include the candidate in the list to sort.
-		return true;
+	$server_objects = array_filter( $server_objects, function ( $candidate ) use ( $config ) {
+		return $candidate->can_read() && ! (
+			$candidate->can_write() && ! $config->allow_writes()
+		);
 	} );
-
-	if ( empty( $_db_servers ) ) {
-		echo "Error: No database servers are available to fulfill this request.\n";
-		exit( 30 );
-	}
 
 	// Sort the replicas in ascending order of the write priority (if allowed), else sort by read priority.
-	usort( $_db_servers, function ( $c0, $c1 ) use ( $allow_writes ) {
-		if ( $allow_writes ) {
-			return $c0[5] <=> $c1[5];
+	usort( $server_objects, function ( $c0, $c1 ) use ( $config ) {
+		if ( $config->allow_writes() ) {
+			return $c0->write_priority() <=> $c1->write_priority();
 		}
-		return $c0[4] <=> $c1[4];
+		return $c0->read_priority() <=> $c1->read_priority();
 	} );
 
-	$server = end( $_db_servers );
+	return end( $server_objects );
+}
 
-	if ( ! defined( 'DB_HOST' ) ) {
-		define( 'DB_HOST', $server[0] );
-	}
+function add_db_before_run_command( Config $config ) {
+	WP_CLI::add_hook( 'before_run_command', function ( $command ) use ( $config ) {
+		if ( ! ( isset( $command[0] ) && 'db' === $command[0] ) ) {
+			// Don't do anything for any command other than `db`
+			return;
+		}
 
-	if ( ! defined( 'DB_USER' ) ) {
-		define( 'DB_USER', $server[1] );
-	}
-	if ( ! defined( 'DB_PASSWORD' ) ) {
-		define( 'DB_PASSWORD', $server[2] );
-	}
-	if ( ! defined( 'DB_NAME' ) ) {
-		define( 'DB_NAME', $server[3] );
-	}
+		$write_specific_subcommands = [
+			'clean',
+			'create',
+			'drop',
+			'import',
+			'optimize',
+			'repair',
+			'reset',
+		];
 
-	// TODO: Logging
-} );
+		$subcommand = $command[1];
+		if ( ! $config->allow_writes() && in_array( $subcommand, $write_specific_subcommands ) ) {
+			throw new Exception( "Error: The 'wp db $subcommand' subcommand is not currently allowed for this site." );
+		}
+
+		$server = get_database_server( $config );
+		$server->define_variables();
+		// TODO: Logging
+	} );
+}
+
+init();
