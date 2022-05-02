4115b6db0def33c6bbedd58b074e5a8c7cb10433
Jeff Bowen <jblz@users.noreply.github.com>
2022-05-01 20:23:06 -0400

    Load the helper after vip config is sourced

--- 000-post-vip-config/requires.php
+++ 000-post-vip-config/requires.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * This file contains everything that's loaded before vip-config.php
+ * This file contains everything that's loaded after the VIP configuration is applied.
+ * This is one of the last things that happens before WordPress starts to load.
  *
  * You should make sure to include this file locally in case you're not using VIP Local Development Environment
  * @see https://docs.wpvip.com/how-tos/local-development/use-the-vip-local-development-environment/
@@ -9,11 +10,7 @@
 $mu_plugins_base = dirname( __DIR__ );
 
 $files = [
-	'/lib/wpcom-error-handler/wpcom-error-handler.php',
-	'/lib/class-vip-request-block.php',
-	'/lib/environment/class-environment.php',
-	'/lib/helpers/environment.php',
-	'/lib/utils/class-context.php',
+	'/lib/helpers/wp-cli-db.php',
 ];
 
 foreach ( $files as $file ) {
