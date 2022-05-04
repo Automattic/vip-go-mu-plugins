6bf89a7af49d402d72718e2d8761d9b3492aedb1
Jeff Bowen <jblz@users.noreply.github.com>
2022-05-04 11:13:59 -0400

    Drop the 000-post-vip-config init... load elsewhere

--- lib/helpers/wp-cli-db.php
+++ lib/helpers/wp-cli-db.php
@@ -2,8 +2,8 @@
 
 namespace Automattic\VIP\Helpers\WP_CLI_DB;
 
-require __DIR__ . '/wp-cli-db/class-config.php';
-require __DIR__ . '/wp-cli-db/class-db-server.php';
-require __DIR__ . '/wp-cli-db/class-wp-cli-db.php';
+require_once __DIR__ . '/wp-cli-db/class-config.php';
+require_once __DIR__ . '/wp-cli-db/class-db-server.php';
+require_once __DIR__ . '/wp-cli-db/class-wp-cli-db.php';
 
 ( new Wp_Cli_Db( new Config() ) )->early_init();
