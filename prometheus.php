198ba673779ca369e91f290df58bbe4a76c8ef7d
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-07-21 13:44:12 +0300

    Add OpCache collector

--- prometheus.php
+++ prometheus.php
@@ -2,6 +2,7 @@
 
 use Automattic\VIP\Prometheus\APCu_Collector;
 use Automattic\VIP\Prometheus\Cache_Collector;
+use Automattic\VIP\Prometheus\OpCache_Collector;
 
 if ( defined( 'ABSPATH' ) ) {
 	if ( file_exists( __DIR__ . '/prometheus/index.php' ) ) {
@@ -11,11 +12,12 @@ if ( defined( 'ABSPATH' ) ) {
 	if ( file_exists( __DIR__ . '/prometheus-collectors/class-cache-collector.php' ) ) {
 		require_once __DIR__ . '/prometheus-collectors/class-cache-collector.php';
 		require_once __DIR__ . '/prometheus-collectors/class-apcu-collector.php';
-
+		require_once __DIR__ . '/prometheus-collectors/class-opcache-collector.php';
 
 		add_filter( 'vip_prometheus_collectors', function ( array $collectors ): array {
 			$collectors[] = new Cache_Collector();
 			$collectors[] = new APCu_Collector();
+			$collectors[] = new OpCache_Collector();
 			return $collectors;
 		} );
 	}
