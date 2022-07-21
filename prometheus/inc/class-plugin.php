b71f548b002d3e175e8abfbb79012dbd357f301a
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-07-21 14:13:23 +0300

    Do not interfere with WP Core Tests

--- prometheus/inc/class-plugin.php
+++ prometheus/inc/class-plugin.php
@@ -12,8 +12,8 @@ use WP;
 use WP_Query;
 
 final class Plugin {
-	private static ?Plugin $instance = null;
-	private RegistryInterface $registry;
+	private static ?Plugin $instance     = null;
+	private ?RegistryInterface $registry = null;
 	/** @var CollectorInterface[] */
 	private array $collectors = [];
 
@@ -29,13 +29,13 @@ final class Plugin {
 	}
 
 	private function __construct() {
-		$this->registry = self::create_registry();
-
 		add_action( 'plugins_loaded', [ $this, 'plugins_loaded' ] );
 		add_action( 'init', [ $this, 'init' ] );
 	}
 
 	public function plugins_loaded(): void {
+		$this->registry = self::create_registry();
+
 		$available_collectors = apply_filters( 'vip_prometheus_collectors', [] );
 		if ( ! is_array( $available_collectors ) ) {
 			$available_collectors = [];
@@ -53,16 +53,18 @@ final class Plugin {
 	}
 
 	public function init(): void {
-		add_rewrite_endpoint( 'metrics', EP_ROOT );
-		add_filter( 'wp_headers', [ $this, 'wp_headers' ], 10, 2 );
-		add_action( 'template_redirect', [ $this, 'template_redirect' ] );
+		if ( ! defined( 'WP_RUN_CORE_TESTS' ) || ! WP_RUN_CORE_TESTS ) {
+			add_rewrite_endpoint( 'metrics', EP_ROOT );
+			add_filter( 'wp_headers', [ $this, 'wp_headers' ], 10, 2 );
+			add_action( 'template_redirect', [ $this, 'template_redirect' ] );
 
-		$version = (int) get_option( 'vip_prometheus_version', 0 );
-		if ( $version < 1 && wp_cache_add( self::FLUSH_RULES_LOCK_NAME, 1, self::FLUSH_RULES_LOCK_GROUP, 10 ) ) {
-			// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.flush_rewrite_rules_flush_rewrite_rules -- there are no activation hooks for mu-plugins
-			flush_rewrite_rules();
-			update_option( 'vip_prometheus_version', 1 );
-			wp_cache_delete( self::FLUSH_RULES_LOCK_NAME, self::FLUSH_RULES_LOCK_GROUP );
+			$version = (int) get_option( 'vip_prometheus_version', 0 );
+			if ( $version < 1 && wp_cache_add( self::FLUSH_RULES_LOCK_NAME, 1, self::FLUSH_RULES_LOCK_GROUP, 10 ) ) {
+				// phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.flush_rewrite_rules_flush_rewrite_rules -- there are no activation hooks for mu-plugins
+				flush_rewrite_rules();
+				update_option( 'vip_prometheus_version', 1 );
+				wp_cache_delete( self::FLUSH_RULES_LOCK_NAME, self::FLUSH_RULES_LOCK_GROUP );
+			}
 		}
 	}
 
@@ -99,9 +101,19 @@ final class Plugin {
 	private static function create_registry(): RegistryInterface {
 		/** @var Adapter $storage */
 		if ( extension_loaded( 'apcu' ) && apcu_enabled() ) {
-			$storage = new APCng();
+			$storage_backend = APCng::class;
 		} else {
+			$storage_backend = InMemory::class;
+		}
+
+		$storage_backend = apply_filters( 'vip_prometheus_storage_backend', $storage_backend );
+		if ( is_string( $storage_backend ) && class_exists( $storage_backend ) ) {
+			$storage = new $storage_backend();
+		} elseif ( ! is_object( $storage_backend ) || ! ( $storage_backend instanceof Adapter ) ) {
+			trigger_error( 'Invalid storage backend', E_USER_WARNING );
 			$storage = new InMemory();
+		} else {
+			$storage = $storage_backend;
 		}
 
 		return new CollectorRegistry( $storage );
