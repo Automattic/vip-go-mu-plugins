9a2b04f244c6854027d623b903bff9e91e035641
jesse-greathouse <jessegreathouse@gmail.com>
2022-07-27 11:43:05 -0600

    Squashed 'jetpack-11.2/' content from commit f9941f638d
    git-subtree-dir: jetpack-11.2
    git-subtree-split: f9941f638d82e5dc06c0ddfe880d845caae487e4

---
+++ jetpack_vendor/automattic/jetpack-search/build/customberg/jp-search-configure.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url', 'wp-viewport'), 'version' => '2bd930fa1aff79ec577d');
