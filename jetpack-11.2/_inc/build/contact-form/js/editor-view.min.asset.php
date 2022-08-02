f3cd4ad81b4a3629e3d279f5bc350abf25897306
jesse-greathouse <jessegreathouse@gmail.com>
2022-08-02 13:06:29 -0600

    Removing jetpack-11.2 for subtree replacement to 11.2

--- jetpack-11.2/_inc/build/contact-form/js/editor-view.min.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'be23ac1ecdd756597e40');
--- jetpack-11.2/jetpack_vendor/automattic/jetpack-search/build/instant-search/jp-search.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-i18n', 'wp-jp-i18n-loader', 'wp-polyfill'), 'version' => 'bca06db32e1793dab97c');
