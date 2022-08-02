3a123603f89e2cd74f7cb32b1fedc8888295cbd9
Volodymyr Kolesnykov <sjinks@users.noreply.github.com>
2022-08-03 00:59:05 +0300

    [CANTINA-889] Change the way the collectors are loaded (#3453)
    * Change the way the collectors are loaded
    
    * Bug fix

--- prometheus.php
+++ prometheus.php
@@ -15,12 +15,15 @@ if ( defined( 'ABSPATH' ) ) {
 		require_once __DIR__ . '/prometheus-collectors/class-apcu-collector.php';
 		require_once __DIR__ . '/prometheus-collectors/class-opcache-collector.php';
 
-		add_filter( 'vip_prometheus_collectors', function ( array $collectors ): array {
-			$collectors[] = new Cache_Collector();
-			$collectors[] = new APCu_Collector();
-			$collectors[] = new OpCache_Collector();
+		add_filter( 'vip_prometheus_collectors', function ( array $collectors, string $hook ): array {
+			if ( 'vipgo_mu_plugins_loaded' === $hook ) {
+				$collectors[] = new Cache_Collector();
+				$collectors[] = new APCu_Collector();
+				$collectors[] = new OpCache_Collector();
+			}
+
 			return $collectors;
-		} );
+		}, 10, 2 );
 	}
 }
 // @codeCoverageIgnoreEnd
--- prometheus/inc/class-collectorinterface.php
+++ prometheus/inc/class-collectorinterface.php
@@ -5,7 +5,7 @@ namespace Automattic\VIP\Prometheus;
 use Prometheus\RegistryInterface;
 
 interface CollectorInterface {
-	public function initialize( RegistryInterface $registry );
+	public function initialize( RegistryInterface $registry ): void;
 
 	/**
 	 * Last chance to collect metrics before sending them to the scraper.
