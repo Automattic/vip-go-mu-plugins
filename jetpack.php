e96191cbc8dd8f0bf271a6d10409780d50d12884
Jesse Greathouse <46167019+jesse-greathouse@users.noreply.github.com>
2022-08-04 14:37:46 -0600

    Jetpack 11.2 default (#3473)
    * Removing jetpack-11.2 for subtree replacement to 11.2
    
    * Squashed 'jetpack-11.2/' content from commit a8dd6ecff1
    
    git-subtree-dir: jetpack-11.2
    git-subtree-split: a8dd6ecff13fba01eae7fd86088f93862425d0a6
    
    * This change makes Jetpack 11.2 the default Jetpack version on the platform

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://jetpack.com
  * Description: Security, performance, and marketing tools made by WordPress experts. Jetpack keeps your site protected so you can focus on more important things.
  * Author: Automattic
- * Version: 11.1
+ * Version: 11.2
  * Author URI: https://jetpack.com
  * License: GPL2+
  * Text Domain: jetpack
@@ -27,7 +27,7 @@ if ( ! defined( 'VIP_JETPACK_DEFAULT_VERSION' ) ) {
 	} elseif ( version_compare( $wp_version, '5.9', '<' ) ) {
 		define( 'VIP_JETPACK_DEFAULT_VERSION', '10.9' );
 	} else {
-		define( 'VIP_JETPACK_DEFAULT_VERSION', '11.1' );
+		define( 'VIP_JETPACK_DEFAULT_VERSION', '11.2' );
 	}
 }
 
