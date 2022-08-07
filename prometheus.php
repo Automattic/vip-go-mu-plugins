38584b80dc8085cda1fd5c4f8d687e0407dcfa7c
Volodymyr Kolesnykov <volodymyr.kolesnykov@a8c.com>
2022-08-07 03:53:06 +0300

    [CANTINA-889] Add post stats collector

--- prometheus.php
+++ prometheus.php
@@ -4,6 +4,7 @@ use Automattic\VIP\Prometheus\APCu_Collector;
 use Automattic\VIP\Prometheus\Cache_Collector;
 use Automattic\VIP\Prometheus\Login_Stats_Collector;
 use Automattic\VIP\Prometheus\OpCache_Collector;
+use Automattic\VIP\Prometheus\Post_Stats_Collector;
 
 // @codeCoverageIgnoreStart -- this file is loaded before tests start
 if ( defined( 'ABSPATH' ) ) {
@@ -16,6 +17,7 @@ if ( defined( 'ABSPATH' ) ) {
 		require_once __DIR__ . '/prometheus-collectors/class-apcu-collector.php';
 		require_once __DIR__ . '/prometheus-collectors/class-opcache-collector.php';
 		require_once __DIR__ . '/prometheus-collectors/class-login-stats-collector.php';
+		require_once __DIR__ . '/prometheus-collectors/class-post-stats-collector.php';
 
 		add_filter( 'vip_prometheus_collectors', function ( array $collectors, string $hook ): array {
 			if ( 'vip_mu_plugins_loaded' === $hook ) {
@@ -23,6 +25,7 @@ if ( defined( 'ABSPATH' ) ) {
 				$collectors[] = new APCu_Collector();
 				$collectors[] = new OpCache_Collector();
 				$collectors[] = new Login_Stats_Collector();
+				$collectors[] = new Post_Stats_Collector();
 			}
 
 			return $collectors;
