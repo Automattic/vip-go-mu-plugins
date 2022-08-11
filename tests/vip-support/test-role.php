15c558e41533bc1274bf8c2fd5130c1b9b205a28
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-08-11 14:32:02 -0600

    Make tests faster

--- tests/vip-support/test-role.php
+++ tests/vip-support/test-role.php
@@ -14,27 +14,32 @@ use WP_UnitTestCase;
  */
 class VIPSupportRoleTest extends WP_UnitTestCase {
 
-	public function setUp(): void {
-		parent::setUp();
+	public static $vip_support_user;
+	public static $admin_user;
 
+	public static function wpSetUpBeforeClass( $factory ): void {
 		delete_option( 'vipsupportrole_version' );
 
-		$this->vip_support_user = User::add( array(
+		self::$vip_support_user = User::add( array(
 			'user_email' => 'vip-support@automattic.com',
 			'user_login' => 'vip-support',
 			'user_pass'  => 'password',
 		) );
 
-		$this->admin_user = $this->factory->user->create( [
+		self::$admin_user = $factory->user->create( [
 			'user_email' => 'admin@automattic.com',
 			'user_login' => 'vip_admin',
 			'role'       => 'administrator',
 		] );
+
+		Role::init()->maybe_upgrade_version();
+	}
+
+	public function setUp(): void {
+		parent::setUp();
 	}
 
 	public function test_role_existence() {
-		Role::init()->maybe_upgrade_version();
-
 		$roles = wp_roles()->roles;
 
 		$this->assertArrayHasKey( Role::VIP_SUPPORT_ROLE, $roles );
@@ -42,9 +47,7 @@ class VIPSupportRoleTest extends WP_UnitTestCase {
 	}
 
 	public function test_editable_role__vipsupport() {
-		Role::init()->maybe_upgrade_version();
-
-		wp_set_current_user( $this->vip_support_user );
+		wp_set_current_user( self::$vip_support_user );
 
 		$roles      = get_editable_roles();
 		$role_names = array_keys( $roles );
@@ -57,9 +60,7 @@ class VIPSupportRoleTest extends WP_UnitTestCase {
 	}
 
 	public function test_editable_role__admin() {
-		Role::init()->maybe_upgrade_version();
-
-		wp_set_current_user( $this->admin_user );
+		wp_set_current_user( self::$admin_user );
 
 		$roles      = get_editable_roles();
 		$role_names = array_keys( $roles );
@@ -72,13 +73,10 @@ class VIPSupportRoleTest extends WP_UnitTestCase {
 	}
 
 	public function test__only_run_upgrade_once() {
-		// Run initial upgrade.
-		Role::init()->maybe_upgrade_version();
-
 		// Remove a role which we'll use to verify our test.
 		remove_role( Role::VIP_SUPPORT_ROLE );
 
-		// Attempt to run upgrade again.
+		// Attempt to run upgrade again. It already has run in wpSetUpBeforeClass().
 		Role::init()->maybe_upgrade_version();
 
 		// Verify that the role was not added again (because the upgrade didn't run).
