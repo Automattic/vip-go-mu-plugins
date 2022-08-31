43b32ab11988130497dbd83d874cc8b9478928b2
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-08-31 10:55:28 -0600

    Squashed 'jetpack-11.3/' content from commit d0f1c79d2 (#3588)
    git-subtree-dir: jetpack-11.3
    git-subtree-split: d0f1c79d2a36c84ba0f3321a4ed0e4dbe78a098f

---
+++ jetpack-11.3/_inc/blocks/map/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-polyfill', 'wp-token-list'), 'version' => 'd1dfa1f1691045da7053');
---
+++ jetpack-11.3/_inc/build/contact-form/js/grunion-frontend.min.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '529c9d52a58b3c436d90');
