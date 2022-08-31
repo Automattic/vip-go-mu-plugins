a24d92f4dcfe80dcc3df66c4a205fe9b7d57e578
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-08-31 10:55:28 -0600

    Squashed 'jetpack-11.3/' content from commit d0f1c79d2 (#3588)
    git-subtree-dir: jetpack-11.3
    git-subtree-split: d0f1c79d2a36c84ba0f3321a4ed0e4dbe78a098f

---
+++ jetpack-11.3/_inc/blocks/podcast-player/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-a11y', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-escape-html', 'wp-i18n', 'wp-polyfill'), 'version' => '9a53bbfb714d58bf8dfa');
---
+++ jetpack-11.3/jetpack_vendor/automattic/jetpack-wordads/build/dashboard/jp-wordads-dashboard.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => '5a617db3c02bd8b61b1d');
