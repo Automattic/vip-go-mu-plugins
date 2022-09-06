07a23ad53ce1819fb5a21b68cefbdf96c5a573f6
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-09-06 09:12:26 -0600

    Jetpack: Update 11.3-beta2 to 11.3-beta3 (#3604)
    * Removing jetpack-11.3 for subtree replacement to 11.3-beta3
    
    * Squashed 'jetpack-11.3/' content from commit 528964e4a9
    
    git-subtree-dir: jetpack-11.3
    git-subtree-split: 528964e4a90f1ed0080e154cf6cba0a3b3548a8d

--- jetpack-11.3/jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
+++ jetpack-11.3/jetpack_vendor/automattic/jetpack-my-jetpack/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'ef234302efce2d8f505a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '9a5eb8929b7f06e091e2');
