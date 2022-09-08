228d033f8479b1ab7ed320247d1cca11255edeaa
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-09-08 16:10:16 -0600

    Removing jetpack-11.3 for subtree replacement to 11.3.1

--- jetpack-11.3/_inc/blocks/button/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '628e1156710e7b64019f');
--- jetpack-11.3/jetpack_vendor/automattic/jetpack-videopress/src/client/admin/components/checkbox/types.ts
+++
@@ -1,6 +0,0 @@
-import { InputHTMLAttributes } from 'react';
-
-export interface CheckboxProps extends InputHTMLAttributes< HTMLInputElement > {
-	checked?: boolean;
-	onChange?: ( checked: boolean ) => void;
-}
