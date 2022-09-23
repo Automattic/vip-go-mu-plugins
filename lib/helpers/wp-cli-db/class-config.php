5d883165a7028abfe995e2ae4e5a1345cb6c0cd9
Pavel Schoffer <xschof00@gmail.com>
2022-09-23 10:02:16 +0200

    Revert "Allow for running `wp db` CLI commands in a sandbox (#3645)"
    This reverts commit 2f591840e44b8c8b1e7abf9b7838c70b5b039284.

--- lib/helpers/wp-cli-db/class-config.php
+++ lib/helpers/wp-cli-db/class-config.php
@@ -2,18 +2,15 @@
 
 namespace Automattic\VIP\Helpers\WP_CLI_DB;
 
-use Automattic\VIP\Environment;
 use Exception;
 
 class Config {
 	private bool $enabled      = false;
 	private bool $allow_writes = false;
-	private bool $is_sandbox   = false;
 
 	public function __construct() {
-		$this->is_sandbox   = class_exists( Environment::class ) && Environment::is_sandbox_container( gethostname(), [] );
-		$this->enabled      = $this->is_sandbox || defined( 'WPVIP_ENABLE_WP_DB' ) && 1 === constant( 'WPVIP_ENABLE_WP_DB' );
-		$this->allow_writes = $this->is_sandbox || defined( 'WPVIP_ENABLE_WP_DB_WRITES' ) && 1 === constant( 'WPVIP_ENABLE_WP_DB_WRITES' );
+		$this->enabled      = defined( 'WPVIP_ENABLE_WP_DB' ) && 1 === constant( 'WPVIP_ENABLE_WP_DB' );
+		$this->allow_writes = defined( 'WPVIP_ENABLE_WP_DB_WRITES' ) && 1 === constant( 'WPVIP_ENABLE_WP_DB_WRITES' );
 	}
 
 	public function enabled(): bool {
@@ -22,10 +19,6 @@ class Config {
 
 	public function allow_writes(): bool {
 		return $this->allow_writes;
-	}
-
-	public function is_sandbox(): bool {
-		return $this->is_sandbox;
 	}
 
 	/**
