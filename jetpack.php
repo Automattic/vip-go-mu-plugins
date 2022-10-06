46f0e69e76cf55aa81c4022764288dc75792b827
Jesse Greathouse <46167019+jesse-greathouse@users.noreply.github.com>
2022-10-06 14:02:56 -0600

    Set Jetpack 11.4 to the default version (#3744)

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://jetpack.com
  * Description: Security, performance, and marketing tools made by WordPress experts. Jetpack keeps your site protected so you can focus on more important things.
  * Author: Automattic
- * Version: 11.2
+ * Version: 11.4
  * Author URI: https://jetpack.com
  * License: GPL2+
  * Text Domain: jetpack
@@ -27,7 +27,7 @@ if ( ! defined( 'VIP_JETPACK_DEFAULT_VERSION' ) ) {
 	} elseif ( version_compare( $wp_version, '5.9', '<' ) ) {
 		define( 'VIP_JETPACK_DEFAULT_VERSION', '10.9' );
 	} else {
-		define( 'VIP_JETPACK_DEFAULT_VERSION', '11.3' );
+		define( 'VIP_JETPACK_DEFAULT_VERSION', '11.4' );
 	}
 }
 
