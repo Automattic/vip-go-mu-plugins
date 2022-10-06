99515a1dfc8ccc1398f83f0ecd9d7bfbe0f4b069
Andrea Grassi <andrea.grassi@automattic.com>
2022-10-06 12:28:44 +0200

    added fastcgi_finish_request to flush content and renamed functions

--- config/class-sync.php
+++ config/class-sync.php
@@ -16,7 +16,7 @@ class Sync {
 	 * Setting this value to true will make it so that Site Details data will be updated on shutdown hook
 	 * @var bool
 	 */
-	private $send_sds_data_on_shutdown = false;
+	private $should_run_sds_sync = false;
 
 	public static function instance() {
 		if ( ! ( static::$instance instanceof Sync ) ) {
@@ -29,27 +29,32 @@ class Sync {
 
 	public function init() {
 		$this->maybe_setup_cron();
-		// TODO should we run these only when -not- on cron?
 		$this->init_listeners();
 	}
 
 	public function init_listeners() {
-		add_action( 'update_option_siteurl', array( $this, 'unlock_sds_sync' ) );
+		add_action( 'update_option_siteurl', array( $this, 'trigger_sds_sync' ) );
+		add_action( 'update_option_home', [ $this, 'trigger_sds_sync' ] );
+
+		add_action( 'shutdown', [ $this, 'maybe_do_sds_sync' ], PHP_INT_MAX );
 		// TODO should we also intercept deleted sites with add_action( 'wp_delete_site'...)?
-		add_action( 'shutdown', array( $this, 'maybe_do_sds_sync' ) );
 	}
 
-	public function unlock_sds_sync() {
-		$this->send_sds_data_on_shutdown = true;
+	public function trigger_sds_sync() {
+		$this->should_run_sds_sync = true;
 	}
 
 	public function maybe_do_sds_sync() {
-		if ( $this->send_sds_data_on_shutdown ) {
-			$this->log( 'info', 'SDS Shutdown sync was triggered (probably a url change)' );
-			// TODO should we use wp_schedule_single_event instead? this way we won't block the return
-			// wp_schedule_single_event( time(), self::CRON_EVENT_NAME);
-			$this->put_site_details();
+		if ( ! $this->should_run_sds_sync ) {
+			return;
 		}
+
+		if ( function_exists( 'fastcgi_finish_request' ) ) {
+			// Flush content to client first to prevent slow page load.
+			fastcgi_finish_request();
+		}
+
+		$this->put_site_details();
 	}
 
 	public function maybe_setup_cron() {
