c0d8332c1dfd4ae282cb0bad259776e1f54bf839
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-10-18 16:08:12 -0600

    Media: Account for new filter wp_allow_query_attachment_by_filename

--- performance/media.php
+++ performance/media.php
@@ -3,7 +3,7 @@
 // Prevent core from doing filename lookups for media search.
 // https://core.trac.wordpress.org/ticket/39358
 function vip_filter_query_attachment_filenames() {
-	remove_filter( 'posts_clauses', '_filter_query_attachment_filenames' );
+	add_filter( 'wp_allow_query_attachment_by_filename', '__return_false', PHP_INT_MAX );
 }
 
 if ( ! defined( 'WP_RUN_CORE_TESTS' ) || ! WP_RUN_CORE_TESTS ) {
