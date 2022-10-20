dc15235f0c1a37bcc66db91fb20a90d172d8e392
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-10-20 11:33:28 +0300

    refactor: Move all E2E stuff to __tests__/e2e

--- __tests__/e2e/bin/setup-env.sh
+++ __tests__/e2e/bin/setup-env.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
+
+set -ex
+
+basedir="${0%/*}/.."
 
 version=latest
-pluginPath="./"
-clientCodePath=image
+pluginPath="${basedir}/../../"
+clientCodePath=demo
 
 while getopts v:p:c: flag
 do
@@ -10,15 +14,20 @@ do
         v) version=${OPTARG};;
         p) pluginPath=${OPTARG};;
         c) clientCodePath=${OPTARG};;
+        *) echo "WARNING: Unexpected option ${flag}";;
     esac
 done
 
+if [ -z "${version}" ]; then
+    version=${WORDPRESS_VERSION:-latest}
+fi
+
 # Destroy existing test site
-vip dev-env destroy --slug=e2e-test-site
+vip dev-env destroy --slug=e2e-test-site || true
 
 # Create and run test site
-vip --slug=e2e-test-site dev-env create --title="E2E Testing site" --mu-plugins="${pluginPath}" --wordpress="5.9" --multisite=false --client-code="${clientCodePath}"
-vip --slug=e2e-test-site dev-env start
+vip --slug=e2e-test-site dev-env create --title="E2E Testing site" --mu-plugins="${pluginPath}" --wordpress="5.9" --multisite=false --app-code="${clientCodePath}" --php 8.0 --xdebug false --phpmyadmin false --elasticsearch 7.17.2
+vip dev-env start --slug e2e-test-site --skip-wp-versions-check
 
 # Enable Enterprise Search
 docker exec e2etestsite_wordpress_1 sh -c '\
@@ -33,4 +42,5 @@ docker exec e2etestsite_php_1 sh -c "\
     wp plugin install --activate --allow-root classic-editor; \
     wp core update --allow-root --version=\"${version}\" --force; \
     wp --allow-root vip-search index --setup --skip-confirm; \
+    chown -R www-data:www-data /wp
 "
