7072df20923d8680a500dd864a19ebadd3215da0
Mayank Chugh <65539648+iamchughmayank@users.noreply.github.com>
2022-11-09 21:42:53 +0000

    Update Cypress reporters for ForeSight Integration (#3868)
    * QUALITY-77: This commit updates the ForeSight integration to test phases GH actions
    1. Adds new reporter to create XML reports
    2. Adds scripts to combine spec XML report to one execution report
    3. Updates the Cypress config to .js from .json since it will be required once we move to ^10.0
    4. Adds reporter config to the cypress tests to support multi reports
    5. Changes the workflow yml to include the new scripts and file name for ForeSight integration
    6. Installs packages required for this reporting
    
    * Update: Indentation fix and removal of file that is no longer required
    
    * Update cypress.config.js
    
    Co-authored-by: Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>

---
+++ tests/search/e2e/cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    "fixturesFolder": "tests/search/e2e/fixtures",
+    "integrationFolder": "tests/search/e2e/integration",
+    "pluginsFile": "tests/search/e2e/plugins/index.js",
+    "screenshotsFolder": "tests/search/e2e/screenshots",
+    "videosFolder": "tests/search/e2e/videos",
+    "downloadsFolder": "tests/search/e2e/downloads",
+    "supportFile": "tests/search/e2e/support/index.js",
+    "video": false,
+  
+    "retries": {
+      "runMode": 1
+    },
+    "elasticPressIndexTimeout": 100000,
+    "numTestsKeptInMemory": 0,
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+    configFile: 'tests/search/e2e/cypress-reporter-config.json'
+  }
+})
