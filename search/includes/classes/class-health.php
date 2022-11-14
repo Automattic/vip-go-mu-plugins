7ed5f3ab7931e70e87494e3d17f67cb14f13235c
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-11-14 12:27:17 -0700

    Run `maybe_process_build()` if incorrect mapping found

--- search/includes/classes/class-health.php
+++ search/includes/classes/class-health.php
@@ -1139,22 +1139,17 @@ class Health {
 	/**
 	 * Verify if the post mapping is incorrect from running `wp vip-search index` without the --setup flag on initial indexing.
 	 *
-	 * @return bool Whether mapping is incorrect
+	 * @param mixed $indexable Instance of an ElasticPress post Indexable
+	 * @return bool Whether mapping is correct
 	 */
-	public function validate_incorrect_post_index_mapping() {
-		$indexable = $this->indexables->get( 'post' );
-		if ( ! $indexable ) {
-			// No post Indexable exists, bail early.
-			return false;
-		}
-
+	public function validate_post_index_mapping( $indexable ) {
 		$index_name = $indexable->get_index_name();
 		$mapping    = $indexable->get_mapping( $index_name );
 
 		if ( ! isset( $mapping[ $index_name ]['mappings']['_meta']['mapping_version'] ) ) {
-			return true;
+			return false;
 		}
 
-		return false;
+		return true;
 	}
 }
