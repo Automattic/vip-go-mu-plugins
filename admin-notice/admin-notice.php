3a67af232dad862b3b26324db8d3bdc7bffc0621
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2022-11-15 10:18:40 -0700

    Wp-admin: Remove PHP 8 notice

--- admin-notice/admin-notice.php
+++ admin-notice/admin-notice.php
@@ -27,24 +27,6 @@ add_action(
 	}
 );
 
-// PHP 8 migration nudge on non-prods
-add_action(
-	'vip_admin_notice_init',
-	function( $admin_notice_controller ) {
-		$message = 'All WordPress environments on the VIP Platform that are not running PHP 8.0 or above by Monday, the 15th of November 2022, <a href="https://lobby.vip.wordpress.com/2022/07/06/working-together-the-path-to-php-8-0/" target="_blank">will be updated by the WordPress VIP team</a>. Please upgrade your application to PHP 8.0 or 8.1 ahead of this date, to address any potential compatibility issues. Applications updated by the WordPress VIP team will not have a rollback option to a prior PHP version, and WordPress VIP will not be responsible for any issues the update may cause to your site(s). <a href="https://wpvip.com/2022/07/06/how-to-prepare-your-wordpress-site-for-php-8/" target="_blank">Visit our PHP 8 Update Guide to get started.</a>';
-		$admin_notice_controller->add(
-			new Admin_Notice(
-				$message,
-				[
-					new Expression_Condition( version_compare( PHP_VERSION, '8.0', '<' ) ),
-				],
-				'php8-migrations-3',
-				'info'
-			)
-		);
-	}
-);
-
 // Old WP version w/o pinned Jetpack version
 add_action(
 	'vip_admin_notice_init',
