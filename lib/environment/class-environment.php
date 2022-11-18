529975a15884b9c1aa12c6b06f1c1777a2835b81
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2022-11-18 21:06:33 +0200

    fix: Fix code style

--- lib/environment/class-environment.php
+++ lib/environment/class-environment.php
@@ -9,15 +9,15 @@ class Environment {
 	 */
 	const VAR_PREFIX = 'VIP_ENV_VAR';
 
-    /**
-     * Checks existence of custom environment variable values based on the supplied key.
-     *
-     * @param string $key The name of the environment variable.
-     * @return bool
-     */
-    public static function has_var( string $key ): bool {
-        return defined( self::calculate_env_const_name( $key ) );
-    }
+	/**
+	 * Checks existence of custom environment variable values based on the supplied key.
+	 *
+	 * @param string $key The name of the environment variable.
+	 * @return bool
+	 */
+	public static function has_var( string $key ): bool {
+		return defined( self::calculate_env_const_name( $key ) );
+	}
 
 	/**
 	 * Attempts to return custom environment variable values based on the supplied key.
@@ -38,9 +38,9 @@ class Environment {
 	 * @param string $key
 	 * @return string
 	 */
-    private static function calculate_env_const_name( string $key ): string {
-        return sprintf( '%s_%s', self::VAR_PREFIX, strtoupper( $key ) );
-    }
+	private static function calculate_env_const_name( string $key ): string {
+		return sprintf( '%s_%s', self::VAR_PREFIX, strtoupper( $key ) );
+	}
 
 	public static function is_sandbox_container( $hostname, $env = array() ) {
 		if ( false !== strpos( $hostname, '_web_dev_' ) || false !== strpos( $hostname, '-sbx-u' ) ) {
