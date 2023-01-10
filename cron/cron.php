081ae06c164f1911fddc4e8484054b273aee43b7
Caleb Burks <caleb.burks@automattic.com>
2023-01-10 15:05:52 -0600

    Unregister JP-related cron events when disabled

--- cron/cron.php
+++ cron/cron.php
@@ -11,3 +11,31 @@ if ( file_exists( __DIR__ . '/action-scheduler-dynamic-queue.php' ) ) {
 		( new Action_Scheduler_Dynamic_Queue() )->init();
 	}, 9 );
 }
+
+// Unregister Jetpack-related cron events when disabled.
+add_action( 'cli_init', function() {
+	$jetpack_is_disabled    = defined( 'VIP_JETPACK_SKIP_LOAD' ) && true === VIP_JETPACK_SKIP_LOAD;
+	$vaultpress_is_disabled = $jetpack_is_disabled || ( defined( 'VIP_VAULTPRESS_SKIP_LOAD' ) && true === VIP_VAULTPRESS_SKIP_LOAD );
+
+	$events_to_unregister = [];
+	if ( $jetpack_is_disabled ) {
+		$events_to_unregister = array_merge( $events_to_unregister, [
+			'jetpack_sync_cron',
+			'jetpack_sync_full_cron',
+			'jetpack_clean_nonces',
+			'jetpack_waf_rules_update_cron',
+			'jetpack_v2_heartbeat',
+		] );
+	}
+
+	if ( $vaultpress_is_disabled ) {
+		$events_to_unregister = array_merge( $events_to_unregister, [ 'vp_scan_site', 'vp_scan_next_batch' ] );
+	}
+
+	// When/if the cron event hook runs, unschedule the event so it runs no more.
+	foreach ( $events_to_unregister as $event_hook ) {
+		add_action( $event_hook, function() use ( $event_hook ) {
+			wp_unschedule_hook( $event_hook );
+		} );
+	}
+} );
