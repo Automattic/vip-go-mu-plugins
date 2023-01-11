4c27003d4366eaad7ddcfad0fb4de81df04a8df7
Rinat Khaziev <rinat.khaziev@gmail.com>
2023-01-11 16:40:12 -0600

    Remove versioned WP-Parse.ly (part of ext now) and add unversioned as a submodule at 3.6.0

--- wp-parsely-3.5/src/blocks/recommendations/components/parsely-recommendations-title.tsx
+++
@@ -1,9 +0,0 @@
-interface ParselyRecommendationsTitleProps {
-	title: string;
-}
-
-const ParselyRecommendationsTitle = ( { title }: ParselyRecommendationsTitleProps ) => (
-	title ? <p className="parsely-recommendations-list-title">{ title }</p> : <></>
-);
-
-export default ParselyRecommendationsTitle;
