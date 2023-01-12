b57645b2ad26a38db1342699716f36745547f987
Rick Hurst <rick.hurst@automattic.com>
2023-01-12 11:22:36 +0000

    Format for consistency

--- plugin-fixes.php
+++ plugin-fixes.php
@@ -31,7 +31,7 @@ function vip_disable_wpcf7_cleanup_upload_files() {
     if ( false !== $priority ){
         if ( function_exists( 'wpcf7_upload_tmp_dir' ) ) {
             // Check if the directories match (e.g. plugin is an old version, or has been patched)
-            if ( WPCF7_UPLOADS_TMP_DIR !== wpcf7_upload_tmp_dir() ){
+            if ( WPCF7_UPLOADS_TMP_DIR !== wpcf7_upload_tmp_dir() ) {
                 remove_action( 'shutdown', 'wpcf7_cleanup_upload_files', $priority );
             }
         }
