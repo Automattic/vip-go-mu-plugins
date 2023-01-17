76157452b9b1641ee536bb2d35c8c51129f4790d
Derrick Tennant <emrikol@users.noreply.github.com>
2023-01-17 10:39:40 -0500

    Bugfix: Patching Core bug #51058 in wpcom_vip_attachment_url_to_postid() (#4052)
    * Update vip-caching.php
    
    Patching core bug https://core.trac.wordpress.org/ticket/51058
    
    This will allow wpcom_vip_attachment_url_to_postid to work with "scaled" image URLs as well as full size image URLs.
    
    Props to our friends at Yoast for bringing this up and providing a fix.
    
    * Update vip-caching.php
    
    I had that backwards, we don't need to do the str_contains check
    
    * Update vip-caching.php
    
    Removing extra ID check that's unneeded.

--- vip-helpers/vip-caching.php
+++ vip-helpers/vip-caching.php
@@ -610,6 +610,24 @@ function wpcom_vip_attachment_url_to_postid( $url ) {
 	$id        = wp_cache_get( $cache_key );
 	if ( false === $id ) {
 		$id = attachment_url_to_postid( $url );
+
+		/**
+		 * If no ID was found, maybe we're dealing with a scaled big image. So, let's try that.
+		 *
+		 * This shouldn't normally happen on VIP due to our `big_image_size_threshold` filter,
+		 * but it could be possible that content was imported with the scaled media already in place.
+		 *
+		 * @see https://core.trac.wordpress.org/ticket/51058
+		 */
+		if ( empty( $id ) ) {
+			$path_parts = pathinfo( $url );
+
+			if ( isset( $path_parts['dirname'], $path_parts['filename'], $path_parts['extension'] ) ) {
+				$scaled_url = trailingslashit( $path_parts['dirname'] ) . $path_parts['filename'] . '-scaled.' . $path_parts['extension'];
+				$id         = attachment_url_to_postid( $scaled_url );
+			}
+		}
+
 		if ( empty( $id ) ) {
 			// phpcs:ignore WordPress.WP.AlternativeFunctions.rand_mt_rand, WordPressVIPMinimum.Performance.LowExpiryCacheTime.CacheTimeUndetermined
 			wp_cache_set( $cache_key, 'not_found', 'default', 12 * HOUR_IN_SECONDS + mt_rand( 0, 4 * HOUR_IN_SECONDS ) );
