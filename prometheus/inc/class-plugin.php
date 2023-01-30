6daa59bbd835f5504f5deda430a08df001840daf
Rinat Khaziev <rinat.khaziev@gmail.com>
2023-01-30 14:23:15 -0600

    Remove rendering logic from the Plugin class

--- prometheus/inc/class-plugin.php
+++ prometheus/inc/class-plugin.php
@@ -1,16 +1,11 @@
 <?php
-
 namespace Automattic\VIP\Prometheus;
 
-use Automattic\VIP\Utils\Context;
 use Prometheus\CollectorRegistry;
 use Prometheus\RegistryInterface;
-use Prometheus\RenderTextFormat;
 use Prometheus\Storage\Adapter;
 use Prometheus\Storage\APCng;
 use Prometheus\Storage\InMemory;
-use WP;
-use WP_Query;
 
 class Plugin {
 	protected static ?Plugin $instance     = null;
@@ -34,13 +29,9 @@ class Plugin {
 	 */
 	protected function __construct() {
 		add_action( 'vip_mu_plugins_loaded', [ $this, 'init_registry' ], 9 );
-
 		add_action( 'vip_mu_plugins_loaded', [ $this, 'load_collectors' ] );
 		add_action( 'mu_plugins_loaded', [ $this, 'load_collectors' ] );
 		add_action( 'plugins_loaded', [ $this, 'load_collectors' ] );
-		add_action( 'plugins_loaded', [ $this, 'intercept_request' ] );
-
-		add_action( 'init', [ $this, 'init' ] );
 
 		do_action( 'vip_prometheus_loaded' );
 	}
@@ -66,55 +57,6 @@ class Plugin {
 		array_walk( $available_collectors, fn ( CollectorInterface $collector ) => $collector->initialize( $this->registry ) );
 
 		$this->collectors = array_merge( $this->collectors, $available_collectors );
-	}
-
-	public function init(): void {
-		if ( ! defined( 'WP_RUN_CORE_TESTS' ) || ! WP_RUN_CORE_TESTS ) {
-			add_filter( 'request', [ $this, 'request' ] );
-		}
-	}
-
-	public function request( $query_vars ): array {
-		if ( ! is_array( $query_vars ) ) {
-			$query_vars = [];
-		}
-
-		if ( Context::is_prom_endpoint_request() ) {
-			unset( $query_vars['error'] );
-			add_filter( 'pre_handle_404', [ $this, 'pre_handle_404' ], 10, 2 );
-		}
-
-		return $query_vars;
-	}
-
-	public function pre_handle_404( $_result, WP_Query $query ): bool {
-		unset( $query->query_vars['error'] );
-		return true;
-	}
-
-	public function intercept_request(): void {
-		if ( ! Context::is_prom_endpoint_request() ) {
-			return;
-		}
-
-		nocache_headers();
-		header( 'Content-Type: ' . RenderTextFormat::MIME_TYPE );
-
-		array_walk( $this->collectors, fn ( CollectorInterface $collector ) => $collector->collect_metrics() );
-
-		$renderer = new RenderTextFormat();
-		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- this is a text/plain endpoint
-		echo $renderer->render( $this->registry->getMetricFamilySamples() );
-		die();
-
-		// In case you want or need to debug queries:
-		// 1. Comment out the calls to `$renderer->render()` and `die()` above;
-		// 2. Uncomment the following lines:
-		//
-		// remove_all_actions( current_action() );
-		// do_action( 'wp_head' );
-		// do_action( 'wp_footer' );
-		// die();
 	}
 
 	private static function create_registry(): RegistryInterface {
