ffa99321e399c3bb020ea386b99854cce1c3ae07
Rinat Khaziev <rinat.khaziev@gmail.com>
2023-02-10 14:39:43 -0600

    Ignore codeCoverage for endpoint.php

--- prometheus/endpoint.php
+++ prometheus/endpoint.php
@@ -11,7 +11,7 @@ use Prometheus\Storage\InMemory;
 
 require_once __DIR__ . '/vendor/autoload.php';
 require_once dirname( __DIR__ ) . '/lib/utils/class-context.php';
-
+// @codeCoverageIgnoreStart -- APCu may or may not be available during tests
 // Someone is trying to be sneaky, we can't have that.
 // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotValidated
 if ( defined( 'ABSPATH' ) || ! Context::is_prom_endpoint_request() ) {
@@ -20,27 +20,24 @@ if ( defined( 'ABSPATH' ) || ! Context::is_prom_endpoint_request() ) {
 
 $registry = create_registry();
 
-// @codeCoverageIgnoreStart -- this file is loaded before tests start
 header( 'Content-Type: text/plain' );
 $renderer = new RenderTextFormat();
 // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- this is a text/plain endpoint
 echo $renderer->render( $registry->getMetricFamilySamples() );
-// @codeCoverageIgnoreEnd
 
 /**
  * Create a registry for Prometheus metrics.
  */
 function create_registry(): RegistryInterface {
-	// @codeCoverageIgnoreStart -- APCu may or may not be available during tests
 	/** @var Adapter $storage */
 	if ( extension_loaded( 'apcu' ) && apcu_enabled() ) {
 		$storage_backend = APCng::class;
 	} else {
 		$storage_backend = InMemory::class;
 	}
-	// @codeCoverageIgnoreEnd
 
 	$storage = new $storage_backend();
 
 	return new CollectorRegistry( $storage );
 }
+// @codeCoverageIgnoreEnd
