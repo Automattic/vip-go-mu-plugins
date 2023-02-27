b1f00165fc68c2d06fc788a0e0b9ab77bebad3cc
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2023-02-27 16:24:44 -0700

    Clean up in test__vip_default_jetpack_version()

--- tests/test-jetpack.php
+++ tests/test-jetpack.php
@@ -4,6 +4,9 @@
 
 class VIP_Go__Core__Default_VIP_Jetpack_Version extends WP_UnitTestCase {
 	public function test__vip_default_jetpack_version() {
+		global $wp_version;
+		$saved_wp_version = $wp_version;
+
 		$versions_map = [
 			// WordPress version => Jetpack version
 			'5.8.6' => '10.9',
@@ -15,10 +18,11 @@ class VIP_Go__Core__Default_VIP_Jetpack_Version extends WP_UnitTestCase {
 		];
 
 		foreach ( $versions_map as $wordpress_version => $jetpack_version ) {
-			global $wp_version;
 			$wp_version = $wordpress_version;
-
 			$this->assertEquals( vip_default_jetpack_version(), $jetpack_version );
 		}
+
+		// Reset back to original value.
+		$wp_version = $saved_wp_version;
 	}
 }
