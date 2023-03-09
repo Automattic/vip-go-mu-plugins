e89b33ad299f7dbd23a3dc2a5e6b2e7e4e685c59
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2023-03-09 14:37:22 -0700

    Feature: is_enabled() should also check if enabled by ID and adds is_enabled_by_env() & get_features() and `wp vip-feature` CLI commands (#4191)
    * Feature: is_enabled() should also check if enabled by ID
    
    * Add is_enabled_by_env to control environments too
    
    * Add get_features()
    
    * Address code review feedback
    
    * Add CLI
    
    * Change to use `wp vip feature` rather than `wp vip-feature`
    
    * Update lib/feature/class-feature-cli.php
    
    Co-authored-by: Rinat K <rinat@automattic.com>
    
    * Update lib/feature/class-feature-cli.php
    
    Co-authored-by: Rinat K <rinat@automattic.com>
    
    ---------
    
    Co-authored-by: Rinat K <rinat@automattic.com>

---
+++ lib/feature/class-feature-cli.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Automattic\VIP;
+
+use WP_CLI;
+use \Automattic\VIP\Feature as Feature;
+
+// phpcs:ignore WordPressVIPMinimum.Classes.RestrictedExtendClasses.wp_cli
+class Feature_CLI_Command extends \WP_CLI_Command {
+	/**
+	 * List all available VIP features.
+	 *
+	 * @subcommand list
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp vip feature list [--format=<table|json|csv|yaml>]
+	 */
+	public function list_features( $args, $assoc_args ) {
+		$format = $assoc_args['format'] ?? 'table';
+		if ( ! in_array( $format, [ 'table', 'json', 'csv', 'yaml' ], true ) ) {
+			WP_CLI::error( __( '--format only accepts the following values: table, json, csv, yaml' ) );
+		}
+
+		$features = Feature::get_features();
+		if ( empty( $features ) ) {
+			WP_CLI::error( __( 'No features found.' ) );
+		}
+
+		$listed_features = [];
+		foreach ( $features as $feature ) {
+			$listed_features[] = [
+				'feature' => $feature,
+				'status'  => Feature::is_enabled( $feature ) ? 'enabled' : 'disabled',
+			];
+		}
+
+		WP_CLI\Utils\format_items( $format, $listed_features, [ 'feature', 'status' ] );
+	}
+
+	/**
+	 * Get status of an available VIP feature.
+	 *
+	 * @subcommand get
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     wp vip feature get <feature-slug>
+	 */
+	public function get_feature( $args ) {
+		if ( ! isset( $args[0] ) ) {
+			WP_CLI::error( 'Missing feature slug. Usage as follows: `wp vip feature get <slug>`' );
+		}
+
+		$feature = $args[0];
+
+		$features = Feature::get_features();
+		if ( ! in_array( $feature, $features, true ) ) {
+			WP_CLI::error( "Invalid feature slug '$feature'" );
+		}
+
+		WP_CLI::line( sprintf( '%1$s is %2$s.', $feature, Feature::is_enabled( $feature ) ? 'enabled' : 'disabled' ) );
+	}
+}
+
+WP_CLI::add_command( 'vip feature', __NAMESPACE__ . '\Feature_CLI_Command' );
