0521a42085ab758cffa373283b9e50596421edea
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2023-03-16 16:56:33 +0200

    Debug

--- tests/test-schema.php
+++ tests/test-schema.php
@@ -5,7 +5,12 @@ require_once ABSPATH . '/wp-admin/includes/upgrade.php';
 class VIP_Go_Schema_Test extends WP_UnitTestCase {
 	public function test__dbDelta__verify_blog_tables() {
 		$deltas = dbDelta( 'blog', false ); // phpcs:ignore WordPressVIPMinimum.Functions.RestrictedFunctions.dbDelta_dbdelta
-		
+
+		if ( count( $deltas ) !== 1 ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_print_r
+			print_r( $deltas );
+		}
+
 		$this->assertCount( 1, $deltas, 'More deltas than expected for blogs table' );
 		$this->assertEquals( $deltas[0], 'Added index wptests_postmeta KEY `vip_meta_key_value` (`meta_key`(191),`meta_value`(100))', 'Delta did not find vip_meta_key_value index or assertion needs updating.' );
 	}
