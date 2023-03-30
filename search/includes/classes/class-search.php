59a75ef5106450b0d772746c1241b66970f3d63c
Rinat Khaziev <rinat.khaziev@gmail.com>
2023-03-30 18:34:26 -0500

    Prototype disabling SyncManager

--- search/includes/classes/class-search.php
+++ search/includes/classes/class-search.php
@@ -2257,6 +2257,12 @@ class Search {
 
 	public function action__init() {
 		remove_action( 'wp_initialize_site', [ \ElasticPress\Indexables::factory()->get( 'post' )->sync_manager, 'action_create_blog_index' ] );
+		// Force disable sync hooks for indexables that don't have an index, this prevents the sync manager from creating the index with broken mappings
+		foreach ( \ElasticPress\Indexables::factory()->get_all() as $indexable ) {
+			if ( ! $indexable->index_exists() && is_callable( [ $indexable->sync_manager, 'disable_sync_hooks' ] ) ) {
+				$indexable->sync_manager->disable_sync_hooks();
+			}
+		}
 	}
 
 	/**
