8d5ca4530459e634c8b2fa0776fa62e525a3be3f
Rinat K <rinat@automattic.com>
2023-04-18 16:59:30 -0500

    Prometheus: re-add the dedicated endpoint with better locking mechanism, introduce Error stats collector (#4109)
    * Revert "Revert "Prom: speed things up by moving out the rendering logic into a non-WP endpoint (#3989)" (#4104)"
    
    This reverts commit 7d27f61c6516d671cb74ad6f6989e0eeedad1458.
    
    * Fix the locking logic to actually set the lock PRIOR the expensive operation. Bump interval to 90 seconds and only run on authenticated (author+) requests
    
    * Fix underscores vs dashes in cache key and group
    
    * Ignore codeCoverage for endpoint.php
    
    * Gate Posts collectors at 10% of envs
    
    * Move post stats collection off the web request path
    
    * Implement a cron event + adjust the CollectorInterface to include process_metrics for heavier operations
    
    * Regenerate the bundle
    
    * Implement error collector: add a new action php_error_handler which we use in our custom error handler, add the callback to the new Error_Stats_Collector
    
    * Lint
    
    * Be even more explicit about process_metrics and skip the execution for web requests
    
    * Code comments
    
    * Amend code comments and comment out process_metrics to avoid deploy disruptions
    
    * Address review feedback: implement a more robust and safer initializing logic for collectors, fix inaccurate code comments
    
    * Add get_site_label method to safeguard against large number of network sites
    
    * Use the new helper in our collectors
    
    * Second pass at site id label
    
    * Don't add Search Collector to the list of available Prom collectors from within the collector class
    
    * Fix missed blog_id swap for Login Stats Collector
    
    * Rename the hooks, general code cleanup, bump the interval for cron to 1 hr
    
    * More naming consistency, bump cache lock to 5 minutes
    
    * Load collectors earlier - we need to gather as much stats as possible
    
    * Include Prometheus early
    
    * Define request time buckets for ES collector
    
    * Safeguard Search collector usage a bit
    
    * No need for an empty foreach
    
    * Use feature definition to enable Post collector
    
    * Disable Prom post collector on a new sites
    
    * Disable post stats crunching for larger-than-threshold number of sites
    
    * fix: Fix 'Undefined array key "format"' warning (#4277)
    
    * Search: Improve get-last-indexed-post-id to include timestamp for debugging (#4266)
    
    * Search: Improve get-last-indexed-post-id to include timestamp for debugging purposes
    
    * Remove removal of option after indexing has completed
    
    * Revert "Remove removal of option after indexing has completed"
    
    This reverts commit 7c7e99b28104450c09738fb4ba5686c70fbbba14.
    
    * Check whether FILES_CLIENT_SITE_ID is defined
    
    * Make sure site_label is only set once per request - even if we switch_to_blog we should treat the stats as originating from the initial block
    
    * Implement a vip_generic_cron_hourly and schedule it with an offset for multisites
    
    * Use the generic cron hook instead of adding a custom one, un-gate the collection logic for all requests - the lock should be enough, but in case a collection is taking longer than expected also trigger E_USER_WARNING
    
    * Simplify things a bit
    
    * s/generic/aggregated/
    
    ---------
    
    Co-authored-by: WordPress VIP Bot <no-reply@automattic.com>
    Co-authored-by: Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
    Co-authored-by: Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>

--- prometheus.php
+++ prometheus.php
@@ -1,37 +1,49 @@
 <?php
-
+use Automattic\VIP\Feature;
 use Automattic\VIP\Prometheus\APCu_Collector;
 use Automattic\VIP\Prometheus\Cache_Collector;
 use Automattic\VIP\Prometheus\Login_Stats_Collector;
 use Automattic\VIP\Prometheus\OpCache_Collector;
 use Automattic\VIP\Prometheus\Post_Stats_Collector;
-
+use Automattic\VIP\Prometheus\Error_Stats_Collector;
 // @codeCoverageIgnoreStart -- this file is loaded before tests start
 if ( defined( 'ABSPATH' ) ) {
+	require_once __DIR__ . '/prometheus/index.php';
+
 	$files = [
-		'/prometheus/index.php',
 		'/prometheus-collectors/class-cache-collector.php',
 		'/prometheus-collectors/class-apcu-collector.php',
 		'/prometheus-collectors/class-opcache-collector.php',
 		'/prometheus-collectors/class-login-stats-collector.php',
-		'/prometheus-collectors/class-post-stats-collector.php',
+		'/prometheus-collectors/class-error-stats-collector.php',
 	];
 
+	$should_enable_post_collector = Feature::is_enabled( 'prom-post-collection' );
+
+	if ( $should_enable_post_collector ) {
+		$files[] = '/prometheus-collectors/class-post-stats-collector.php';
+	}
+
 	foreach ( $files as $file ) {
-		if ( ! file_exists( __DIR__ . $file ) ) {
-			return; // Bail early if one of the files doesn't exist.
-		} else {
+		if ( file_exists( __DIR__ . $file ) ) {
 			require_once __DIR__ . $file;
 		}
 	}
 
 	add_filter( 'vip_prometheus_collectors', function ( array $collectors, string $hook ): array {
-		if ( 'vip_mu_plugins_loaded' === $hook ) {
-			$collectors[] = new Cache_Collector();
-			$collectors[] = new APCu_Collector();
-			$collectors[] = new OpCache_Collector();
-			$collectors[] = new Login_Stats_Collector();
-			$collectors[] = new Post_Stats_Collector();
+		$to_init = [
+			'cache'   => Cache_Collector::class,
+			'apcu'    => APCu_Collector::class,
+			'opcache' => OpCache_Collector::class,
+			'login'   => Login_Stats_Collector::class,
+			'error'   => Error_Stats_Collector::class,
+			'post'    => Post_Stats_Collector::class,
+		];
+
+		foreach ( $to_init as $slug => $class ) {
+			if ( class_exists( $class ) && ! isset( $collectors[ $slug ] ) ) {
+				$collectors[ $slug ] = new $class();
+			}
 		}
 
 		return $collectors;
