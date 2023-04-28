ced2eade22da937867d756fdaf5ed1a53ce3ac66
Alec Geatches <alec.geatches@automattic.com>
2023-04-28 13:55:36 +0800

    Remove PluginLoadStrategy code.
    Since this value is hardcoded, simplify loading code by removing it

--- vip-integrations.php
+++ vip-integrations.php
@@ -16,23 +16,8 @@ if ( ! ( defined( 'VIP_BLOCK_DATA_API_ENABLE' ) && true === constant( 'VIP_BLOCK
 
 const VIP_BLOCK_API_LATEST_VERSION = '1.0.0';
 
-class PluginLoadStrategy {
-	const PREFER_MU_PLUGINS_VERSION    = 'PREFER_MU_PLUGINS_VERSION';
-	const PREFER_CUSTOMER_CODE_VERSION = 'PREFER_CUSTOMER_CODE_VERSION';
-}
-
-$plugin_load_strategy = PluginLoadStrategy::PREFER_CUSTOMER_CODE_VERSION;
-
-function load_block_data_api() {
+add_action( 'plugins_loaded', function() {
 	if ( ! defined( 'VIP_BLOCK_DATA_API_LOADED' ) ) {
 		require __DIR__ . '/vip-integrations/vip-block-data-api-' . VIP_BLOCK_API_LATEST_VERSION . '/vip-block-data-api.php';
 	}
-}
-
-if ( PluginLoadStrategy::PREFER_MU_PLUGINS_VERSION === $plugin_load_strategy ) {
-	load_block_data_api();
-} elseif ( PluginLoadStrategy::PREFER_CUSTOMER_CODE_VERSION === $plugin_load_strategy ) {
-	add_action( 'plugins_loaded', function() {
-		load_block_data_api();
-	}, 1 );
-}
+}, 1 );
