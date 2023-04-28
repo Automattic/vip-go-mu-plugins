b73e9dfa26899ddc89f5e12455d8548d89d48100
Alec Geatches <alec.geatches@automattic.com>
2023-04-28 12:53:53 +0800

    Simplify block data API loading, only support latest version

--- vip-integrations.php
+++ vip-integrations.php
@@ -14,11 +14,7 @@ if ( ! ( defined( 'VIP_BLOCK_DATA_API_ENABLE' ) && true === constant( 'VIP_BLOCK
 	return;
 }
 
-const VIP_BLOCK_API_SUPPORTED_VERSIONS = [
-	'0.2.1',
-];
-
-const VIP_BLOCK_API_LATEST_VERSION = VIP_BLOCK_API_SUPPORTED_VERSIONS[0];
+const VIP_BLOCK_API_LATEST_VERSION = '1.0.0';
 
 class PluginLoadStrategy {
 	const PREFER_MU_PLUGINS_VERSION    = 'PREFER_MU_PLUGINS_VERSION';
@@ -28,21 +24,15 @@ class PluginLoadStrategy {
 $plugin_load_strategy = PluginLoadStrategy::PREFER_CUSTOMER_CODE_VERSION;
 
 function load_block_data_api() {
-	$plugin_version = VIP_BLOCK_API_LATEST_VERSION;
-
-	if ( defined( 'VIP_BLOCK_DATA_API_VERSION' ) && in_array( constant( 'VIP_BLOCK_DATA_API_VERSION' ), VIP_BLOCK_API_SUPPORTED_VERSIONS ) ) {
-		$plugin_version = constant( 'VIP_BLOCK_DATA_API_VERSION' );
+	if ( ! defined( 'VIP_BLOCK_DATA_API_LOADED' ) ) {
+		require __DIR__ . '/vip-integrations/vip-block-data-api-' . VIP_BLOCK_API_LATEST_VERSION . '/vip-block-data-api.php';
 	}
-
-	require __DIR__ . '/vip-integrations/vip-block-data-api-' . $plugin_version . '/vip-block-data-api.php';
 }
 
 if ( PluginLoadStrategy::PREFER_MU_PLUGINS_VERSION === $plugin_load_strategy ) {
 	load_block_data_api();
 } elseif ( PluginLoadStrategy::PREFER_CUSTOMER_CODE_VERSION === $plugin_load_strategy ) {
 	add_action( 'plugins_loaded', function() {
-		if ( ! defined( 'VIP_BLOCK_DATA_API_LOADED' ) ) {
-			load_block_data_api();
-		}
+		load_block_data_api();
 	}, 1 );
 }
