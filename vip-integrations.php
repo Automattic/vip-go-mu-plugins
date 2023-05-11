4bcdb4be61001c7f2d189850dfec11a0f3126a33
ingeniumed <ingeniumed@users.noreply.github.com>
2023-05-11 11:44:30 +1000

    Flip the conditions around

--- vip-integrations.php
+++ vip-integrations.php
@@ -14,25 +14,25 @@ const BLOCK_DATA_API_SLUG = 'block-data-api';
 
 /**
  * Load the integration based on the slug provided.
- * 
+ *
  * @param string $slug The slug of the integration to load.
  * @param array $config The configuration for the integration, if supported.
  */
 function load_integration( $slug, $config = array() ) {
-	if ( $slug !== BLOCK_DATA_API_SLUG ) {
+	if ( BLOCK_DATA_API_SLUG !== $slug ) {
 		return;
 	}
 
-    // New integration paths go here
-   const BLOCK_DATA_API_PATH = '/vip-integrations/vip-block-data-api-1.0.0/vip-block-data-api.php';
+	// New integration paths go here
+	$block_data_api_path = '/vip-integrations/vip-block-data-api-1.0.0/vip-block-data-api.php';
 
 	// Use the plugins_loaded filter to ensure customer code version of the plugin overrides the mu-plugins version.
 	add_action( 'plugins_loaded', function() {
-        // New integration requires go here
+		// New integration requires go here
 
-        // Block Data API defines this when it is loaded, so it's a guard against loading twice.
+		// Block Data API defines this when it is loaded, so it's a guard against loading twice.
 		if ( ! defined( 'VIP_BLOCK_DATA_API_LOADED' ) ) {
-            require_once __DIR__ . BLOCK_DATA_API_PATH;
+			require_once __DIR__ . $block_data_api_path;
 		}
 	}, 1 );
 }
