3e8986ef668a3572941ab47503c2f4a3de4c7c19
Alec Geatches <alec.geatches@automattic.com>
2023-05-23 11:18:18 -0600

    Refactor integration actions into class

--- vip-integrations.php
+++ vip-integrations.php
@@ -17,33 +17,39 @@ require_once __DIR__ . '/vip-integration-helper/integrations/integration.php';
 require_once __DIR__ . '/vip-integration-helper/integrations/integrations.php';
 require_once __DIR__ . '/vip-integration-helper/integrations/block-data-api.php';
 
-// Register integrations
+class VipIntegrations {
+	public function setup() {
+		add_action( 'muplugins_loaded', [ $this, 'register' ], /* priority */ 4 );
+		add_action( 'muplugins_loaded', [ $this, 'activate' ], /* priority */ 6 );
+		add_action( 'muplugins_loaded', [ $this, 'integrate' ], /* priority */ 8 );
+	}
 
-add_action( 'muplugins_loaded', __NAMESPACE__ . '\\run_register_action', /* priority */ 4 );
-/**
- * Run action to register integrations.
- */
-function run_register_action(): void {
-	do_action( 'register_vip_integrations' );
+	// Actions
+
+	public function register(): void {
+		add_action( 'register_vip_integrations', function() {
+			// Register VIP integrations here
+			Integrations::instance()->register( 'block-data-api', BlockDataApi::class );
+		} );
+
+		do_action( 'register_vip_integrations' );
+	}
+
+	public function activate(): void {
+		do_action( 'activate_vip_integrations' );
+	}
+
+	public function integrate(): void {
+		add_action( 'integrate_vip_integrations', function() {
+			Integrations::instance()->integrate();
+		} );
+
+		do_action( 'integrate_vip_integrations' );
+	}
 }
 
-add_action( 'register_vip_integrations', __NAMESPACE__ . '\\register_vip_integrations' );
-/**
- * Register valid integrations.
- */
-function register_vip_integrations(): void {
-	Integrations::instance()->register( 'block-data-api', BlockDataApi::class );
-}
-
-// Activate integrations
-
-add_action( 'muplugins_loaded', __NAMESPACE__ . '\\run_activate_action', /* priority */ 6 );
-/**
- * Run action to activate integrations.
- */
-function run_activate_action(): void {
-	do_action( 'activate_vip_integrations' );
-}
+$vip_integrations = new VipIntegrations();
+$vip_integrations->setup();
 
 /**
  * Activates an integration with an optional configuration value.
@@ -60,23 +66,5 @@ function activate( string $integration_slug, array $config = [] ) {
 		} else {
 			$integration->activate( $config );
 		}
-	}, /* priority */ 6 );
-}
-
-// Run integration code
-
-add_action( 'muplugins_loaded', __NAMESPACE__ . '\\run_integrate_action', /* priority */ 8 );
-/**
- * Run integration action.
- */
-function run_integrate_action(): void {
-	do_action( 'integrate_vip_integrations' );
-}
-
-add_action( 'integrate_vip_integrations', __NAMESPACE__ . '\\integrate_vip_integrations' );
-/**
- * After code has the opportunity to activate integrations, integrate them.
- */
-function integrate_vip_integrations(): void {
-	Integrations::instance()->integrate();
+	} );
 }
