7c229f9975e60f50d2e3e27d63d6b8b80cf9ef91
Pavel Schoffer <xschof00@gmail.com>
2023-05-25 14:49:04 +0200

    Filter blog_public sooner (#4494)
    * Filter blog_public sooner
    
    * cleanup

--- security/class-private-sites.php
+++ security/class-private-sites.php
@@ -54,10 +54,7 @@ class Private_Sites {
 
 		add_filter( 'jetpack_active_modules', array( $this, 'filter_jetpack_active_modules' ) );
 		add_filter( 'jetpack_get_available_modules', array( $this, 'filter_jetpack_get_available_modules' ) );
-		add_filter( 'jetpack_sync_before_enqueue_added_option', array( $this, 'filter_blog_public_option_for_sync' ) );
-		add_filter( 'jetpack_sync_before_enqueue_updated_option', array( $this, 'filter_blog_public_option_for_sync' ) );
-		add_filter( 'jetpack_sync_before_enqueue_deleted_option', array( $this, 'filter_blog_public_option_for_sync' ) );
-		add_filter( 'jetpack_sync_before_send_jetpack_full_sync_options', array( $this, 'filter_blog_public_option_for_full_sync' ), 11 );
+		add_filter( 'pre_option_blog_public', fn() => '-1' );
 
 		$this->disable_core_feeds();
 		$this->block_unnecessary_access();
@@ -119,50 +116,5 @@ class Private_Sites {
 		unset( $modules['enhanced-distribution'] );
 
 		return $modules;
-	}
-
-	/**
-	 * Filter the blog_public option when syncing to JP
-	 *
-	 * @param array $args {
-	 *  Sync values.
-	 * @type string Option name.
-	 * @type mixed Old value.
-	 * @type mixed New value.
-	 * }
-	 *
-	 * @return array
-	 */
-	public function filter_blog_public_option_for_sync( $args ) {
-		if ( ! is_array( $args ) ) {
-			return $args;
-		}
-
-		if ( 'blog_public' === $args[0] ) {
-			$args[2] = '-1';
-		}
-
-		return $args;
-	}
-
-	/**
-	 * Filter the blog_public option when syncing to JP
-	 *
-	 * @param array $args {
-	 *  Sync option values.
-	 * @type string Option name.
-	 * @type mixed Value.
-	 * }
-	 *
-	 * @return array
-	 */
-	public function filter_blog_public_option_for_full_sync( $args ) {
-		if ( ! is_array( $args ) ) {
-			return $args;
-		}
-
-		$args['blog_public'] = '-1';
-
-		return $args;
 	}
 }
