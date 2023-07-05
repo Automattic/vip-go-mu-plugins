a3b1b79b6af9e8a9f9a38e77a7e4246558af45f0
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2023-07-05 16:02:11 +0500

    💫 UPDATE: parsely integration from loading files to using VIP_PARSELY_ENABLED constant

--- integrations/parsely.php
+++ integrations/parsely.php
@@ -13,16 +13,6 @@ namespace Automattic\VIP\Integrations;
  * @private
  */
 class ParselyIntegration extends Integration {
-
-	/**
-	 * The version of the parsely plugin to load.
-	 *
-	 * This should be the latest version available in `parsely` config https://github.com/Automattic/vip-go-mu-plugins-ext/blob/trunk/config.json
-	 *
-	 * @var string
-	 */
-	private string $version = '3.8'; // If updated then make sure to update SUPPORTED_VERSIONS in /wp-parsely.php (legacy support).
-
 	/**
 	 * Applies hooks to load plugin.
 	 *
@@ -36,21 +26,10 @@ class ParselyIntegration extends Integration {
 			return;
 		}
 
-		// Load the version of the plugin that should be set to the latest version.
-		$latest_plugin_path = WPMU_PLUGIN_DIR . '/wp-parsely-' . $this->version . '/wp-parsely.php';
-		if ( file_exists( $latest_plugin_path ) ) {
-			require_once $latest_plugin_path;
-			return;
-		}
-
-		// Load submodule if specified version does not exist.
-		$plugin_path = WPMU_PLUGIN_DIR . '/wp-parsely/wp-parsely.php';
-		if ( file_exists( $plugin_path ) ) {
-			require_once $plugin_path;
-			return;
-		}
-
-		// Deactivate the integration if not found.
-		$this->is_active_by_customer = false;
+		// Activating the integration via setting constant whose implementation is already
+		// handled via Automattic\VIP\WP_Parsely_Integration (ideally we should move
+		// all the implementation here such that there will be only one way of managing
+		// the plugin).
+		define( 'VIP_PARSELY_ENABLED', true );
 	}
 }
