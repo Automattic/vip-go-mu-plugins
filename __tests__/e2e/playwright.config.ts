b9c726c703df1b52f3c9ce29c41132f0a3eecf87
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2023-07-31 16:57:47 +0300

    Revert "test: increase timeout for E2E tests (#4726)"
    This reverts commit 4518f73e91049c02b5d3117945f6674a82fe5ccc.

--- __tests__/e2e/playwright.config.ts
+++ __tests__/e2e/playwright.config.ts
@@ -6,7 +6,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     retries: 1,
     globalSetup: require.resolve( './lib/global-setup' ),
-    timeout: 120000,
+    timeout: 60000,
     reporter: process.env.CI ? [ [ 'github' ] ] : 'line',
     reportSlowTests: null,
     workers: process.env.CI ? 1 : undefined,
