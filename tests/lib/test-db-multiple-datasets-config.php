daada8e1786f1b0ee32c87d4dab6caaf82da48c9
Luis Henrique Mulinari <luis.henrique.mulinari@automattic.com>
2023-09-01 14:45:17 -0300

    Extract error_handler

--- tests/lib/test-db-multiple-datasets-config.php
+++ tests/lib/test-db-multiple-datasets-config.php
@@ -12,6 +12,18 @@ class VIP_DatabaseMultipleDatasetsConfig_Test extends WP_UnitTestCase {
 	public function setUp(): void {
 		parent::setUp();
 		$this->wpdb_mock = new Wpdb_Mock();
+
+		// As of PHPUnit 10.x, expectWarning() is removed. We'll use a custom error handler to test for warnings.
+		// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_set_error_handler
+		set_error_handler(static function ( int $errno, string $errstr ) {
+			throw new Exception( $errstr, $errno );
+		}, E_USER_NOTICE );
+	}
+
+	public function tearDown(): void {
+		restore_error_handler();
+
+		parent::tearDown();
 	}
 
 	public function test__table_belonging_to_main_site() {
@@ -124,36 +136,24 @@ class VIP_DatabaseMultipleDatasetsConfig_Test extends WP_UnitTestCase {
 	}
 
 	public function test__multiple_datasets_pre_get_users__with_has_published_posts_should_throw_warning_and_cleanup_field() {
-		set_error_handler(static function ( int $errno, string $errstr ) {
-			throw new Exception( $errstr, $errno );
-		}, E_USER_NOTICE );
-
 		$this->expectExceptionMessage( 'WP_User_Query was called incorrectly. `has_published_posts` can not be used on sites with multiple datasets, users and posts tables use different DBs.' );
 
 		$wp_user_query = new WP_User_Query( [ 'has_published_posts' => true ] );
 
 		multiple_datasets_pre_get_users_cleanup( $wp_user_query );
 		self::assertNull( $wp_user_query->query_vars['has_published_posts'] );
-
-		restore_error_handler();
 	}
 
 	/**
 	 * @dataProvider orderby_post_count_provider
 	 */
 	public function test__multiple_datasets_pre_get_users__with_orderby_post_count_should_throw_warning_and_cleanup_field( $orderby, $expected_orderby ) {
-		set_error_handler(static function ( int $errno, string $errstr ) {
-			throw new Exception( $errstr, $errno );
-		}, E_USER_NOTICE );
-
 		$this->expectExceptionMessage( 'WP_User_Query was called incorrectly. `orderby = post_count` can not be used on sites with multiple datasets, users and posts tables use different DBs.' );
 
 		$wp_user_query = new WP_User_Query( [ 'orderby' => $orderby ] );
 
 		multiple_datasets_pre_get_users_cleanup( $wp_user_query );
 		self::assertEquals( $expected_orderby, $wp_user_query->query_vars['orderby'] );
-
-		restore_error_handler();
 	}
 
 	public function orderby_post_count_provider()
