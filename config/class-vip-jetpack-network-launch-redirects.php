a01a5e0ba28dd37a9e18aa28b3f093ce80df79c2
Andrea Grassi <andrea.grassi@automattic.com>
2023-09-15 18:22:07 +0200

    Revert "temporary disable code to check for is_jetpack"
    This reverts commit adf3405c4d02553f5390db77dd0c042768973671.

--- config/class-vip-jetpack-network-launch-redirects.php
+++ config/class-vip-jetpack-network-launch-redirects.php
@@ -22,11 +22,7 @@ class VIP_Jetpack_Network_Launch_Redirects {
 		if ( defined( 'DISABLE_VIP_LAUNCH_REDIRECTS' ) && true === constant( 'DISABLE_VIP_LAUNCH_REDIRECTS' ) ) {
 			return;
 		}
-		/**
-		 * Temporary disabled to see if it's causing issues with the redirect
-		 * TODO Re-enable me
-		 */
-		/*
+
 		// if they are not enrolled in the SYNC IDC program, skip redirects
 		if ( ! defined( 'JETPACK_SYNC_IDC_OPTIN' ) || true !== constant( 'JETPACK_SYNC_IDC_OPTIN' ) ) {
 			return;
@@ -35,8 +31,7 @@ class VIP_Jetpack_Network_Launch_Redirects {
 		// we care only about jetpack requests
 		if ( ! vip_is_jetpack_request() ) {
 			return;
-		}*/
-
+		}
 
 		// TODO we'll need to have some hashing in the options to avoid S&R to change it.
 		// TODO should we have some kind of validation to ensure the data is consistent?
