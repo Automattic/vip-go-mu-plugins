96ef57603ca5a2899fbe18a2730b245f94697281
Rebecca Hum <16962021+rebeccahum@users.noreply.github.com>
2023-09-21 15:02:07 -0600

    Add 12.6 to jetpack.php version meta

--- jetpack.php
+++ jetpack.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://jetpack.com
  * Description: Security, performance, and marketing tools made by WordPress experts. Jetpack keeps your site protected so you can focus on more important things.
  * Author: Automattic
- * Version: 12.5
+ * Version: 12.6
  * Author URI: https://jetpack.com
  * License: GPL2+
  * Text Domain: jetpack
