f9cf630b9af8d5bc63c70bfcc07c551087c341eb
ingeniumed <ingeniumed@users.noreply.github.com>
2023-10-03 09:52:02 +1100

    Initial rollout safeguard

--- vip-integrations.php
+++ vip-integrations.php
@@ -22,12 +22,16 @@ require_once __DIR__ . '/integrations/enums.php';
 require_once __DIR__ . '/integrations/integration-vip-config.php';
 require_once __DIR__ . '/integrations/block-data-api.php';
 require_once __DIR__ . '/integrations/parsely.php';
-require_once __DIR__ . '/integrations/vip-governance.php';
 
 // Register VIP integrations here.
 IntegrationsSingleton::instance()->register( new BlockDataApiIntegration( 'block-data-api' ) );
 IntegrationsSingleton::instance()->register( new ParselyIntegration( 'parsely' ) );
-IntegrationsSingleton::instance()->register( new VipGovernanceIntegration( 'vip-governance' ) );
+
+// ToDo: Remove this after the initial deployment of the VIP Governance integration.
+if ( file_exists( __DIR__ . '/integrations/vip-governance.php' ) ) {
+	require_once __DIR__ . '/integrations/vip-governance.php';
+	IntegrationsSingleton::instance()->register( new VipGovernanceIntegration( 'vip-governance' ) );
+}
 
 // @codeCoverageIgnoreEnd
 
