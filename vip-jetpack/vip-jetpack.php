d6426a2794a75c6339249e28b2703bd39a999e6f
Ahmed Sayeed Wasif <ahmed.wasif@automattic.com>
2023-10-25 18:21:19 +0600

    Remove my-jetpack-admin page

--- vip-jetpack/vip-jetpack.php
+++ vip-jetpack/vip-jetpack.php
@@ -73,6 +73,11 @@ add_filter( 'jetpack_get_available_modules', function ( $modules ) {
 }, 999 );
 
 /**
+ * Do not initialize my jetpack admin page
+ */
+add_filter( 'jetpack_my_jetpack_should_initialize', '__return_false' );
+
+/**
  * Lock down the jetpack_sync_settings_max_queue_size to an allowed range
  *
  * Still allows changing the value per site, but locks it into the range
