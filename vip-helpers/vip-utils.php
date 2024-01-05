5a87185df68997a35e0e03a041aac7c063390b68
Rebecca Hum <rebecca.hum@automattic.com>
2024-01-05 13:11:46 -0700

    Update vip-helpers/vip-utils.php
    Co-authored-by: Gary Jones <gary.jones@automattic.com>

--- vip-helpers/vip-utils.php
+++ vip-helpers/vip-utils.php
@@ -1642,11 +1642,12 @@ function vip_get_hyper_servers( $hyperdb, $operation = 'all', $dataset = 'global
 }
 
 /**
- * Checks if a given domain is a VIP convenience domain. Examples of a VIP convenience
- * domain are `example.go-vip.co` and `example.go-vip.net`.
+ * Checks if a given string looks like a VIP convenience domain.
  *
- * @param string $domain Domain to check
- * @return bool True if the domain is a convenience domain
+ * Examples of a VIP convenience domain are `example.go-vip.co` and `example.go-vip.net`.
+ *
+ * @param string $domain Domain to check.
+ * @return bool True if the domain is a convenience domain.
  */
 function is_vip_convenience_domain( string $domain ) {
 	if ( '' === $domain ) {
