69c3024a7d094fbe608fe52e6352efe7ece39fb8
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2024-01-12 00:40:33 +0200

    test: fix flaky E2E tests, properly set up linting, fix code

--- __tests__/e2e/lib/pages/wp-editor-page.ts
+++ __tests__/e2e/lib/pages/wp-editor-page.ts
@@ -4,224 +4,224 @@
 import { Page } from '@playwright/test';
 
 const selectors = {
-    // Editor
-    editorTitle: '.editor-post-title__input',
-    editorTitleContainer: '.edit-post-visual-editor__post-title-wrapper',
+	// Editor
+	editorTitle: '.editor-post-title__input',
+	editorTitleContainer: '.edit-post-visual-editor__post-title-wrapper',
 
-    // Block inserter
-    blockInserterToggle: 'button.edit-post-header-toolbar__inserter-toggle',
-    blockInserterPanel: '.block-editor-inserter__content',
-    blockSearch: '.block-editor-inserter__search input[type="search"]',
-    blockInserterResultItem: '.block-editor-block-types-list__list-item',
+	// Block inserter
+	blockInserterToggle: 'button.edit-post-header-toolbar__inserter-toggle',
+	blockInserterPanel: '.block-editor-inserter__content',
+	blockSearch: '.block-editor-inserter__search input[type="search"]',
+	blockInserterResultItem: '.block-editor-block-types-list__list-item',
 
-    // Within the editor body.
-    blockAppender: '.block-editor-default-block-appender',
-    blockInserter: '.block-editor-inserter__toggle',
-    paragraphBlocks: 'p.wp-block-paragraph',
-    block: '.wp-block[id*="block-"][data-empty="false"]',
-    blockWarning: '.block-editor-warning',
-    imageBlocks: '.editor-block-list-item-image',
-    uploadImageButton: '.block-editor-media-placeholder__upload-button',
-    firstEmptyBlock: '.wp-block-paragraph[data-empty="true"]',
-    spinner: '.components-spinner',
+	// Within the editor body.
+	blockAppender: '.block-editor-default-block-appender',
+	blockInserter: '.block-editor-inserter__toggle',
+	paragraphBlocks: 'p.wp-block-paragraph',
+	block: '.wp-block[id*="block-"][data-empty="false"]',
+	blockWarning: '.block-editor-warning',
+	imageBlocks: '.editor-block-list-item-image',
+	uploadImageButton: '.block-editor-media-placeholder__upload-button',
+	firstEmptyBlock: '.wp-block-paragraph[data-empty="true"]',
+	spinner: '.components-spinner',
 
-    // Top bar selectors.
-    postToolbar: '.edit-post-header',
-    settingsToggle: '.edit-post-header__settings .interface-pinned-items button:first-child',
-    saveDraftButton: '.editor-post-save-draft',
-    previewButton: ':is(button:text("Preview"), a:text("Preview"))',
-    publishButton: ( parentSelector: string ) => `${ parentSelector } button:text("Publish")[aria-disabled=false]`,
-    updateButton: '.editor-post-publish-button',
-    // Settings panel.
-    settingsPanel: '.interface-complementary-area',
+	// Top bar selectors.
+	postToolbar: '.edit-post-header',
+	settingsToggle: '.edit-post-header__settings .interface-pinned-items button:first-child',
+	saveDraftButton: '.editor-post-save-draft',
+	previewButton: ':is(button:text("Preview"), a:text("Preview"))',
+	publishButton: ( parentSelector: string ) => `${ parentSelector } button:text("Publish")[aria-disabled=false]`,
+	updateButton: '.editor-post-publish-button',
+	// Settings panel.
+	settingsPanel: '.interface-complementary-area',
 
-    // Publish panel (including post-publish)
-    publishPanel: '.editor-post-publish-panel',
-    viewButton: '.editor-post-publish-panel a:has-text("View")',
-    addNewButton: '.editor-post-publish-panel a:text-matches("Add a New P(ost|age)")',
-    closePublishPanel: 'button[aria-label="Close panel"]',
+	// Publish panel (including post-publish)
+	publishPanel: '.editor-post-publish-panel',
+	viewButton: '.editor-post-publish-panel a:has-text("View")',
+	addNewButton: '.editor-post-publish-panel a:text-matches("Add a New P(ost|age)")',
+	closePublishPanel: 'button[aria-label="Close panel"]',
 
-    // Welcome tour
-    welcomeTourCloseButton: '.edit-post-welcome-guide .components-modal__header button',
+	// Welcome tour
+	welcomeTourCloseButton: '.edit-post-welcome-guide .components-modal__header button',
 
-    // Block editor sidebar
-    desktopEditorSidebarButton: 'button[aria-label="Block editor sidebar"]:visible',
-    desktopDashboardLink: 'a[aria-description="Returns to the dashboard"]:visible',
-    mobileDashboardLink: 'a[aria-current="page"]:visible',
+	// Block editor sidebar
+	desktopEditorSidebarButton: 'button[aria-label="Block editor sidebar"]:visible',
+	desktopDashboardLink: 'a[aria-description="Returns to the dashboard"]:visible',
+	mobileDashboardLink: 'a[aria-current="page"]:visible',
 
-    // Choose a pattern
-    choosePatternCloseButton: '.components-modal__screen-overlay .components-modal__header button',
+	// Choose a pattern
+	choosePatternCloseButton: '.components-modal__screen-overlay .components-modal__header button',
 };
 
 export class EditorPage {
-    private page: Page;
+	private page: Page;
 
-    /**
-     * Constructs an instance of the component.
-     *
-     * @param { Page } page The underlying page
-     */
-    constructor( page: Page ) {
-        this.page = page;
-    }
+	/**
+	 * Constructs an instance of the component.
+	 *
+	 * @param { Page } page The underlying page
+	 */
+	constructor( page: Page ) {
+		this.page = page;
+	}
 
-    /**
-     * Dismisses the Welcome Tour (card) if it is present.
-     */
-    async dismissWelcomeTour(): Promise<void> {
-        try {
-            await this.page.waitForSelector( selectors.welcomeTourCloseButton, {
-                state: 'visible',
-                timeout: 5000,
-            } );
-        } catch ( err ) {
-            return;
-        }
+	/**
+	 * Dismisses the Welcome Tour (card) if it is present.
+	 */
+	async dismissWelcomeTour(): Promise<void> {
+		try {
+			await this.page.waitForSelector( selectors.welcomeTourCloseButton, {
+				state: 'visible',
+				timeout: 5000,
+			} );
+		} catch ( err ) {
+			return;
+		}
 
-        const closeButton = this.page.locator( selectors.welcomeTourCloseButton );
-        return closeButton.click( {
-            delay: 20,
-        } );
-    }
+		const closeButton = this.page.locator( selectors.welcomeTourCloseButton );
+		return closeButton.click( {
+			delay: 20,
+		} );
+	}
 
-    async dismissPatternSelector(): Promise<void> {
-        try {
-            await this.page.waitForSelector( selectors.choosePatternCloseButton, {
-                state: 'visible',
-                timeout: 5000,
-            } );
-        } catch ( err ) {
-            return;
-        }
+	async dismissPatternSelector(): Promise<void> {
+		try {
+			await this.page.waitForSelector( selectors.choosePatternCloseButton, {
+				state: 'visible',
+				timeout: 5000,
+			} );
+		} catch ( err ) {
+			return;
+		}
 
-        const closeButton = this.page.locator( selectors.choosePatternCloseButton );
-        return closeButton.click( {
-            delay: 20,
-        } );
-    }
+		const closeButton = this.page.locator( selectors.choosePatternCloseButton );
+		return closeButton.click( {
+			delay: 20,
+		} );
+	}
 
-    /**
-     * Enter Title of page or post
-     *
-     * @param {string} title Page/Post Title
-     */
-    async enterTitle( title: string ): Promise<void> {
-        await this.page.click( selectors.editorTitleContainer );
-        await this.page.fill( selectors.editorTitle, title );
-    }
+	/**
+	 * Enter Title of page or post
+	 *
+	 * @param {string} title Page/Post Title
+	 */
+	async enterTitle( title: string ): Promise<void> {
+		await this.page.click( selectors.editorTitleContainer );
+		await this.page.fill( selectors.editorTitle, title );
+	}
 
-    /**
-     * Enter text in to page or post
-     *
-     * @param {string} text Text to enter
-     */
-    async enterText( text: string ): Promise<void> {
-        const lines = text.split( '\n' );
-        if ( await this.page.isVisible( selectors.blockAppender ) ) {
-            await this.page.click( selectors.blockAppender );
-        } else {
-            await this.page.click( selectors.paragraphBlocks );
-        }
+	/**
+	 * Enter text in to page or post
+	 *
+	 * @param {string} text Text to enter
+	 */
+	async enterText( text: string ): Promise<void> {
+		const lines = text.split( '\n' );
+		if ( await this.page.isVisible( selectors.blockAppender ) ) {
+			await this.page.click( selectors.blockAppender );
+		} else {
+			await this.page.click( selectors.paragraphBlocks );
+		}
 
-        // Playwright does not break up newlines in Gutenberg. This causes issues when we expect
-        // text to be broken into new lines/blocks. This presents an unexpected issue when entering
-        // text such as 'First sentence\nSecond sentence', as it is all put in one line.
-        // frame.type() will respect newlines like a human would, but it is slow.
-        // This approach will run faster than using frame.type() while respecting the newline chars.
-        await Promise.all(
-            lines.map( async ( line, index ) => {
-                await this.page.fill( `${ selectors.paragraphBlocks }:nth-of-type(${ index + 1 })`, line );
-                await this.page.keyboard.press( 'Enter' );
-            } ),
-        );
-    }
+		// Playwright does not break up newlines in Gutenberg. This causes issues when we expect
+		// text to be broken into new lines/blocks. This presents an unexpected issue when entering
+		// text such as 'First sentence\nSecond sentence', as it is all put in one line.
+		// frame.type() will respect newlines like a human would, but it is slow.
+		// This approach will run faster than using frame.type() while respecting the newline chars.
+		await Promise.all(
+			lines.map( async ( line, index ) => {
+				await this.page.fill( `${ selectors.paragraphBlocks }:nth-of-type(${ index + 1 })`, line );
+				await this.page.keyboard.press( 'Enter' );
+			} ),
+		);
+	}
 
-    /**
-     * Clear Title of page or post
-     */
-    async clearTitle(): Promise<void> {
-        await this.page.click( selectors.editorTitle );
-        await this.page.keyboard.down( 'Shift' );
-        await this.page.keyboard.press( 'Home' );
-        await this.page.keyboard.up( 'Shift' );
-        await this.page.keyboard.press( 'Backspace' );
-    }
+	/**
+	 * Clear Title of page or post
+	 */
+	async clearTitle(): Promise<void> {
+		await this.page.click( selectors.editorTitle );
+		await this.page.keyboard.down( 'Shift' );
+		await this.page.keyboard.press( 'Home' );
+		await this.page.keyboard.up( 'Shift' );
+		await this.page.keyboard.press( 'Backspace' );
+	}
 
-    /**
-     * Clear text of page or post
-     */
-    async clearText(): Promise<void> {
-        /* eslint-disable no-await-in-loop */
-        while ( await this.page.isVisible( selectors.block ) ) {
-            await this.page.click( selectors.block );
-            await this.page.keyboard.down( 'Shift' );
-            await this.page.keyboard.press( 'Home' );
-            await this.page.keyboard.up( 'Shift' );
-            await this.page.keyboard.press( 'Backspace' );
-            await this.page.keyboard.press( 'Backspace' );
-        }
-        /* eslint-enable no-await-in-loop */
-    }
+	/**
+	 * Clear text of page or post
+	 */
+	async clearText(): Promise<void> {
+		/* eslint-disable no-await-in-loop */
+		while ( await this.page.isVisible( selectors.block ) ) {
+			await this.page.click( selectors.block );
+			await this.page.keyboard.down( 'Shift' );
+			await this.page.keyboard.press( 'Home' );
+			await this.page.keyboard.up( 'Shift' );
+			await this.page.keyboard.press( 'Backspace' );
+			await this.page.keyboard.press( 'Backspace' );
+		}
+		/* eslint-enable no-await-in-loop */
+	}
 
-    /**
-     * Add Image to Post or Page
-     *
-     * @param {string} fileName Name of image file to add
-     */
-    async addImage( fileName: string ): Promise<void> {
-        if ( await this.page.isVisible( selectors.blockAppender ) ) {
-            await this.page.click( selectors.blockAppender );
-        } else {
-            await this.page.click( selectors.editorTitleContainer );
-            await this.page.click( selectors.blockInserter );
-        }
-        await this.page.click( selectors.imageBlocks );
+	/**
+	 * Add Image to Post or Page
+	 *
+	 * @param {string} fileName Name of image file to add
+	 */
+	async addImage( fileName: string ): Promise<void> {
+		if ( await this.page.isVisible( selectors.blockAppender ) ) {
+			await this.page.click( selectors.blockAppender );
+		} else {
+			await this.page.click( selectors.editorTitleContainer );
+			await this.page.click( selectors.blockInserter );
+		}
+		await this.page.click( selectors.imageBlocks );
 
-        const [ fileChooser ] = await Promise.all( [
-            // It is important to call waitForEvent before click to set up waiting.
-            this.page.waitForEvent( 'filechooser' ),
-            // This has to click twice, the first focuses in the block, the second opens the upload
-            this.page.click( selectors.uploadImageButton ),
-            this.page.click( selectors.uploadImageButton ),
-        ] );
-        await fileChooser.setFiles( fileName );
-        await this.page.waitForSelector( selectors.spinner, { state: 'detached' } );
-    }
+		const [ fileChooser ] = await Promise.all( [
+			// It is important to call waitForEvent before click to set up waiting.
+			this.page.waitForEvent( 'filechooser' ),
+			// This has to click twice, the first focuses in the block, the second opens the upload
+			this.page.click( selectors.uploadImageButton ),
+			this.page.click( selectors.uploadImageButton ),
+		] );
+		await fileChooser.setFiles( fileName );
+		await this.page.waitForSelector( selectors.spinner, { state: 'detached' } );
+	}
 
-    /**
-     * Publishes the post or page.
-     *
-     * @param {boolean} visit Whether to then visit the page.
-     * @return {string} Url of published post or page
-     */
-    async publish( { visit = false }: { visit?: boolean } = {} ): Promise<string> {
-        await this.page.click( selectors.publishButton( selectors.postToolbar ) );
-        await this.page.click( selectors.publishButton( selectors.publishPanel ) );
-        const viewPublishedArticleButton = await this.page.waitForSelector( selectors.viewButton );
-        const publishedURL = ( await viewPublishedArticleButton.getAttribute( 'href' ) ) as string;
+	/**
+	 * Publishes the post or page.
+	 *
+	 * @param {boolean} visit Whether to then visit the page.
+	 * @return {string} Url of published post or page
+	 */
+	async publish( { visit = false }: { visit?: boolean } = {} ): Promise<string> {
+		await this.page.click( selectors.publishButton( selectors.postToolbar ) );
+		await this.page.click( selectors.publishButton( selectors.publishPanel ) );
+		const viewPublishedArticleButton = await this.page.waitForSelector( selectors.viewButton );
+		const publishedURL = ( await viewPublishedArticleButton.getAttribute( 'href' ) )!;
 
-        if ( visit ) {
-            await this.visitPublishedPost( publishedURL );
-        }
-        return publishedURL;
-    }
+		if ( visit ) {
+			await this.visitPublishedPost( publishedURL );
+		}
+		return publishedURL;
+	}
 
-    /**
-     * Updates the post or page.
-     */
-    update(): Promise<void> {
-        return this.page.click( selectors.updateButton );
-    }
+	/**
+	 * Updates the post or page.
+	 */
+	update(): Promise<void> {
+		return this.page.click( selectors.updateButton );
+	}
 
-    /**
-     * Visits the published entry from the post-publish sidebar.
-     *
-     * @param {string} url Url to visit
-     */
-    private visitPublishedPost( url: string ): Promise<unknown> {
-        return Promise.all( [
-            this.page.waitForNavigation( { waitUntil: 'networkidle', url } ),
-            this.page.click( selectors.viewButton ),
-        ] );
-    }
+	/**
+	 * Visits the published entry from the post-publish sidebar.
+	 *
+	 * @param {string} url Url to visit
+	 */
+	private visitPublishedPost( url: string ): Promise<unknown> {
+		return Promise.all( [
+			this.page.waitForURL( url, { waitUntil: 'load' } ),
+			this.page.click( selectors.viewButton ),
+		] );
+	}
 }
--- __tests__/e2e/specs/page__edit.spec.ts
+++ __tests__/e2e/specs/page__edit.spec.ts
@@ -6,70 +6,76 @@ import { expect, test } from '@playwright/test';
 /**
  * Internal dependencies
  */
-import { EditorPage } from '../lib/pages/wp-editor-page';
-import { PublishedPagePage } from '../lib/pages/published-page-page';
-import { PageListPage } from '../lib/pages/page-list-page';
 import * as DataHelper from '../lib/data-helper';
+import { PageListPage } from '../lib/pages/page-list-page';
+import { PublishedPagePage } from '../lib/pages/published-page-page';
+import { EditorPage } from '../lib/pages/wp-editor-page';
 import * as WPAPIHelper from '../lib/wp-api-helper';
 
 let titleText = DataHelper.getRandomPhrase();
 let bodyText =
-    '<!-- wp:paragraph --><p>"Sometimes you will never know the value of a moment, until it becomes a memory."</p><!-- /wp:paragraph -->' +
-    '<!-- wp:paragraph --><p>– Dr. Seuss</p><!-- /wp:paragraph -->';
+	'<!-- wp:paragraph --><p>"Sometimes you will never know the value of a moment, until it becomes a memory."</p><!-- /wp:paragraph -->' +
+	'<!-- wp:paragraph --><p>– Dr. Seuss</p><!-- /wp:paragraph -->';
 let postID: string;
 let postURL: string;
 
 test.beforeAll( async ( { request } ) => {
-    // Create new page to edit
-    const response = await WPAPIHelper.createPost( request, {
-        title: titleText,
-        body: bodyText,
-        postType: 'page',
-    } );
-    const responseJSON = await response.json();
-    postID = responseJSON.id;
-    postURL = responseJSON.link;
-    expect( response.ok() ).toBeTruthy();
+	interface JSONResponse {
+		id: string;
+		link: string;
+	}
+
+	// Create new page to edit
+	const response = await WPAPIHelper.createPost( request, {
+		title: titleText,
+		body: bodyText,
+		postType: 'page',
+	} );
+	const responseJSON = await response.json() as JSONResponse;
+	postID = responseJSON.id;
+	postURL = responseJSON.link;
+	expect( response.ok() ).toBeTruthy();
 } );
 
 test.afterAll( async ( { request } ) => {
-    // Delete created page
-    const response = await WPAPIHelper.deletePost( request, postID, 'page' );
-    expect( response.ok() ).toBeTruthy();
+	// Delete created page
+	const response = await WPAPIHelper.deletePost( request, postID, 'page' );
+	expect( response.ok() ).toBeTruthy();
 } );
 
+// eslint-disable-next-line playwright/expect-expect
 test( 'edit a Page', async ( { page } ) => {
-    let editorPage: EditorPage;
-    let pageListPage: PageListPage;
+	let editorPage: EditorPage;
+	let pageListPage: PageListPage;
 
-    await test.step( 'Go to Page List page', () => {
-        pageListPage = new PageListPage( page );
-        return pageListPage.visit();
-    } );
+	await test.step( 'Go to Page List page', () => {
+		pageListPage = new PageListPage( page );
+		return pageListPage.visit();
+	} );
 
-    await test.step( 'Select post to edit', () => {
-        return pageListPage.editPageByID( postID );
-    } );
+	await test.step( 'Select post to edit', () => {
+		return pageListPage.editPageByID( postID );
+	} );
 
-    await test.step( 'Edit Page', async () => {
-        titleText = DataHelper.getRandomPhrase();
-        bodyText =
-            '"Many of life’s failures are people who did not realize how close they were to success when they gave up. \n' +
-            '– Thomas A. Edison';
-        editorPage = new EditorPage( page );
-        await editorPage.clearText();
-        await editorPage.clearTitle();
-        await editorPage.enterTitle( titleText );
-        await editorPage.enterText( bodyText );
-    } );
+	await test.step( 'Edit Page', async () => {
+		titleText = DataHelper.getRandomPhrase();
+		bodyText =
+			'"Many of life’s failures are people who did not realize how close they were to success when they gave up. \n' +
+			'– Thomas A. Edison';
+		editorPage = new EditorPage( page );
+		await editorPage.clearText();
+		await editorPage.clearTitle();
+		await editorPage.enterTitle( titleText );
+		await editorPage.enterText( bodyText );
+	} );
 
-    await test.step( 'Publish changes and visit page', async () => {
-        await editorPage.update();
-        return page.goto( postURL );
-    } );
+	await test.step( 'Publish changes and visit page', async () => {
+		await editorPage.update();
+		return page.goto( postURL );
+	} );
 
-    await test.step( 'Validate published page', async () => {
-        const publishedPagePage = new PublishedPagePage( page );
-        return publishedPagePage.validateTextInPost( titleText );
-    } );
+	await test.step( 'Validate published page', async () => {
+		const publishedPagePage = new PublishedPagePage( page );
+		return publishedPagePage.validateTextInPost( titleText );
+	} );
 } );
