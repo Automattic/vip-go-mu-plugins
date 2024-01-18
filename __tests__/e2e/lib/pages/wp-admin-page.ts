c7ced887246dbf5035914a4b441a294c87797f8d
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2024-01-18 18:34:50 +0200

    test(e2e): fix Unexpected use of page.waitForSelector()

--- __tests__/e2e/lib/pages/wp-admin-page.ts
+++ __tests__/e2e/lib/pages/wp-admin-page.ts
@@ -1,7 +1,4 @@
-/**
- * External dependencies
- */
-import { Locator, Page } from '@playwright/test';
+import type { Locator, Page } from '@playwright/test';
 
 const selectors = {
 	adminBar: '#wpadminbar',
@@ -12,18 +9,18 @@ export class WPAdminPage {
 	readonly adminBar: Locator;
 
 	/**
-     * Constructs an instance of the component.
-     *
-     * @param { Page } page The underlying page
-     */
+	 * Constructs an instance of the component.
+	 *
+	 * @param { Page } page The underlying page
+	 */
 	constructor( page: Page ) {
 		this.page = page;
 		this.adminBar = page.locator( selectors.adminBar );
 	}
 
 	/**
-     * Navigate to WP Admin
-     */
+	 * Navigate to WP Admin
+	 */
 	visit(): Promise<unknown> {
 		return this.page.goto( '/wp-admin' );
 	}
