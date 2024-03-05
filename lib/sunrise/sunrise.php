f1e0b436ca54661fc87e44505f77280dbe9d44ac
Luis Henrique Mulinari <luis.henrique.mulinari@automattic.com>
2024-03-05 16:06:37 -0300

    Remove unecessary comments

--- lib/sunrise/sunrise.php
+++ lib/sunrise/sunrise.php
@@ -75,8 +75,6 @@ function handle_not_found_error( $error_type ) {
 
 	$is_web_request = Context::is_web_request();
 	if ( $is_web_request ) {
-		// Sunrise is loaded before any mu plugins, including 000-vip-init.php
-		// So we can't use the WPVIP_MU_PLUGIN_DIR constant here
 		$mu_plugin_dir       = defined( 'WPVIP_MU_PLUGIN_DIR' ) ? constant( 'WPVIP_MU_PLUGIN_DIR' ) : constant( 'WP_CONTENT_DIR' ) . '/mu-plugins';
 		$is_maintenance_mode = Context::is_maintenance_mode();
 		if ( $is_maintenance_mode ) {
