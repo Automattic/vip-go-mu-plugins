e08a29004735dba11fd0c7a0a405e2bb273ed434
Alex Cicovic <23142906+acicovic@users.noreply.github.com>
2024-05-14 11:57:07 +0300

    Add and set wp-parsely 3.15 as the default version

--- wp-parsely.php
+++ wp-parsely.php
@@ -21,19 +21,12 @@ namespace Automattic\VIP\WP_Parsely_Integration;
  * The default version is the first entry in the SUPPORTED_VERSIONS list.
  */
 const SUPPORTED_VERSIONS = [
+	'3.15',
 	'3.14',
 	'3.13',
 	'3.12',
-	'3.11',
-	'3.10',
-	'3.9',
-	'3.8',
 	'3.7',
-	'3.6',
 	'3.5',
-	'3.3',
-	'3.2',
-	'3.1',
 ];
 
 /**
