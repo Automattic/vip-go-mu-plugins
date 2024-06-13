ea9f2efb42ad407e36f47f7830a8284bd0b2a236
Mehmood Ahmad <31419912+mehmoodak@users.noreply.github.com>
2024-06-13 18:02:01 +0500

    ADDS: Support for handling multiple configs of the integration

--- integrations/vip-integrations-config.php
+++ integrations/vip-integrations-config.php
@@ -7,9 +7,6 @@
 
 namespace Automattic\VIP\Integrations;
 
-use Org_Integration_Status;
-use Env_Integration_Status;
-
 /**
  * Class for managing configuration of integrations provided by VIP.
  *
@@ -88,7 +85,6 @@ class VipIntegrationsConfig {
 		return self::$instance;
 	}
 
-
 	/**
 	 * Read configs provided by VIP.
 	 */
@@ -128,102 +124,13 @@ class VipIntegrationsConfig {
 				$type = $config['type'];
 				unset( $config['type'] );
 
-				if ( ! isset( $result[ $type ] ) ) {
-					$this->configs[ $type ] = $config;
+				if ( ! isset( $this->configs[ $type ] ) ) {
+					$this->configs[ $type ] = [];
 				}
+
+				$this->configs[ $type ][] = $config;
 			}
 		}
-	}
-
-	/**
-	 * Returns `true` if the integration is enabled in VIP config else `false`.
-	 *
-	 * @param string $slug A unique identifier for the integration.
-	 *
-	 * @return bool
-	 *
-	 * @private
-	 */
-	public function is_active_via_vip( string $slug ): bool {
-		return Env_Integration_Status::ENABLED === $this->get_site_status( $slug );
-	}
-
-	/**
-	 * Get site status.
-	 *
-	 * @param string $slug A unique identifier for the integration.
-	 *
-	 * @return string|null
-	 */
-	private function get_site_status( string $slug ): ?string {
-		$vip_config = $this->get_vip_config( $slug );
-
-		if ( $this->get_value_from_config( $vip_config, 'org', 'status' ) === Org_Integration_Status::BLOCKED ) {
-			return Org_Integration_Status::BLOCKED;
-		}
-
-		// Look into network_sites config before and then fallback to env config.
-		return $this->get_value_from_config( $vip_config, 'network_sites', 'status' ) ??
-			$this->get_value_from_config( $vip_config, 'env', 'status' );
-	}
-
-	/**
-	 * Get site config.
-	 *
-	 * @param string $slug A unique identifier for the integration.
-	 *
-	 * @return array
-	 */
-	public function get_site_config( string $slug ) {
-		$vip_config = $this->get_vip_config( $slug );
-
-		if ( is_multisite() ) {
-			$config = $this->get_value_from_config( $vip_config, 'network_sites', 'config' );
-			// If network site config is not found then fallback to env config if it exists.
-			if ( empty( $config ) && true === $this->get_value_from_config( $config, 'env', 'cascade_config' ) ) {
-				$config = $this->get_value_from_config( $vip_config, 'env', 'config' );
-			}
-		} else {
-			$config = $this->get_value_from_config( $vip_config, 'env', 'config' );
-		}
-
-		return $config ?? array(); // To keep function signature consistent.
-	}
-
-	/**
-	 * Get config value based on given type and key.
-	 *
-	 * @param array<string, array<string, mixed> $vip_config Configurations provided by VIP.
-	 * @param string                             $config_type Type of the config whose data is needed i.e. org, env, network-sites etc.
-	 * @param string                             $key Key of the config from which we have to extract the data.
-	 *
-	 * @return null|string|array Returns `null` if key is not found, `string` if key is "status" and `array` if key is "config".
-	 */
-	private function get_value_from_config( array $vip_config, string $config_type, string $key ) {
-
-		if ( ! in_array( $config_type, [ 'org', 'env', 'network_sites' ], true ) ) {
-			trigger_error( 'config_type param (' . esc_html( $config_type ) . ') must be one of org, env or network_sites.', E_USER_WARNING ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-			return null;
-		}
-
-		if ( ! isset( $vip_config[ $config_type ] ) ) {
-			return null;
-		}
-
-		// Look for key inside org or env config.
-		if ( 'network_sites' !== $config_type && isset( $vip_config[ $config_type ][ $key ] ) ) {
-			return $vip_config[ $config_type ][ $key ];
-		}
-
-		// Look for key inside network-sites config.
-		$network_site_id = get_current_blog_id();
-		if ( 'network_sites' === $config_type && isset( $vip_config[ $config_type ][ $network_site_id ] ) ) {
-			if ( isset( $vip_config[ $config_type ][ $network_site_id ][ $key ] ) ) {
-				return $vip_config[ $config_type ][ $network_site_id ][ $key ];
-			}
-		}
-
-		return null;
 	}
 
 	/**
@@ -233,7 +140,7 @@ class VipIntegrationsConfig {
 	 *
 	 * @return array
 	 */
-	public function get_vip_config( string $slug ): array {
+	public function get_vip_configs( string $slug ): array {
 		return $this->configs[ $slug ] ?? [];
 	}
 }
