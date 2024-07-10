ada6e6f4b0a6a3e1007a04e323a933fd0991f7d9
Rebecca Hum <rebeccahum@gmail.com>
2024-07-10 14:32:52 -0600

    Remove Admin Notice displayed for WP 5.9 since VIP no longer supports it

--- admin-notice/admin-notice.php
+++ admin-notice/admin-notice.php
@@ -26,24 +26,3 @@ add_action(
 		do_action( 'vip_admin_notice_init', $admin_notice_controller );
 	}
 );
-
-// Old WP version w/o pinned Jetpack version
-add_action(
-	'vip_admin_notice_init',
-	function ( $admin_notice_controller ) {
-		global $wp_version;
-		$message = "We've noticed that you are running WordPress {$wp_version}, which is an outdated version. This prevents you from running the latest version of Jetpack, as the current version of Jetpack only supports 5.9 and up. Please upgrade to the most recent WordPress version to use the latest features of Jetpack.";
-
-		$admin_notice_controller->add(
-			new Admin_Notice(
-				$message,
-				[
-					new Expression_Condition( version_compare( $wp_version, '5.9', '<' ) ),
-					new Expression_Condition( ! defined( 'VIP_JETPACK_PINNED_VERSION' ) ),
-				],
-				'old-wp-versions',
-				'error'
-			)
-		);
-	}
-);
