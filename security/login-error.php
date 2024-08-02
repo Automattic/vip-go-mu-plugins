40585525feef916fa279afc14a61bcdcd1fc2b49
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2024-08-03 02:52:27 +0300

    test: update tests

--- security/login-error.php
+++ security/login-error.php
@@ -41,7 +41,7 @@ function use_ambiguous_login_error( $error ): string {
 add_filter( 'login_errors', __NAMESPACE__ . '\use_ambiguous_login_error', 99, 1 );
 
 function use_ambiguous_password_reset_confirmation( $errors ) {
-	if ( is_wp_error( $errors ) && $errors->has_errors() && ! headers_sent() ) {
+	if ( isset( $_SERVER['REQUEST_METHOD'] ) && 'POST' === $_SERVER['REQUEST_METHOD'] && is_wp_error( $errors ) && $errors->has_errors() && ! headers_sent() ) {
 		// phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
 		$redirect_to = ! empty( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : 'wp-login.php?checkemail=confirm';
 		wp_safe_redirect( $redirect_to );
@@ -49,9 +49,7 @@ function use_ambiguous_password_reset_confirmation( $errors ) {
 	}
 }
 
-if ( isset( $_SERVER['REQUEST_METHOD'] ) && 'POST' === $_SERVER['REQUEST_METHOD'] ) {
-	add_action( 'lost_password', __NAMESPACE__ . '\\use_ambiguous_password_reset_confirmation', 0 );
-}
+add_action( 'lost_password', __NAMESPACE__ . '\\use_ambiguous_password_reset_confirmation', 0 );
 
 /**
  * Use a message that does not reveal the type of login error in an attempted brute-force on forget password.
