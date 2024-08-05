17ecf7792c84b89ea58bff6c3591993683d8feff
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2024-08-05 21:44:00 +0300

    test: add REST and XML-RPC smoke tests

--- __tests__/e2e/specs/generic.spec.ts
+++ __tests__/e2e/specs/generic.spec.ts
@@ -9,4 +9,32 @@ test.describe( 'Generic Checks', () => {
 		const html = await page.content();
 		expect( html ).toContain( '</html>' );
 	} );
+
+	test( 'REST API smoke test', async ( { request } ) => {
+		const response = await request.get( './wp-json/' );
+		expect( response.status() ).toBe( 200 );
+		const data: unknown = await response.json();
+		expect( typeof data ).toBe( 'object' );
+		expect.soft( data ).toHaveProperty( 'name' );
+		expect.soft( data ).toHaveProperty( 'description' );
+		expect.soft( data ).toHaveProperty( 'url' );
+		expect.soft( data ).toHaveProperty( 'routes' );
+	} );
+
+	test( 'XML RPC smoke test', async ( { request } ) => {
+		const xmlPayload = '<?xml version="1.0"?><methodCall><methodName>demo.sayHello</methodName><params/></methodCall>';
+
+		const response = await request.post( './xmlrpc.php', {
+			headers: {
+				'Content-Type': 'text/xml',
+			},
+			data: xmlPayload,
+		} );
+
+		expect( response.status() ).toBe( 200 );
+		const responseText = await response.text();
+		expect( responseText ).toContain( '<methodResponse>' );
+		expect( responseText ).not.toContain( '<fault>' );
+		expect( responseText ).toContain( '<string>Hello!</string>' );
+	} );
 } );
