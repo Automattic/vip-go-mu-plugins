0e0b2844736582705d64527cb8bf64d9232a3ced
Alexey Kopytko <alexey.kopytko@automattic.com>
2024-09-18 16:17:04 +0900

    tests

--- tests/telemetry/tracks/test-class-tracks-event.php
+++ tests/telemetry/tracks/test-class-tracks-event.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Automattic\VIP\Telemetry;
 
+use Automattic\Test\Constant_Mocker;
 use WP_UnitTestCase;
-use Automattic\VIP\Support_User\Role;
+use WP_Error;
 
 class Tracks_Event_Test extends WP_UnitTestCase {
 
@@ -18,9 +19,9 @@ class Tracks_Event_Test extends WP_UnitTestCase {
 	public static function setUpBeforeClass(): void {
 		parent::setUpBeforeClass();
 
-		define( 'VIP_TELEMETRY_SALT', self::VIP_TELEMETRY_SALT );
-		define( 'VIP_GO_APP_ENVIRONMENT', self::VIP_GO_APP_ENVIRONMENT );
-		define( 'VIP_ORG_ID', self::VIP_ORG_ID );
+		Constant_Mocker::define( 'VIP_TELEMETRY_SALT', self::VIP_TELEMETRY_SALT );
+		Constant_Mocker::define( 'VIP_GO_APP_ENVIRONMENT', self::VIP_GO_APP_ENVIRONMENT );
+		Constant_Mocker::define( 'VIP_ORG_ID', self::VIP_ORG_ID );
 	}
 
 	public function test_should_create_event() {
@@ -35,7 +36,7 @@ class Tracks_Event_Test extends WP_UnitTestCase {
 
 		$event = new Tracks_Event( 'prefix_', 'test_event', [ 'property1' => 'value1' ] );
 
-		$this->assertInstanceOf( \stdClass::class, $event->get_data() );
+		$this->assertInstanceOf( Tracks_Event_DTO::class, $event->get_data() );
 		$this->assertSame( 'prefix_test_event', $event->get_data()->_en );
 		$this->assertSame( 'value1', $event->get_data()->property1 );
 		$this->assertSame( hash_hmac( 'sha256', $user->user_email, self::VIP_TELEMETRY_SALT ), $event->get_data()->_ui );
@@ -47,24 +48,24 @@ class Tracks_Event_Test extends WP_UnitTestCase {
 	public function test_should_return_error_on_invalid_event_name() {
 		$event = new Tracks_Event( 'prefix_', 'invalid_event', [ 'property1' => 'value1' ] );
 
-		$this->assertInstanceOf( \WP_Error::class, $event->get_data() );
+		$this->assertInstanceOf( WP_Error::class, $event->get_data() );
 	}
 
 	public function test_should_return_error_on_invalid_property_name() {
 		$event = new Tracks_Event( 'prefix_', 'test_event', [ 'invalid_property' => 'value1' ] );
 
-		$this->assertInstanceOf( \WP_Error::class, $event->get_data() );
+		$this->assertInstanceOf( WP_Error::class, $event->get_data() );
 	}
 
 	public function test_should_return_error_on_invalid_property_value() {
 		$event = new Tracks_Event( 'prefix_', 'test_event', [ 'property1' => [ 'value1' ] ] );
 
-		$this->assertInstanceOf( \WP_Error::class, $event->get_data() );
+		$this->assertInstanceOf( WP_Error::class, $event->get_data() );
 	}
 
 	public function test_is_recordable_should_return_wp_error_on_invalid_event() {
 		$event = new Tracks_Event( 'prefix_', 'invalid_event', [ 'property1' => 'value1' ] );
 
-		$this->assertInstanceOf( \WP_Error::class, $event->is_recordable() );
+		$this->assertInstanceOf( WP_Error::class, $event->is_recordable() );
 	}
 }
