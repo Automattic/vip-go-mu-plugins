4dcfae12e42e3d8cc5e0830bdc09723ebcfa6fa0
Rebecca Hum <rebeccahum@gmail.com>
2024-11-20 11:43:51 -0700

    Jetpack: Update default to 14.0

--- tests/test-jetpack.php
+++ tests/test-jetpack.php
@@ -7,7 +7,7 @@ class VIP_Go__Core__Default_VIP_Jetpack_Version extends WP_UnitTestCase {
 		global $wp_version;
 		$saved_wp_version = $wp_version;
 
-		$latest = '13.9';
+		$latest = '14.0';
 
 		$versions_map = [
 			// WordPress version => Jetpack version
