9b88c3f0ceec62ba3b7137b8037605deba03502c
Roberson Gomes <roberson.gomes@automattic.com>
2025-09-08 15:01:56 -0300

    Add support for Twilio Verify for 2FA via SMS

--- wpcom-vip-two-factor/sms-provider.php
+++ wpcom-vip-two-factor/sms-provider.php
@@ -1,18 +1,15 @@
 <?php
 
+require_once __DIR__ . '/twilio/interface-two-factor-twilio-sms.php';
+require_once __DIR__ . '/twilio/class-two-factor-twilio-sms-api.php';
+require_once __DIR__ . '/twilio/class-two-factor-twilio-verify-api.php';
+
 /**
  * Class for creating an sms provider.
  *
  * @package Two_Factor
  */
 class Two_Factor_SMS extends Two_Factor_Provider {
-
-	/**
-	 * The user meta token key.
-	 *
-	 * @type string
-	 */
-	const TOKEN_META_KEY = '_two_factor_sms_token';
 
 	const PHONE_META_KEY = '_vip_two_factor_phone';
 
@@ -35,63 +32,26 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	}
 
 	/**
+	 * @param int $user_id
+	 * @return Two_Factor_Twilio_SMS
+	 */
+	private function get_sms_strategy( int $user_id ): Two_Factor_Twilio_SMS {
+		$phone = get_user_meta( $user_id, self::PHONE_META_KEY, true );
+
+		if ( Two_Factor_Twilio_Verify_API::is_available() ) {
+			if ( preg_match( '/^\+?974[3-7]\d{7}$/', $phone ) ) { // Qatar number
+				return new Two_Factor_Twilio_Verify_API( $user_id, $phone );
+			}
+		}
+
+		return new Two_Factor_Twilio_SMS_API( $user_id, $phone );
+	}
+
+	/**
 	 * Returns the name of the provider.
 	 */
 	public function get_label() {
 		return _x( 'SMS', 'Provider Label', 'two-factor' );
-	}
-
-	/**
-	 * Generate the user token.
-	 *
-	 * @param int $user_id User ID.
-	 * @return string
-	 */
-	public function generate_token( $user_id ) {
-		$token = $this->get_code();
-		update_user_meta( $user_id, self::TOKEN_META_KEY, wp_hash( $token ) );
-		return $token;
-	}
-
-	/**
-	 * Validate the user token.
-	 *
-	 * @param int    $user_id User ID.
-	 * @param string $token User token.
-	 * @return boolean
-	 */
-	public function validate_token( $user_id, $token ) {
-		$hashed_token = get_user_meta( $user_id, self::TOKEN_META_KEY, true );
-		$correct      = wp_hash( $token );
-		if ( ! hash_equals( $hashed_token, $correct ) ) {
-			return false;
-		}
-		$this->delete_token( $user_id );
-		return true;
-	}
-
-	/**
-	 * Delete the user token.
-	 *
-	 * @param int $user_id User ID.
-	 */
-	public function delete_token( $user_id ) {
-		delete_user_meta( $user_id, self::TOKEN_META_KEY );
-	}
-
-	/**
-	 * Standardize the format of the SMS message
-	 *
-	 * @param string $token User token.
-	 */
-	public function format_sms_message( $verification_code ) {
-		$site_title                = get_bloginfo();
-		$parse                     = wp_parse_url( home_url() );
-		$home_url_without_protocol = $parse['host'] . ( $parse['path'] ?? '' );
-
-		$format = '%1$s is your %2$s verification code.' . "\n\n" . '@%3$s #%1$s';
-
-		return sprintf( $format, $verification_code, $site_title, $home_url_without_protocol );
 	}
 
 	/**
@@ -100,14 +60,11 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	 * @param WP_User $user WP_User object of the logged-in user.
 	 */
 	public function generate_and_send_token( $user ) {
-		require_once WPVIP_MU_PLUGIN_DIR . '/lib/sms.php';
+		$result = $this->get_sms_strategy( $user->ID )->send_code( $this->get_code() );
 
-		$token   = $this->generate_token( $user->ID ); // Store in variable to prevent generating code twice
-		$message = $this->format_sms_message( $token );
-
-		$sms = get_user_meta( $user->ID, self::PHONE_META_KEY, true );
-
-		\Automattic\VIP\SMS\send_sms( $sms, $message );
+		if ( is_wp_error( $result ) ) {
+			return new WP_Error( 'verification_failed', __( 'Failed to send verification code.', 'two-factor' ) );
+		}
 	}
 
 	/**
@@ -155,7 +112,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 		}
 
 		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
-		return $this->validate_token( $user->ID, $_REQUEST['two-factor-sms-code'] );
+		return $this->get_sms_strategy( $user->ID )->verify_code( $_REQUEST['two-factor-sms-code'] );
 	}
 
 	/**
@@ -176,7 +133,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	 */
 	public function user_options( $user ) {
 		$sms            = get_user_meta( $user->ID, self::PHONE_META_KEY, true );
-		$hashed_token   = get_user_meta( $user->ID, self::TOKEN_META_KEY, true );
+		$is_pending     = $this->get_sms_strategy( $user->ID )->has_pending_metadata();
 		$sms_configured = get_user_meta( $user->ID, self::SMS_CONFIGURED_META_KEY, true );
 		wp_nonce_field( 'user_two_factor_sms_options', '_nonce_user_two_factor_sms_options', false );
 		?>
@@ -185,7 +142,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 				Correctly configured for <?php echo esc_attr( $sms ); ?>.
 				<input type="submit" class="button" name="vip-two-factor-phone-delete"
 					value="<?php esc_attr_e( 'Delete', 'two-factor' ); ?>"/>
-			<?php elseif ( ! empty( $hashed_token ) ) : ?>
+			<?php elseif ( $is_pending ) : ?>
 				<p>
 					Verification code has been sent to <?php echo esc_attr( $sms ); ?>
 					<input type="submit" class="button" name="vip-two-factor-phone-send-code"
@@ -228,7 +185,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 		if ( isset( $_POST['vip-two-factor-phone-delete'] ) ) {
 			delete_user_meta( $user_id, self::SMS_CONFIGURED_META_KEY );
 			delete_user_meta( $user_id, self::PHONE_META_KEY );
-			delete_user_meta( $user_id, self::TOKEN_META_KEY );
+			$this->get_sms_strategy( $user_id )->cleanup_verification_data();
 		}
 
 		if ( isset( $_POST['vip-two-factor-phone'] ) ) {
---
+++ wpcom-vip-two-factor/twilio/class-two-factor-twilio-verify-api.php
@@ -0,0 +1,169 @@
+<?php
+
+use Twilio\Exceptions\TwilioException;
+
+/**
+ * Twilio SMS strategy for 2FA using Twilio's Verify API.
+ *
+ * @package Two_Factor
+ */
+class Two_Factor_Twilio_Verify_API implements Two_Factor_Twilio_SMS {
+
+	const VERIFICATION_SID_META_KEY = '_two_factor_twilio_verification_sid';
+
+	const TWILIO_VERIFY_FRIENDLY_NAME_MAX_LENGTH = 30;
+
+	private static Twilio\Rest\Client $twilio_client;
+
+	private int $user_id;
+	private string $phone;
+
+	private static function get_client(): Twilio\Rest\Client {
+		if ( ! isset( self::$twilio_client ) ) {
+			require_once __DIR__ . '/../../twilio-sdk/src/Twilio/autoload.php';
+			self::$twilio_client = new Twilio\Rest\Client( TWILIO_SID, TWILIO_SECRET );
+		}
+		return self::$twilio_client;
+	}
+
+	public function __construct( int $user_id, string $phone ) {
+		$this->user_id = $user_id;
+		$this->phone   = str_starts_with( $phone, '+' ) ? $phone : '+' . $phone;
+	}
+
+	/**
+	 * Check if the Twilio Verify API is available.
+	 */
+	public static function is_available(): bool {
+		return defined( 'TWILIO_SID' ) && defined( 'TWILIO_SECRET' ) && defined( 'VIP_TWILIO_VERIFY_SERVICE_SID' );
+	}
+
+	/**
+	 * Send a code to the user via SMS
+	 *
+	 * @param string $code Code to send to the user.
+	 * @return bool|WP_Error true on success, or WP_Error on failure.
+	 */
+	public function send_code( string $code ): bool|WP_Error {
+		$parse                     = wp_parse_url( home_url() );
+		$home_url_without_protocol = $parse['host'] . ( $parse['path'] ?? '' );
+
+		try {
+			$verification = self::get_client()->verify->v2
+				->services( VIP_TWILIO_VERIFY_SERVICE_SID )
+				->verifications
+				->create( $this->phone, 'sms', [
+                    // phpcs:ignore Squiz.PHP.CommentedOutCode.Found
+					// 'customFriendlyName' => $this->get_friendly_name( $home_url_without_protocol ), // FIXME: Use once Twilio enables Custom Friendly Names
+					'tags' => wp_json_encode([
+						'blog_id'        => get_current_blog_id(),
+						'domain'         => $home_url_without_protocol,
+						'environment_id' => VIP_GO_APP_ID,
+						'user_id'        => $this->user_id,
+					] ),
+				] );
+
+			update_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY, $verification->sid );
+		} catch ( TwilioException $e ) {
+
+			$masked_number = substr( $this->phone, 0, ( (int) strlen( $this->phone ) / 1.5 ) ) . 'xxx';
+            // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			trigger_error( sprintf( 'Failed to send SMS to %s: %s %s #vip-go-sms-error', esc_html( $masked_number ), esc_html( $e->getCode() ), esc_html( $e->getMessage() ) ), E_USER_WARNING );
+
+			return new WP_Error( 'twilio_verify_failed', sprintf(
+				// phpcs:ignore WordPress.WP.I18n.MissingTranslatorsComment
+				__( 'Failed to send verification code to user %1$d: %2$s', 'two-factor' ), $this->user_id,
+				$e->getMessage()
+			) );
+		}
+		return true;
+	}
+
+	/**
+	 * Verify the code provided by the user.
+	 *
+	 * @param string $code User token.
+	 * @return bool True if the code is valid, false otherwise.
+	 */
+	public function verify_code( string $code ): bool {
+		$verification_sid = get_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY, true );
+
+		if ( empty( $verification_sid ) ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			error_log( sprintf( 'Could not find a Twilio Verify SID for user %d to verify the 2FA code ', $verification_sid ) );
+			return false;
+		}
+
+		try {
+			$verification_check = self::get_client()->verify->v2
+				->services( VIP_TWILIO_VERIFY_SERVICE_SID )
+				->verificationChecks->create([
+					'verificationSid' => $verification_sid,
+					'code'            => $code,
+				]);
+
+			if ( 'approved' === $verification_check->status ) {
+				$this->cleanup_verification_data();
+				return true;
+			}
+
+			return false;
+		} catch ( TwilioException $e ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			error_log( sprintf('Twilio Verify validation request failed for user %d and sid %s: %s',
+				$this->user_id,
+				$verification_sid,
+				$this->get_message_from_twilio_exception( $e )
+			) );
+
+			$this->cleanup_verification_data();
+
+			return false;
+		}
+	}
+
+	/**
+	 * Clean up any verification data associated with the user.
+	 */
+	public function cleanup_verification_data(): void {
+		delete_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY );
+	}
+
+	/**
+	 * Check if the user has pending metadata.
+	 */
+	public function has_pending_metadata(): bool {
+		return $this->get_verification_sid() !== null;
+	}
+
+	private function get_message_from_twilio_exception( TwilioException $e ): string {
+		return sprintf( 'Twilio Verify Client failed with error (%s): %s', $e->getCode(), $e->getMessage() );
+	}
+
+	private function get_verification_sid(): string|null {
+		$verification_sid = get_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY, true );
+		return empty( $verification_sid ) ? null : $verification_sid;
+	}
+
+	private function get_friendly_name( string $domain ): string {
+		$max_length = $this::TWILIO_VERIFY_FRIENDLY_NAME_MAX_LENGTH;
+		$domain     = preg_replace( '/^www\\./', '', $domain );
+
+		if ( strlen( $domain ) <= $max_length ) {
+			return $domain;
+		}
+
+		// If the domain has at least 3 parts, return it
+		$partial = substr( $domain, max( 0, strlen( $domain ) - $max_length ), $max_length );
+		$partial = substr( $partial, strpos( $partial, '.' ) + 1 );
+		if ( substr_count( $partial, '.' ) > 2 ) {
+			return $partial;
+		}
+
+		// Returns firstpart[...]secondpart.com limited to TWILIO_VERIFY_FRIENDLY_NAME_MAX_LENGTH
+		$ellipsis           = '[...]';
+		$first_part_length  = floor( ( $max_length - strlen( $ellipsis ) ) / 2 );
+		$second_part_length = $max_length - strlen( $ellipsis ) - $first_part_length;
+		return substr( $domain, 0, $first_part_length ) . $ellipsis . substr( $domain, -$second_part_length );
+	}
+}
