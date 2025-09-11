6842f82d5a7769febe9a81639fd594f5ad3f3cf2
Roberson Gomes <roberson.gomes@automattic.com>
2025-09-11 18:14:58 -0300

    Use curl instead of the Twilio SDK

--- wpcom-vip-two-factor/twilio/class-two-factor-twilio-verify-api.php
+++ wpcom-vip-two-factor/twilio/class-two-factor-twilio-verify-api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Twilio\Exceptions\TwilioException;
-
 /**
  * Twilio SMS strategy for 2FA using Twilio's Verify API.
  *
@@ -13,18 +11,10 @@ class Two_Factor_Twilio_Verify_API implements Two_Factor_Twilio_SMS {
 
 	const TWILIO_VERIFY_FRIENDLY_NAME_MAX_LENGTH = 30;
 
-	private static Twilio\Rest\Client $twilio_client;
+	const TWILIO_VERIFY_BASE_URL = 'https://verify.twilio.com/v2';
 
 	private int $user_id;
 	private string $phone;
-
-	private static function get_client(): Twilio\Rest\Client {
-		if ( ! isset( self::$twilio_client ) ) {
-			require_once __DIR__ . '/../../twilio-sdk/src/Twilio/autoload.php';
-			self::$twilio_client = new Twilio\Rest\Client( TWILIO_SID, TWILIO_SECRET );
-		}
-		return self::$twilio_client;
-	}
 
 	public function __construct( int $user_id, string $phone ) {
 		$this->user_id = $user_id;
@@ -48,34 +38,72 @@ class Two_Factor_Twilio_Verify_API implements Two_Factor_Twilio_SMS {
 		$parse                     = wp_parse_url( home_url() );
 		$home_url_without_protocol = $parse['host'] . ( $parse['path'] ?? '' );
 
-		try {
-			$verification = self::get_client()->verify->v2
-				->services( VIP_TWILIO_VERIFY_SERVICE_SID )
-				->verifications
-				->create( $this->phone, 'sms', [
-                    // phpcs:ignore Squiz.PHP.CommentedOutCode.Found
-					// 'customFriendlyName' => $this->get_friendly_name( $home_url_without_protocol ), // FIXME: Use once Twilio enables Custom Friendly Names
-					'tags' => wp_json_encode([
-						'blog_id'        => get_current_blog_id(),
-						'domain'         => $home_url_without_protocol,
-						'environment_id' => VIP_GO_APP_ID,
-						'user_id'        => $this->user_id,
-					] ),
-				] );
+		$endpoint = sprintf( '%s/Services/%s/Verifications', self::TWILIO_VERIFY_BASE_URL, VIP_TWILIO_VERIFY_SERVICE_SID );
 
-			update_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY, $verification->sid );
-		} catch ( TwilioException $e ) {
+		$body = array(
+			'To'      => $this->phone,
+			'Channel' => 'sms',
+			// phpcs:ignore Squiz.PHP.CommentedOutCode.Found
+			// 'CustomFriendlyName' => $this->get_friendly_name( $home_url_without_protocol ), // FIXME: Use once Twilio enables Custom Friendly Names
+			'Tags'    => wp_json_encode([
+				'blog_id'        => get_current_blog_id(),
+				'domain'         => $home_url_without_protocol,
+				'environment_id' => VIP_GO_APP_ID,
+				'user_id'        => $this->user_id,
+			] ),
+		);
 
+		$response = wp_remote_post( $endpoint, array(
+			'body'    => $body,
+			'headers' => array(
+				'Authorization' => 'Basic ' . base64_encode( TWILIO_SID . ':' . TWILIO_SECRET ),
+			),
+		) );
+
+		if ( is_wp_error( $response ) ) {
 			$masked_number = substr( $this->phone, 0, ( (int) strlen( $this->phone ) / 1.5 ) ) . 'xxx';
-            // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
-			trigger_error( sprintf( 'Failed to send SMS to %s: %s %s #vip-go-sms-error', esc_html( $masked_number ), esc_html( $e->getCode() ), esc_html( $e->getMessage() ) ), E_USER_WARNING );
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			trigger_error( sprintf( 'Failed to send SMS to %s: %s #vip-go-sms-error', esc_html( $masked_number ), esc_html( $response->get_error_message() ) ), E_USER_WARNING );
 
 			return new WP_Error( 'twilio_verify_failed', sprintf(
 				// phpcs:ignore WordPress.WP.I18n.MissingTranslatorsComment
 				__( 'Failed to send verification code to user %1$d: %2$s', 'two-factor' ), $this->user_id,
-				$e->getMessage()
+				$response->get_error_message()
 			) );
 		}
+
+		$body_raw  = wp_remote_retrieve_body( $response );
+		$body_data = json_decode( $body_raw );
+
+		$status_code = wp_remote_retrieve_response_code( $response );
+		if ( $status_code >= 300 ) {
+			$masked_number = substr( $this->phone, 0, ( (int) strlen( $this->phone ) / 1.5 ) ) . 'xxx';
+
+			if ( $body_data && isset( $body_data->code ) && isset( $body_data->message ) ) {
+				$error_message = sprintf( 'Twilio Verify API responded with error (%d): %s', $body_data->code, $body_data->message );
+			} else {
+				$error_message = sprintf( 'Twilio Verify API responded with HTTP response code: %d', $status_code );
+			}
+
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			trigger_error( sprintf( 'Failed to send SMS to %s: %s #vip-go-sms-error', esc_html( $masked_number ), esc_html( $error_message ) ), E_USER_WARNING );
+
+			return new WP_Error( 'twilio_verify_failed', sprintf(
+				// phpcs:ignore WordPress.WP.I18n.MissingTranslatorsComment
+				__( 'Failed to send verification code to user %1$d: %2$s', 'two-factor' ), $this->user_id,
+				$error_message
+			) );
+		}
+
+		if ( $body_data && isset( $body_data->sid ) ) {
+			update_user_meta( $this->user_id, self::VERIFICATION_SID_META_KEY, $body_data->sid );
+		} else {
+			return new WP_Error( 'twilio_verify_failed', sprintf(
+				// phpcs:ignore WordPress.WP.I18n.MissingTranslatorsComment
+				__( 'Failed to send verification code to user %1$d: Invalid response from Twilio', 'two-factor' ), $this->user_id
+			) );
+		}
+
 		return true;
 	}
 
@@ -94,32 +122,61 @@ class Two_Factor_Twilio_Verify_API implements Two_Factor_Twilio_SMS {
 			return false;
 		}
 
-		try {
-			$verification_check = self::get_client()->verify->v2
-				->services( VIP_TWILIO_VERIFY_SERVICE_SID )
-				->verificationChecks->create([
-					'verificationSid' => $verification_sid,
-					'code'            => $code,
-				]);
+		$endpoint = sprintf( '%s/Services/%s/VerificationCheck', self::TWILIO_VERIFY_BASE_URL, VIP_TWILIO_VERIFY_SERVICE_SID );
 
-			if ( 'approved' === $verification_check->status ) {
-				$this->cleanup_verification_data();
-				return true;
-			}
+		$body = array(
+			'VerificationSid' => $verification_sid,
+			'Code'            => $code,
+		);
 
-			return false;
-		} catch ( TwilioException $e ) {
+		$response = wp_remote_post( $endpoint, array(
+			'body'    => $body,
+			'headers' => array(
+				'Authorization' => 'Basic ' . base64_encode( TWILIO_SID . ':' . TWILIO_SECRET ),
+			),
+		) );
+
+		if ( is_wp_error( $response ) ) {
 			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
 			error_log( sprintf('Twilio Verify validation request failed for user %d and sid %s: %s',
 				$this->user_id,
 				$verification_sid,
-				$this->get_message_from_twilio_exception( $e )
+				$response->get_error_message()
 			) );
 
 			$this->cleanup_verification_data();
-
 			return false;
 		}
+
+		$body_raw  = wp_remote_retrieve_body( $response );
+		$body_data = json_decode( $body_raw );
+
+		$status_code = wp_remote_retrieve_response_code( $response );
+		if ( $status_code >= 300 ) {
+			$error_message = 'Unknown error';
+			if ( $body_data && isset( $body_data->code ) && isset( $body_data->message ) ) {
+				$error_message = sprintf( 'Twilio Verify API responded with error (%d): %s', $body_data->code, $body_data->message );
+			} else {
+				$error_message = sprintf( 'Twilio Verify API responded with HTTP response code: %d', $status_code );
+			}
+
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
+			error_log( sprintf('Twilio Verify validation request failed for user %d and sid %s: %s',
+				$this->user_id,
+				$verification_sid,
+				$error_message
+			) );
+
+			$this->cleanup_verification_data();
+			return false;
+		}
+
+		if ( $body_data && isset( $body_data->status ) && 'approved' === $body_data->status ) {
+			$this->cleanup_verification_data();
+			return true;
+		}
+
+		return false;
 	}
 
 	/**
@@ -134,10 +191,6 @@ class Two_Factor_Twilio_Verify_API implements Two_Factor_Twilio_SMS {
 	 */
 	public function has_pending_metadata(): bool {
 		return $this->get_verification_sid() !== null;
-	}
-
-	private function get_message_from_twilio_exception( TwilioException $e ): string {
-		return sprintf( 'Twilio Verify Client failed with error (%s): %s', $e->getCode(), $e->getMessage() );
 	}
 
 	private function get_verification_sid(): string|null {
