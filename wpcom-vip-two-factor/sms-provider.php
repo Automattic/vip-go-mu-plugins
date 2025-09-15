1c0731cc82ff68a6dd39ac2dddfb156cd08891d8
Roberson Gomes <roberson.gomes@automattic.com>
2025-09-15 19:58:56 -0300

    Add support for Twilio Verify for 2FA via SMS (#6514)

--- wpcom-vip-two-factor/sms-provider.php
+++ wpcom-vip-two-factor/sms-provider.php
@@ -1,5 +1,15 @@
 <?php
 
+if ( file_exists( __DIR__ . '/twilio/interface-two-factor-twilio-sms.php' ) ) {
+	require_once __DIR__ . '/twilio/interface-two-factor-twilio-sms.php';
+}
+if ( file_exists( __DIR__ . '/twilio/class-two-factor-twilio-sms-api.php' ) ) {
+	require_once __DIR__ . '/twilio/class-two-factor-twilio-sms-api.php';
+}
+if ( file_exists( __DIR__ . '/twilio/class-two-factor-twilio-verify-api.php' ) ) {
+	require_once __DIR__ . '/twilio/class-two-factor-twilio-verify-api.php';
+}
+
 /**
  * Class for creating an sms provider.
  *
@@ -7,16 +17,11 @@
  */
 class Two_Factor_SMS extends Two_Factor_Provider {
 
-	/**
-	 * The user meta token key.
-	 *
-	 * @type string
-	 */
-	const TOKEN_META_KEY = '_two_factor_sms_token';
-
 	const PHONE_META_KEY = '_vip_two_factor_phone';
 
 	const SMS_CONFIGURED_META_KEY = '_vip_two_factor_sms_configured';
+
+	const QATAR_PHONE_REGEX = '/^\+?974[3-7]\d{7}$/';
 
 	public static function get_instance() {
 		static $instance;
@@ -35,63 +40,26 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	}
 
 	/**
+	 * @param int $user_id
+	 * @return Two_Factor_Twilio_SMS
+	 */
+	public function get_sms_strategy( int $user_id ): Two_Factor_Twilio_SMS {
+		$phone = get_user_meta( $user_id, self::PHONE_META_KEY, true );
+
+		if ( Two_Factor_Twilio_Verify_API::is_available() ) {
+			if ( preg_match( self::QATAR_PHONE_REGEX, $phone ) ) {
+				return new Two_Factor_Twilio_Verify_API( $user_id, $phone );
+			}
+		}
+
+		return new Two_Factor_Twilio_SMS_API( $user_id, $phone );
+	}
+
+	/**
 	 * Returns the name of the provider.
 	 */
 	public function get_label() {
 		return _x( 'SMS', 'Provider Label', 'two-factor' );
-	}
-
-	/**
-	 * Generate the user token.
-	 *
-	 * @param int $user_id User ID.
-	 * @return string
-	 */
-	public function generate_token( $user_id ) {
-		$token = $this->get_code();
-		update_user_meta( $user_id, self::TOKEN_META_KEY, wp_hash( $token ) );
-		return $token;
-	}
-
-	/**
-	 * Validate the user token.
-	 *
-	 * @param int    $user_id User ID.
-	 * @param string $token User token.
-	 * @return boolean
-	 */
-	public function validate_token( $user_id, $token ) {
-		$hashed_token = get_user_meta( $user_id, self::TOKEN_META_KEY, true );
-		$correct      = wp_hash( $token );
-		if ( ! hash_equals( $hashed_token, $correct ) ) {
-			return false;
-		}
-		$this->delete_token( $user_id );
-		return true;
-	}
-
-	/**
-	 * Delete the user token.
-	 *
-	 * @param int $user_id User ID.
-	 */
-	public function delete_token( $user_id ) {
-		delete_user_meta( $user_id, self::TOKEN_META_KEY );
-	}
-
-	/**
-	 * Standardize the format of the SMS message
-	 *
-	 * @param string $token User token.
-	 */
-	public function format_sms_message( $verification_code ) {
-		$site_title                = get_bloginfo();
-		$parse                     = wp_parse_url( home_url() );
-		$home_url_without_protocol = $parse['host'] . ( $parse['path'] ?? '' );
-
-		$format = '%1$s is your %2$s verification code.' . "\n\n" . '@%3$s #%1$s';
-
-		return sprintf( $format, $verification_code, $site_title, $home_url_without_protocol );
 	}
 
 	/**
@@ -100,14 +68,13 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	 * @param WP_User $user WP_User object of the logged-in user.
 	 */
 	public function generate_and_send_token( $user ) {
-		require_once WPVIP_MU_PLUGIN_DIR . '/lib/sms.php';
+		$strategy = $this->get_sms_strategy( $user->ID );
+		$result   = $strategy->send_code( $this->get_code() );
 
-		$token   = $this->generate_token( $user->ID ); // Store in variable to prevent generating code twice
-		$message = $this->format_sms_message( $token );
-
-		$sms = get_user_meta( $user->ID, self::PHONE_META_KEY, true );
-
-		\Automattic\VIP\SMS\send_sms( $sms, $message );
+		if ( is_wp_error( $result ) ) {
+			$strategy->cleanup_verification_data();
+			return new WP_Error( 'verification_failed', __( 'Failed to send verification code.', 'two-factor' ) );
+		}
 	}
 
 	/**
@@ -155,7 +122,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 		}
 
 		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.NonceVerification.Recommended
-		return $this->validate_token( $user->ID, $_REQUEST['two-factor-sms-code'] );
+		return $this->get_sms_strategy( $user->ID )->verify_code( $_REQUEST['two-factor-sms-code'] );
 	}
 
 	/**
@@ -176,7 +143,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 	 */
 	public function user_options( $user ) {
 		$sms            = get_user_meta( $user->ID, self::PHONE_META_KEY, true );
-		$hashed_token   = get_user_meta( $user->ID, self::TOKEN_META_KEY, true );
+		$is_pending     = $this->get_sms_strategy( $user->ID )->has_pending_metadata();
 		$sms_configured = get_user_meta( $user->ID, self::SMS_CONFIGURED_META_KEY, true );
 		wp_nonce_field( 'user_two_factor_sms_options', '_nonce_user_two_factor_sms_options', false );
 		?>
@@ -185,7 +152,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 				Correctly configured for <?php echo esc_attr( $sms ); ?>.
 				<input type="submit" class="button" name="vip-two-factor-phone-delete"
 					value="<?php esc_attr_e( 'Delete', 'two-factor' ); ?>"/>
-			<?php elseif ( ! empty( $hashed_token ) ) : ?>
+			<?php elseif ( $is_pending ) : ?>
 				<p>
 					Verification code has been sent to <?php echo esc_attr( $sms ); ?>
 					<input type="submit" class="button" name="vip-two-factor-phone-send-code"
@@ -228,7 +195,7 @@ class Two_Factor_SMS extends Two_Factor_Provider {
 		if ( isset( $_POST['vip-two-factor-phone-delete'] ) ) {
 			delete_user_meta( $user_id, self::SMS_CONFIGURED_META_KEY );
 			delete_user_meta( $user_id, self::PHONE_META_KEY );
-			delete_user_meta( $user_id, self::TOKEN_META_KEY );
+			$this->get_sms_strategy( $user_id )->cleanup_verification_data();
 		}
 
 		if ( isset( $_POST['vip-two-factor-phone'] ) ) {
