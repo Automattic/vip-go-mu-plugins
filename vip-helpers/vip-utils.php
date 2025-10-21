64aecc8a632142f1331b6761ba9449dc7094a7b3
Volodymyr Kolesnykov <volodymyr.kolesnykov@automattic.com>
2025-10-21 15:28:11 +0300

    fix: readability

--- vip-helpers/vip-utils.php
+++ vip-helpers/vip-utils.php
@@ -1533,15 +1533,14 @@ function vip_is_jetpack_request() {
 	$jetpack_ips = Jetpack_IP_Manager::get_jetpack_ips();
 
 	// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPressVIPMinimum.Variables.RestrictedVariables.cache_constraints___SERVER__REMOTE_ADDR__
-	$remote_addr = $_SERVER['REMOTE_ADDR'] ? [ trim( $_SERVER['REMOTE_ADDR'] ) ] : [];
+	$all_ips = $_SERVER['REMOTE_ADDR'] ? [ trim( $_SERVER['REMOTE_ADDR'] ) ] : [];
 
-	// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.InputNotValidated, WordPress.Security.ValidatedSanitizedInput.MissingUnslash, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
-	$forwarded_for = $_SERVER['HTTP_X_FORWARDED_FOR'] ?? '';
-
-	// Support multiple IPs in X_FORWARDED_FOR to handle proxies.
-	$forwarded_ips = strpos( $forwarded_for, ',' ) !== false ? array_map( 'trim', explode( ',', $forwarded_for ) ) : ( $forwarded_for ? [ $forwarded_for ] : [] );
-
-	$all_ips = array_merge( $remote_addr, $forwarded_ips );
+	// phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
+	if ( ! empty( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) {
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
+		$forwarded_ips = array_map( 'trim', explode( ',', $_SERVER['HTTP_X_FORWARDED_FOR'] ) );
+		$all_ips       = array_merge( $all_ips, $forwarded_ips );
+	}
 
 	foreach ( $all_ips as $ip ) {
 		if ( IpUtils::check_ip( $ip, $jetpack_ips ) ) {
