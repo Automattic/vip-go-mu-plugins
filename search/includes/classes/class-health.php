b28d1ca5b85f648a978e0c3a5920f3c8dc2b9581
Rebecca Hum <rebeccahum@gmail.com>
2025-11-12 13:47:14 -0700

    better messag

--- search/includes/classes/class-health.php
+++ search/includes/classes/class-health.php
@@ -176,8 +176,10 @@ class Health {
 				add_filter( 'ep_exclude_password_protected_from_search', '__return_false' );
 			}
 
-			// Avoid modifying query_vars to avoid breaking `wp vip-search health validate-counts` CLI.
+			// Ensure query_vars stay un-modified because some third-party plugins modify it 
+			// (e.g. orderby) and break `wp health validate-counts` CLI.
 			remove_all_actions( 'parse_query' );
+
 			$query = self::query_objects( $query_args, $indexable->slug );
 			if ( 'user' === $indexable->slug && isset( $query->query_vars['blog_id'] ) ) {
 				// Since the user indexable is global, we want to include ALL in count.
