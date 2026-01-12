992ccbe73bac01895fea43f6117191a4d36ddc0f
Jacob Smith <smithjw1@users.noreply.github.com>
2026-01-12 14:05:02 -0800

    Update tests/integrations/fake-integration.php
    Co-authored-by: Chris Zarate <chris.zarate@automattic.com>

--- tests/integrations/fake-integration.php
+++ tests/integrations/fake-integration.php
@@ -20,18 +20,6 @@ class FakeIntegration extends Integration {
 	public function configure(): void { }
 }
 
-class FakeIntegrationWithPendoTracking extends Integration {
-
-	public function __construct( string $slug ) {
-		parent::__construct( $slug );
-		$this->enable_pendo_tracking = true;
-	}
-
-	public function is_loaded(): bool {
-		return false;
-	}
-
-	public function load(): void { }
-
-	public function configure(): void { }
+class FakeIntegrationWithPendoTracking extends FakeIntegration {
+	protected bool $enable_pendo_tracking = true;
 }
