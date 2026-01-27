57848c94cefaf208b356864c51bec46d7f967121
Max Schmeling <max.schmeling@automattic.com>
2026-01-27 13:55:00 -0600

    Update RTC (0.2) and Gutenberg version for real-time-collaboration

--- integrations/real-time-collaboration.php
+++ integrations/real-time-collaboration.php
@@ -18,14 +18,14 @@ class RealTimeCollaborationIntegration extends Integration {
 	 * Version of the vip-real-time-collaboration plugin to load.
 	 * Used to control staged rollouts (e.g., staging gets new version first).
 	 */
-	const VIP_RTC_PLUGIN_VERSION = '0.1';
+	const VIP_RTC_PLUGIN_VERSION = '0.2';
 
 	/**
 	 * Version of the Gutenberg plugin to load.
 	 * Empty string means load from the unversioned 'gutenberg' folder.
 	 * A version number (e.g., '1.0') loads from 'gutenberg-1.0' folder.
 	 */
-	const VIP_RTC_GUTENBERG_VERSION = '';
+	const VIP_RTC_GUTENBERG_VERSION = '22.4.1-f48ad9c';
 
 	/**
 	 * Enable Pendo tracking for this integration.
