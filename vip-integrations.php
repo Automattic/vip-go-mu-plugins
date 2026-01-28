9faf756d064f871865796c23bd4667f7ae98eeb6
Bruno Basto <b.basto@gmail.com>
2026-01-28 17:50:48 -0300

    Add VIP Agentforce integration loader
    Enable the vip-agentforce integration to be loaded on VIP sites via
    the integrations framework. Follows the same versioned plugin pattern
    as security-boost.
    
    References: VIP-2637

--- vip-integrations.php
+++ vip-integrations.php
@@ -25,6 +25,7 @@ require_once __DIR__ . '/integrations/parsely.php';
 require_once __DIR__ . '/integrations/vip-governance.php';
 require_once __DIR__ . '/integrations/enterprise-search.php';
 require_once __DIR__ . '/integrations/security-boost.php';
+require_once __DIR__ . '/integrations/vip-agentforce.php';
 
 if ( file_exists( __DIR__ . '/integrations/remote-data-blocks.php' ) ) {
 	require_once __DIR__ . '/integrations/remote-data-blocks.php';
@@ -44,6 +45,7 @@ IntegrationsSingleton::instance()->register( new ParselyIntegration( 'parsely' )
 IntegrationsSingleton::instance()->register( new VipGovernanceIntegration( 'vip-governance' ) );
 IntegrationsSingleton::instance()->register( new EnterpriseSearchIntegration( 'enterprise-search' ) );
 IntegrationsSingleton::instance()->register( new SecurityBoostIntegration( 'security-boost' ) );
+IntegrationsSingleton::instance()->register( new VipAgentforceIntegration( 'vip-agentforce' ) );
 
 if ( class_exists( __NAMESPACE__ . '\\RemoteDataBlocksIntegration' ) ) {
 	IntegrationsSingleton::instance()->register( new RemoteDataBlocksIntegration( 'remote-data-blocks' ) );
